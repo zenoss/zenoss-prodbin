8ca788fe508ba40510930a636607532a0a2ebdc3
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2004-05-19 00:36:38 +0000

    add loader back
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@13478 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenRRD/RRDLoader.py
@@ -0,0 +1,79 @@
+#################################################################
+#
+#   Copyright (c) 2003 Confmon Corporation. All rights reserved.
+#
+#################################################################
+
+__doc__="""RRDLoader
+
+load rrd objects into the dmd
+File format is as follows:
+
+RRDTargetType|name|datasources,|views,|
+RRDView|name|datasources,|height|width|units|linewidth|log|colors,|
+RRDDataSource|name|rpn|limit|linetype|color|
+RRDThreshold|name|datasources,|minval|maxval|minfunc|maxfunc|
+
+$Id: RRDLoader.py,v 1.2 2004/02/18 16:19:18 edahl Exp $"""
+
+__version__ = "$Revision: 1.2 $"[11:-2]
+
+import sys
+
+import Globals
+
+from OFS.Folder import manage_addFolder
+
+from Products.ConfUtils.Utils import getObjByPath
+from Products.ConfUtils.Utils import lookupClass
+
+from Products.ConfUtils.BasicLoader import BasicLoader
+
+from Products.RRDProduct.RRDTargetType import RRDTargetType
+from Products.RRDProduct.RRDView import RRDView
+from Products.RRDProduct.RRDDataSource import RRDDataSource
+from Products.RRDProduct.RRDThreshold import RRDThreshold
+
+
+class RRDLoader(BasicLoader):
+
+    def __init__(self):
+        BasicLoader.__init__(self)
+        self.configroot = getObjByPath(self.dmd, self.options.configroot)
+        if not self.configroot:
+            path = '/'.join(self.options.configroot.split('/')[:-1])
+            context = getObjByPath(self.dmd, path)
+            if not context:
+                print "ERROR: can't find configroot %s" % \
+                            self.options.configroot
+                sys.exit(1)
+            else:
+                manage_addFolder(context, 'rrdconfig')
+                self.configroot = context._getOb('rrdconfig')
+                get_transaction().commit()
+
+        
+    def loaderBody(self, line):
+        classname, id = line.split('|')[:2]
+        self.log.info('loading %s type = %s' % (id, classname))
+        constructor = lookupClass('Products.RRDProduct.'+classname)
+        if constructor:
+            obj = constructor(id)
+        else:
+            raise "NoConstructor", "No Constructor for %s" % classname 
+        obj.textload(line.split('|')[2:])
+        self.configroot._setObject(obj.id, obj)
+
+
+
+    def buildOptions(self):
+        BasicLoader.buildOptions(self)
+        self.parser.add_option('-r', '--configroot',
+                    dest='configroot',
+                    default='/Devices/rrdconfig',
+                    help='load location path (ie /Devices/rrdconfig)')
+
+
+if __name__ == '__main__':
+    rrdloader = RRDLoader()
+    rrdloader.loadDatabase()
