eb669012c197b7bd11bca63f55ad23ea2b7d3cd3
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2004-05-20 21:50:25 +0000

    new service parser
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@13490 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/CommandParsers/__init__.py
+++ Products/DataCollector/CommandParsers/__init__.py
@@ -1,10 +1,12 @@
 from Uname_A import Uname_A
 from Unix_df_k import Unix_df_k
+from Unix_netstat_an import Unix_netstat_an
 from CiscoDhcpHelperAddress import CiscoDhcpHelperAddress
 from CiscoShowHardware import CiscoShowHardware
 
 def initCommandParsers(dataCollector):
     dataCollector.addCommandParser(Uname_A)
     dataCollector.addCommandParser(Unix_df_k)
+    dataCollector.addCommandParser(Unix_netstat_an)
     dataCollector.addCommandParser(CiscoDhcpHelperAddress)
     dataCollector.addCommandParser(CiscoShowHardware)
