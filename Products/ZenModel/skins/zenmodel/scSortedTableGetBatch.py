3a570cd98ae7d030c6ce87f972e67ca6715b5eda
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2005-10-13 17:33:46 +0000

    - scScripts still needed for event screens :(
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@13842 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenModel/skins/zenmodel/scSortedTableGetBatch.py
@@ -0,0 +1,89 @@
+## Script (Python) "scSortedTableGetBatch"
+##parameters=tableName,objects,defaultSort="primarySortKey",defaultSortRule='cmp',batchSize=20,usebatch=1,sortedSence='asc'
+##bind context=context
+##title=Manage session information for table and get nessesary batch
+
+import ZTUtils
+
+request=context.REQUEST
+session=request.SESSION
+
+if hasattr(context, 'userDefaultBatchSize'):
+    defaultBatchSize = context.userDefaultBatchSize
+elif hasattr(context, 'defaultBatchSize'):
+    defaultBatchSize = context.defaultBatchSize
+else:
+    defaultBatchSize = batchSize
+
+tableatts = ( 'sortedHeader',
+            'sortedSence',
+            'sortRule',
+            'start',
+            'filter',
+            'filterattribute',
+            'negatefilter',
+            'severity',
+            'batchSize',
+            )
+
+if (session.has_key(tableName) 
+    and session[tableName].has_key('url')
+    and session[tableName]['url']==request.URL):
+    sortedTableState = session[tableName]
+    if request.has_key('tableName') and request['tableName'] == tableName:
+        for attname in tableatts:
+            if request.has_key(attname):
+                sortedTableState[attname] = request[attname]
+        if request.has_key('batchSize'):
+            sortedTableState['start'] = 0
+        if not request.has_key("negatefilter"):
+            sortedTableState['negatefilter'] = 0
+else:
+    sortedTableState = {
+        'start' : 0,
+        'url' : request.URL,
+        'sortedHeader' : defaultSort,
+        'sortRule' : defaultSortRule,
+        'sortedSence' : sortedSence,
+        'batchSize' : defaultBatchSize,
+        }
+
+severity = sortedTableState.get('severity')
+if severity:
+    objects = filter(lambda x: x.getSeverityNumber() >= severity, objects)
+            
+
+if sortedTableState.has_key('filter') and sortedTableState['filter']:
+    regex = sortedTableState['filter']
+    if sortedTableState.has_key('filterattribute'):
+        filteratt = sortedTableState['filterattribute']
+    else:
+        filteratt = 'getId'
+    if sortedTableState.has_key('negatefilter'):
+        negatefilter = sortedTableState['negatefilter']
+    else: 
+        negatefilter = 0
+    objects = context.filterObjectsRegex(regex, objects, 
+                                        filteratt, negatefilter)
+   
+sortedTableState['totalobjs'] = len(objects)
+if sortedTableState['start'] > sortedTableState['totalobjs']:
+    sortedTableState['start'] = 0
+
+if sortedTableState['sortedHeader']:
+    sortOn = (( sortedTableState['sortedHeader'], 
+                sortedTableState['sortRule'], 
+                sortedTableState['sortedSence']),)
+    sortedObjects = sequence.sort(objects, sortOn)
+else:
+    sortedObjects = objects
+
+if usebatch:
+    batch = ZTUtils.Batch(sortedObjects, size=sortedTableState['batchSize'], 
+                    start=sortedTableState['start'], orphan=0)
+else:
+    batch = sortedObjects
+
+session[tableName] = sortedTableState
+
+return batch
---
+++ Products/ZenModel/skins/zenmodel/scSortedTableHeader.py
@@ -0,0 +1,33 @@
+## Script (Python) "scSortedTableHeader"
+##parameters=tableName,fieldName,fieldTitle,sortRule='cmp',style='tableheader',attributes=None
+##bind context=context
+##title=Make a tag for a table header that allows sorting
+
+request = context.REQUEST
+session = request.SESSION
+
+if session.has_key(tableName):
+    sortedTableState = session[tableName]
+    sortedHeader = sortedTableState['sortedHeader']
+    sortedSence = sortedTableState['sortedSence']
+    if sortedHeader == fieldName:
+        style = style + "selected"
+        #if not sortedSence:
+        #    sortedSence = 'asc'
+        if sortedSence == 'asc':
+            sortedSence = 'desc'
+        elif sortedSence == 'desc':
+            fieldName = ''
+            sortedSence = ''
+    else:
+        sortedSence = 'asc'
+
+    tag = """<th class="%s" %s> <a class="%s" href="%s""" % (
+                            style,attributes,style,request.URL)
+    tag += ("?tableName=%s&sortedHeader=%s&sortedSence=%s&sortRule=%s\">" 
+                % (tableName, fieldName, sortedSence, sortRule))
+    tag += fieldTitle + "</a></th>\n"
+    return tag
+else:
+    raise "SortedTableSessionError", \
+            "Can't find %s tableinfo in session" % tableName
