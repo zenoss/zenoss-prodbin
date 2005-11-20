0f9306f0a00489cccb910f9d70e07ad9da15e548
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2005-11-20 19:56:29 +0000

    - fix logging bug
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14016 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/Syslogd.py
+++ Products/ZenEvents/Syslogd.py
@@ -183,7 +183,7 @@ class Syslogd(ThreadingUDPServer, ZenDaemon):
         identifier = "|".join((host, "Syslog", facility, str(sev), msg))
         self.ev.sendEvent(host, "Syslog", msg, sev, Component=facility, 
                             Identifier=identifier,IpAddress=client)
-        sys.log.debug(client + ' ' + host + ': ' + msg.strip() + '\n')
+        self.log.debug(client + ' ' + host + ': ' + msg.strip())
 
     
     
