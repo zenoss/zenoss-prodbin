48cfc58527bf7f95806bbeec0dbaa6a4f1099eb0
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-01-20 16:34:38 +0000

    - fix bug in condition of cisco map
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14274 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zentinel/snmp/CiscoMap.py
+++ Products/DataCollector/plugins/zentinel/snmp/CiscoMap.py
@@ -30,7 +30,7 @@ class CiscoMap(SnmpPlugin):
     def condition(self, device, log):
         """does device meet the proper conditions for this collector to run"""
         return (device.snmpOid.startswith('.1.3.6.1.4.1.9')
-                and self.modelcheck(device.model.getRelatedId()))
+                and self.modelcheck(device.hw.getProductName()))
 
     def process(self, device, results, log):
         """collect snmp information from this device"""
