7c2ec326415b5054417d775ff2cf93476dff1507
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-01-22 15:06:12 +0000

    - cleanup daemon code so that all open file descripters are closed     mainly we need to close stdout/in/err
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14281 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/ZenDaemon.py
+++ Products/ZenUtils/ZenDaemon.py
@@ -20,6 +20,22 @@ import logging
 
 from CmdBase import CmdBase
 
+# Daemon creation code below based on Recipe by Chad J. Schroeder
+# File mode creation mask of the daemon.
+UMASK = 0
+# Default working directory for the daemon.
+WORKDIR = "/"
+
+# Default maximum for the number of available file descriptors.
+MAXFD = 1024
+
+# The standard I/O file descriptors are redirected to /dev/null by default.
+if (hasattr(os, "devnull")):
+   REDIRECT_TO = os.devnull
+else:
+   REDIRECT_TO = "/dev/null"
+
+
 class ZenDaemon(CmdBase):
     
     def __init__(self, noopts=0, keeproot=False):
@@ -32,7 +48,11 @@ class ZenDaemon(CmdBase):
         if not noopts:
             signal.signal(signal.SIGINT, self.sigTerm)
             signal.signal(signal.SIGTERM, self.sigTerm)
-            if self.options.daemon:self.becomeDaemon() 
+            if self.options.daemon:
+                self.changeUser()
+                self.becomeDaemon() 
+                #need to redo logging because files are closed in becomeDaemon
+                self.setupLogging()
 
 
     def setupLogging(self):
@@ -59,17 +79,7 @@ class ZenDaemon(CmdBase):
             logging.basicConfig()
 
 
-    def becomeDaemon(self):
-        """fork twice to become a daemon"""
-        pid = 0
-        try:
-            pid = os.fork()
-            if pid > 0:
-                sys.exit(0)
-        except OSError, e:
-            print >>sys.stderr, "ERROR: fork #1 failed: %d (%s)" % (
-                                e.errno, e.strerror)
-        os.chdir("/")
+    def changeUser(self):
         if not self.keeproot:
             try:
                 cname = pwd.getpwuid(os.getuid())[0]
@@ -78,22 +88,55 @@ class ZenDaemon(CmdBase):
             except KeyError:
                 print >>sys.stderr, "WARN: user:%s not found running as:%s"%(
                                     self.options.uid,cname)
-        os.setsid()
-        os.umask(0)
+
+
+    def becomeDaemon(self):
+        """Code below comes from the excelent recipe by Chad J. Schroeder.
+        """
         try:
             pid = os.fork()
-            if pid > 0:
-                sys.exit(0)
         except OSError, e:
-            print >>sys.stderr, "ERROR: fork #2 failed: %d (%s)" % (
-                                e.errno, e.strerror)
+            raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if (pid == 0):	# The first child.
+            os.setsid()
+            try:
+                pid = os.fork()	# Fork a second child.
+            except OSError, e:
+                raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+            if (pid == 0):	# The second child.
+                os.chdir(WORKDIR)
+                os.umask(UMASK)
+            else:
+                os._exit(0)	# Exit parent (the first child) of the second child.
+        else:
+            os._exit(0)	# Exit parent of the first child.
+
+        import resource		# Resource usage information.
+        maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+        if (maxfd == resource.RLIM_INFINITY):
+            maxfd = MAXFD
+
+        # Iterate through and close all file descriptors.
+        for fd in range(0, maxfd):
+            try:
+                os.close(fd)
+            except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+                pass
+
+        os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
+        # Duplicate standard input to standard output and standard error.
+        os.dup2(0, 1)			# standard output (1)
+        os.dup2(0, 2)			# standard error (2)
+        return(0)
+
         if os.path.exists(self.zenvar):
             file = open(self.pidfile, 'w')
             file.write(str(os.getpid()))
             file.close()
         else:
-            print >>sys.stderr, "ERROR: unable to open pid file %s" % pidfile
-            sys.exit(1) 
+            raise SystemExit("ERROR: unable to open pid file %s" % pidfile)
 
 
     def sigTerm(self, signum, frame):
