bda5e098ad04202b90358f5c906006ad32d477af
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-01-27 23:23:13 +0000

    - fixes
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14347 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zentinel/snmp/DellPCIMap.py
+++ Products/DataCollector/plugins/zentinel/snmp/DellPCIMap.py
@@ -31,6 +31,6 @@ class DellPCIMap(SnmpPlugin):
         for card in pcitable.values():
             om = self.objectMap(card)
             om.id = self.prepId("%s" % om.slot)
-            om.setProductKey("%s %s" % (om._manuf, om._model))
+            om.setProductKey = "%s %s" % (om._manuf, om._model)
             rm.append(om)
         return rm
