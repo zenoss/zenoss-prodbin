77e8d24be833c9cd6c1f0e2a612a2e6537f2851a
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-03-21 18:03:13 +0000

    * add powerware devicmap
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14608 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/DataCollector/plugins/zenoss/snmp/PowerwareDeviceMap.py
@@ -0,0 +1,25 @@
+#################################################################
+#
+#   Copyright (c) 2006 Confmon Corporation. All rights reserved.
+#
+#################################################################
+
+from CollectorPlugin import SnmpPlugin, GetMap
+
+class PowerwareDeviceMap(SnmpPlugin):
+    """Map mib elements from Dell Open Manage mib to get hw and os products.
+    """
+
+    maptype = "PowerwareDeviceMap" 
+
+    snmpGetMap = GetMap({ 
+        '.1.3.6.1.4.1.534.1.1.1.0': 'setHWProductKey',
+         })
+
+
+    def process(self, device, results, log):
+        """collect snmp information from this device"""
+        log.info('processing Powerware device info on device %s' % device.id)
+        getdata, tabledata = results
+        om = self.objectMap(getdata)
+        return om
