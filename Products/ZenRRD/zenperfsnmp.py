6fe472a065893b57da014442e9a5cbadcbfe7ef4
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-04-14 00:46:40 +0000

    check for unresponsive machines before trying to talk to them send events for start/stop/heartbeat start fetching performance data after we have all the config data add a method for making all the xmlrpc proxy objects we use more doc strings
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14680 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/zenperfsnmp.py
+++ Products/ZenRRD/zenperfsnmp.py
@@ -14,6 +14,7 @@ $Id$
 __version__ = "$Revision$"[11:-2]
 
 import os
+import socket
 
 import Globals
 
@@ -22,33 +23,49 @@ from twisted.internet import reactor, defer
 from Products.ZenUtils.ZenDaemon import ZenDaemon
 from Products.ZenUtils.Utils import basicAuthUrl
 from Products.ZenUtils.TwistedAuth import AuthProxy
+from Products.ZenEvents.SendEvent import SendEvent
 
 from twistedsnmp.agentproxy import AgentProxy
-from twistedsnmp import snmpprotocol 
+from twistedsnmp import snmpprotocol
 
 DEFAULT_URL = 'http://localhost:8080/zport/dmd/Monitors/Cricket/localhost'
+EVENT_URL = 'http://localhost:8080/zport/dmd/ZenEventManager'
 SAMPLE_CYCLE_TIME = 5*60
 CONFIG_CYCLE_TIME = 20*60
 MAX_OIDS_PER_REQUEST = 200
 MAX_SNMP_REQUESTS = 100
 
-SAMPLE_CYCLE_TIME = 5
+COMMON_EVENT_INFO = {
+    'device':socket.getfqdn(),
+    'component':'zenperfsnmp'
+    }
 
 def chunk(lst, n):
+    'break lst into n-sized chunks'
     return [lst[i:i+n] for i in range(0, len(lst), n)]
 
 def rrdPath(branch):
+    'compute where the RDD perf files should go'
     root = os.path.join(os.getenv('ZENHOME'), 'perf')
     r = os.path.join(root, branch[1:] + '.rrd')
     return r
 
 def nameFromPath(branch):
+    'Pick an RRD data source name from the path name'
     name = os.path.split(branch)[-1]
     name = ''.join(name.split(' '))
     name = '_'.join(name.split('-'))
     return name
 
 class ZenPerformanceFetcher(ZenDaemon):
+    "Periodically query all devices for SNMP values to archive in RRD files"
+    
+    startevt = {'eventClass':'/App/Start', 'summary': 'started', 'severity': 0}
+    stopevt = {'eventClass':'/App/Stop', 'summary': 'stopped', 'severity': 4}
+    heartbeat = {'eventClass':'/Heartbeat'}
+    for event in [startevt, stopevt, heartbeat]:
+        event.update(COMMON_EVENT_INFO)
+
 
     def __init__(self):
         ZenDaemon.__init__(self)
@@ -56,6 +73,12 @@ class ZenPerformanceFetcher(ZenDaemon):
         self.proxies = {}
         self.snmpPort = snmpprotocol.port()
         self.queryWorkList = []
+        self.zem = SendEvent('PerformanceFetcher',
+                             self.options.zopeusername,
+                             self.options.zopepassword,
+                             self.options.zem)
+        self.deviceLoopStarted = False
+        self.unresponsiveDevices = []
 
 
     def buildOptions(self):
@@ -71,16 +94,37 @@ class ZenPerformanceFetcher(ZenDaemon):
             dest="zopeusername", help="username for zope server",
             default='admin')
         self.parser.add_option("-p", "--zopepassword", dest="zopepassword")
+        self.parser.add_option(
+            '--zem', dest='zem',
+            help="XMLRPC path to an ZenEventManager instance",
+            default=EVENT_URL)
         self.parser.add_option("--debug", dest="debug", action='store_true')
+
+
+    def buildProxy(self, url):
+        "create AuthProxy objects with our config and the given url"
+        url = basicAuthUrl(self.options.zopeusername,
+                           self.options.zopepassword,
+                           url)
+        return AuthProxy(url)
+
+
+    def startFetchUnresponsiveDevices(self, devices):
+        "start fetching the devices that are unresponsive"
+        proxy = self.buildProxy(self.options.zem)
+        d = proxy.callRemote('getWmiConnIssues')
+        d.addCallbacks(self.setUnresponsiveDevices, self.logError)
+
+
+    def setUnresponsiveDevices(self, deviceStatus):
+        "remember all the unresponsive devices"
+        self.unresponsiveDevices = [d[0] for d in deviceStatus]
 
 
     def startUpdateConfig(self):
         'Periodically ask the Zope server for the device list.'
         if not self.model:
-            url = basicAuthUrl(self.options.zopeusername,
-                               self.options.zopepassword,
-                               self.options.zopeurl)
-            self.model = AuthProxy(url)
+            self.model = self.buildProxy(self.options.zopeurl)
         deferred = self.model.callRemote('cricketDeviceList', True)
         deferred.addCallbacks(self.updateConfig, self.reconnect)
         reactor.callLater(CONFIG_CYCLE_TIME, self.startUpdateConfig)
@@ -89,8 +133,13 @@ class ZenPerformanceFetcher(ZenDaemon):
     def updateConfig(self, deviceList):
         '''Update the list of devices, and initiate a request
         to get the device configuration'''
+        lst = []
         for device in deviceList:
-            self.startDeviceConfig(device)
+            lst.append(self.startDeviceConfig(device))
+        defer.DeferredList(lst, consumeErrors=1).addCallback(self.readDevices1)
+
+        self.startFetchUnresponsiveDevices(deviceList)
+            
         # stop collecting those no longer in the list
         doomed = set(self.proxies.keys()) - set(deviceList)
         for device in doomed:
@@ -100,27 +149,29 @@ class ZenPerformanceFetcher(ZenDaemon):
 
     def startDeviceConfig(self, deviceUrl):
         'Kick off a request to get the configuration of a device'
-        devUrl = basicAuthUrl(self.options.zopeusername,
-                              self.options.zopepassword,
-                              deviceUrl)
-        proxy = AuthProxy(devUrl)
+        proxy = self.buildProxy(deviceUrl)
         d = proxy.callRemote('getSnmpOidTargets')
         d.addCallbacks(self.updateDeviceConfig, self.logError)
+        return d
 
 
     def updateDeviceConfig(self, snmpTargets):
         'Save the device configuration and create an SNMP proxy to talk to it'
         deviceName, ip, port, community, oidData = snmpTargets
-        p = AgentProxy(ip=ip, port=port,
-                       community=community,
-                       protocol=self.snmpPort.protocol,
-                       allowCache=True)
+        p = AgentProxy(ip=ip, port=port, community=community,
+                       protocol=self.snmpPort.protocol, allowCache=True)
         p.badOidMode = False
         p.oidMap = {}
         for oid, path, dsType in oidData:
             p.oidMap[oid] = path, dsType
         self.proxies[deviceName] = p
+        return snmpTargets
 
+    def readDevices1(self, *ignored):
+        "Start the read devices timer after we have collected device configs"
+        if self.deviceLoopStarted: return
+        self.deviceLoopStarted = True
+        self.readDevices()
 
     def readDevices(self):
         'Periodically fetch the performance values from all known devices'
@@ -131,7 +182,8 @@ class ZenPerformanceFetcher(ZenDaemon):
                              "waiting for them to finish" %
                              len(self.queryWorkList))
         else:
-            self.queryWorkList = self.proxies.keys()
+            self.queryWorkList =  set(self.proxies.keys())
+            self.queryWorkList -= set(self.unresponsiveDevices)
             for i in range(MAX_SNMP_REQUESTS):
                 if not self.queryWorkList: break
                 self.startReadDevice(self.queryWorkList.pop())
@@ -235,8 +287,21 @@ class ZenPerformanceFetcher(ZenDaemon):
         self.log.error('connection to Zope server dropped (%s), '
                        'reconnecting' % error)
 
+    def beat(self):
+        self.zem.server.sendEvent(self.heartbeat)
+        reactor.callLater(SAMPLE_CYCLE_TIME, self.beat)
+
+    def main(self):
+        "Run forever, fetching and storing"
+        self.heartbeat['timeout'] = SAMPLE_CYCLE_TIME * 3
+        self.zem.server.sendEvent(self.startevt)
+        
+        zpf.startUpdateConfig()
+        self.beat()
+        reactor.run()
+        
+        self.zem.server.sendEvent(self.stopevt)
+
 if __name__ == '__main__':
     zpf = ZenPerformanceFetcher()
-    zpf.startUpdateConfig()
-    zpf.readDevices()
-    reactor.run()
+    zpf.main()
