874632a0c1efe50d8e233877fb7981c98cc2decd
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-04-19 17:09:44 +0000

    * convert Erik's data migration script to the new framework
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14718 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/zenrrd.py
+++ Products/ZenModel/migrate/zenrrd.py
@@ -1,3 +1,25 @@
+#################################################################
+#
+#   Copyright (c) 2006 Zenoss, Inc. All rights reserved.
+#
+#################################################################
 
-for dc in dmd.Devices.getSubOrganizers():
-    dc.buildRelations()
+__doc__="""RebuildRRDRelations
+
+Rebuild Device relations for RRD refactor
+
+$Id$"""
+
+__version__ = "$Revision$"[11:-2]
+
+import Migrate
+
+class RebuildRRDRelations(Migrate.Step):
+    version = 20.0
+
+    def cutover(self, dmd):
+        for dc in dmd.Devices.getSubOrganizers():
+            dc.buildRelations()
+        dmd.Devices.buildRelations()
+
+RebuildRRDRelations()
