6e08afc0088ee89994adb07aa947e30156664b01
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-05-03 18:17:28 +0000

    * there is no snmp under module SnmpCollector * indent module-scope code inside conditional for pychecker
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14812 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/identify.py
+++ Products/ZenModel/identify.py
@@ -12,21 +12,22 @@ $Id: identify.py,v 1.1 2002/09/17 19:47:32 edahl Exp $"""
 
 __version__ = "$Revision: 1.1 $"[11:-2]
 
-from Products.SnmpCollector.snmp import SnmpSession
+from Products.SnmpCollector import SnmpSession
 import sys
 
-filename = sys.argv[1]
-community = sys.argv[2]
-if len(sys.argv) > 3:
-    port = sys.argv[3] or 161
-lines = open(filename).readlines()
-lineNumber = 0
-for line in lines:
-    lineNumber += 1
-    dev = line.strip()
-    sess = SnmpSession(dev, community, timeout=1, retries=1)
-    print "device", dev,
-    try:
-        print "descr =", sess.get('.1.3.6.1.2.1.1.1.0').values()[0]
-    except:
-        print "problem with device", dev
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    community = sys.argv[2]
+    if len(sys.argv) > 3:
+        port = sys.argv[3] or 161
+    lines = open(filename).readlines()
+    lineNumber = 0
+    for line in lines:
+        lineNumber += 1
+        dev = line.strip()
+        sess = SnmpSession(dev, community, timeout=1, retries=1)
+        print "device", dev,
+        try:
+            print "descr =", sess.get('.1.3.6.1.2.1.1.1.0').values()[0]
+        except:
+            print "problem with device", dev
