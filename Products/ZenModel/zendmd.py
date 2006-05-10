978bbd725b5c82b46fc3710ab82736d109678c59
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-05-10 13:27:15 +0000

    * add login/logout to zendmd
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14863 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/zendmd.py
+++ Products/ZenModel/zendmd.py
@@ -1,6 +1,9 @@
 import socket
 import Globals
 import transaction
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManagement import noSecurityManager
+
 from Products.ZenUtils.ZCmdBase import ZCmdBase
 
 class zendmd(ZCmdBase): pass
@@ -30,6 +33,19 @@ if __name__ == '__main__':
         dmd.Manufacturers.reIndex()
         dmd.Networks.reIndex()
         commit()
+
+    def login(name):
+        '''Logs in.'''
+        uf = zport.acl_users
+        user = uf.getUserById(name)
+        if not hasattr(user, 'aq_base'):
+            user = user.__of__(uf)
+        newSecurityManager(None, user)
+
+    def logout():
+        '''Logs out.'''
+        noSecurityManager()
+
 
     print "Welcome to zenoss dmd command shell!"
     print "use zhelp() to list commnads"
