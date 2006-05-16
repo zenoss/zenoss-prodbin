999aad30967dfb15c6179570c6dd0f8868456a51
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-05-16 12:26:54 +0000

    * turn events back on
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14899 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/zenperfsnmp.py
+++ Products/ZenRRD/zenperfsnmp.py
@@ -295,7 +295,6 @@ class zenperfsnmp(ZenDaemon):
 
     def sendEvent(self, event, **kw):
         'convenience function for pushing events to the Zope server'
-        return
         ev = event.copy()
         ev.update(COMMON_EVENT_INFO)
         ev.update(kw)
