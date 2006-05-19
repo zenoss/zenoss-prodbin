e52c668fcdf726a0fdbbfe45b3f35b51f9fb328b
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-05-19 18:42:37 +0000

    #108 AJAX reload of events would stop if there were errors or delays * use MochiKit to fetch list * promote cancelWithTimeout to zenutils * remove dojo javascript
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@14932 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/skins/zenevents/zeneventsfuncs.js
+++ Products/ZenEvents/skins/zenevents/zeneventsfuncs.js
@@ -1,29 +1,31 @@
 var refreshControl, loadStatusDefered;
 var autoRefresh = 0;
-dojo.require("dojo.io.bind");
-//dojo.require("dojo.io.*");
 
-errorHandler = function(type,error) {
-    log("error loading events type=", type);
-    log("msg=", error.message);
-    log("code=", error.number);
-    resetLoadStatus();
-    errmsg = TBODY('eventsBody',TR(null,
-            TD(null,{'class':'errorTitle'},'Events Unavailable')));
-    swapDOM("eventsBody", errmsg);
+var printError = function(err) {
+    log("error: ", err);
 }
 
-processData = function(type, data, evt) {
+var errorHandler = function(error) {
+    log("error loading events type=", error);
+    resetLoadStatus();
+    var node = document.getElementById("loadingStatus");
+    if (node) {
+	node.innerHTML = 'Events Unavailable';
+    }
+}
+
+var processData = function(evt) {
+    log("processingData evt=", evt);
     log("processingData status=", evt.statusText);
     /*bodyNode = req.responseXML.getElementById("eventsBody");
     log("processingData tbody=", bodyNode.id);*/
     if (evt.status == 200) {
-        renderTableBody(data);
+        renderTableBody(evt.responseText);
     }
 };
 
 //render the table rows by putting the returned data into the tbody element.
-renderTableBody = function(html) {
+var renderTableBody = function(html) {
     log("renderTableBody");
     resetLoadStatus();
     evbody = document.getElementById("eventsBody");
@@ -33,10 +35,10 @@ renderTableBody = function(html) {
 };
 
 var statusCount = 0;
-loadStatus = function() {
-    node = document.getElementById("loadingStatus");
+var loadStatus = function() {
+    var node = document.getElementById("loadingStatus");
     if (node) {
-        msg = "loading...";
+        var msg = "loading...";
         for (var i=0;i<statusCount;i++) {
             msg = msg + ".";
         }
@@ -47,7 +49,7 @@ loadStatus = function() {
     }
 };
 
-resetLoadStatus = function() {
+var resetLoadStatus = function() {
     if (loadStatusDefered) { //very stange if this isn't defined
         loadStatusDefered.cancel();
         loadStatusDefered = null;
@@ -55,33 +57,29 @@ resetLoadStatus = function() {
     }
 };
 
-//load the event list using dojo.io.bind
-getTablePage = function(form) {
+//load the event list
+var getTablePage = function(form) {
     log("getTablePage url=", form.url.value);
     if (refreshControl) { refreshControl.cancel(); }
-    if (loadStatusDefered) { //oh shit we are resubmiting!
+    if (loadStatusDefered) { // Oops! We are already resubmiting!
         log("resubmit while query pending!!") 
-        return
     } else {
         loadStatusDefered = loadStatus()
+	var d = doSimpleXMLHttpRequest(form.url.value + "?" + 
+				       queryString(form));
+	d = cancelWithTimeout(d, 30);
+	d.addCallbacks(processData, errorHandler);
+	d.addErrback(printError);
     }
-    /*
-    d = doSimpleXMLHttpRequest(form.url.value, formContents(form))
-    d.addCallback(processData)
-    d.addCallback(errorHandler)
-    */
-    dojo.io.bind({url: form.url.value, formNode: form, 
-        load: processData,
-        error: errorHandler,
-        mimetype:"text/plain"})
-    rate = parseInt(form.refreshRate.value)
+
     if (autoRefresh) {
+	var rate = parseInt(form.refreshRate.value)
         refreshControl = callLater(rate,getTablePage,form);
     }
 };
 
 //set the sortedHeader and sence and submit form
-setSortedHeader = function(name, form) {
+var setSortedHeader = function(name, form) {
     sence = form.sortedSence.value;
     if (name == form.sortedHeader.value) {
         if (sence == "") { sence = "asc"; }
@@ -98,14 +96,14 @@ setSortedHeader = function(name, form) {
 }
 
 // load new page after navigation button is clicked.
-getPageNavButton = function(name, form) {
+var getPageNavButton = function(name, form) {
     form.navbutton.value = name;
     log("navbutton=", form.navbutton.value);
     getTablePage(form);
 }
 
 //look for enter press while in text input and execute getTablePage
-getPageViaEnter = function(evt) {
+var getPageViaEnter = function(evt) {
     evt = (evt) ? evt : event;
     var target = (evt.target) ? evt.target : evt.srcElement;
     var form = target.form;
@@ -120,7 +118,7 @@ getPageViaEnter = function(evt) {
 };
 
 
-startRefresh = function(button, form) {
+var startRefresh = function(button, form) {
     if (autoRefresh) {
         log("stopAutoRefresh");
         refreshControl.cancel()
@@ -135,7 +133,7 @@ startRefresh = function(button, form) {
 };
 
 //display an event detail in its own native window
-eventWindow = function(manager, evid, width, height) {
+var eventWindow = function(manager, evid, width, height) {
     url = "/zport/dmd/"+manager+"/viewEventFields?evid=" + evid
     windowprops = "width=500,height=650,resizable=yes,scrollbars=yes";
     evwindow = window.open(url, evid, windowprops);
