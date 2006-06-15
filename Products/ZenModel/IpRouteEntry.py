c810ad12ebc306b6ed7c630aa41221c92d72b8f8
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-06-15 22:18:52 +0000

    * fix problem with interface name on iprouteentry
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15105 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/IpRouteEntry.py
+++ Products/ZenModel/IpRouteEntry.py
@@ -132,7 +132,7 @@ class IpRouteEntry(OSComponent):
         If no interface is found return 'No Interface'.
         """
         if self.interface():
-            return self.interface().name
+            return self.interface().name()
         return "No Interface"
 
        
