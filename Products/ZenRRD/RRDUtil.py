160ebc36f744495919fef8ad0c70feaf8c2fa790
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-06-30 13:42:47 +0000

    * code that deals with storing a value in an RRD file
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15215 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenRRD/RRDUtil.py
@@ -0,0 +1,41 @@
+
+import logging
+log = logging.getLogger("zen.RRDUtil")
+
+from Products.ZenModel.PerformanceConf import performancePath
+
+class RRDUtil:
+    def __init__(self, defaultRrdCreateCommand, cycleTime):
+        self.defaultRrdCreateCommand = defaultRrdCreateCommand
+        self.cycleTime = cycleTime
+
+    def save(self, path, value, rrdType, rrdCommand = None):
+        import rrdtool, os
+        filename = performancePath(path) + '.rrd'
+        if not rrdCommand:
+            rrdCommand = self.defaultRrdCreateCommand
+        if not os.path.exists(filename):
+            log.debug("create new rrd %s", filename)
+            dirname = os.path.dirname(filename)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            dataSource = 'DS:%s:%s:%d:0:U' % ('ds0', rrdType,
+                                              3*self.cycleTime)
+            rrdtool.create(filename,
+                           "--step",  str(self.cycleTime),
+                           dataSource, *rrdCommand.split())
+        
+        try:
+            rrdtool.update(filename, 'N:%s' % value)
+        except rrdtool.error, err:
+            # may get update errors when updating too quickly
+            log.error('rrd error %s %s', err, path)
+
+        if rrdType == 'COUNTER':
+            startStop, names, values = \
+                       rrdtool.fetch(filename, 'AVERAGE',
+                                     '-s', 'now-%d' % self.cycleTime*2,
+                                     '-e', 'now')
+            value = values[0][0]
+        return value
+
