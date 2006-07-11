a4033a571b6b47e4a8864453ed2e2a47f4a493c2
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-11 14:30:22 +0000

    * test SendEvents
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15318 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/xtest.py
+++ Products/ZenEvents/xtest.py
@@ -1,11 +1,11 @@
 import xmlrpclib
 
 s = xmlrpclib.ServerProxy('http://localhost:8081')
-for i in range(100):
-    s.sendEvent({
-        'device':'eros',
+ev = {  'device':'eros',
         'eventClass':'xyzzy',
         'summary':'This is a test event',
         'severity':2,
         'component':'',
-        })
+        }
+for i in range(100):
+    s.sendEvents([ev, ev])
