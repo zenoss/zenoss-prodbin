20f018a281a24ccdf5cfa1734317c4d5facbb14e
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-12 19:30:51 +0000

    * tiny bit of testing
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15339 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/xtest.py
+++ Products/ZenEvents/xtest.py
@@ -1,17 +1,31 @@
 import xmlrpclib
+import time
 
 s = xmlrpclib.ServerProxy('http://localhost:8081')
-ev = {  'device':'eros',
-        'eventClass':'xyzzy',
-        'summary':'This is a test event',
-        'severity':2,
-        'component':'',
-        }
-s.sendEvent(ev)
-ev2 = ev.copy()
-ev2['severity'] = 0
-for i in range(10):
-    s.sendEvents([ev, ev2])
-issues = s.getDevicePingIssues()
-for i in issues:
-    print i
+
+event = dict(device='eros', 
+             eventClass='/Unknown',
+             summary='This is a test event: %d' % time.time(), 
+             severity=3,
+             component='xyzzy')
+clear = ev.copy()
+clear.update(dict(severity=0, summary="All better now!"))
+
+def main():
+    "perormance test"
+    for i in range(100):
+        s.sendEvents([event, clear])
+
+def coverage():
+    s.sendEvents([event, clear])
+    s.sendEvent(event)
+    issues = s.getDevicePingIssues()
+    for i in issues:
+        print i
+
+def simple():
+    s.sendEvent(event)
+    time.sleep(10)
+    s.sendEvent(clear)
+
+simple()
