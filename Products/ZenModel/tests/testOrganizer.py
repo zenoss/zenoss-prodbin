066646646c5afb5a534110e39141ede36886c52f
nate <nate@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-17 15:03:18 +0000

    * more testOrganizer
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15350 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/tests/testOrganizer.py
+++ Products/ZenModel/tests/testOrganizer.py
@@ -15,21 +15,36 @@ from Products.ZenModel.Exceptions import *
   
 from Products.ZenModel.Organizer import *
   
-class OrganizerTest(ZenModelBaseTest):
+class TestOrganizer(ZenModelBaseTest):
   
     def testOrganizer(self):
         org = self.create(self.dmd, Organizer, "org")
         org.dmdRootName = "org"
         bar = org.createOrganizer("/foo/bar")
         foo = org._getOb("foo")
+        
         self.assert_(foo in org.children())
         self.assert_("foo" in org.childIds())
+
         self.assert_(org.countChildren() == 2)
         self.assert_(org.unrestrictedTraverse("foo/bar") == bar)
         self.assert_(org.getOrganizer("/foo/bar") == bar)
         self.assert_(bar.getOrganizerName() == "/foo/bar")
+
         self.assert_('/foo' in org.getOrganizerNames())
         self.assert_('/foo/bar' in org.getOrganizerNames())
+        self.assert_('/foo' in org.deviceMoveTargets())
+        self.assert_('/foo/bar' in org.deviceMoveTargets())
+        self.assert_('/foo' in org.childMoveTargets())
+        self.assert_('/foo/bar' in org.childMoveTargets())
+
+        self.assert_(org.getDmdKey() == '/')
+        self.assert_(foo in org.getSubOrganizers())
+        self.assert_(bar in org.getSubOrganizers())
+
+        self.assert_(org.getChildMoveTarget('/foo') == foo)
+        self.assert_(org.getChildMoveTarget('/foo/bar') == bar)
+
 
 def main():
     unittest.TextTestRunner().run(test_suite())
