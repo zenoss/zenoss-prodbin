250213109bf3eb9044c6eb550ceda0f49c9fc816
nate <nate@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-20 16:52:43 +0000

    * update testDeviceClass
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15390 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/tests/testDeviceClass.py
+++ Products/ZenModel/tests/testDeviceClass.py
@@ -25,7 +25,7 @@ class TestDeviceClass(ZenModelBaseTest):
 
 
     def testCreateInstanceDevice(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         devices.createCatalog()#necessary, don't know why
         dev = devices.createInstance("testdev")
         self.assert_(isinstance(dev, Device))
@@ -33,10 +33,11 @@ class TestDeviceClass(ZenModelBaseTest):
         self.assert_(dev.getDeviceClassName() == "/")
         self.assert_(devices.countDevices() == 1)
         self.assert_(dev in devices.getSubDevices())
+        self.assert_(devices.getPythonDeviceClass() == Device)
 
     
     def testCreateInstanceDeviceAndIndex(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         devices.createCatalog()
         dev = devices.createInstance("testdev")
         self.assert_(isinstance(dev, Device))
@@ -45,21 +46,21 @@ class TestDeviceClass(ZenModelBaseTest):
 
 
     def testSearchDevicesOneDevice(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         devices.createCatalog()
         dev = devices.createInstance("testdev")
         self.assertRaises(Redirect, devices.searchDevices, "testdev")
 
     
     def testSearchDevicesNoDevice(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         devices.createCatalog()
         dev = devices.createInstance("testdev")
         self.assert_(len(devices.searchDevices("adsf"))==0)
 
     
     def testSearchDevicesMultipleDevices(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         devices.createCatalog()
         dev = devices.createInstance("testdev")
         dev = devices.createInstance("testdev2")
@@ -67,7 +68,7 @@ class TestDeviceClass(ZenModelBaseTest):
 
     
     def testGetPeerDeviceClassNames(self):
-        devices = self.create(self.dmd, DeviceClass, "Devices")
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
         routers = devices.createOrganizer("/NetworkDevice/Router")
         devices.createOrganizer("/NetworkDevice/Router/Firewall")
         devices.createOrganizer("/NetworkDevice/Router/RSM")
@@ -88,8 +89,33 @@ class TestDeviceClass(ZenModelBaseTest):
         self.assert_(dev not in self.dmd.Devices.NetworkDevice.Router.getSubDevices())
                         
 
-    
-                            
+    def testOrganizer(self):
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
+        dc = devices.createOrganizer('/Test')
+        self.assert_(dc in devices.children())
+        self.assert_(dc in devices.getSubOrganizers())
+        self.assert_(devices.countChildren() == 1)
+        self.assert_('Test' in devices.childIds())
+        self.assert_('/Test' in devices.getOrganizerNames())
+        self.assert_(devices.getOrganizer('/Test') == dc)
+        layer = devices.createOrganizer('/Layer')
+        devices.moveOrganizer('Layer',['Test']) 
+        self.assert_('/Layer' in devices.getOrganizerNames())
+        self.assert_(dc not in devices.children())
+        self.assert_(dc in devices.getSubOrganizers())
+        devices.manage_deleteOrganizers(['/Layer'])
+        self.assert_(layer not in devices.children())
+        self.assert_(dc not in devices.getSubOrganizers())
+
+
+    def testDeviceOrganizer(self):
+        devices = self.create(self.dmd, DeviceClass, 'Devices')
+        devices.createCatalog()
+        dev = devices.createInstance('testdev')
+        dc = devices.createOrganizer('/Test')
+        self.assert_(devices.countDevices() == 1)
+        self.assert_(dev in devices.getSubDevices())
+        
 
 def main():
     unittest.TextTestRunner().run(test_suite())
