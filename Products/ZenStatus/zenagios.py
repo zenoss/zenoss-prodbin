a7adf36a1670118777316c75b10669a4aa252b4f
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-31 18:14:11 +0000

    * optionally run processes remotely using SSH
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15472 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenStatus/zenagios.py
+++ Products/ZenStatus/zenagios.py
@@ -4,6 +4,16 @@
 #
 #################################################################
 
+__doc__=''' ZeNagios
+
+Run Nagios pluggins periodically.
+
+$Id$'''
+
+__version__ = "$Revision$"[11:-2]
+
+# FIXME: not using Cmd.loginTimeout
+
 import time
 import logging
 log = logging.getLogger("zen.zenagios")
@@ -17,57 +27,172 @@ from Products.ZenUtils.Driver import drive, driveLater
 from Products.ZenEvents import Event
 
 from Products.ZenRRD.RRDDaemon import RRDDaemon
+from Products.DataCollector.SshClient import SshClient
 
 from sets import Set
 
-MAX=2
+MAX_CONNECTIONS=50
 
-class TimeoutError(Exception): pass
+class TimeoutError(Exception):
+    "Error for a defered call taking too long to complete"
+    def __init__(self, *args):
+        Exception.__init__(self)
+        self.args = args
 
-def Timeout(deferred, seconds):
-    def _timeout(deferred):
-        deferred.errback(failure.Failure(TimeoutError))
+def Timeout(deferred, seconds, obj):
+    "Cause an error on a deferred when it is taking too long to complete"
+    def _timeout(deferred, obj):
+        "took too long... call an errback"
+        deferred.errback(failure.Failure(TimeoutError(obj)))
     def _cb(arg, timer):
+        "the command finished, possibly by timing out"
         if not timer.called:
             timer.cancel()
         return arg
-    timer = reactor.callLater(seconds, _timeout, deferred)
+    timer = reactor.callLater(seconds, _timeout, deferred, obj)
     deferred.addBoth(_cb, timer)
     return deferred
 
 class ProcessRunner(ProcessProtocol):
+    "Provide deferred process execution"
     stopped = None
     exitCode = None
-    output = []
+    output = ''
 
-    def __init__(self, cmd):
-        self.defer = defer.Deferred()
-        self.command = cmd
-        
-    def start(self):
-        self.output = []
+    def start(self, cmd):
+        "Kick off the process: run it local"
         reactor.spawnProcess(self, '/bin/sh',
-                             ('/bin/sh', '-c', 'exec ' + self.command))
-        self.stopped = Timeout(defer.Deferred(), MAX)
+                             ('/bin/sh', '-c', 'exec ' + cmd.command))
+        self.stopped = Timeout(defer.Deferred(), cmd.commandTimeout, cmd)
         self.stopped.addErrback(self.timeout)
         return self.stopped
 
-    def timeout(self, ignored):
+    def timeout(self, unused):
+        "Kill a process if it takes too long"
         self.transport.signalProcess('KILL')
-        return ignored
+        return unused
 
     def outReceived(self, data):
-        self.output.append(data)
+        "Store up the output as it arrives from the process"
+        self.output += data
 
     def processEnded(self, reason):
+        "notify the starter that their process is complete"
         self.exitCode = reason.value.exitCode
-        self.output = [s.strip() for s in ''.join(self.output).split('\n')][0]
+        self.output = [s.strip() for s in self.output.split('\n')][0]
         if self.stopped:
             d, self.stopped = self.stopped, None
             d.callback(self)
 
+class MySshClient(SshClient):
+    "Connection to SSH server at the remote device"
+
+    def __init__(self, *args, **kw):
+        SshClient.__init__(self, *args, **kw)
+        self.defers = []
+    
+    def addCommand(self, command):
+        "Run a command against the server"
+        d = defer.Deferred()
+        self.defers.append(d)
+        SshClient.addCommand(self, command)
+        return d
+
+    def addResult(self, command, data, code):
+        "Forward the results of the command execution to the starter"
+        SshClient.addResult(self, command, data, code)
+        d = self.defers.pop(0)
+        if not d.called:
+            d.callback((data, code))
+
+    def check(self, ip):
+        "Turn off blocking SshClient.test method"
+        return True
+
+    def clientFinished(self):
+        "We don't need to track commands/results when they complete"
+        SshClient.clientFinished(self)
+        self.commands = []
+        self.results = []
+
+class SshPool:
+    "Cache all the Ssh connections so they can be managed" 
+    def __init__(self):
+        self.pool = {}
+
+    def get(self, cmd):
+        "Make an ssh connection if there isn't one available"
+        result = self.pool.get(cmd.device, None)
+        if result is None:
+            log.debug("Creating connection to %s", cmd.device)
+            options = Options(cmd.username, cmd.password,
+                              cmd.loginTimeout, cmd.commandTimeout)
+            result = MySshClient(cmd.device, cmd.ipAddress, options=options)
+            result.run()
+            self.pool[cmd.device] = result
+        return result
+
+    def _close(self, device):
+        "close the SSH connection to a device, if it exists"
+        c = self.pool.get(device, None)
+        if c:
+            log.debug("Closing connection to %s", device)
+            if c.connection and c.connection.transport:
+                c.connection.transport.loseConnection()
+            del self.pool[device]
+        
+    def close(self, cmd):
+        "symetric close that matches get() method"
+        self._close(cmd.device)
+
+    def trimConnections(self, schedule):
+        "reduce the number of connections using the schedule for guidance"
+        # compute device list in order of next use
+        devices = []
+        for c in schedule:
+            if c.device not in devices:
+                devices.append(c.device)
+        # close as many devices as needed
+        while devices and len(self.pool) > MAX_CONNECTIONS:
+            self._close(devices.pop())
+            
+
+class SshRunner:
+    "Run a single command across a cached Ssh connection"
+    exitCode = None
+    output = None
+
+    def __init__(self, pool):
+        self.pool = pool
+
+    def start(self, cmd):
+        "Initiate a command on the remote device"
+        self.defer = defer.Deferred()
+        c = self.pool.get(cmd)
+        d = Timeout(c.addCommand(cmd.command), cmd.commandTimeout, cmd)
+        d.addErrback(self.timeout)
+        d.addBoth(self.processEnded)
+        return d
+
+    def timeout(self, arg):
+        "Deal with slow executing command/connection (close it)"
+        cmd, = arg.value.args
+        # we could send a kill signal, but then we would need to track
+        # the command channel to send it to: just close the connection
+        self.pool.close(cmd)
+        return arg
+
+    def processEnded(self, value):
+        "Deliver ourselves to the starter with the proper attributes"
+        if isinstance(value, failure.Failure):
+            return value
+        self.output, self.exitCode = value
+        return self
+
 class Cmd:
+    "Holds the config of every command to be run"
     device = None
+    ipAddress = None
     username = None
     password = None
     command = None
@@ -80,9 +205,6 @@ class Cmd:
     lastStart = 0
     lastStop = 0
     result = None
-
-    def __init__(self, sshConnections = None):
-        self.sshConnections = sshConnections
 
     def running(self):
         return self.lastStop < self.lastStart 
@@ -99,10 +221,13 @@ class Cmd:
             return self.lastStart
         return self.lastStop + self.cycleTime
 
-    def start(self):
+    def start(self, pool):
         self.lastStart = time.time()
-        pr = ProcessRunner(self.command)
-        d = pr.start()
+        if self.useSsh:
+            pr = SshRunner(pool)
+        else:
+            pr = ProcessRunner()
+        d = pr.start(self)
         log.debug('Process %s started' % self.name())
         d.addBoth(self.processEnded)
         return d
@@ -121,12 +246,17 @@ class Cmd:
             return self
         return pr
 
-    def updateConfig(self,device,username, password, useSsh,
-                     cycleTime, eventKey, eventClass, component, severity,
+    def updateConfig(self,device,ipAddress, username, password,
+                     loginTimeout, commandTimeout,
+                     cycleTime, useSsh,
+                     eventKey, eventClass, component, severity,
                      command, **kw):
         self.device = device
+        self.ipAddress = ipAddress
         self.username = username
         self.password = password
+        self.loginTimeout = loginTimeout
+        self.commandTimeout = commandTimeout
         self.useSsh = useSsh
         self.cycleTime = max(cycleTime, 1)
         self.eventKey = eventKey
@@ -134,7 +264,17 @@ class Cmd:
         self.component = component
         self.severity = severity
         self.command = command
-        
+
+class Options:
+    loginTries=1
+    searchPath=''
+    existenceTest=None
+    def __init__(self, username, password, loginTimeout, commandTimeout):
+        self.username = username
+        self.password = password
+        self.loginTimeout=loginTimeout
+        self.commandTimeout=commandTimeout
+
 class zenagios(RRDDaemon):
     heartbeatTimeout = RRDDaemon.configCycleInterval*3
     properties = RRDDaemon.properties + ("configCycleInterval",)
@@ -145,6 +285,7 @@ class zenagios(RRDDaemon):
         self.timeout = None
         self.deviceIssues = Set()
         self.flushEvents()
+        self.pool = SshPool()
 
     def flushEvents(self):
         self.sendEvents()
@@ -154,7 +295,11 @@ class zenagios(RRDDaemon):
         table = dict([((c.device,c.command), c) for c in self.schedule])
 
         for c in config:
-            device, username, password, commandPart = c
+            (device, ipAddress,
+             username, password,
+             loginTimeout, commandTimeout, commandPart) = c
+            if self.options.device and self.options.device != device:
+                continue
             for cmd in commandPart:
                 (useSsh, cycleTime,
                  eventKey, eventClass, component, severity, command) = cmd
@@ -182,51 +327,66 @@ class zenagios(RRDDaemon):
                 self._shutdown()
                 return
         try:
-            if self.timeout:
+            if self.timeout and not self.timeout.called:
                 self.timeout.cancel()
+                self.timeout = None
             self.schedule.sort(key=Cmd.nextRun)
+            self.pool.trimConnections(self.schedule)
             earliest = None
             running = 0
+            now = time.time()
             for c in self.schedule:
                 if c.running():
                     running += 1
-                elif c.nextRun() <= time.time():
-                    c.start().addBoth(self.finished)
+                elif c.nextRun() <= now:
+                    c.start(self.pool).addBoth(self.finished)
                     running += 1
                 else:
-                    earliest = c.nextRun()
+                    earliest = c.nextRun() - now
                     break
                 if running >= self.options.parallel:
                     break
             if earliest is not None:
-                self.timeout = reactor.callLater(earliest, self.processSchedule)
+                log.debug("Next command in %f seconds", earliest)
+                self.timeout = reactor.callLater(earliest,
+                                                 self.processSchedule)
         except Exception, ex:
             log.exception(ex)
             
     def finished(self, cmd):
         if isinstance(cmd, failure.Failure):
-            log.exception(cmd.value)
-            return
-        msg, values = cmd.result.output.split('|', 1)
-        exitCode = cmd.result.exitCode
-        severity = cmd.severity
-        issueKey = cmd.device, cmd.eventClass
-        if exitCode == 0:
-            severity = 0
-        elif exitCode == 2:
-            severity = min(severity + 1, 5)
-        if severity or issueKey in self.deviceIssues:
-            self.sendThresholdEvent(device=cmd.device,
-                                    summary=msg,
-                                    severity=severity,
-                                    message=msg,
-                                    performanceData=values,
-                                    eventKey=cmd.eventKey,
-                                    eventClass=cmd.eventClass,
-                                    component=cmd.component)
-            self.deviceIssues.add(issueKey)
-        if severity == 0:
-            self.deviceIssues.discard(issueKey)
+            if isinstance(cmd.value, TimeoutError):
+                cmd, = cmd.value.args
+                log.error("Command timed out on device %s: %s",
+                          cmd.device,
+                          cmd.command)
+            else:
+                log.exception(cmd.value)
+        else:
+            output = cmd.result.output
+            if output.find('|') >= 0:
+                msg, values = output.split('|', 1)
+            else:
+                msg, values = 'unparsable response', ''
+            exitCode = cmd.result.exitCode
+            severity = cmd.severity
+            issueKey = cmd.device, cmd.eventClass
+            if exitCode == 0:
+                severity = 0
+            elif exitCode == 2:
+                severity = min(severity + 1, 5)
+            if severity or issueKey in self.deviceIssues:
+                self.sendThresholdEvent(device=cmd.device,
+                                        summary=msg,
+                                        severity=severity,
+                                        message=msg,
+                                        performanceData=values,
+                                        eventKey=cmd.eventKey,
+                                        eventClass=cmd.eventClass,
+                                        component=cmd.component)
+                self.deviceIssues.add(issueKey)
+            if severity == 0:
+                self.deviceIssues.discard(issueKey)
         self.processSchedule()
 
     def fetchConfig(self):
@@ -256,9 +416,9 @@ class zenagios(RRDDaemon):
         RRDDaemon.buildOptions(self)
 
         self.parser.add_option('--parallel', dest='parallel', 
-                default=50, type='int',
-                help="number of devices to collect at one time")
-
+                               default=50, type='int',
+                               help="number of devices to collect at one time")
+        
     def main(self):
         self.sendEvent(self.startevt)
         drive(self.start).addCallbacks(self.processSchedule, self.error)
