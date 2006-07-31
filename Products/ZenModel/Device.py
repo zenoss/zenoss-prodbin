adda2bea2428a5eab6a839b14fcfb01fee9d9841
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-07-31 18:18:51 +0000

    * don't send devices without commands
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15474 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/Device.py
+++ Products/ZenModel/Device.py
@@ -442,11 +442,12 @@ class Device(ManagedEntity):
             if o.monitored(): cmds.extend(o.getNagiosCmds())
         for o in self.hw.harddisks(): 
             if o.monitored(): cmds.extend(o.getNagiosCmds())
-        return (self.id, self.getManageIp(),
-                self.zCommandUsername, self.zCommandPassword,
-                self.zCommandLoginTimeout, self.zCommandCommandTimeout,
-                cmds)
-        
+        if cmds:
+            return (self.id, self.getManageIp(),
+                    self.zCommandUsername, self.zCommandPassword,
+                    self.zCommandLoginTimeout, self.zCommandCommandTimeout,
+                    cmds)
+
 
     def getRRDTemplate(self, name=None):
         """Return the closest RRDTemplate named name by walking our aq chain.
