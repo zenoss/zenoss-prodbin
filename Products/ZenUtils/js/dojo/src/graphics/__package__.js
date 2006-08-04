44b5292077ffc32caef73dc809f4a508ebdde37b
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-08-04 14:12:36 +0000

    * remove dojo for now
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15530 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/js/dojo/src/graphics/__package__.js
+++
@@ -1,4 +0,0 @@
-/* Copyright (c) 2004-2005 The Dojo Foundation, Licensed under the Academic Free License version 2.1 or above */dojo.hostenv.conditionalLoadModule({
-	browser:	["dojo.graphics.htmlEffects"]
-});
-dojo.hostenv.moduleLoaded("dojo.graphics.*");
--- Products/ZenUtils/js/dojo/src/text/Text.js
+++
@@ -1,3 +0,0 @@
-/* Copyright (c) 2004-2005 The Dojo Foundation, Licensed under the Academic Free License version 2.1 or above */dojo.provide("dojo.text.Text");
-dj_deprecated("dojo.text.Text is being replaced by dojo.text.String");
-dojo.require("dojo.text.String");
