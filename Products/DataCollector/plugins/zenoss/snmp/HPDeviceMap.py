5725cd5017097517c28d014ed7ed08ed009682d1
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-08-15 19:11:08 +0000

    * cut-n-paste error in debug message
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15628 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zenoss/snmp/HPDeviceMap.py
+++ Products/DataCollector/plugins/zenoss/snmp/HPDeviceMap.py
@@ -21,7 +21,7 @@ class HPDeviceMap(SnmpPlugin):
 
     def process(self, device, results, log):
         """collect snmp information from this device"""
-        log.info('processing dell device info on device %s' % device.id)
+        log.info('processing HP device info on device %s' % device.id)
         getdata, tabledata = results
         om = self.objectMap(getdata)
         if om.setOSProductKey.find("NetWare") > -1:
