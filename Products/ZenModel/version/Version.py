e1945f7bc93c3f4342cd1351317878b295935875
oubiwann <oubiwann@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-09-26 18:16:49 +0000

    2006.09.26
     * Added code for parsing Version numbers using a classmethod that can be called
       without instantiating a Version Object.
     * Added unit tests (using doctest) to ZenModel.version.Version.
       
    
    
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15866 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/version/Version.py
+++ Products/ZenModel/version/Version.py
@@ -19,18 +19,18 @@ class Version(object):
     A class for obtaining and manipulating version numbers as well as creating
     the necessary version files Zenoss utilizes.
     """
-    def __init__(self, name, major, minor, micro):
+    def __init__(self, name, major, minor, micro, revision=''):
         self.name = name
         self.major = major
         self.minor = minor
         self.micro = micro
-        self.revision = ''
+        self.revision = revision
 
     def _getSVNVersion13(self):
         """
         For all versions of Subversion that support XML .svn/entries files.
         """
-        pass
+        return ''
 
     def _getSVNVersion14(self):
         """
@@ -38,9 +38,17 @@ class Version(object):
         supported. This method is for extracting revision information from SVN
         1.4+.
         """
-        pass
+        return ''
+
+    def _formatSVNRevision(self):
+        svnrev = self.revision
+        if svnrev:
+            svnrev = '  r%s' % svnrev
+        return svnrev
 
     def getSVNVersion(self):
+        if self.revision:
+            return self.revision
         try:
             v = self._getSVNVersion13()
         except ComponentVersionError:
@@ -48,11 +56,91 @@ class Version(object):
         self.revision = v
         return self.revision
 
+    def __repr__(self):
+        return '%s(%s, %d, %d, %d, %s)' % (
+            self.__class__.__name__,
+            self.name,
+            self.major,
+            self.minor,
+            self.micro,
+            self._formatSVNRevision())
+
+    def __str__(self):
+        return '[%s, version %d.%d.%d%s]' % (
+            self.name,
+            self.major,
+            self.minor,
+            self.micro,
+            self._formatSVNRevision())
+
     def createCurrentVersionModule(self):
         """
         This method creates/overwrites Products.ZenMode.version.Current with
         the version information stored in this Version() object.
         """
+
+    def parseVersion(self, versionString):
+        """
+        Parse the version info from a string. This method is usable without
+        having instantiated Version, and returns an instantiation.
+
+        The expected form is the following:
+            software_name W.X.Y rZ
+        where W, X, and Y represent the major, minor, and micro version numbers
+        and Z is the subversion revision number.
+
+        Only the software name is required.
+
+        The version number is expected to have at least a major version number.
+        Minor and micro version numbers are optional, but if they are provided,
+        they must be dot-delimited.
+
+        Here are some example usages:
+
+        >>> v = Version.parseVersion('Zenoss')
+        >>> repr(v)
+        'Version(Zenoss, 0, 0, 0, )'
+        >>> print v
+        [Zenoss, version 0.0.0]
+
+        >>> v = Version.parseVersion('Zenoss 1')
+        >>> repr(v)
+        'Version(Zenoss, 1, 0, 0, )'
+        >>> print v
+        [Zenoss, version 1.0.0]
+
+        >>> v = Version.parseVersion('Zenoss 0.26.4')
+        >>> repr(v)
+        'Version(Zenoss, 0, 26, 4, )'
+        >>> print v
+        [Zenoss, version 0.26.4]
+
+
+        >>> v = Version.parseVersion('Zenoss 0.32.1 r13667')
+        >>> repr(v)
+        'Version(Zenoss, 0, 32, 1,   r13667)'
+        >>> print v
+        [Zenoss, version 0.32.1  r13667]
+        """
+        versionParts = versionString.strip().split()
+        name = versionParts.pop(0)
+        #raise str(versionParts)
+        try:
+            versions = versionParts.pop(0).split('.')
+            # we want to always have a tuple of the right size returned,
+            # regardless of the number of elements in versions
+            (major, minor, micro) = (lambda x,y=0,z=0:
+                (int(x),int(y),int(z)))(*versions)
+            try:
+                revision = versionParts.pop(0).strip('r')
+            except IndexError:
+                revision = ''
+        except IndexError:
+            major = minor = micro = 0
+            revision = ''
+        self = Version(name, major, minor, micro, revision)
+        return self
+    parseVersion = classmethod(parseVersion)
 
 def getOSVersion():
     pass
@@ -97,3 +185,10 @@ def getCurrentVersions():
     should be accessed for all component software.
     """
     pass
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    _test()
