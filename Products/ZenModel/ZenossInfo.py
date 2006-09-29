fc1324a24b676245b8d80f72a92ca353ce4c697a
oubiwann <oubiwann@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-09-29 02:29:59 +0000

    * Added methods to ZenossInfo for information gathering on Zenoss daemons.  * Added content to zenossInfo.pt for displaying Zenoss status.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@15910 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZenossInfo.py
+++ Products/ZenModel/ZenossInfo.py
@@ -1,3 +1,5 @@
+import os
+import re
 import logging
 log = logging.getLogger("zen.ZenossInfo")
 
@@ -64,5 +66,80 @@ class ZenossInfo(SimpleItem, PropertyManager):
         uptimes.append(zope)
         return uptimes
     security.declareProtected('View','getAllUptimes')
+
+    def getZenossDaemonStates(self):
+        """
+        Return a data structures representing the states of the supported
+        Zenoss daemons.
+        """
+        states = []
+        activeButtons = {'button1': 'Restart', 'button2': 'Stop', 'button2state': True}
+        inactiveButtons = {'button1': 'Start', 'button2': 'Stop', 'button2state': False}
+        for daemon in self._getDaemonList():
+            pid = self._getDaemonPID(daemon)
+            if pid:
+                buttons = activeButtons
+                msg = 'Up'
+                color = '#0F0'
+            else:
+                buttons = inactiveButtons
+                msg = 'Down'
+                color = '#F00'
+            states.append({
+                'name': daemon,
+                'pid': pid,
+                'msg': msg,
+                'color': color,
+                'buttons': buttons})
+        return states
+
+    def _getDaemonPID(self, name):
+        """
+        For a given daemon name, return its PID from a .pid file.
+        """
+        if name == 'zopectl':
+            name = 'Z2'
+        elif name == 'zeoctl':
+            name = 'ZEO'
+        pidFile = os.path.join(os.getenv('ZENHOME'), 'var', '%s.pid' % name)
+        if os.path.exists(pidFile):
+            pid = open(pidFile).read()
+        else:
+            pid = None
+        return pid
+
+    def _getDaemonList(self):
+        """
+        Get the list of supported Zenoss daemons.
+        """
+        masterScript = os.path.join(os.getenv('ZENHOME'), 'bin', 'zenoss')
+        daemons = []
+        for line in open(masterScript).readlines():
+            match = re.match('C="\$C (.*)"$', line)
+            if match:
+                daemons.append(match.groups()[0])
+        return daemons
+
+    def manage_daemonAction(self, REQUEST=None):
+        """
+        Start, stop, or restart Zenoss daemons from a web interface.
+        """
+        # XXX
+        # this exception is for testing purposes only
+        raise 'Problem! :' + str(REQUEST.form)
+        if not REQUEST:
+            return self.callZenScreen(REQUEST)
+        legalValues = ['start', 'restart', 'stop']
+        action = (REQUEST.form.get('action') or '').lower()
+        if action not in legalValues:
+            return self.callZenScreen(REQUEST)
+        daemon = os.path.join(os.getenv('ZENHOME'), 'bin',
+            REQUEST.form.get('daemon'))
+        # we actually want to block here, so that the page doesn't refresh
+        # until the action has completed
+        log.info("Processing a '%s' for '%s' through the web..." % (action, daemon))
+        os.system("%s %s" % (daemon, action))
+    security.declareProtected('Manage DMD','manage_daemonAction')
+        
 
 InitializeClass(ZenossInfo)
