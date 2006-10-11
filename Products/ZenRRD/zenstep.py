f20443ee8d4b842d5c1fd682b9122a59630138d9
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-10-11 14:32:56 +0000

    * now gets all historical points
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16046 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/zenstep.py
+++ Products/ZenRRD/zenstep.py
@@ -20,61 +20,95 @@ import rrdtool
 import logging
 log = logging.getLogger('zen.updateStep')
 
-DEFAULT='''RRA:AVERAGE:0.5:1:1800
-RRA:AVERAGE:0.5:6:1800
-RRA:AVERAGE:0.5:24:1800
-RRA:AVERAGE:0.5:288:1800
-RRA:MAX:0.5:288:1800'''.split()
-
-def round(x):
-    return int(x + 0.5)
-
 class UpdateStep(ZCmdBase):
     """Run over the rrd files in a directory and write the data
     into updated config files"""
 
+    def processRRA(self, fullpath, rra, info, dump):
+        "process one archive in the file"
+        step = info['step']
+        end = info['last_update']
+        pdp_per_row = rra['pdp_per_row']
+        rows = rra['rows']
+        resolution = step*pdp_per_row
+        start = end - resolution*rows
+        start = start/resolution*resolution
+        end = end/resolution*resolution
+        data = rrdtool.fetch(fullpath,
+                             'AVERAGE',
+                             '--resolution', str(resolution),
+                             '--start', str(start),
+                             '--end', str(end))
+        (start, end, resolution), ds0, data = data
+        rnd = lambda x: x
+        if info['ds']['ds0']['type'] == 'COUNTER':
+            rnd = lambda x: int(x+0.5)
+        result = []
+        for i, (v,) in enumerate(data):
+            if v != None:
+                dump[start + i*resolution] = rnd(v)
+        return result
+                
+
     def process(self, fullpath):
+        "convert a single file"
         log.debug("processing %s" % fullpath)
-        newpath = os.path.join(os.path.dirname(fullname),
-                               '.' + os.path.basename(file))
-        # get type, old step?
+        newpath = os.path.join(os.path.dirname(fullpath),
+                               '.' + os.path.basename(fullpath))
+        # get type, old step
         info = rrdtool.info(fullpath)
-        type = info['ds']['ds0']['type']
+        dataType = info['ds']['ds0']['type']
+        rra = info['rra']
         try:
             os.unlink(newpath)
         except OSError:
             pass
-        data = rrdtool.fetch(fullpath, 'AVERAGE')
-        (start, stop, step), source, points = data
-        last = start
+        # Collect some information about the current file:
+        # how far back can the data go?
+        earliest = start = info['last_update']
+        # how wide is the biggest data point?
+        biggest = 0
+        for rra in info['rra']:
+            step = info['step']
+            pdp_per_row = rra['pdp_per_row']
+            rows = rra['rows']
+            earliest = min(earliest, start - pdp_per_row*step*rows)
+            biggest = max(biggest, pdp_per_row*step)
+        # create a file with the correct step to accept the data
         rrdtool.create(
             newpath,
-            'DS:ds0:%s:%d:U:U' % (type, 3*self.options.step),
-            '--start', str(start - step),
+            'DS:ds0:%s:%d:U:U' % (dataType, biggest * 2),
+            '--start', str(earliest),
             '--step', str(self.options.step),
-            *defaultRRDCommand.split())
-
-        rnd = lambda x: x
-        if type == 'COUNTER':
-            rnd = round
-        for i, t in enumerate(range(start, stop, step)):
-            p = points[i][0]
-            if p is not None:
-                rrdtool.update(newpath, '%d@%s' % (t, rnd(p)))
+            *self.defaultRRDCommand.split())
+        # extract the time and values from each archive
+        updates = {}
+        for rra in info['rra']:
+            self.processRRA(fullpath, rra, info, updates)
+        # get the times in order
+        updates = updates.items()
+        updates.sort()
+        rrdtool.update(newpath, *[('%d@%s' % (t, v)) for t, v in updates])
+        # use a reasonable heartbeat
+        rrdtool.tune(newpath, '-h','ds0:%d' % (self.options.step*3))
         if self.options.commit:
             os.rename(newpath, fullpath)
+
 
     def run(self):
         monitors = self.dmd.Monitors.Performance
         monitor = monitors._getOb(self.options.monitor)
-        defaultRRDCommand = monitor.getDefaultRRDCreateCommand()
-        for root, dirs, files in os.walk(self.options.root):
-            for file in files:
-                # skip workin files
-                if file.startswith('.'): continue
-                if not file.endswith('.rrd'): continue
-                fullpath = os.path.join(root, file)
-                process(fullpath)
+        self.defaultRRDCommand = monitor.getDefaultRRDCreateCommand()
+        if self.options.filename:
+            self.process(filename)
+        else:
+            for root, dirs, files in os.walk(self.options.root):
+                for filename in files:
+                    # skip working files
+                    if filename.startswith('.'): continue
+                    if not filename.endswith('.rrd'): continue
+                    fullpath = os.path.join(root, filename)
+                    self.process(fullpath)
                 
 
     def buildOptions(self):
@@ -83,6 +117,8 @@ class UpdateStep(ZCmdBase):
                                default=300, help='Default step size in seconds')
         self.parser.add_option('-r', '--root', dest='root', 
                                default=performancePath(''),
+                               help='Root tree to convert')
+        self.parser.add_option('-f', '--file', dest='filename', 
                                help='Root tree to convert')
         self.parser.add_option('--commit', dest='commit', action='store_true',
                                default=False,
