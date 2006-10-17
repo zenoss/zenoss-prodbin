98545ffe79522376b5a7beccd70fc874823b305e
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-10-17 02:14:50 +0000

    * redirect to /zport/dmd
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16151 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/skins/zenmodel/logoutUser.py
+++ Products/ZenModel/skins/zenmodel/logoutUser.py
@@ -6,7 +6,10 @@
 
 req = context.REQUEST
 context.acl_users.resetCredentials(req, req.RESPONSE)
-dest = '/zport/dmd/logged_out'
+# we want to display logged_out when user is logged in with basic auth
+# need to figureout how to do this.
+#dest = '/zport/dmd/logged_out'
+dest = '/zport/dmd/'
 if req.get('HTTP_REFERER') != dest:
     req.RESPONSE.redirect(dest)
 return
