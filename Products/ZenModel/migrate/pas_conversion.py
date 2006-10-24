efd425f6c90f8ac1d12ca26051b3f1d6668f6fda
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-10-24 17:38:37 +0000

    * fix "unknown attribute addLoginForm" error
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16281 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/pas_conversion.py
+++ Products/ZenModel/migrate/pas_conversion.py
@@ -14,6 +14,7 @@ Old users, passwords and roles are migrated to PAS with this script.
 __version__ = "$Revision$"[11:-2]
 
 from Products.ZenUtils.Security import migratePAS
+from Products.ZenUtils import addLoginForm
 
 import Migrate
 
@@ -28,7 +29,7 @@ class MigrateToPAS(Migrate.Step):
             # note that the addLoginForm() is not PAS-native; it's part of a
             # monkey patch we have applied to allow for a file-system-based
             # login page template. See ZenUtils.__init__ and ZenUtils.Security.
-            context.acl_users.cookieAuthHelper.addLoginForm()
+            addLoginForm(context.acl_users.cookieAuthHelper)
 
 MigrateToPAS()
 
