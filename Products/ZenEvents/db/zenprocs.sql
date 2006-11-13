69b0e2cac4c0c93d82c8a793a01b9162f433ffb4
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-13 22:39:25 +0000

    * move history aging out to separate procedure
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16603 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/db/zenprocs.sql
+++ Products/ZenEvents/db/zenprocs.sql
@@ -26,9 +26,17 @@ CREATE PROCEDURE clean_old_events()
 BEGIN
     DELETE FROM status where 
         DATE_ADD(StateChange, INTERVAL 4 HOUR) < NOW() and severity < 4;   
---    DELETE h,j,d FROM history h
---        LEFT JOIN log j ON h.evid = j.evid 
---        LEFT JOIN detail d ON h.evid = d.evid
---        WHERE DATE_ADD(StateChange, INTERVAL 3 MONTH) < NOW();
+END;//
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS clean_history_events;
+DELIMITER //
+CREATE PROCEDURE clean_history_events()
+BEGIN
+    DELETE h,j,d FROM history h
+        LEFT JOIN log j ON h.evid = j.evid 
+        LEFT JOIN detail d ON h.evid = d.evid
+        WHERE DATE_ADD(StateChange, INTERVAL 3 MONTH) < NOW();
 END;//
 DELIMITER ;
