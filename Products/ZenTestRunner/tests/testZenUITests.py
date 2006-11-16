cd550a76c87e71d6d0723039295700a27d6a70f5
oubiwann <oubiwann@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-16 22:54:39 +0000

    * Edited and renamed ZenTestRunner.tests.testZenTestUI to testZenUITests.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16655 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenTestRunner/tests/testZenUITests.py
+++ Products/ZenTestRunner/tests/testZenUITests.py
@@ -1,4 +1,4 @@
 def test_suite():
-    from Products import ZenTestUI as product
+    from Products import ZenUITests as product
     from Products.ZenTestRunner import getTestSuites
     return getTestSuites(product)
