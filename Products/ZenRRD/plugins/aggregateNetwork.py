42b6aa63cb06006ce0910e8732e9ea429d184d37
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-17 16:37:36 +0000

    * eye candy plugin
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16661 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenRRD/plugins/aggregateNetwork.py
@@ -0,0 +1,87 @@
+import os
+import time
+import glob
+import rrdtool
+import random
+try:
+    from Products.ZenRRD.plugins.plugin import *
+except ImportError:
+    from plugin import *
+
+# set variables for command-line testing
+locals().setdefault('REQUEST', None)
+locals().setdefault('name', 'test')
+
+title = 'Aggregate Network Traffic'
+label = 'Mbs'
+width = 500
+height = 100
+start='-1d'
+end='now'
+rpn = ',8,*'
+
+env = locals().copy()
+if REQUEST:
+    REQUEST.response.setHeader('Content-type', 'image/png')
+    env.update(dict(zip(REQUEST.keys(), REQUEST.values())))
+fname = "%s/graph-%s.png" % (TMPDIR,name)
+cmd = [fname,
+       '--imgformat=PNG',
+       '--start=%(start)s' % env,
+       '--end=%(end)s' % env,
+       '--title=%(title)s' % env,
+       '--base=1000',
+       '--height=%(height)d' % env,
+       '--width=%(width)d' % env,
+       '--alt-autoscale-max',
+       '--lower-limit=0',
+       '--units-exponent=6',
+       '--vertical-label=%(label)s' % env]
+ifiles = []
+ofiles = []
+perf = os.path.join(os.environ['ZENHOME'], 'perf')
+rpn = env['rpn']
+for d, _, fs in os.walk(perf):
+    for f in fs:
+        if f.find('ifInOctets') >= 0:
+            ifiles.append(os.path.join(d, f))
+        if f.find('ifOutOctets') >= 0:
+            ofiles.append(os.path.join(d, f))
+files = ifiles + ofiles
+count = len(files)
+
+defs = []
+for i, f in enumerate(files):
+    defs.append('DEF:d%d=%s:ds0:AVERAGE' % (i, f))
+cdefs = []
+for i in range(count):
+    cdefs.append('CDEF:c%d=d%d%s' % (i, i, rpn))
+lcdef = ['CDEF:lcdef=']
+now = time.time()
+for i in range(count):
+    lcdef.append('TIME,%d,GT,d%d,d%d,UN,0,d%d,IF,IF,' % (now, i, i, i))
+lcdef.append('+,'*(len(files) - 1))
+if rpn:
+    lcdef.append(rpn[1:])
+stacks=[]
+inputFilesCount = len(ifiles)
+for i in range(count):
+    color =  (0xf00f037 << (i % 24))
+    if i < inputFilesCount:
+        color |= 0x707070
+    color = '#%06x' % (color & 0xffffff)
+    if i == 0:
+        stacks.append('AREA:c%d%s' % (i, color))
+    else:
+        stacks.append('STACK:c%d%s' % (i, color))
+cmd.extend(defs)
+cmd.extend(cdefs)
+cmd.append(''.join(lcdef))
+cmd.extend(stacks)
+cmd.extend(['GPRINT:lcdef:LAST:Current\\:%8.2lf %s',
+            'GPRINT:lcdef:AVERAGE:Average\\:%8.2lf %s',
+            'GPRINT:lcdef:MAX:Maximum\\:%8.2lf %s'])
+cmd = [c.strip() for c in cmd if c.strip()]
+import rrdtool
+rrdtool.graph(*cmd)
+graph = read(fname)
