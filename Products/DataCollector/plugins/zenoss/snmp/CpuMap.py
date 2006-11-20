d7d4b80471e98572cbefef3bd09137160b1a1967
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-20 14:53:24 +0000

    #612 collect cpu type and count from SNMP
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16671 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/DataCollector/plugins/zenoss/snmp/CpuMap.py
@@ -0,0 +1,69 @@
+#################################################################
+#
+#   Copyright (c) 2006 Zenoss Corporation. All rights reserved.
+#
+#################################################################
+
+__doc__="""CpuMap
+
+CpuMap maps SNMP cpu information onto CPUs
+
+$Id: $"""
+
+__version__ = '$Revision: 1.1 $'[11:-2]
+
+import re
+
+import re
+
+from CollectorPlugin import SnmpPlugin, GetTableMap
+from DataMaps import ObjectMap
+
+class CpuMap(SnmpPlugin):
+
+    maptype = "CPUMap"
+    compname = "hw"
+    relname = "cpus"
+    modname = "Products.ZenModel.CPU"
+
+    snmpGetTableMaps = (
+        GetTableMap('deviceTableOid', '.1.3.6.1.2.1.25.3.2.1',
+            {
+             '.2': '_type',
+             '.3': 'description',
+             }
+        ),
+    )
+
+    hrDeviceProcessor = ".1.3.6.1.2.1.25.3.1.3"
+
+    def process(self, device, results, log):
+        """collect snmp information from this device"""
+        log.info('processing processor resources %s' % device.id)
+        getdata, tabledata = results
+        table = tabledata.get("deviceTableOid")
+        maps = []
+        rm = self.relMap()
+        slot = 0
+        import pdb
+        pdb.set_trace()
+        for row in table.values():
+            if row['_type'] != self.hrDeviceProcessor: continue
+            desc = row['description']
+            # try and find the cpu speed from the description
+            match = re.search('([.0-9]+) *([mg]hz)', desc.lower())
+            if match:
+                try:
+                    speed = float(match.group(1))
+                    if match.group(2).startswith('g'):
+                        speed *= 1000
+                    row['clockspeed'] = int(speed)
+                except ValueError:
+                    pass
+            om = self.objectMap(row)
+            om.setProductKey = desc
+            om.id = '%d' % slot
+            slot += 1
+            rm.append(om)
+        maps.append(rm)
+        return maps
