515fb84aeeb50f14d28764a6330c6a696e69b329
oubiwann <oubiwann@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-21 20:32:12 +0000

    * Resolved python2.3 issue with running unittests.  * Updated ZenUITests to support both commandline as well as zopectl usage.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16681 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/testBrowserRecordings.py
+++ Products/ZenUITests/tests/testBrowserRecordings.py
@@ -3,19 +3,27 @@ from glob import glob
 
 from testgen.mechunit import MechanizeUnitTest
 
-from Products import ZenUITests
-
-def test_suite():
-    basePath = ZenUITests.__path__[0]
+def convertFilesToSuite(filenames):
     suites = []
-    for filename in glob('%s/tests/TestGen4Web/*.xml' % basePath):
+    for filename in filenames:
         loader = unittest.TestLoader()
         test = MechanizeUnitTest
         test.sourceFilename = filename
         suite = loader.loadTestsFromTestCase(test)
-        suite = unittest.TestSuite(suite)
+        suite = unittest.TestSuite([suite])
         suites.append(suite)
     return unittest.TestSuite(suites)
 
+def test_suite():
+    from Products import ZenUITests
+    basePath = ZenUITests.__path__[0]
+    filenames = glob('%s/tests/TestGen4Web/*.xml' % basePath)
+    return convertFilesToSuite(filenames)
+
+def nonZopeSuite():
+    filenames = glob('tests/TestGen4Web/*.xml')
+    filenames += glob('TestGen4Web/*.xml')
+    return convertFilesToSuite(filenames)
+
 if __name__ == '__main__':
-    unittest.TextTestRunner().run(test_suite())
+    unittest.TextTestRunner().run(nonZopeSuite())
