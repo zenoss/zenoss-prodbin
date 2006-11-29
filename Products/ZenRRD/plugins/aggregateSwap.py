c0d392f9cc9d4d574a6b22b25bf7bd448f72ae9a
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-11-29 21:37:18 +0000

    add aggregate swap
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16780 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenRRD/plugins/aggregateSwap.py
@@ -0,0 +1,83 @@
+import os
+import time
+import re
+import rrdtool
+import random
+try:
+    import Globals
+    from Products.ZenRRD.plugins.plugin import *
+except ImportError:
+    from plugin import *
+
+title = 'Aggregate Free Swap'
+label = 'Mbs'
+width = 500
+height = 100
+start='-1d'
+end='now'
+rpn = ',8,*'
+
+env = locals().copy()
+args = getArgs(REQUEST, env)
+locals().update(env)
+fname = "%s/graph-%s.png" % (TMPDIR,name)
+cmd = [fname,
+       '--imgformat=PNG',
+       '--start=%(start)s' % env,
+       '--end=%(end)s' % env,
+       '--title=%(title)s' % env,
+       '--base=1000',
+       '--height=%(height)s' % env,
+       '--width=%(width)s' % env,
+       '--vertical-label=%(label)s' % env]
+ifiles = []
+ofiles = []
+perf = os.path.join(os.environ['ZENHOME'], 'perf')
+rpn = env['rpn']
+devicePat = re.compile('.*(' + env.get('devices', '') + ').*')
+for d, _, fs in os.walk(perf):
+    if not devicePat.match(d): continue
+    for f in fs:
+        if f.find('memAvailSwap') >= 0:
+            ifiles.append(os.path.join(d, f))
+files = ifiles + ofiles
+count = len(files)
+
+defs = []
+for i, f in enumerate(files):
+    defs.append('DEF:d%d=%s:ds0:AVERAGE' % (i, f))
+cdefs = []
+for i, f in enumerate(files):
+    if f.find("/mem_") >= 0:
+        cdefs.append('CDEF:c%d=d%d,1024,/%s' % (i, i, rpn))
+    else:
+        cdefs.append('CDEF:c%d=d%d%s' % (i, i, rpn))
+lcdef = ['CDEF:lcdef=']
+now = time.time()
+for i in range(count):
+    lcdef.append('TIME,%d,GT,d%d,d%d,UN,0,d%d,IF,IF,' % (now, i, i, i))
+lcdef.append('+,'*(len(files) - 1))
+if rpn:
+    lcdef.append(rpn[1:])
+stacks=[]
+inputFilesCount = len(ifiles)
+for i in range(count):
+    color =  (0xf00f037 << (i % 24))
+    if i < inputFilesCount:
+        color |= 0x707070
+    color = '#%06x' % (color & 0xffffff)
+    if i == 0:
+        stacks.append('AREA:c%d%s::STACK' % (i, color))
+cmd.extend(defs)
+cmd.extend(cdefs)
+cmd.append(''.join(lcdef))
+cmd.extend(stacks)
+cmd.extend(['GPRINT:lcdef:LAST:Current\\:%8.2lf %s',
+            'GPRINT:lcdef:AVERAGE:Average\\:%8.2lf %s',
+            'GPRINT:lcdef:MAX:Maximum\\:%8.2lf %s'])
+cmd = [c.strip() for c in cmd if c.strip()]
+import rrdtool
+graph = None
+if defs:
+    rrdtool.graph(*cmd)
+    graph = open(fname, 'rb').read()
