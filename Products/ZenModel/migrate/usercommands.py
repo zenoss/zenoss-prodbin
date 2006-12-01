1710108f5467ed9e61536403cb92d92078ee1906
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-01 23:28:56 +0000

    also add usercommands to Services, OsProcesses.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16823 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/usercommands.py
+++ Products/ZenModel/migrate/usercommands.py
@@ -6,7 +6,7 @@
 
 __doc__='''
 
-Add usercommands to all device organizers
+Add usercommands to all device organizers, OsProcesses, Services, etc
 
 $Id:$
 '''
@@ -18,11 +18,30 @@ class UserCommands(Migrate.Step):
     def cutover(self, dmd):
         for dev in dmd.Devices.getSubDevices():
             dev.buildRelations()
-        for name in ['Devices', 'Systems', 'Groups', 'Locations', 'Services', 'Processes']:
-            org = getattr(dmd, name)
-            org.buildRelations()
-            for org in org.getSubOrganizers():
-                org.buildRelations()
+        for name in ['Devices', 'Systems', 'Groups', 'Locations',
+                        'Services', 'Processes']:
+            top = getattr(dmd, name)
+            orgs = top.getSubOrganizers()
+            orgs.insert(0, top)
+            for o in orgs:
+                o.buildRelations()
+                if name == 'Devices':
+                    for d in o.devices():
+                        d.buildRelations()
+                        if getattr(d, 'os', None):
+                            for n in ['ipservices', 'winservices', 'processes']:
+                                for p in getattr(d.os, n)():
+                                    p.buildRelations()
+                if name == 'Services':
+                    for sc in o.serviceclasses():
+                        sc.buildRelations()
+                if name == 'Processes':
+                    for pc in o.osProcessClasses():
+                        pc.buildRelations()
+    # when walking devices also to winserv ipserv and processes
+    # then when walking processes/services hit the classes but not the
+    # instances of the services
+                
         #for us in dmd.ZenUsers.getAllUserSettings():
         #    us.buildRelations()
 
