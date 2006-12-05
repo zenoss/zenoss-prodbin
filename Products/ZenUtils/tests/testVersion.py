5e393d1eaa892c6fff1d6df72c1dd667a9f7a175
oubiwann <oubiwann@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-05 19:14:01 +0000

    * Cleaned up ZenUtils/tests/testVersion.py and added a usage note.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16844 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/tests/testVersion.py
+++ Products/ZenUtils/tests/testVersion.py
@@ -1,7 +1,15 @@
+'''
+Note that this is meant to be run from zopecctl using the "test" option. If you
+would like to run these tests from python, simply to the following:
+
+    python ZenUtils/Version.py
+'''
 import unittest
-from zope.testing import doctest
+from zope.testing.doctestunit import DocTestSuite
 
 def test_suite():
-    from zope.testing.doctestunit import DocTestSuite
     suite = DocTestSuite('Products.ZenUtils.Version')
     return unittest.TestSuite([suite])
+
+if __name__ == '__main__':
+    print __doc__
