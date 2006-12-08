ae320e1b975d80a122904cdce42676746e6fb57f
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-08 20:48:22 +0000

    * clear message when going from bad regex to blank one
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16894 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenWidgets/ZenTableManager.py
+++ Products/ZenWidgets/ZenTableManager.py
@@ -186,7 +186,10 @@ class ZenTableManager(SimpleItem, PropertyManager):
     def filterObjects(self, objects, regex, filterFields):
         """filter objects base on a regex in regex and list of fields
         in filterFields."""
-        if not regex: return objects
+        if self.REQUEST.SESSION.has_key('message'): 
+            self.REQUEST.SESSION.delete('message')
+        if not regex: 
+            return objects
         try: search = re.compile(regex,re.I).search
         except re.error, e:
             self.REQUEST.SESSION['message'] = "Invalid regular expression." 
@@ -203,7 +206,6 @@ class ZenTableManager(SimpleItem, PropertyManager):
                 target.append(value)
             targetstring = " ".join(target)
             if search(targetstring): filteredObjects.append(obj)
-        if self.REQUEST.SESSION.has_key('message'): self.REQUEST.SESSION.delete('message')
         return filteredObjects
 
 
