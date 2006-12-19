c7da5fef61ae199babc2d959975b0eba1414b3cc
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-19 20:31:15 +0000

    * convert all counters to DERIVE with a min value of zero
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@16986 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/rrdmin2.py
+++ Products/ZenModel/migrate/rrdmin2.py
@@ -7,16 +7,35 @@ def _cutoverTemplate(template):
         for p in s.datapoints():
             if p.rrdmax == -1:
                 p.rrdmax = None
+
+def _convertFile(filename):
+    import rrdtool
+    try:
+        if rrdtool.info(filename)['ds']['ds0']['type'] == 'COUNTER':
+            rrdtool.tune(filename,
+                         '-d', 'ds0:DERIVE',
+                         '-i', 'ds0:0')
+    except KeyError:
+        pass
     
 
 class RRDMinValue2(Migrate.Step):
     version = Migrate.Version(1, 1, 0)
 
     def cutover(self, dmd):
+        import os
+        from Products.ZenModel.PerformanceConf import PERF_ROOT
+
+        # get all the templates
         for org in dmd.Devices.getSubOrganizers():
             map(_cutoverTemplate, org.getRRDTemplates())
+        # get all the device-specific templates
         for dev in dmd.Devices.getSubDevices():
             map(_cutoverTemplate, dev.getRRDTemplates())
-                
+
+        for d, _, files in os.walk(PERF_ROOT):
+            for f in files:
+                if f.endswith('.rrd'):
+                    _convertFile(os.path.join(d, f))
 
 RRDMinValue2()
