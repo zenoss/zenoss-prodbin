383476b50df4a3dc38063c6f96083a19e098f2b4
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-26 14:29:05 +0000

    #708 refactor zensyslog to use Twisted, not UDPServer  * use inherited thread/queue from EventServer  * use inherited main() from EventServer  * use twisted.names for asych, caching name lookup  * use twisted UDP packet protocol to process events
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17025 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/zensyslog.py
+++ Products/ZenEvents/zensyslog.py
@@ -12,51 +12,34 @@ Turn syslog messages into events.
 $Id$
 '''
 
-import sys
 import time
 import socket 
-import select
 import os
-import logging
-import Queue
-from SocketServer import UDPServer
 
-import Globals
-
-import transaction
-
-from Products.ZenUtils.Utils import basicAuthUrl
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.names.client import getHostByName
+from twisted.python import failure
 
 from EventServer import EventServer
-from Event import Event, EventHeartbeat
-from SyslogProcessingThread import SyslogProcessingThread
-
-from ZenEventClasses import AppStart, AppStop
-from Products.ZenEvents.Exceptions import ZenBackendFailure
+from SyslogProcessing import SyslogProcessor
 
 SYSLOG_PORT = socket.getservbyname('syslog', 'udp')
 
-class ZenSyslog(UDPServer, EventServer):
+class ZenSyslog(DatagramProtocol, EventServer):
 
+    name = 'zensyslog'
 
-    def __init__(self, addr=''):
+    def __init__(self):
         EventServer.__init__(self)
-        UDPServer.__init__(self, (addr, self.options.syslogport), None)
         self.changeUser()
         self.minpriority = self.options.minpriority
-        self.phost = self.options.parsehost
-        self.zempath = os.path.join(self.options.dmdpath, "ZenEventManager")
-        self.evtcount=0L
-        self.rcptqueue = Queue.Queue()
-        self._lastheartbeat = 0
-        self._threadlist = []
-        self._hostmap = {}       # client address/name mapping
-
-        spt = SyslogProcessingThread(self.rcptqueue, self.dmd.ZenEventManager, 
-                 self.options.minpriority, self.options.parsehost)
-        spt.start() 
+        self.processor = SyslogProcessor(self.dmd.ZenEventManager, 
+                                         self.options.minpriority,
+                                         self.options.parsehost)
 
         if self.options.logorig:
+            import logging
             self.olog = logging.getLogger("origsyslog")
             self.olog.setLevel(20)
             self.olog.propagate = False
@@ -64,89 +47,27 @@ class ZenSyslog(UDPServer, EventServer):
             hdlr = logging.FileHandler(lname)
             hdlr.setFormatter(logging.Formatter("%(message)s"))
             self.olog.addHandler(hdlr)
-        self.evtheartbeat = EventHeartbeat(
-            socket.getfqdn(), "zensyslog", self.options.heartbeat*3)
-        self.sendEvent(Event(device=socket.getfqdn(), 
-                        eventClass=AppStart, 
-                        summary="zensyslog collector started",
-                        severity=0, component="zensyslog"))
-        self.log.info("started")
-        
-
-    def resolvaddr(self, clientip):
-        # build the client address/name mapping
-        host = self._hostmap.get(clientip, False)
-        if host: return host
-        try:
-            host = socket.gethostbyaddr(clientip)[0]
-            #host = host.split('.')[0]       # keep only the host name
-        except socket.error:
-            host = clientip
-        self._hostmap[clientip] = host   
-        return host
+        reactor.listenUDP(self.options.syslogport, self)
 
 
-    def process_request(self, request, client_address):
-        """Start a new thread to process the request."""
-        ipaddr = client_address[0]
-        host = self.resolvaddr(ipaddr)
-        msg = request[0]
-        rtime = time.time()
+    def datagramReceived(self, msg, client_address):
+        """Use a separate thread to process the request."""
+        host, port = client_address
         if self.options.logorig: 
             self.olog.info(msg)
-        self.evtcount += 1
-        self.rcptqueue.put((msg, ipaddr, host, rtime))
-  
-
-    def handle_request(self, seltimeout=1):
-        """Handle one request, and don't block"""
-        res = select.select([self.socket,],[],[],seltimeout)
-        if not res[0]: return
-        request, client_address = self.get_request()
-        try:
-            self.process_request(request, client_address)
-        except (SystemExit, KeyboardInterrupt): raise
-        except:
-            self.log.exception("request from %s:%d failed" % client_address)
+        getHostByName(host).addBoth(self.gotHostname, (msg, host, time.time()) )
 
 
-    def serve(self):
-        "Run the service until it is stopped with the stop() method."
-        self.running = True
-        last = 0
-        while self.running:
-            try:
-                seltimeout = 1
-                if not self.rcptqueue.empty(): seltimeout = 0
-                self.handle_request(seltimeout)
-                if self.options.stats and last+60<time.time():
-                    self.log.info("count=%d pool=%d, queue=%d", 
-                                self.evtcount, self.available(), 
-                                self.rcptqueue.qsize())
-                    last = time.time()
-                self.sendHeartbeat()
-            except (SystemExit, KeyboardInterrupt): raise
-            except:
-                self.log.exception("unexpected exception")
-        self.log.info("stopped")
+    def gotHostname(self, host, data):
+        "send the resolved address, if possible, and the event via the thread"
+        if isinstance(host, failure.Failure):
+            host = host.value.args[0]
+        self.q.put( (host,) + data )
 
 
-    def stop(self):
-        self.log.info("stopping...")
-        if hasattr(self, "_threadlist"):
-            map(lambda t: t.stop(), self._threadlist)
-            map(lambda t: t.join(2), self._threadlist)
-        self.sendEvent(Event(device=socket.getfqdn(), 
-                        eventClass=AppStop, 
-                        summary="zensyslog collector stopped",
-                        severity=4, component="zensyslog"))
-        self.running = False
-
-
-    def sendHeartbeat(self):
-        if self._lastheartbeat + self.options.heartbeat < time.time():
-            self.sendEvent(self.evtheartbeat)
-            self._lastheartbeat = time.time()
+    def doHandleRequest(self, ipaddr, msg, host, rtime):
+        "process a single syslog message, called from the inherited thread"
+        self.processor.process(msg, ipaddr, host, rtime)
 
 
     def buildOptions(self):
@@ -177,7 +98,5 @@ class ZenSyslog(UDPServer, EventServer):
             help="Port number to use for syslog events")
 
 
-                
-
 if __name__ == '__main__':
-    ZenSyslog().serve()
+    ZenSyslog().main()
