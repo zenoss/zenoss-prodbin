cb99dbc44de9ca1d8f5183fc079d2e4f48717f1f
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2006-12-27 18:41:54 +0000

    * Ticket #168: import and export devices from and to XML
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17039 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenRelations/ExportDevices.py
@@ -0,0 +1,99 @@
+import sys
+from xml.dom.minidom import parseString
+import StringIO
+import re
+
+import Globals
+
+from Products.ZenUtils.ZCmdBase import ZCmdBase
+from Products.ZenRelations.RelationshipManager import RelationshipManager
+
+_newlines = re.compile('\n[\t \r]*\n', re.M)
+
+class ExportDevices(ZCmdBase):
+
+    def __init__(self):
+        ZCmdBase.__init__(self)
+        if not self.options.outfile:
+            self.outfile = sys.stdout
+        else:
+            self.outfile = open(self.options.outfile, 'w')
+        
+    
+    def buildOptions(self):
+        """basic options setup sub classes can add more options here"""
+        ZCmdBase.buildOptions(self)
+        self.parser.add_option('-o', '--outfile',
+                    dest="outfile",
+                    help="output file for export default is stdout")
+        self.parser.add_option('--ignore', action="append",
+                    dest="ignorerels", default=[],
+                    help="relations that should be ignored can be many")
+
+
+    def stripUseless(self, doc):
+
+        _retain_class = (
+            "Products.ZenModel.DeviceClass",
+            "Products.ZenModel.Device",
+            )
+
+        _retain_props = (
+            "description",
+            "productionState",
+            "priority",
+            "monitors",
+            )
+
+        def denewline(s):
+            while re.search(_newlines, s):
+                s = re.sub(_newlines, '\n', s)
+            return s
+        
+        def clearObjects(node):
+            try: devs = node.getElementsByTagName('object')
+            except AttributeError: pass
+            else:
+                for dev in devs:
+                    if dev.hasAttribute('module') and \
+                    dev.getAttribute('module') not in _retain_class:
+                        dev.parentNode.removeChild(dev)
+
+        def clearProps(node):
+            try: props = node.getElementsByTagName('property')
+            except AttributeError: pass
+            else:
+                for prop in props:
+                    if prop.getAttribute('module') not in _retain_props:
+                        prop.parentNode.removeChild(prop)
+        
+        root = doc.getElementsByTagName('objects')[0]
+        clearObjects(root)
+        clearProps(root)
+
+        #for dev in getDevices(doc): print dev.getAttribute('id')
+
+        return denewline(doc.toprettyxml().replace('\t', ' '*4))
+
+
+    def export(self):
+        root = self.dmd.Devices
+        buffer = StringIO.StringIO()
+        if hasattr(root, "exportXml"):
+            buffer.write("""<?xml version="1.0"?>\n""")
+            buffer.write("<objects>\n")
+            root.exportXml(buffer,self.options.ignorerels,True)
+            buffer.write("</objects>\n")
+            doc = parseString(buffer.getvalue())
+            finalxml = self.stripUseless(doc)
+            self.outfile.write(finalxml)
+            doc.unlink()
+            buffer.close()
+        else:
+            print "ERROR: root object not a exportable (exportXml not found)"
+            
+
+
+if __name__ == '__main__':
+    ex = ExportDevices()
+    ex.export()
---
+++ Products/ZenRelations/ImportDevices.py
@@ -0,0 +1,169 @@
+#################################################################
+#
+#   Copyright (c) 2003 Zenoss, Inc. All rights reserved.
+#
+#################################################################
+
+__doc__="""ImportRM
+
+Export RelationshipManager objects from a zope database
+
+$Id: ImportRM.py,v 1.3 2003/10/03 16:16:01 edahl Exp $"""
+
+__version__ = "$Revision: 1.3 $"[11:-2]
+
+import sys
+import os
+import types
+import urllib2
+import transaction
+from urlparse import urlparse
+from xml.dom.minidom import parse
+from optparse import OptionParser
+
+from Acquisition import aq_base
+
+from DateTime import DateTime
+
+from Products.ZenUtils.ZCmdBase import ZCmdBase
+from Products.ZenUtils.Utils import importClass
+
+from Products.ZenRelations.Exceptions import *
+
+
+class ImportDevices(ZCmdBase):
+
+    def getDevicePath(self, device):
+        
+        def _getParentDevClass(node):
+            ancestor = node.parentNode
+            while ancestor.getAttribute('class') != 'DeviceClass':
+                ancestor = ancestor.parentNode
+            return ancestor
+
+        ancestor = _getParentDevClass(device)
+        path = []
+        while ancestor.getAttribute('id') != '/zport/dmd/Devices':
+            path.append(ancestor.getAttribute('id'))
+            ancestor = _getParentDevClass(ancestor)
+        path.reverse()
+        return '/' + '/'.join(path)
+
+    def processLinks(self, device):
+        def parse_objid(objid, last=False):
+            if last: objid = objid.split('/')[-1]
+            else: objid = '/'.join(objid.split('/')[4:])
+            objid = objid.encode('ascii')
+            return objid
+        d = dict(
+            systemPaths = [],
+            groupPaths = [],
+            statusMonitors = [],
+            performanceMonitor = '',
+            locationPath = ''
+        )
+        tomanys = device.getElementsByTagName('tomany')
+        toones = device.getElementsByTagName('toone')
+        for tomany in tomanys:
+            id = tomany.getAttribute('id')
+            links = tomany.getElementsByTagName('link')
+            if id == 'monitors':
+                for link in links:
+                    d['statusMonitors'].append(parse_objid(link.getAttribute('objid'),
+                                                           True))
+            elif id == 'systems':
+                for link in links:
+                    d['systemPaths'].append(parse_objid(link.getAttribute('objid')))
+            elif id == 'groups':
+                for link in links:
+                    d['groupPaths'].append(parse_objid(link.getAttribute('objid')))
+        for toone in toones:
+            id = toone.getAttribute('id')
+            objid = toone.getAttribute('objid')
+            if id=='perfServer': d['performanceMonitor'] = parse_objid(objid,
+                                                                       True)
+            elif id=='location': d['locationPath'] = parse_objid(objid)
+        return d
+
+
+    def handleDevices(self):
+        devs = self.doc.getElementsByTagName('object')
+        for dev in devs:
+            if dev.getAttribute('class') != 'Device': continue
+            device = {
+                'deviceName' : dev.getAttribute('id').encode('ascii'),
+                'devicePath' : self.getDevicePath(dev).encode('ascii')
+                }
+            device.update(self.processLinks(dev))
+            print "Loading %s into %s..." % (device['deviceName'],
+                                             device['devicePath'])
+            self.dmd.DeviceLoader.loadDevice(**device)
+
+
+    def buildOptions(self):
+        """basic options setup sub classes can add more options here"""
+        ZCmdBase.buildOptions(self)
+
+        self.parser.add_option('-i', '--infile',
+                    dest="infile",
+                    help="input file for import default is stdin")
+        print "Build option infile"
+        
+        self.parser.add_option('-x', '--commitCount',
+                    dest='commitCount',
+                    default=20,
+                    type="int",
+                    help='how many lines should be loaded before commit')
+
+        self.parser.add_option('--noindex',
+                    dest='noindex',action="store_true",default=False,
+                    help='Do not try to index data that was just loaded')
+
+        self.parser.add_option('-n', '--noCommit',
+                    dest='noCommit',
+                    action="store_true",
+                    default=0,
+                    help='Do not store changes to the Dmd (for debugging)')
+
+
+    def loadObjectFromXML(self, objstack=None, xmlfile=''):
+        """This method can be used to load data for the root of Zenoss (default
+        behavior) or it can be used to operate on a specific point in the
+        Zenoss hierarchy (ZODB).
+
+        Upon loading the XML file to be processed, the content of the XML file
+        is handled (processed) by the methods in this class.
+        """
+        if xmlfile:
+            # check to see if we're getting the XML from a URL ...
+            schema, host, path, null, null, null = urlparse(xmlfile)
+            if schema and host:
+                self.infile = urllib2.urlopen(xmlfile)
+            # ... or from a file on the file system
+            else:
+                self.infile = open(xmlfile)
+        elif self.options.infile:
+            self.infile = open(self.options.infile)
+        else:
+            self.infile = sys.stdin
+        self.doc = parse(self.infile)
+        self.handleDevices()
+        self.doc.unlink()
+        self.infile.close()
+
+    def loadDatabase(self):
+        """The default behavior of loadObjectFromXML() will be to use the Zope
+        app object, and thus operatate on the whole of Zenoss.
+        """
+        self.loadObjectFromXML()
+
+    def commit(self):
+        trans = transaction.get()
+        trans.note('Import from file %s using %s' 
+                    % (self.options.infile, self.__class__.__name__))
+        trans.commit()
+
+
+if __name__ == '__main__':
+    im = ImportDevices()
+    im.loadDatabase()
