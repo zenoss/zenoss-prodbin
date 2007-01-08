411a4bf622e3722814bffe5858e9b9e8fdf74387
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-01-08 21:53:16 +0000

    * Changed name of severity argument so as to not conflict with status.severity
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17140 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/db/zenprocs.sql
+++ Products/ZenEvents/db/zenprocs.sql
@@ -1,10 +1,10 @@
 
 DROP PROCEDURE IF EXISTS age_events;
 DELIMITER //
-CREATE PROCEDURE age_events(IN hours INT, IN severity INT)
+CREATE PROCEDURE age_events(IN hours INT, IN sev INT)
 BEGIN
     DELETE FROM status where
-     StateChange < DATE_SUB(NOW(), INTERVAL hours HOUR) and severity < severity;
+     StateChange < DATE_SUB(NOW(), INTERVAL hours HOUR) and severity < sev;
 END;//
 DELIMITER ;
 
