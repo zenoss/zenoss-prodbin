853914db5c5eb742d0e17742851a7c19a665ba84
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-01-24 00:50:13 +0000

    Need to change trigger after adding column to status and history
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17348 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/eventclassmapping.py
+++ Products/ZenModel/migrate/eventclassmapping.py
@@ -3,12 +3,53 @@
 #   Copyright (c) 2007 Zenoss, Inc. All rights reserved.
 #
 #################################################################
+
 __doc__='Add eventClassMapping to status and history tables'
 
+import Migrate
+from MySQLdb import OperationalError
 import logging
 log = logging.getLogger("zen.migrate")
 
-import Migrate
+trigger = """
+    CREATE TRIGGER status_delete BEFORE DELETE ON status
+    FOR EACH ROW INSERT INTO history VALUES (
+            OLD.dedupid,
+            OLD.evid,
+            OLD.device,
+            OLD.component,
+            OLD.eventClass,
+            OLD.eventKey,
+            OLD.summary,
+            OLD.message,
+            OLD.severity,
+            OLD.eventState,
+            OLD.eventClassKey,
+            OLD.eventGroup,
+            OLD.stateChange,
+            OLD.firstTime,
+            OLD.lastTime,
+            OLD.count,
+            OLD.prodState,
+            OLD.suppid,
+            OLD.manager,
+            OLD.agent,
+            OLD.DeviceClass,
+            OLD.Location,
+            OLD.Systems,
+            OLD.DeviceGroups,
+            OLD.ipAddress,
+            OLD.facility,
+            OLD.priority,
+            OLD.ntevid,
+            OLD.ownerid,
+            NULL,
+            OLD.clearid,
+            OLD.eventClassMapping
+            )"""
+            
+
+
 
 class EventClassMapping(Migrate.Step):
     version = Migrate.Version(1, 2, 0)
@@ -25,6 +66,11 @@ class EventClassMapping(Migrate.Step):
                     cur.execute('alter table %s ' % table +
                                 'add column eventClassMapping '
                                 'varchar(128) default ""')
+            try:
+                cur.execute('drop trigger status_delete')
+            except OperationalError:
+                pass
+            cur.execute(trigger)
         finally:
             conn.close()
 EventClassMapping()
