aa2baa9b4ec420ae9dc385b7c7601af847df6a2c
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-02-01 15:49:00 +0000

    * Fixed stupid deprecation message. I echo r3990.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17465 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/SiteScopeParser/__init__.py
+++ Products/SiteScopeParser/__init__.py
@@ -1,5 +1,5 @@
 from ParserProduct import ParserProduct, manage_addParserForm, manage_addParser
-from ImageFile import ImageFile
+from App.ImageFile import ImageFile
 
 def initialize(registrar):
     registrar.registerClass(
