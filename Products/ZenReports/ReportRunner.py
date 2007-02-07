954d0abfa5f3828d283214a8cd540dfa4e710298
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-02-07 14:05:32 +0000

    #582: ZenPacks add report plugins  * make plugins into classes  * rename Plugin to Utils  * move Utils and Utilization up to ZenReport level  * add driver to run reports from the command line
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17498 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenReports/ReportRunner.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import Globals
+from Products.ZenUtils.ZCmdBase import ZCmdBase
+
+class ReportRunner(ZCmdBase):
+
+    def main(self):
+        plugin =  self.args[0]
+        args = {}
+        for a in self.args[1:]:
+            if args.find('=') > -1:
+                key, value = args.split('=', 1)
+                args[key] = value
+        self.log.debug("Running '%s' with %r", plugin, args)
+        result = self.dmd.ReportServer.plugin(plugin, args)
+        import pprint
+        pprint.pprint(result)
+
+if __name__ == '__main__':
+    rr = ReportRunner()
+    rr.main()
+
