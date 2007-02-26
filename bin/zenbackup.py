44735257e9b22a3192f7631ec5ae657a04b56c51
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-02-26 16:43:12 +0000

    testing svn sync
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17596 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- bin/zenbackup.py
+++ bin/zenbackup.py
@@ -5,7 +5,7 @@
 #
 #################################################################
 
-#Change saved at 11:35
+#Change saved at 11:43
 __doc__='''zenbackup
 
 Creates backup of zope data files, zenoss conf files and the events database.
