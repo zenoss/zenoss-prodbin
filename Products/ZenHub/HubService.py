e47e7470b7ba7af2fa992dd90ad462e84557d7d6
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-04 16:16:20 +0000

    * track remote collectors as listeners  * allow service name to be optional
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@17965 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/HubService.py
+++ Products/ZenHub/HubService.py
@@ -3,8 +3,11 @@ from twisted.spread import pb
 
 class HubService(pb.Referenceable):
 
-    def __init__(self, dmd, instance = None):
+    def __init__(self, dmd, instance):
         self.dmd = dmd
         self.zem = dmd.ZenEventManager
         self.instance = instance
+        self.listeners = []
 
+    def addListener(self, remote):
+        self.listeners.append(remote)
