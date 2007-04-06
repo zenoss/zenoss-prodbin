06f19119a164ec526763acacd4f92d53d6eca0cc
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-06 22:24:00 +0000

    * check for property before adding it
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18026 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/zCollectorPlugins.py
+++ Products/ZenModel/migrate/zCollectorPlugins.py
@@ -1,10 +1,12 @@
 import Migrate
+from Acquisition import aq_base
 
 class zCollectorPlugins(Migrate.Step):
     version = Migrate.Version(1, 2, 0)
 
     def cutover(self, dmd):
-        dmd.Devices._setProperty("zCollectorPlugins", [], type='lines')
+        if not hasattr(aq_base(dmd.Devices), 'zCollectorPlugins'):
+            dmd.Devices._setProperty("zCollectorPlugins", [], type='lines')
 
 zCollectorPlugins()
  
