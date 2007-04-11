f8ae0f53bb66f8cc690c33666ad6adfdea5a0c54
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-11 02:30:20 +0000

    * support a whole lot more renderserver features
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18091 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/services/ZenRender.py
+++ Products/ZenHub/services/ZenRender.py
@@ -9,6 +9,8 @@ from Products.ZenHub.HubService import HubService
 from twisted.web import resource, server
 from twisted.internet import reactor
 
+import xmlrpclib
+
 htmlResource = None
 
 import logging
@@ -26,13 +28,18 @@ class Render(resource.Resource):
 
 
     def render_GET(self, request):
-        gopts = request.args['gopts'][0]
-        drange = request.args['drange'][0]
-        ftype = request.args.get('ftype', ['PNG'])[0]
+        "Deal with http requests"
+        args = request.args.copy()
+        for k, v in args.items():
+            if len(v) == 1:
+                args[k] = v[0]
         for instance, renderer in self.renderers.items():
             for listener in renderer.listeners:
                 try:
-                    d = listener.callRemote('render', gopts, drange, ftype)
+                    command = request.postpath[-1]
+                    args.setdefault('ftype', 'PNG')
+                    ftype = args['ftype']
+                    del args['ftype']
                     request.setHeader('Content-type', 'image/%s' % ftype)
                     def write(result):
                         request.write(result)
@@ -40,12 +47,36 @@ class Render(resource.Resource):
                     def error(reason):
                         log.error("Unable to fetch graph: %s", reason)
                         request.finish()
+                    d = listener.callRemote(command, **args)
                     d.addCallbacks(write, error)
                     return server.NOT_DONE_YET
                 except Exception, ex:
+                    log.exception(ex)
                     log.warning("Skipping renderer %s" % instance)
         raise Exception("No renderer registered")
 
+    def render_POST(self, request):
+        "Deal with XML-RPC requests"
+        for instance, renderer in self.renderers.items():
+            for listener in renderer.listeners:
+                try:
+                    args, command = xmlrpclib.loads(request.content.read())
+                    request.setHeader('Content-type', 'text/xml')
+                    d = listener.callRemote(str(command), *args)
+                    def write(result):
+                        response = xmlrpclib.dumps((result,),
+                                                   methodresponse=True)
+                        request.write(response)
+                        request.finish()
+                    def error(reason):
+                        log.error("Unable to fetch graph: %s", reason)
+                        request.finish()
+                    d.addCallbacks(write, error)
+                    return server.NOT_DONE_YET
+                except Exception, ex:
+                    log.exception(ex)
+                    log.warning("Skipping renderer %s" % instance)
+        raise Exception("No renderer registered")
 
     def getChild(self, path, request):
         "Handle all paths"
--- Products/ZenRRD/zenrender.py
+++ Products/ZenRRD/zenrender.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python 
+#################################################################
+#
+#   Copyright (c) 2007 Zenoss, Inc. All rights reserved.
+#
+#################################################################
+
 import Globals
 
 from Products.ZenHub.PBDaemon import PBDaemon
@@ -27,8 +34,33 @@ class zenrender(PBDaemon):
         PBDaemon.__init__(self, 'zenrender')
         self.rs = RenderServer(self.name)
 
-    def remote_render(self, gopts, drange, ftype):
-        return self.rs.render(gopts, drange, ftype=ftype)
+    def remote_render(self, *args, **kw):
+        return self.rs.render(*args, **kw)
+
+    def remote_packageRRDFiles(self, *args, **kw):
+        return self.rs.packageRRDFiles(*args, **kw)
+
+    def remote_unpackageRRDFiles(self, *args, **kw):
+        return self.rs.unpackageRRDFiles(*args, **kw)
+
+    def remote_receiveRRDFiles(self, *args, **kw):
+        return self.rs.receiveRRDFiles(*args, **kw)
+
+    def remote_sendRRDFiles(self, *args, **kw):
+        return self.rs.sendRRDFiles(*args, **kw)
+
+    def remote_moveRRDFiles(self, *args, **kw):
+        return self.rs.moveRRDFiles(*args, **kw)
+
+    def remote_plugin(self, *args, **kw):
+        return self.rs.plugin(*args, **kw)
+
+    def remote_summary(self, *args, **kw):
+        return self.rs.summary(*args, **kw)
+
+    def remote_currentValues(self, *args, **kw):
+        return self.rs.currentValues(*args, **kw)
+
 
 if __name__ == '__main__':
     zr = zenrender()
