67c2133be86939d32cf3a73662f867503a536fda
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-17 12:07:53 +0000

    Threshold and ThresholdManager moved into their own file.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18174 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/RRDDaemon.py
+++ Products/ZenRRD/RRDDaemon.py
@@ -25,6 +25,7 @@ from twisted.internet import reactor, defer
 from twisted.python import failure
 
 from Products.ZenHub.PBDaemon import FakeRemote, PBDaemon as Base
+from Products.ZenRRD.ThresholdManager import ThresholdManager, Threshold
 
 BAD_SEVERITY=Event.Warning
 
@@ -35,85 +36,7 @@ DEFAULT_URL = BASE_URL + '/Monitors/Performance/localhost'
 COMMON_EVENT_INFO = {
     'manager': socket.getfqdn(),
     }
-
-class Threshold:
-    'Hold threshold config and send events based on the current value'
-    count = 0
-    label = ''
-    minimum = None
-    maximum = None
-    eventClass = Perf_Snmp
-    severity = Event.Info
-    escalateCount = 0
-
-    def __init__(self, label, minimum, maximum, eventClass, severity, count):
-        self.label = label
-        self.update(minimum, maximum, eventClass, severity, count)
-
-
-    def update(self, minimum, maximum, eventClass, severity, count):
-        self.minimum = minimum
-        self.maximum = maximum
-        self.eventClass = eventClass
-        self.severity = severity
-        self.escalateCount = count
-
-
-    def check(self, device, cname, oid, value, eventCb):
-        'Check the value for min/max thresholds, and post events'
-        if value is None:
-            return
-        thresh = None
-        if self.maximum is not None and value > self.maximum:
-            thresh = self.maximum
-            how = 'exceeded'
-        if self.minimum is not None and value < self.minimum:
-            thresh = self.minimum
-            how = 'not met'
-        if thresh is not None:
-            self.count += 1
-            severity = self.severity
-            if self.escalateCount and self.count >= self.escalateCount:
-                severity += 1
-            summary = '%s %s threshold of %s %s: current value %.2f' % (
-                device, self.label, thresh, how, float(value))
-            eventCb(device=device,
-                    summary=summary,
-                    eventClass=self.eventClass,
-                    eventKey=oid,
-                    component=cname,
-                    severity=severity)
-        else:
-            if self.count:
-                summary = '%s %s threshold restored current value: %.2f' % (
-                    device, self.label, value)
-                eventCb(device=device,
-                        summary=summary,
-                        eventClass=self.eventClass,
-                        eventKey=oid,
-                        component=cname,
-                        severity=Event.Clear)
-            self.count = 0
-
-class ThresholdManager:
-    "manage a collection of thresholds"
     
-    def __init__(self):
-        self.thresholds = {}
-
-    def update(self, config):
-        before = self.thresholds
-        self.thresholds = {}
-        for label, minimum, maximum, eventClass, severity, count in config:
-            t = before.get(label, None)
-            if t:
-                t.update(minimum, maximum, eventClass, severity, count)
-            else:
-                t = Threshold(label, minimum, maximum, eventClass, severity, count)
-            self.thresholds[label] = t
-
-    def __iter__(self):
-        return iter(self.thresholds.values())
 
 class RRDDaemon(Base):
     'Holds the code common to performance gathering daemons.'
