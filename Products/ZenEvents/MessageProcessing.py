fd9116ef2bb6d1c4aedd71ca316fecc5075b41b3
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-17 22:07:09 +0000

    updated to handle the localtime/UTC dilemna
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18192 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/MessageProcessing.py
+++ Products/ZenEvents/MessageProcessing.py
@@ -6,6 +6,8 @@
 #################################################################
 
 import email, socket, time, rfc822
+import calendar
+from datetime import tzinfo, timedelta, datetime
 
 from Event import Event
 
@@ -18,9 +20,32 @@ class MailEvent(Event):
     eventGroup="mail" 
 
 
+ZERO = timedelta(0)
+
+# A class building tzinfo objects for fixed-offset time zones.
+# Note that FixedOffset(0, "UTC") is a different way to build a
+# UTC tzinfo object.
+class FixedOffset(tzinfo):
+    """Fixed offset in minutes east from UTC."""
+
+    def __init__(self, offset, name):
+        self.__offset = timedelta(minutes = offset)
+        self.__name = name
+
+    def utcoffset(self, dt):
+        return self.__offset
+
+    def tzname(self, dt):
+        return self.__name
+
+    def dst(self, dt):
+        return ZERO
+
+
 class MessageProcessor(object):
     def __init__(self, zem): 
         self.zem = zem
+
 
     def process(self, messageStr):
         message = email.message_from_string(messageStr)
@@ -32,13 +57,30 @@ class MessageProcessor(object):
             log.error('hostname lookup failed for host: %s' % fromAddr, exc_info=1)
 
         subject = message.get('Subject')
-        rtime = rfc822.parsedate_tz(message.get('Date'))
-        tz_offset = rtime[-1]
-        rtime_secs = time.mktime(rtime[0:9]) - tz_offset
+
+        # this is tricky...  date comes in with an offset value that
+        # represents the number of seconds of difference between the
+        # parsed timezone and UTC.  the events database wants all time
+        # as seconds since the epoch and treats it as UTC.  as a
+        # result we have to use the datetime class to do the
+        # conversion because the functions in the time module do all
+        # kinds of covnersions "to be helpful"
+        t = rfc822.parsedate_tz(message.get('Date'))
+
+        offset_secs = t[-1]
+
+        # convert the offset in seconds to minutes.  calendar wants minutes
+        offset_mins = offset_secs / 60
+        tz = FixedOffset(offset_mins, "Unknown")
+
+        # construct dt using the date and time as well as the timezone 
+        dt = datetime(t[0], t[1], t[2], t[3], t[4], t[5], 0, tz)
+        secs = calendar.timegm(dt.utctimetuple())
+        log.info('timestamp of event (should be in UTC): %f' % secs)
+
+        event = MailEvent(device=fromAddr, ipAddress=fromIp, rcvtime=secs)
 
         body = message.get_payload()
-        
-        event = MailEvent(device=fromAddr, ipAddress=fromIp, rcvtime=rtime_secs)
         event.summary = body
         self.enrich(event, subject)
 
