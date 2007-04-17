65de7275ed7521e6769f49045f99c106a40a882e
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-17 15:16:44 +0000

    * mostly convert zenwin to a PBDeamon
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18179 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenWin/zenwin.py
+++ Products/ZenWin/zenwin.py
@@ -1,46 +1,42 @@
 #################################################################
 #
-#   Copyright (c) 2006 Zenoss, Inc. All rights reserved.
+#   Copyright (c) 2007 Zenoss, Inc. All rights reserved.
 #
 #################################################################
 
 import sys
 import os
-import gc
 import time
-import logging
-import xmlrpclib
-import socket
-import pywintypes
+from socket import getfqdn
 import pythoncom
 
-from zenutils.Utils import basicAuthUrl
-from zenutils.StatusMonitor import StatusMonitor
-from zenutils.SendEvent import SendEvent
+from twisted.internet import reactor, defer
+
+import Globals
+from Products.ZenHub.PBDaemon import FakeRemote, PBDaemon as Base
+from Products.ZenHub.services import WmiConfig
+from Products.ZenEvents.ZenEventClasses import Heartbeat
+
 from StatusTest import StatusTest
 from WinServiceTest import WinServiceTest
 from WinEventlog import WinEventlog
 
-class zenwin(StatusMonitor):
-    
-    startevt = {'eventClass':'/App/Start', 'device':socket.getfqdn(),
-                'summary': 'zenwin started', 
-                'component':'zenwin',
-                'severity':0}
-    stopevt = {'eventClass':'/App/Stop', 'device':socket.getfqdn(),
-                'summary': 'zenwin stopped', 
-                'component':'zenwin', 
-                'severity': 4}
-    heartbeat = {'eventClass':'/Heartbeat', 'device':socket.getfqdn(),
-                'component': 'zenwin'}
-    
+class zenwin(Base):
 
-    def __init__(self, config=""):
-        StatusMonitor.__init__(self, config=config)
+    agent = "zenwin"
+    cycleInterval = 60.
+    configCycleInterval = 20.
+
+    initialServices = ['EventService', 'WmiConfig']
+
+    heartbeat = dict(eventClass=Heartbeat,
+                     device=socket.getfqdn(),
+                     component='zenwin')
+
+    def __init__(self):
         self.wmiprobs = []
-        self.configCycleInterval = 20
         self.devices = []
-
+        Base.__init__(self)
 
 
     def validConfig(self):
@@ -48,28 +44,11 @@ class zenwin(StatusMonitor):
         return self.devices
 
 
-    def loadConfig(self):
-        """get the config data from server"""
-        if time.time()-self.configTime > self.configCycleInterval*60:
-            self.log.info("reloading configuration")
-            url = basicAuthUrl(self.username, self.password,self.winurl)
-            server = xmlrpclib.Server(url, encoding='iso-8859-1')
-            devices = []
-            for n,u,p,s in server.getWinServices():
-	        if self.options.device and self.options.device != n: continue
-                st = StatusTest(self.zem,n,u,p,s, self.options.debug)
-                st.setPlugins(self.getPlugins())
-                devices.append(st) 
-            if devices: self.devices = devices
-            self.configTime = time.time()
-
-
     def getPlugins(self):
         """Build a list of plugin instances for a device.
         """
         plugins = (WinServiceTest(), WinEventlog())
         if not self.options.load:
-            #self.options.load = ['WinServiceTest', 'WinEventlog']
             self.options.load = ['WinServiceTest']
         plugins = [p for p in plugins if p.name in self.options.load]
         if not plugins:
@@ -77,32 +56,35 @@ class zenwin(StatusMonitor):
             raise SystemExit("No plugins found for list: '%s'" % (
                              ",".join(self.options.load)))
         pnames = [ p.name for p in plugins ] 
-        #self.log.debug("loaded plugins: %s", ",".join(pnames))
         return plugins
 
 
-    def processLoop(self):
+    def processLoop(self, unused=None):
         """Run WMI queries in two stages ExecQuery in semi-sync mode.
         then process results later (when they have already returned)
         """
         self.count = 0
         if self.options.debug:
-        # in debug mode open wmi connections sequentially (ie no threads)
+            # in debug mode open wmi connections sequentially (ie no threads)
             for srec in self.devices:
-                if not self.checkwmi(srec.name): srec.run()
-                else: self.log.warn("skipping %s no wmi connection",srec.name)
+                if srec.name not in self.wmiprobs:
+                    srec.run()
+                else:
+                    self.log.warn("skipping %s no wmi connection",srec.name)
         else:
-        # connect to WMI service in separate thread if no ping problem detected.
+            # connect to WMI service in separate thread if no
+            # ping problem detected.
             devices = self.devices[:]
             running = []
             now = time.time()
             while devices or running:
-                #self.log.debug("devices:%d runing:%d",len(devices),len(running))
+                self.log.debug("devices:%d runing:%d",
+                               len(devices), len(running))
                 running = [ srec for srec in running if not srec.done() ]
                 needthreads = self.options.threads - len(running)
                 while devices and needthreads > 0:
                     srec = devices.pop()
-                    if not self.checkwmi(srec.name):
+                    if srec.name not in self.wmiprobs:
                         srec.start()
                         self.count += 1
                         self.log.debug("count = %d", self.count)
@@ -110,14 +92,15 @@ class zenwin(StatusMonitor):
                         needthreads -= 1
                     else: 
                         self.log.warn("skipping %s no wmi connection",srec.name)
-                if needthreads == 0 or not devices: time.sleep(1)
-                if time.time() - now > self.cycle*2:
+                if needthreads == 0 or not devices:
+                    time.sleep(1)
+                if time.time() - now > self.cycleInterval*2:
                     problems = ', '.join([r.name for r in running])
                     self.log.warning('%d servers (%s) still collecting '
                                      'after %d seconds, giving up',
                                      len(running),
                                      problems,
-                                     self.cycle*2)
+                                     self.cycleInterval*2)
                     for r in running[:]:
                         evt = { 'eventClass': '/Status/Wmi/Conn',
                                 'agent': 'zenwin',
@@ -125,30 +108,96 @@ class zenwin(StatusMonitor):
                                 'summary': 'Timeout failure during WMI check',
                                 'device': r.name,
                                 'component' : ''}
-                        self.zem.sendEvent(evt)
+                        self.sendEvent(evt)
                         running.remove(r)
                     break
-                                
+
         #[ srec.close() for srec in self.devices ]
         sys.stdout.flush()
-        self.zem.sendEvent(self.heartbeat)
-        gc.collect()
+        self.sendEvent(self.heartbeat)
+        import gc; gc.collect()
         self.log.info("Com InterfaceCount: %d", pythoncom._GetInterfaceCount())
         self.log.info("Com GatewayCount: %d", pythoncom._GetGatewayCount())
         if hasattr(sys, "gettotalrefcount"):
             self.log.info("ref: %d", sys.gettotalrefcount())
 
 
+    def scanCycle(self, ignored=None):
+        now = time.time()
+        try:
+            self.processLoop()
+        except Exception, ex:
+            self.log.exception("Error processing main loop")
+        delay = time.time() - now
+        reactor.callLater(min(0, self.cycleInterval - delay), self.scanCycle)
+
+        
     def buildOptions(self):
-        StatusMonitor.buildOptions(self)
-        self.parser.add_option('--threads', dest='threads', 
-            default=4, type="int",
-            help="number of parallel threads during collection")
-        self.parser.add_option("-l", "--load", action="append", 
-            dest="load", default=[],
-            help="plugin name to load can have more than one")
+        Base.buildOptions(self)
+        self.parser.add_option('-d', '--device', 
+                               dest='device', 
+                               default=None,
+                               help="single device to collect")
+        self.parser.add_option('--debug', 
+                               dest='debug', 
+                               default=False,
+                               help="turn on additional debugging")
+        self.parser.add_option('--threads',
+                               dest='threads', 
+                               default=4,
+                               type="int",
+                               help="number of parallel threads "
+                                    "during collection")
+        self.parser.add_option("-l", "--load",
+                               action="append", 
+                               dest="load",
+                               default=[],
+                               help="plugin name to load can "
+                                    "have more than one")
+
+    def configService(self):
+        return self.services.get('WmiConfig', FakeRemote())
+
+
+    def updateConfig(self, cfg):
+        for a, v in cfg.monitor:
+            current = getattr(self, a, None)
+            if current is not None and current != v:
+                self.log.info("Setting %s to %r", a, v);
+                setattr(self, a, v)
+        self.heartbeat['timeout'] = self.cycleInterval*3
+        self.log.debug('Device data: %r', (cfg.devices,))
+        devices = []
+        for n,u,p,s in cfg.devices:
+            if self.options.device and self.options.device != n: continue
+            st = StatusTest(self, n, u, p, s, self.options.debug)
+            st.setPlugins(self.getPlugins())
+            devices.append(st) 
+        if devices:
+            self.devices = devices
+
+
+    def error(self, why):
+        why.printTraceback()
+        self.log.error(why.getErrorMessage())
+
+
+    def reconfigure(self):
+        try:
+            d = self.configService().callRemote('getConfig')
+            d.addCallbacks(self.updateConfig, self.error)
+        except Exception, ex:
+            self.log.exception("Error fetching config")
+            return defer.fail(ex)
+        reactor.callLater(self.configCycleInterval, self.reconfigure)
+        return d
+
+
+    def connected(self):
+        d = self.reconfigure()
+        d.addCallback(self.scanCycle)
 
 
 if __name__ == "__main__":
     zw = zenwin()
-    zw.mainLoop()
+    zw.run()
