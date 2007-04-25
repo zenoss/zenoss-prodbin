49ecbdaf86805131c91dbbbf770d596917249daa
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-04-25 20:02:29 +0000

    * copyrights
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18327 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/testBrowserRecordings.py
+++ Products/ZenUITests/tests/testBrowserRecordings.py
@@ -1,3 +1,15 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 import unittest
 from glob import glob
 
@@ -29,3 +41,4 @@ def nonZopeSuite():
 
 if __name__ == '__main__':
     unittest.TextTestRunner().run(nonZopeSuite())
+
