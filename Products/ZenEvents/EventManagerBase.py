688a0bebf29e99bba34c488a5fa8b72608392c12
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-05-07 21:59:21 +0000

    * i missed a f**ckin import!
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18688 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/EventManagerBase.py
+++ Products/ZenEvents/EventManagerBase.py
@@ -42,11 +42,10 @@ from EventDetail import EventDetail
 from BetterEventDetail import BetterEventDetail
 from EventCommand import EventCommand
 from Exceptions import *
-from FakeRequest import FakeRequest
 
 from Products.ZenModel.ZenModelRM import ZenModelRM
 from Products.ZenRelations.RelSchema import *
-from Products.ZenUtils import Time
+from Products.ZenUtils import Time, FakeRequest
 from Products.ZenEvents.ZenEventClasses import Status_Ping, Status_Wmi_Conn
 import StringIO
 import csv
