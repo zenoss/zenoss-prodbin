829d4ca659b0833c6251f4a46adce5861cdce735
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-05-08 19:13:41 +0000

    makin a new beta
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18717 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="1.9.1"
+VERSION="1.9.2"
