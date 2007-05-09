50113031aaa02963c194fd0ab244e3a98589912b
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-05-09 15:44:06 +0000

    new beta version
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@18728 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="1.9.3"
+VERSION="1.9.4"
