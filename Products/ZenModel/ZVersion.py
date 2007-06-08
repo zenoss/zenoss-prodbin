472e7039bd1e55092f2e5b29f1e0e383052330fe
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-06-08 22:11:02 +0000

    and away we go!  (tagged v2.0)
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19168 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="1.9.8"
+VERSION="2.0.0"
