b19fa83d416fdad596f3b74d4e0453ee5a00ee56
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-07-03 19:29:15 +0000

    updated to listen on a tcp port...
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19328 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/zenmail.py
+++ Products/ZenEvents/zenmail.py
@@ -23,13 +23,12 @@ $Id$
 
 from EventServer import EventServer
 
-from twisted.mail.pop3client import POP3Client
-from twisted.internet import reactor, protocol, defer, task
-from twisted.internet.ssl import ClientContextFactory
+from twisted.mail import smtp
+from twisted.internet import reactor, protocol, defer
 from zope.interface import implements
 from Products.ZenRRD.RRDDaemon import RRDDaemon
-from Products.ZenUtils.Driver import drive, driveLater
 
+from email.Header import Header
 
 from MessageProcessing import MessageProcessor
 
@@ -37,148 +36,93 @@ import logging
 log = logging.getLogger("zen.mail")
 
 
-class POPProtocol(POP3Client):
-    ''' protocol that is responsible for conversing with a pop server
-    after a connection has been established.  downloads messages (and
-    deletes them by default), and passes the messages back up to the
-    factory to process (and turn into events)'''
+class MessageWriter(object):
+    implements(smtp.IMessage)
 
-    allowInsecureLogin = True
-    timeout = 15
-
-    def serverGreeting(self, greeting):
-        log.info('server greeting received.')
-        log.info('logging in...')
-
-        login = self.login(self.factory.user, self.factory.passwd)
-        login.addCallback(self._loggedIn)
-        login.addErrback(self.factory.deferred.errback)
- 
-
-    def _loggedIn(self, result):
-        log.info('logged in')
-        return self.retrieveAndParse()
+    def __init__(self):
+        self.lines = []
 
 
-    def retrieveAndParse(self):
-        d = self.listSize()
-        d.addCallback(self._gotMessageSizes)
-
-        return d
+    def lineReceived(self, line):
+        self.lines.append(line)
 
 
-    def _gotMessageSizes(self, sizes):
-        log.info('messages to retrieve: %d' % len(sizes))
+    def eomReceived(self):
+        log.info('message data completed.')
+        self.lines.append('')
+        messageData = '\n'.join(self.lines)
 
-        self.sizes = sizes
-
-        retreivers = []
-        for i in range(len(sizes)):
-            log.info('retrieving message #%d...' % i)
-            d = self.retrieve(i)
-            d.addCallback(self._gotMessageLines)
-            retreivers.append(d)
-
-        deferreds = defer.DeferredList(retreivers) 
-        deferreds.addCallback(self._delete)
-
-        if self.factory.cycle:
-            return deferreds.addCallback(self._finished)
-        else:
-            return deferreds.addCallback(self.factory.scanComplete)
+        if True:
+            return defer.succeed(None)
+        else: 
+            return defer.failure(None)
 
 
-    def _gotMessageLines(self, messageLines):
-        log.info('passing message up to factory')
-        self.factory.handleMessage("\r\n".join(messageLines))
+    def connectionLost(self):
+        log.info('connection lost unexpectedly')
+        del(self.lines)
 
 
-    def _delete(self, results):
-        if not self.factory.nodelete:
-            deleters = []
-            for index in range(len(self.sizes)):
-                log.info('deleting message  #%d...' % index)
-                d = self.delete(index)
-                deleters.append(d)
+class ZenossDelivery(object):
+    implements(smtp.IMessageDelivery)
 
-        deferreds = defer.DeferredList(deleters)
-        return deferreds
+    def __init__(self):
+        pass
 
 
-    def _finished(self, downloadResults):
-        log.info('sleeping for %d seconds.' % self.factory.cycletime)
-        reactor.callLater(self.factory.cycletime, self.retrieveAndParse)
-
-    
-    
-class POPFactory(protocol.ClientFactory):
-    ''' factory that stores the configuration the protocol uses to do it's
-    job.'''
-    
-    protocol = POPProtocol
-
-    def __init__(self, user, passwd, 
-                 processor, cycletime, cycle, nodelete, finish):
-        self.user = user
-        self.passwd = passwd
-        self.processor = processor
-        self.cycletime = cycletime
-        self.cycle = cycle
-        self.deferred = defer.Deferred()
-        self.nodelete = nodelete
-        self.finish = finish
-
-
-    def handleMessage(self, messageData):
-        self.processor.process(messageData)
-
-
-    def clientConnectionFailed(self, connection, reason):
-        self.deferred.errback(reason)
-
-
-    def scanComplete(self, results):
-        log.info("scan complete")
-        self.finish()
-
-
-class MailDaemon(EventServer, RRDDaemon):
-    def __init__(self, name):
-        EventServer.__init__(self)
-        RRDDaemon.__init__(self, name)
+    def receivedHeader(self, helo, origin, recipients):
+        myHostname, clientIP = helo
+        headerValue = 'by %s from %s with ESMTP ; %s' % (
+            myHostname, clientIP, smtp.rfc822date())
         
+        log.info('relayed (or sent directly) from: %s' % clientIP)
+        
+        header = 'Received: %s' % Header(headerValue)
+        return header
 
-class ZenMail(MailDaemon):
+
+    def validateTo(self, user):
+        log.info('to: %s' % user.dest)
+        return self.makeMessageWriter
+
+
+    def makeMessageWriter(self):
+        return MessageWriter()
+
+
+    def validateFrom(self, helo, originAddress):
+        log.info("from: %s" % originAddress)
+        return originAddress
+    
+    
+class SMTPFactory(protocol.ServerFactory):
+    def __init__(self):
+        pass
+
+    def buildProtocol(self, addr):
+        delivery = ZenossDelivery()
+        smtpProtocol = smtp.SMTP(delivery)
+        smtpProtocol.factory = self
+        return smtpProtocol
+
+
+class ZenMail(EventServer, RRDDaemon):
     name = 'zenmail'
 
     def __init__(self):
-        MailDaemon.__init__(self, ZenMail.name)
+        EventServer.__init__(self)
+        RRDDaemon.__init__(self, ZenMail.name)
 
         self.changeUser()
         self.processor = MessageProcessor(self.dmd.ZenEventManager)
-        host = self.options.pophost
-        port = self.options.popport
-        popuser = self.options.popuser
-        poppasswd = self.options.poppass
-        usessl = self.options.usessl
-        cycletime = int(self.options.cycletime)
-        cycle = int(self.options.cycle)
-        nodelete = int(self.options.nodelete)
 
-        log.info("credentials user: %s; pass: %s" % (popuser, 
-                                                     len(poppasswd) * '*'))
-        self.factory = POPFactory(popuser, poppasswd, 
-                                  self.processor, cycletime, cycle, nodelete,
-                                  self._finish)
-        log.info("connecting to pop server: %s:%s" % (host, port))
-        self.factory.deferred.addErrback(self.handleError)
-        
-        if usessl:
-            log.info("connecting to server using SSL")
-            reactor.connectSSL(host, port, self.factory, ClientContextFactory())
+        self.factory = SMTPFactory()
+
+        if self.options.useFileDescriptor != -1:
+            self.useUdpFileDescriptor(int(self.options.useFileDescriptor))
         else:
-            log.info("connceting to server using plaintext")
-            reactor.connectTCP(host, port, self.factory)
+            reactor.listenTCP(2525, self.factory)
+            reactor.run()
 
 
     def handleError(self, error):
@@ -194,42 +138,16 @@ class ZenMail(MailDaemon):
 
     def buildOptions(self):
         EventServer.buildOptions(self)
-        self.parser.add_option('--usessl',
-                               dest='usessl',
-                               default=False,
-                               action="store_true",
-                               help="Use SSL when connecting to POP server")
-        self.parser.add_option('--nodelete',
-                               dest='nodelete',
-                               default=False,
-                               action="store_true",
-                               help="Leave messages on POP server")
-        self.parser.add_option('--pophost',
-                               dest='pophost', 
-                               default="pop.zenoss.com",
-                               help="POP server to auth against")
-        self.parser.add_option('--popport',
-                               dest='popport', 
-                               default="110",
-                               type="int",
-                               help="POP port to auth against")
-        self.parser.add_option('--popuser',
-                               dest='popuser', 
-                               default="zenoss",
-                               help="POP user to auth using")
-        self.parser.add_option('--poppass',
-                               dest='poppass', 
-                               default="zenoss",
-                               help="POP password to auth using")
-        self.parser.add_option('--cycletime',
-                               dest='cycletime',
-                               default=60,
-                               help="Frequency (in secs) to poll POP server")
         self.parser.add_option('--useFileDescriptor',
-                               dest='fd', 
+                               dest='useFileDescriptor', 
                                default="-1",
+                               type="int",
                                help="File descriptor to use for listening")
-
+        self.parser.add_option('--listenPort',
+                               dest='listenPort', 
+                               default="2525",
+                               type="int",
+                               help="Alternative listen port to use")
 
 
 if __name__ == '__main__':
