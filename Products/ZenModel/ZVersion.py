e02ac20e1513dd0cf21e70293d8b8ef259a9d826
marc <marc@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-07-16 17:39:14 +0000

    Upped version to 2.0.3
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19418 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="2.0.2"
+VERSION="2.0.3"
