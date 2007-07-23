06b3bad617be51e10c6f3f9fb7340120c7271c24
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-07-23 15:39:53 +0000

    * remove old Zentinel copyright, remove reference to non-existant 2.1 GPL
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19471 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenWidgets/ZenTableManager.py
+++ Products/ZenWidgets/ZenTableManager.py
@@ -10,11 +10,6 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-#   Copyright (c) 2004 Zentinel Systems. 
-#   This library is free software; you can redistribute it and/or
-#   modify it under the terms of the GNU Lesser General Public
-#   License as published by the Free Software Foundation; either
-#   version 2.1 of the License, or (at your option) any later version.
 
 __doc__="""ZenTableManager
 
