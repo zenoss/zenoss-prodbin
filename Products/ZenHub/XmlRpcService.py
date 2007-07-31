d93c04d7c8391b09f6823ad144a95db1b5c7ee0b
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-07-31 21:22:48 +0000

    define getConfigs.  it allows you retrieve data sources for a given performance collector.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19511 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/XmlRpcService.py
+++ Products/ZenHub/XmlRpcService.py
@@ -12,7 +12,13 @@
 ###########################################################################
 from twisted.web import xmlrpc
 
+import types
+
 class XmlRpcService(xmlrpc.XMLRPC):
+    # serializable types
+    PRIMITIVES = [types.IntType, types.StringType, types.BooleanType,
+                  types.DictType, types.FloatType, types.LongType,
+                  types.NoneType]
 
     def __init__(self, dmd):
         xmlrpc.XMLRPC.__init__(self)
@@ -46,3 +52,39 @@ class XmlRpcService(xmlrpc.XMLRPC):
         adm = ApplyDataMap()
         adm.applyDataMap(dev, datamap, relname=relname,
                          compname=compname, modname=modname)
+
+
+    def xmlrpc_getConfigs(self, monitor, dstype):
+        '''Return the performance configurations for the monitor name and data
+        source provided. '''
+
+        def toDict(device, ds):
+            '''marshall the fields from the datasource into a dictionary and
+            ignore everything that is not a primitive'''
+
+            vals = {}
+            for key in ds.__dict__.keys():
+                val = ds.__dict__[key]
+                if type(val) in XmlRpcService.PRIMITIVES:
+                    vals[key] = val
+
+            vals['device'] = device.id
+            return vals
+
+
+        result = []
+        
+        # get the performance conf (if it exists)
+        conf = getattr(self.dmd.Monitors.Performance, monitor, None)
+        if conf is None:
+            return result
+
+        # loop over devices that use the performance monitor
+        for device in conf.devices._objects:
+            for template in device.getRRDTemplates():
+                for ds in template.getRRDDataSources():
+                    if ds.sourcetype == dstype:
+                        result.append(toDict(device, ds))
+
+        return result
+
