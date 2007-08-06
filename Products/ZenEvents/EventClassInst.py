ca2f3f2f1ccd045ccd163d615ed0d0daaeb4a870
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-08-06 18:01:22 +0000

    * Fixes #1888.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19535 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/EventClassInst.py
+++ Products/ZenEvents/EventClassInst.py
@@ -58,6 +58,9 @@ class EventClassPropertyMixin(object):
         if sev >= 0:
             if evt.severity > 0:
                 evt.severity = sev
+        resolution = getattr(self, 'resolution', '')
+        explanation = getattr(self, 'explanation', '')
+        evt.updateFromDict({'resolution':resolution,'explanation':explanation})
         return evt
 
     def applyTransform(self, evt, device):
