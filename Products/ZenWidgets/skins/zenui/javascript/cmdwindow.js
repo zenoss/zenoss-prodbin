8ecdcdc73ba2ea033d51a166f2848820514dce1c
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-08-21 17:53:38 +0000

    * Added a dialog that polls a url according to a specified refresh rate
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19716 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenWidgets/skins/zenui/javascript/cmdwindow.js
@@ -0,0 +1,86 @@
+var Class={
+    create:function(){
+        return function(){
+            this.__init__.apply(this,arguments);
+        }
+    }
+}
+
+var PollingDialog = Class.create();
+PollingDialog.prototype = {
+    __init__: function(title, url, refreshRate) {
+        bindMethods(this);
+        this.showDialog(title, url, refreshRate);
+    },
+    fetch: function(did, url) {
+        var d = doSimpleXMLHttpRequest(url);
+        d.addCallback(bind(function(req){
+            this.fill(did, req);
+        }, this));
+    },
+    fill: function(did, request) {
+        $(did+'_innercontent').innerHTML = request.responseText;
+        var elements = getFormElements($(did+'_innercontent'));
+        var first = elements[0];
+        var textboxes = elements[1];
+        var submits = elements[2];
+        var submt = submits[0];
+        var connectTextboxes = function(box) {
+            connect(box, 'onkeyup', function(e){
+                if (e.key().string=='KEY_ENTER') submt.click();
+            });
+        }
+        if (submits.length==1) map(connectTextboxes, textboxes);
+        first.focus();
+    },
+    makeDialog: function(title) {
+        var did = 'dialog'+String(Date.now());
+        var mydialog = DIV({
+            'id':did,
+            'style':'visibility:hidden;position:absolute;top:0;'
+        },
+            [
+                DIV({'class':'x-dlg-hd'}, title),
+                DIV({'class':'x-dlg-bd'},
+                    DIV({'id':did+'_innercontent'}, null)
+                )
+            ]
+        )
+        appendChildNodes($('frame'), mydialog);
+        return did;
+    },
+    showDialog: function(title, url, refreshRate){
+        var did = this.makeDialog(title);
+        this.dialog = new Ext.BasicDialog(did, {
+            width:400,
+            height:400,
+            shadow:true,
+            minWidth:300,
+            minHeight:250,
+            proxyDrag:true
+        });
+        var killdialog = bind(function() {
+            this.destroy(did, this.dialog);
+        }, this);
+        this.dialog.addKeyListener(27, killdialog, did, this.dialog);
+        this.dialog.addButton('Close', killdialog, did, this.dialog);
+        this.dialog.show();
+        this.fetch(did, url);
+        refreshRate = parseInt(refreshRate);
+        if (refreshRate>0)
+            this.startRepeat(this.dialog, did, url, refreshRate);
+    },
+    startRepeat: function(dialog, did, url, refreshRate){
+        dialog.caller = callLater(refreshRate, bind(function(){
+            this.fetch(did, url);
+            this.startRepeat(dialog, did, url, refreshRate);
+        }, this));
+    },
+    destroy: function(did, dialog){
+        if (dialog.caller) dialog.caller.cancel();
+        dialog.destroy();
+        removeElement(did);
+    }
+
+}
+
