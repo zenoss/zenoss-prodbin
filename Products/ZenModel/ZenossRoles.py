ff519f0943292c2a15d09496d3d733fd464e2e33
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-05 03:14:06 +0000

    * add so it can be renamed!
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19822 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenModel/ZenossRoles.py
@@ -0,0 +1,16 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+ZEN_COMMON = 'ZenCommon'
+ZEN_USER_ROLE = 'ZenUser'
+ZEN_MANAGER_ROLE = 'ZenManager'
