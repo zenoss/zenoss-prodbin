0d569aa0fc923cddd8c28c241ebf1492ff338917
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-11 20:19:17 +0000

    * change default login info
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@19942 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/xmlClient.py
+++ Products/ZenHub/xmlClient.py
@@ -18,7 +18,7 @@ from socket import getfqdn
 from zenhub import XML_RPC_PORT
 
 def main():
-    proxy = ServerProxy('http://zenoss:zenoss@localhost:%d' % XML_RPC_PORT)
+    proxy = ServerProxy('http://admin:zenoss@localhost:%d' % XML_RPC_PORT)
     proxy.sendEvent(dict(summary='This is an event',
                          device=getfqdn(),
                          Class='/Status/Ping',
