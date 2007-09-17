bb01bf742c1f0bbe81431ecac4f737b43097bc35
chris <chris@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-17 15:24:22 +0000

    refs #1886: change the writeRRD implementation to delegate to the RRDImpl module (specifically the RRDImpl class).  this code is needed by both PB clients as well as XML-RPC clients.  removed the checkThresholds method as well because that was moved into the RRDImpl.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@20005 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/services/RRDService.py
+++ Products/ZenHub/services/RRDService.py
@@ -16,50 +16,31 @@ __doc__='''RRDService
 Provides RRD services to zenhub clients.
 '''
 
-import os.path
 from HubService import HubService
 from Products.ZenRRD.RRDUtil import RRDUtil
 from Products.ZenRRD.Thresholds import Thresholds
+from RRDImpl import RRDImpl
 import time
 import logging
 log = logging.getLogger("zenhub")
 
 class RRDService(HubService):
 
-
     def __init__(self, dmd, instance):
         HubService.__init__(self, dmd, instance)
+
         # rrd is a dictionary of RRDUtil instances
         self.rrd = {}
         self.thresholds = Thresholds()
+        self.rrdimpl = RRDImpl(dmd)
 
 
     def remote_writeRRD(self, devId, compType, compId, dpName, value):
         '''Write the given data to its rrd file.
         Also check any thresholds and send events if value is out of bounds.
         '''
-        log.debug('Writing %s %s' % (dpName, value))
-        dev = self.getDeviceOrComponent(devId, compType, compId)
-        dp = dev.getRRDDataPoint(dpName)
-        if not dp:
-            log.warn('Did not find datapoint %s on device %s', dpName, devId)
-            return None
-        rrdKey = (dev.getPrimaryPath(), dp.getPrimaryPath())
-        rrdCreateCmd = dp.createCmd or self.getDefaultRRDCreateCommand(dev)
-        if self.rrd.has_key(rrdKey):
-            rrd = self.rrd[rrdKey]
-        else:
-            rrd = RRDUtil(rrdCreateCmd, dp.datasource.cycletime)
-            self.rrd[rrdKey] = rrd
-        value = rrd.save(os.path.join(dev.rrdPath(), dp.name()),
-                         value, 
-                         dp.rrdtype,
-                         rrdCreateCmd,
-                         dp.datasource.cycletime,
-                         dp.rrdmin,
-                         dp.rrdmax)
-        self.checkThresholds(dev, dp, value)
-        return value
+
+        return self.rrdimpl.writeRRD(devId, compType, compId, dpName, value)
 
 
     def getDefaultRRDCreateCommand(self, device):
@@ -84,24 +65,3 @@ class RRDService(HubService):
         return d
         
 
-    def checkThresholds(self, dev, dp, value):
-        ''' Check the given value against any thresholds.  Count the number of
-        times a dp has exceeded a given threshold in self.counts.  Send events
-        as appropriate.
-        '''
-        if value is None:
-            return
-        # Loop through the enabled thresholds on the template containing
-        # this datapoint.
-        thresholds = [t for t in dp.datasource.rrdTemplate.thresholds()]
-        thresholds = [t for t in thresholds if t.enabled]
-        thresholds = [t for t in thresholds if dp.name() in t.dsnames]
-        for t in thresholds:
-            log.debug('Checking %s value of %s against threshold %s' %
-                      (dp.name(), value, t.id))
-            ti = t.createThresholdInstance(dev)
-            self.thresholds.update(ti)
-            for ev in self.thresholds.check(ti.context().fileKey(dp.name()),
-                                            time.time(),
-                                            value):
-                self.zem.sendEvent(ev)
