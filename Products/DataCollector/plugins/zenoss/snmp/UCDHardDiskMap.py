85991386aa589c64e7be4f8a234764513b6231fe
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-26 17:17:27 +0000

    * Fixes #1267 for newer Linux systems.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@20275 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/DataCollector/plugins/zenoss/snmp/UCDHardDiskMap.py
@@ -0,0 +1,46 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+#   Copyright (c) 2006 Zentinel Systems, Inc. All rights reserved.
+
+from CollectorPlugin import SnmpPlugin, GetTableMap
+import re
+
+class UCDHardDiskMap(SnmpPlugin):
+    """Map UCD-DISKIO-MIB to HardDisk"""
+
+    maptype = "HardDiskMap"
+    modname = "Products.ZenModel.HardDisk"
+    relname = "harddisks"
+    compname = "hw"
+
+    snmpGetTableMaps = (
+        GetTableMap('diskIOTable', '.1.3.6.1.4.1.2021.13.15.1.1', {
+            '.2': 'id',
+            }
+        ),
+    )
+
+    def process(self, device, results, log):
+        """collect snmp information from this device"""
+        log.info('processing %s for device %s', self.name(), device.id)
+        getdata, tabledata = results
+        dtable = tabledata.get("diskIOTable")
+        if not dtable: return
+        rm = self.relMap()
+        for oid, disk in dtable.items():
+            om = self.objectMap(disk)
+            if not re.search('^[hs]d[a-z]$', om.id): continue
+            om.id = self.prepId(om.id)
+            om.snmpindex = oid
+            rm.append(om)
+        return rm
