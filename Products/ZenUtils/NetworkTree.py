f93d8095464694a6ee44af40ec220c971f30a680
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-26 21:54:28 +0000

    * fix network links for google maps
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@20293 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/NetworkTree.py
+++ Products/ZenUtils/NetworkTree.py
@@ -90,7 +90,7 @@ def get_edges(rootnode, depth=1, withIcons=False, filter='/'):
 def getDeviceNetworkLinks(rootdevice):
     """ Returns network links to other devices """
     visited = []
-    ifaces = rootdevice.os.interfaces.objectValuesGen()
+    ifaces = rootdevice.os.interfaces()
     ifaceids = [x.getPrimaryId() for x in ifaces]
     for iface in ifaces:
         for ip in iface.ipaddresses.objectValuesGen():
@@ -131,7 +131,7 @@ class NetworkLink(ILink):
         if max([ep.getPingStatus() for ep in eps]) > 0:
             return 5
         zem = eps[0].dmd.ZenEventManager
-        return zem.getMaxSeverity(eps)
+        return max(map(zem.getMaxSeverity,eps))
 
     def getGeomapData(self, context, full=False):
         """ Return the addresses of the endpoints
