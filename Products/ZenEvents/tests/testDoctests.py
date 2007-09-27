5ebd67e76a0a367e44b1ef606ce34f9a9b3a3658
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-09-27 19:08:46 +0000

    * Set up testing framework for ZenEvents
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@20317 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenEvents/tests/testDoctests.py
@@ -0,0 +1,39 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from types import ModuleType
+from unittest import TestSuite, makeSuite
+
+from Products.ZenUtils.ZenDocTest import ZenDocTestRunner
+
+from Products import ZenEvents
+
+def get_submodules(mod):
+    vals = mod.__dict__.values()
+    allmods = filter(lambda x:type(x)==ModuleType, vals)
+    submods = filter(lambda x:x.__name__.startswith(mod.__name__), allmods)
+    return submods
+
+def test_suite():
+    suite = TestSuite()
+    zdtr = ZenDocTestRunner()
+    zdtr.add_modules(get_submodules(ZenEvents))
+    for dtsuite in zdtr.get_suites():
+        suite.addTest(dtsuite)
+    return suite
+
+if __name__=="__main__":
+    framework()
