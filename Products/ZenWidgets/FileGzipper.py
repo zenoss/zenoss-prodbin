b187be435c5d8c4795caa8c894ebdd0ac8f60d0b
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-12-10 16:58:56 +0000

    * Refs #392: Reverted Filegzipper to old version
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21257 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenWidgets/FileGzipper.py
+++ Products/ZenWidgets/FileGzipper.py
@@ -17,51 +17,45 @@ A monkey patch that enables gzip compression on static files
 
 """
 
-from Products.CMFCore.utils import expandpath, _FSCacheHeaders, \
-                                   _checkConditionalGET
 from Products.CMFCore.utils import _setCacheHeaders, _ViewEmulator
 from DateTime import DateTime
 from webdav.common import rfc1123_date
 from Products.CMFCore.FSFile import FSFile
-
 def index_html(self, REQUEST, RESPONSE):
     """ Modified default view for files to enable
         gzip compression on js and css files.
     """
     self._updateFromFS()
-    view = _ViewEmulator().__of__(self)
-
-    # If we have a conditional get, set status 304 and return
-    # no content
-    if _checkConditionalGET(view, extra_context={}):
-        return ''
-
-    # old-style If-Modified-Since header handling.
-    if self._setOldCacheHeaders():
-        # Make sure the CachingPolicyManager gets a go as well
-        _setCacheHeaders(view, extra_context={})
-        return ''
-
     data = self._readFile(0)
     data_len = len(data)
-    RESPONSE.setHeader('Content-Length', data_len)
+    last_mod = self._file_mod_time
+    status = 200
+    # HTTP If-Modified-Since header handling.
+    header=REQUEST.get_header('If-Modified-Since', None)
+    if header is not None:
+        header = header.split(';')[0]
+        try:
+            mod_since=long(DateTime(header).timeTime())
+        except:
+            mod_since=None
+            
+        if mod_since is not None:
+            if last_mod > 0 and last_mod <= mod_since:
+                status = 304
+                data = ''
+    RESPONSE.setStatus(status)
+    RESPONSE.setHeader('Last-Modified', rfc1123_date(last_mod))
+    RESPONSE.setHeader('Content-Type', self.content_type)
 
-    # Here's the modification. We turn on compression, and also make sure the
-    # charset is utf-8 for css and javascript files.
-    content_type = self.content_type
-    if (self.content_type.endswith("css") or
-        self.content_type.endswith("javascript")):
-        content_type = self.content_type + "; charset=utf-8"
-    RESPONSE.setHeader('Content-Type', content_type)
+    # The key line!
     RESPONSE.enableHTTPCompression(force=1)
 
-    #There are 2 Cache Managers which can be in play....
-    #need to decide which to use to determine where the cache headers
-    #are decided on.
+    if status != 304:
+        RESPONSE.setHeader('Content-Length', data_len)
     if self.ZCacheable_getManager() is not None:
         self.ZCacheable_set(None)
     else:
-        _setCacheHeaders(view, extra_context={})
+        _setCacheHeaders(_ViewEmulator().__of__(self), extra_context={})
     return data
 
 FSFile.index_html = index_html
