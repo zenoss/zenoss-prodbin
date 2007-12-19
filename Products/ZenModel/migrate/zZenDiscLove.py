dcf8ad5e52c23f9546b31728e0aa07daa2390885
sjakesch <sjakesch@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-12-19 10:19:44 +0000

    migrate script for zZenDiscCommand and zAutoAllocateScript
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21430 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenModel/migrate/zZenDiscLove.py
@@ -0,0 +1,31 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import Migrate
+
+class zZenDiscLove(Migrate.Step):
+    version = Migrate.Version(2, 1, 2)
+    
+    def cutover(self, dmd):
+        if not dmd.Networks.hasProperty('zAutoAllocateScript'):
+            dmd.Networks._setProperty(
+                "zAutoAllocateScript", ["#your python script here", "#avail objs: dmd, dev, log"], type="lines")
+              
+        if not dmd.Networks.hasProperty('zZenDiscCommand'):
+            dmd.Networks._setProperty(
+                "zZenDiscCommand", "zendisc run --net=${here/id}", type="string")
+
+
+zZenDiscLove()
+
+
