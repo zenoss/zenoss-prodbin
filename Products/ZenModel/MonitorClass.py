211215134bb48159296d4f183d5af50d529f2da4
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2007-12-27 18:58:27 +0000

    * remove copy/paste code (which was, in turn, copied and pasted)
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21505 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/MonitorClass.py
+++ Products/ZenModel/MonitorClass.py
@@ -196,79 +196,17 @@ class MonitorClass(ZenModelRM, Folder):
             return self.callZenScreen(REQUEST)
             
 
-    def manage_copyRRDTemplates(self, ids=(), REQUEST=None):
-        """Put a reference to the objects named in ids in the clip board"""
-        if not ids: return self.callZenScreen(REQUEST)
-        ids = [ id for id in ids if self.rrdTemplates._getOb(id, None) != None]
-        if not ids: return self.callZenScreen(REQUEST)
-        cp = self.rrdTemplates.manage_copyObjects(ids)
-        if REQUEST:
-            resp=REQUEST['RESPONSE']
-            resp.setCookie('__cp', cp, path='/zport/dmd')
-            REQUEST['__cp'] = cp
-            REQUEST['message'] = "Templates copied"
-            return self.callZenScreen(REQUEST)
-        return cp
-
-
-    def manage_pasteRRDTemplates(self, moveTarget=None, cb_copy_data=None, REQUEST=None):
-        """Paste RRDTemplates that have been copied before.
-        """
-        cp = None
-        if cb_copy_data: cp = cb_copy_data
-        elif REQUEST:
-            cp = REQUEST.get("__cp",None)
-        
-        if cp:
-            if moveTarget:
-                target = self.getDmdRoot(self.dmdRootName).getOrganizer(moveTarget)
-            else:
-                target = self
-            target.rrdTemplates.manage_pasteObjects(cp)
-        else:
-            target = None
-            
-        if REQUEST:
-            REQUEST['RESPONSE'].setCookie('__cp', 'deleted', path='/zport/dmd',
-                            expires='Wed, 31-Dec-97 23:59:59 GMT')
-            REQUEST['__cp'] = None
-            if target:
-                message = "Template(s) moved to %s" % moveTarget
-            else:
-                message = None
-            if not isinstance(REQUEST, FakeRequest):
-                url = target.getPrimaryUrlPath() + '/perfConfig'
-                if message:
-                    url += '?message=%s' % message
-                REQUEST['RESPONSE'].redirect(url)
-            else:
-                REQUEST['message'] = message
-                return self.callZenScreen(REQUEST)
-
-
-    def manage_copyAndPasteRRDTemplates(self, ids=(), copyTarget=None, REQUEST=None):
-        ''' Copy the selected templates into the specified device class.
-        '''
-        if not ids:
-            REQUEST['message'] = "No Templates Selected"
-            return self.callZenScreen(REQUEST)
-        if copyTarget is None:
-            REQUEST['message'] = "No Target Selected"
-            return self.callZenScreen(REQUEST)
-        cp = self.manage_copyRRDTemplates(ids)
-        return self.manage_pasteRRDTemplates(copyTarget, cp, REQUEST)
-
-
     def manage_deleteRRDTemplates(self, ids=(), paths=(), REQUEST=None):
-        """Delete RRDTemplates from this DeviceClass 
+        """Delete RRDTemplates from this MonitorClass 
         (skips ones in other Classes)
         """
         if not ids and not paths:
             return self.callZenScreen(REQUEST)
         for id in ids:
-            if (getattr(aq_base(self), 'rrdTemplates', False)
-                and getattr(aq_base(self.rrdTemplates),id,False)):
-                self.rrdTemplates._delObject(id)
+            self.rrdTemplates._delObject(id)
+        for path in paths:
+            id = path.split('/')[-1]
+            self.rrdTemplates._delObject(id)
         if REQUEST: 
             REQUEST['message'] = "Templates deleted"
             return self.callZenScreen(REQUEST)
