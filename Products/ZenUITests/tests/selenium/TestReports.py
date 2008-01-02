ed17a8146530dc1e5b1b0c71f58119d1ffce3509
noel <noel@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-01-02 20:28:01 +0000

    * added another test for the reports pages
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21535 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/selenium/TestReports.py
+++ Products/ZenUITests/tests/selenium/TestReports.py
@@ -16,26 +16,45 @@
 # Contained below is the class that tests elements located under
 # the "Reports" Browse By subheading.
 #
-# Adam Modlin and Nate Avers
+# Adam Modlin, Nate Avers and Noel Brockett
 #
 
 import unittest
+
+from util.selTestUtils import *
 
 from SelTestBase import SelTestBase
 
 class TestReports(SelTestBase):
     """Defines an object that runs tests under the Reports heading"""
+
+    def _addTestReportOrganizer(self):
+        self.waitForElement("link=Reports")
+        self.selenium.click("link=Reports")
+        self.selenium.wait_for_page_to_load(self.WAITTIME)
+        if self.selenium.is_element_present("link=testingString"):
+            self._deleteTestReportOrganizer()
+        self.addDialog(addType="ReportClasslistaddReportClass", new_id=("text", "testingString"))
+        self.selenium.wait_for_page_to_load(self.WAITTIME)
+
+    def _deleteTestReportOrganizer(self):
+        self.waitForElement("link=Reports")
+        self.selenium.click("link=Reports")
+        self.selenium.wait_for_page_to_load(self.WAITTIME)
+        self.deleteDialog(deleteType="ReportClasslistdeleteReportClasses", form_name="reportClassForm")
+        self.selenium.wait_for_page_to_load(self.WAITTIME)
     
     def testReportOrganizer(self):
         """Run tests on the Reports page"""
         
-        self.waitForElement("link=Reports")
-        self.selenium.click("link=Reports")
-        self.selenium.wait_for_page_to_load(self.WAITTIME)
-        self.addDialog(addType="ReportClasslistaddReportClass", new_id=("text", "testingString"))
-        self.selenium.wait_for_page_to_load(self.WAITTIME)
-        self.deleteDialog(deleteType="ReportClasslistdeleteReportClasses", form_name="reportClassForm")
-        self.selenium.wait_for_page_to_load(self.WAITTIME)
+        self._addTestReportOrganizer()
+        #Selects All 
+        self.waitForElement("id=selectall_0")
+        self.selenium.click("id=selectall_0")
+        do_command_byname(self.selenium, "assertChecked", "ids:list")
+        self.selenium.click("id=selectnone_0")
+        do_command_byname(self.selenium, "assertNotChecked", "ids:list")
+        self._deleteTestReportOrganizer()
         
 if __name__ == "__main__":
     unittest.main()
