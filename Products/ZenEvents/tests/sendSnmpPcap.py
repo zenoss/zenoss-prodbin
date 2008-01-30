a4a50c26bda91fadf96f7f8a59173c8e7516d409
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-01-30 14:44:49 +0000

    * copyright and doc
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21722 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/tests/sendSnmpPcap.py
+++ Products/ZenEvents/tests/sendSnmpPcap.py
@@ -1,3 +1,15 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 import os
 import socket
 
@@ -5,9 +17,11 @@ import Globals
 from Products.ZenUtils.Utils import zenPath
 
 def main():
+    "Run a tcpdump PCAP file to zentrap"
     dumpfile = zenPath('Products/ZenEvents/tests/trapdump.pcap')
     packets = []
     packet = []
+    # extract packet data without getting a pcap module:
     for line in os.popen('tcpdump -x -r %s' % dumpfile):
         if line[0] == '\t':
             hex = line.split(':', 1)[1].replace(' ','').strip()
@@ -21,13 +35,10 @@ def main():
         packets.append(''.join(packet))
     assert len(packets) == 2
     for p in packets:
-        assert p[0] == chr(0x45)
+        assert p[0] == chr(0x45)        # check for IP
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     for p in packets:
-        dataoffset = 28
-        p = p[28:]
-        # p = p.replace('\xC0\xA8\x01\x40', '\x7f\x00\x00\x01')
-        print `p`
-        s.sendto( p, ('127.0.0.1', 1162))
+        p = p[28:]                      # get to UDP data
+        s.sendto(p, ('127.0.0.1', 1162))
 
 main()
