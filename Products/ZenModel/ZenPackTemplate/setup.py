fde0b8109757c854eb5284a9c256ab5a8c4e878d
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-02-04 21:48:33 +0000

    refs #2550  * removed unused metadata from setup.py  * added comments to explain most of the lines
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21771 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZenPackTemplate/setup.py
+++ Products/ZenModel/ZenPackTemplate/setup.py
@@ -1,24 +1,38 @@
 from setuptools import setup, find_packages
 
 setup(
+    # This ZenPack metadata should usually be edited with the Zenoss
+    # ZenPack edit page.  Whenever the edit page is submitted it will
+    # overwrite the values below (the ones it knows about) with new values.
     name = 'ZENPACKID',
     version = '1.0',
     author = '',
-    organization = '',
-    description = '',    
-    long_description = '',
-    author_email = '',
-    maintainer = '',
-    maintainer_email = '',
-
+    license = '',
+    
+    # Indicate to setuptools which namespace packages the zenpack
+    # participates in
     namespace_packages = ['ZenPacks', 'ZenPacks.PACKAGE'],
+    
+    # Tell setuptools what packages this zenpack provides.
     packages = ['ZenPacks', 'ZenPacks.PACKAGE', 'ZenPacks.PACKAGE.ZENPACKID'],
+    
+    # Tell setuptools to figure out for itself which files to include
+    # in the binary egg when it is built.
     include_package_data = True,
+    
+    # Tell setuptools what non-python files should also be included
+    # with the binary egg.
     package_data = {
          '': ['*.txt'],
          'ZenPacks.PACKAGE.ZENPACKID': ['objects/*','skins/ZENPACKID/*'],
          },
 
+    # Indicate dependencies on other python modules or ZenPacks.  This line
+    # is modified by zenoss when the ZenPack edit page is submitted.  Zenoss
+    # tries to put add/delete the names it manages at the beginning of this
+    # list, so any manual additions should be added to the end.  Things will
+    # go poorly if this line is broken into multiple lines or modified to
+    # dramatically.
     install_requires = [],
 
     # Every ZenPack egg must define exactly one zenoss.zenpacks entry point
