99b21c35d16759a99fd44d87fcd0bbc8e30f6560
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-02-08 15:37:00 +0000

    * Incrementing version
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21815 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="2.1.71"
+VERSION="2.1.72"
