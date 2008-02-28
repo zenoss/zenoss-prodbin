c68bcc1d938606356add04c2c355c4dae0db6832
rak <rak@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-02-28 19:06:21 +0000

    fixes #2779: 64bit counters not included in interface utilization report
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@21933 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenReports/plugins/interface.py
+++ Products/ZenReports/plugins/interface.py
@@ -13,32 +13,25 @@
 
 import Globals
 from Products.ZenReports import Utils, Utilization
-import re
 
 class interface:
     "The interface usage report"
 
     def run(self, dmd, args):
         summary = Utilization.getSummaryArgs(dmd, args)
-        report = []
-        for d in Utilization.filteredDevices(dmd, args):
 
-            isLocal = re.compile(d.zLocalInterfaceNames)
+        report = []
+        for d in dmd.Devices.getSubDevices():
             for i in d.os.interfaces():
-                if isLocal.match(i.name()): continue
                 if not i.monitored(): continue
                 if i.snmpIgnore(): continue
-                if not i.speed: continue
                 total = None
-                results = i.getRRDValues(['ifHCInOctets',
-                                          'ifInOctets',
-                                          'ifOutOctets',
-                                          'ifHCOutOctets'],
-                                         **summary)
-                input = results.get('ifHCInOctets',
-                                    results.get('ifInOctets', None))
-                output = results.get('ifHCOutOctets',
-                                     results.get('ifOutOctets', None))
+
+                input = i.getRRDValue('ifInOctets', **summary)
+		if not input: input = i.getRRDValue('ifHCInOctets', **summary)
+
+                output = i.getRRDValue('ifOutOctets', **summary)
+		if not output: output = i.getRRDValue('ifHCOutOctets', **summary)
 
                 if None not in [input, output]:
                     total = input + output
@@ -48,6 +41,7 @@ class interface:
                                  input=input,
                                  output=output,
                                  total=total,
-                                 percentUsed=Utils.percent(total, i.speed / 8))
+                                 percentUsed=Utils.percent(total, i.speed))
                 report.append(r)
         return report
+
