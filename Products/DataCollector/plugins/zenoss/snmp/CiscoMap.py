c4243fede99f3c6d710587d4a76cb5242f872430
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-03-11 19:05:22 +0000

    * fixes #2827 : CiscoMap throws errors
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22033 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zenoss/snmp/CiscoMap.py
+++ Products/DataCollector/plugins/zenoss/snmp/CiscoMap.py
@@ -31,7 +31,7 @@ class CiscoMap(SnmpPlugin):
    
     def condition(self, device, log):
         """does device meet the proper conditions for this collector to run"""
-        return device.snmpOid.startswith('.1.3.6.1.4.1.9')
+        return device.snmpOid and device.snmpOid.startswith('.1.3.6.1.4.1.9')
 
 
     def process(self, device, results, log):
