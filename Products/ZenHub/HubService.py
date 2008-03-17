9022dc6487cfa6f2216e171eb9a216d628d0c271
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-03-17 19:12:38 +0000

    * remove duplicate implementation of deferToThread
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22056 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/HubService.py
+++ Products/ZenHub/HubService.py
@@ -17,20 +17,11 @@ hubLog = logging.getLogger("zenhub")
 import time
 
 def threaded(callable):
-    from twisted.internet import reactor
-    from twisted.internet.defer import Deferred
+    from twisted.internet.threads import deferToThread
 
     def callInThread(*args, **kw):
         "Run the callable in a separate thread."
-
-        def run(deferred, callable, *args, **kw):
-            try:
-                reactor.callFromThread(deferred.callback, callable(*args, **kw))
-            except Exception, ex:
-                reactor.callFromThread(deferred.errback, (ex))
-        d = Deferred()
-        reactor.callInThread(run, d, callable, *args, **kw)
-        return d
+        return deferToThread(callable, *args, **kw)
     return callInThread
 
 class HubService(pb.Referenceable):
