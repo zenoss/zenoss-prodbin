4db9018924f0e9f57fd6741e0a281a63e7ef348f
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-03-17 13:01:43 +0000

    * remove any existing monitor relationships
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22052 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/removeStatusMonitor.py
+++ Products/ZenModel/migrate/removeStatusMonitor.py
@@ -6,12 +6,23 @@ Monitor
 
 import Migrate
 
+from Products.ZenRelations.RelSchema import *
+from Products.ZenModel.Device import Device
+
+
 class RemoveStatusMonitor(Migrate.Step):
     version = Migrate.Version(2, 2, 0)
 
     def cutover(self, dmd):
         if hasattr(dmd.Monitors, 'StatusMonitors'):
             dmd.Monitors._delObject('StatusMonitors')
+        Device._relations = Device._relations + (
+            ("monitors", ToMany(ToMany,"Products.ZenModel.StatusMonitorConf","devices")),
+            )
+        for d in self.dmd.Devices.getSubDevices():
+            if hasattr(d, 'monitors'):
+                d._delObject('monitors')
+        Device._relations = tuple([x for x in Device._relations if x[0] != 'monitors'])
         try:
             t = dmd.Monitors.rrdTemplates.PerformanceConf.thresholds
             t = t._getOb('zenping cycle time')
