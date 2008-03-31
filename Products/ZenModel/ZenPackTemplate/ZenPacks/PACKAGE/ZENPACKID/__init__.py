0723aaf7cdc61d41c0d5e890943b7560fae6a073
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-03-31 17:20:26 +0000

    refs #2886
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22213 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZenPackTemplate/ZenPacks/PACKAGE/ZENPACKID/__init__.py
+++
@@ -1,7 +0,0 @@
-
-import Globals
-import os.path
-
-skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
-from Products.CMFCore.DirectoryView import registerDirectory
-registerDirectory(skinsDir, globals())
