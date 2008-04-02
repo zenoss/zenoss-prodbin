fdf3e7812f8206317aa18bc3b1a7cd88bc107696
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-02 23:56:47 +0000

    * fixes #2910 : IpV6ServiceMap does not pick up UDP services
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22272 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zenoss/snmp/IpV6ServiceMap.py
+++ Products/DataCollector/plugins/zenoss/snmp/IpV6ServiceMap.py
@@ -37,24 +37,15 @@ class IpV6ServiceMap(SnmpPlugin):
                     {'.1':'v4', '.2':'v6'}),
         GetTableMap('udpendpoint', '.1.3.6.1.2.1.7.7.1.8',
                     {'.1':'v4', '.2':'v6'}),
-    ) 
-    
-    def process(self, device, results, log):
-        """collect snmp information from this device"""
-        log.info('processing %s for device %s', self.name(), device.id)
-        getdata, tabledata = results
-        tcplisten = tabledata.get("tcplisten")
-        udpendpoint = tabledata.get("udpendpoint")
-        rm = self.relMap()
-        maxport = getattr(device, 'zIpServiceMapMaxPort', 1024)
+    )
 
-        #tcp services
-        tcpports = {}
+
+    def _processTcp(self, tcplisten):
+        result = []
         for oid, value in tcplisten.items():
             log.debug("tcp new %s %s" % (oid,value))
             oidar = oid.split('.')
             port = int(oidar[-1])
-            if port > maxport or port < 1: continue
 
             addr = ''
             if value.has_key('v4'): #ipv4 binding
@@ -70,32 +61,26 @@ class IpV6ServiceMap(SnmpPlugin):
                  # wha?
                 continue
 
-            om = tcpports.get(port, None)
-            if om:
-                if not addr in om.ipaddresses:
-                    om.ipaddresses.append(addr)
-            else:
-                om = self.objectMap()
-                om.id = 'tcp_%05d' % port
-                om.ipaddresses = [addr,]
-                om.protocol = 'tcp'
-                om.port = port
-                om.setServiceClass = {'protocol': 'tcp', 'port':port}
-                om.discoveryAgent = self.name()
-                tcpports[port] = om
-                rm.append(om)
+            om = self.objectMap()
+            om.id = 'tcp_%05d' % port
+            om.ipaddresses = [addr,]
+            om.protocol = 'tcp'
+            om.port = port
+            om.setServiceClass = {'protocol': 'tcp', 'port':port}
+            om.discoveryAgent = self.name()
+            result.append(om)
+        return result
 
-        #udp services
-        udpports = {}
+    def _processUdp(self, udpendpoint):
+        result = []
         for oid, value in udpendpoint.items():
             oidar = oid.split('.')
             port = 0
-            if port > maxport or port < 1: continue
 
             addr = ''
             if value.has_key('v4'): #ipv4 binding
-                port = int(oidar[16])
-                addr = '.'.join(oidar[12:16])
+                port = int(oidar[5])
+                addr = '.'.join(oidar[1:5])
             elif value.has_key('v6'): #ipv4 binding
                 port = int(oidar[28])
                 addr = '.'.join(oidar[12:28])
@@ -108,21 +93,40 @@ class IpV6ServiceMap(SnmpPlugin):
                 # wha?
                 continue
 
-            om = udpports.get(port, None)
+            om = self.objectMap()
+            om.id = 'udp_%05d' % port
+            om.ipaddresses = [addr,]
+            om.protocol = 'udp'
+            om.port = port
+            om.monitor = False
+            om.setServiceClass = {'protocol': 'udp', 'port':port}
+            om.discoveryAgent = self.name()
+            result.append(om)
+        return result
+
+    def _reduceByPort(self, maxport, rm, maps):
+        ports = {}
+        for map in maps:
+            addr = map.ipaddresses[0]
+            port = map.port
+            if port > maxport: continue
+            om = ports.get(port, None)
             if om:
                 if not addr in om.ipaddresses:
                     om.ipaddresses.append(addr)
             else:
-                om = self.objectMap()
-                om.id = 'udp_%05d' % port
-                om.ipaddresses = [addr,]
-                om.protocol = 'udp'
-                om.port = port
-                om.monitor = False
-                om.setServiceClass = {'protocol': 'udp', 'port':port}
-                om.discoveryAgent = self.name()
-                udpports[port] = om
-                rm.append(om)
+                ports[port] = map
+                rm.append(map)
+        
+    
+    def process(self, device, results, log):
+        """collect snmp information from this device"""
+        log.info('processing %s for device %s', self.name(), device.id)
+        getdata, tabledata = results
+        tcplisten = tabledata.get("tcplisten")
+        udpendpoint = tabledata.get("udpendpoint")
+        rm = self.relMap()
+        maxport = getattr(device, 'zIpServiceMapMaxPort', 1024)
+        self._reduceByPort(maxport, rm, self._processTcp(tcplisten))
+        self._reduceByPort(maxport, rm, self._processUdp(udpendpoint))
         return rm
-
-
