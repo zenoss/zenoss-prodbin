f011509b6f9af1e2d87f3ef9e83c6ef75dc5810d
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-03 23:49:43 +0000

    * Refs #2303: Better component path calculation
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22323 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/EventManagerBase.py
+++ Products/ZenEvents/EventManagerBase.py
@@ -660,48 +660,51 @@ class EventManagerBase(ZenModelRM, ObjectCache, DbAccessBase):
         def getcompfrombrains(id):
             for comp in brains:
                 compid = comp.getPrimaryId.split('/')[-1]
-                if compid==id: return comp.getPrimaryId, comp.meta_type
-            return None, None
+                if compid==id: 
+                    return comp.getPrimaryId, comp.meta_type, comp.getPath()
+            return None, None, None
+        devurl = device.getPrimaryUrlPath()
+        ostab = devurl.rstrip('/') + '/os'
         for event in self.getEventListME(device):
             if not len(event.component): continue
-            id, metatype = getcompfrombrains(event.component)
-            if id is None or metatype is None: 
-                id, metatype = event.component, 'Other'
+            id, metatype, url = getcompfrombrains(event.component)
+            if id is None or metatype is None or url is None: 
+                id, metatype, url = event.component, 'Other', devurl + '/os'
             tally = resultdict.setdefault(metatype, 
                             {'sev':event.severity, 
-                             'components': {id: (event.severity, 1, id)}})
+                             'components': {id: (event.severity, 1, id, ostab)}})
             tally.setdefault('sev', event.severity)
-            tally.setdefault('components', {id: (event.severity, 0, id)})
+            tally.setdefault('components', {id: (event.severity, 0, id, ostab)})
             if tally['sev'] < event.severity: tally['sev'] = event.severity
-            comp = tally['components'].setdefault(id, (event.severity, 0, id))
+            comp = tally['components'].setdefault(id, (event.severity, 0, 
+                                                       id, ostab))
             comptotal = tally['components'][id][1]
             compsev = tally['components'][id][0]
             newsev = compsev
             if event.severity > compsev: newsev = event.severity
-            tally['components'][id] = (newsev, comptotal+1, id)
+            tally['components'][id] = (newsev, comptotal+1, id, url)
         r = resultdict
-        categorysort = [(r[x]['sev'],len(r[x]['components']), x,
+        categorysort = [(r[x]['sev'], len(r[x]['components']), x,
                          r[x]['components'].values()) for x in r if r[x]]
         categorysort.sort(); categorysort.reverse()
         categorysort.extend([(0, 0, x, []) for x in r if not r[x]])
-        devurl = device.getPrimaryUrlPath()
         for bunch in categorysort:
             catsev, catnum, catname, comps = bunch
-            catlink = '%s/os' % devurl
+            catlink = ostab
             catcolor = colors[catsev]
-            evpill = evpilltemplate % (catcolor, 
-                                    device.getPrimaryUrlPath())
+            evpill = evpilltemplate % (catcolor, device.getPrimaryUrlPath())
             if catnum: evpill = ''
             devdata.append((linktemplate % (catlink, '', catname), evpill))
             comps.sort()
             for comp in comps:
-                compsev, compnum, complink = comp
+                compsev, compnum, complink, compurl = comp
                 compcolor = colors[compsev]
                 if not complink.startswith('/zport'): 
                     compname = complink
-                    complink = comp.getURL().rstrip('/') + '/viewEvents'
+                    complink = devurl.rstrip('/') + '/viewEvents'
                 else: 
                     compname = complink.split('/')[-1]
+                    complink = compurl.rstrip('/') + '/viewEvents'
                 if not compname: continue
                 compname = "<strong>%s</strong>" % compname
                 devdata.append(
