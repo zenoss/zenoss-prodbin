5b31a57af2315803839cc210672b1b499b8dca4c
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-04 17:33:05 +0000

    fixes #2136  * Adding create_sample_config.sh
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22336 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ bin/create_sample_config.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2008, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+#
+# If no configuration file exists, create a sample one.
+#
+
+. $ZENHOME/bin/zenfunctions
+
+cd daemons
+for daemonctl in * ; do
+     PRGNAME=`awk -F= '/^PRGNAME=/ { print $2; }' $daemonctl | sed -e 's/\.py//' | tail -1`
+
+     #echo "PRGNAME $PRGNAME"
+
+     #
+     # Expand any variables used to define the config file
+     #
+     eval CFGFILE=`awk -F= '/^CFGFILE=/ { print $2; }' $daemonctl | tail -1`
+     #echo "CFGFILE $CFGFILE"
+
+     #
+     # Some sanity checks...
+     #
+     if [ -z "$CFGFILE" ] ; then
+         echo "No configuration file for $PRGNAME found"
+         continue
+
+     elif [ $CFGFILE == '/dev/null' ] ; then
+         echo "No configuration file for $PRGNAME"
+         continue
+     fi
+
+     #
+     # Don't overwrite possibly user-customized files!
+     #
+     if [ ! -f $CFGFILE ] ; then
+        echo "Creating configuration file for $PRGNAME..."
+        `$PRGNAME genconf > $CFGFILE`
+     fi
+done
+
+#
+# Don't cause troubles if one of the above steps had issues.
+#
+exit 0
