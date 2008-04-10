859bdf3dd1901b6a888dc691de931ceea8d3ace5
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-10 21:27:41 +0000

    * Commit to test the buildbot
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22423 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/tests/testGoogleMaps.py
+++ Products/ZenModel/tests/testGoogleMaps.py
@@ -48,6 +48,9 @@ class TestGoogleMaps(ZenModelBaseTest):
         self.dmd.clearGeocodeCache()
         self.assert_(not self.dmd.geocache)
 
+    def testDummyTest(self):
+        self.assert_("IAN IS JUST TESTING THE BUILDBOT"==False)
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
