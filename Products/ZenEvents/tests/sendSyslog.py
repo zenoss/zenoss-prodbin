1d9e6907ca123bfc5ec764ec9aba26cee41fa6d7
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-05-01 15:25:18 +0000

    * change default send address to localhost
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@22616 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/tests/sendSyslog.py
+++ Products/ZenEvents/tests/sendSyslog.py
@@ -18,4 +18,4 @@ now = time.strftime('%b %d %T %Z %Y')
 m = '''<165>%s localhost myproc[10]: %%%% It's time to make the do-nuts.  %%%%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%%%''' % now
 now = time.strftime('%b %d %T')
 m = '''<165>%s foo@win2003 myproc[10]: %%%% It's time to make the do-nuts.  %%%%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%%%''' % now
-s.sendto(m, ('192.168.1.102', 514) )
+s.sendto(m, ('127.0.0.1', 514) )
