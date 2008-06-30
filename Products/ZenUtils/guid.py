a2e7dd65d0b56961006d26578d30b9d10be8f516
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-06-30 16:16:03 +0000

    * don't use socket to get the IP address of the box we run on when calculating the machine part of the guid.  We now lookup the mac address using ifconfig.  * also made several optimizations and simplifications and made it less likely that a colission would happen
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@23062 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/guid.py
+++ Products/ZenUtils/guid.py
@@ -10,49 +10,58 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-import math, sys, time, random, threading, socket, os
+"""
+Generate a globally unique id that is used for events.  The id has 4 parts:
+ * mac address (or random number) - 6 hex characters
+ * the process id - 4 hex characters
+ * time - 8 hex characters - refreshed when counter wraps
+ * decrementing number - 7 hex characters
+"""
+import math, sys, time, random, threading, os, re
 
-make_hexip = lambda parts: ''.join(["%02x" % long(i) for i in parts.split('.')])
+def _getmac():
+    """
+    try to get one of the mac addresses from our interfaces.  if we fail get a
+    random number.  we limit the return value to 8 characters because we used
+    to use ip addresses that were this long.  the length is important because
+    it determins the length of the final guid.
+    """
+    output = ""
+    if os.path.exists('/sbin/ifconfig'):
+        output = os.popen('/sbin/ifconfig -a').read()
+    elif os.path.exists('/usr/sbin/ifconfig'):
+        output = os.popen('/usr/sbin/ifconfig -a').read()
+    m = re.search("([0-9a-fA-F]{1,2}:){5}[0-9a-fA-F]{1,2}",output)
+    if m:
+        mac = m.group(0)
+        mac = mac.replace(':', '')
+    else:
+        mac = "%06x" % random.Random().randrange(0,0xffffff)
+    return mac[-6:]
 
+_mac = _getmac()
 _pid = '%04x' % os.getpid()
-_pid = _pid[-3:]
-  
-ip = ''
-lock = threading.RLock()
-try:  # only need to get the IP addresss once
-  ip = socket.getaddrinfo(socket.gethostname(),0)[-1][-1][0]
-  hexip = make_hexip(ip)
-except: 
-    # if we don't have an ip, default to someting in the 10.x.x.x private range
-    ip = '10'
-    rand = random.Random()
-    for i in range(3):
-        ip += '.' + str(rand.randrange(1, 0xff))  
-        # might as well use IPv6 range if we're making it up
-    hexip = make_hexip(ip)
+_base = _mac + _pid
+
+def _getstarttime():
+    return "%08x" % time.time()
+_starttime = _getstarttime()
 
 def _unique_sequencer():
-    _XUnit_sequence = sys.maxint
-
+    _XUnit_sequence = 0xfffffff
     while 1:
         yield _XUnit_sequence
         _XUnit_sequence -= 1
         if _XUnit_sequence <= 0:
-            _XUnit_sequence = sys.maxint
+            global _starttime 
+            _starttime = _getstarttime()
+            _XUnit_sequence = 0xfffffff
 _uniqueid = _unique_sequencer()
 
+lock = threading.RLock()
 def generate():
     lock.acquire()
     try:
-        frac, secs = math.modf(time.time())
-        days, remain = divmod(secs, 86400)
-        id = _uniqueid.next()
-        return  "%s%s%s%s%s" % (hexip,
-                                hex(int(days))[2:],
-                                hex(int(remain))[2:],
-                                hex(id)[-4:],
-                                _pid)
+        return  "%s%s%07x" % (_base, _starttime, _uniqueid.next())
     finally:
         lock.release()
-
-del ip
