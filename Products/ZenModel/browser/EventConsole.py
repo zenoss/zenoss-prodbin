2d6501c1b89968d21e7ddf4b160a6667286d4207
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-12-02 15:40:36 +0000

    Quiet pyflakes
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@24616 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/browser/EventConsole.py
+++ Products/ZenModel/browser/EventConsole.py
@@ -13,7 +13,7 @@
 
 import urllib
 from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.ZenUtils.json import json
 from Products.ZenUtils.Utils import unused
 
