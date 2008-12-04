df78c9daa88c2010002c7d96e863f35dbf3d8f86
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-12-04 16:56:37 +0000

    * Refs #3987: demotes 'Loading collector plugins' to debug. Reviewed by Jason
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@24667 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/Plugins.py
+++ Products/DataCollector/Plugins.py
@@ -11,7 +11,8 @@
 #
 ###########################################################################
 
-__doc__= """Load plugins from standard locations and from ZenPacks
+__doc__= """Plugins
+Load plugins from standard locations and from ZenPacks
 """
 
 from Products.ZenUtils.Utils import importClass, zenPath
@@ -24,7 +25,11 @@ log = logging.getLogger('zen.plugins')
 
 _pluginskip = ("CollectorPlugin.py", "DataMaps.py")
 def _plfilter(f):
-    """Return a filtered list of plugins
+    """
+    Return a filtered list of plugins
+
+    @param f: plugin name
+    @type f: string
     """
     return (f.endswith(".py")   and
             not f.startswith('.') and
@@ -32,12 +37,20 @@ def _plfilter(f):
             not f.startswith("_") and
             not f in _pluginskip)
 
+
 class pluginImportError(exceptions.ImportError):
-    """Capture extra data from pluing exceptions
+    """
+    Capture extra data from plugin exceptions
     """
 
     def __init__(self, plugin='', traceback='' ):
-        """Initializer
+        """
+        Initializer
+
+        @param plugin: plugin name
+        @type plugin: string
+        @param traceback: traceback from an exception
+        @type traceback: traceback object
         """
         self.plugin = plugin
         self.traceback = traceback
@@ -48,24 +61,36 @@ class pluginImportError(exceptions.ImportError):
 
 from twisted.spread import pb
 class PluginLoader(pb.Copyable, pb.RemoteCopy):
-    """Class to load plugins
+    """
+    Class to load plugins
     """
 
     def __init__(self, package, modpath):
-        """Initializer
+        """
+        Initializer
+
+        @param package: package path where the plugins may be located
+        @type package: string
+        @param modpath: plugin path inside of the package
+        @type modpath: string
         """
         self.package = package
         self.modpath = modpath
 
 
     def pluginName(self):
-        """Return the name of the plugin
+        """
+        Return the name of the plugin
+
+        @return: name of the plugin
+        @rtype: string
         """
         return self.modpath.split('plugins.').pop()
 
 
     def create(self):
-        """Load and compile the code contained in the given plugin
+        """
+        Load and compile the code contained in the given plugin
         """
 
         moduleName = self.modpath.split('.')[0]
@@ -92,10 +117,16 @@ pb.setUnjellyableForClass(PluginLoader, PluginLoader)
 
 
 def _loadPluginDir(pdir):
-    """Load the Zenoss default collector plugins
+    """
+    Load the Zenoss default collector plugins
+
+    @param pdir: plugin path parent directory
+    @type pdir: string
+    @return: list of loadable plugins
+    @rtype: list
     """
     collectorPlugins = []
-    log.info("Loading collector plugins from: %s", pdir)
+    log.debug("Loading collector plugins from: %s", pdir)
     lpdir = len(pdir)+1
     for path, dirname, filenames in os.walk(pdir):
         path = path[lpdir:]
@@ -108,14 +139,21 @@ def _loadPluginDir(pdir):
                     collectorPlugins.append( this_plugin )
             except ImportError:
                 log.exception("Problem loading plugin:%s" % modpath)
+
     return collectorPlugins
 
 
 
 def loadPlugins(dmd):
-    """Load plugins from the Zenoss plugin directory and from the plugin
+    """
+    Load plugins from the Zenoss plugin directory and from the plugin
     directory from each ZenPack.
     Returns them as a list of PluginLoader instances.
+
+    @param dmd: Device Management Database (DMD) reference
+    @type dmd: dmd object
+    @return: list of loadable plugins
+    @rtype: list
     """
     plugins = [x for x in sys.modules if x.startswith("plugins")]
     PDIR = os.path.join(os.path.dirname(__file__), "plugins")
