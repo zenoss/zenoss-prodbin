cd6d3418ed3c2c2794b62822a5894837edf30e27
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-12-12 14:39:08 +0000

    * Fixes #3263: updated MailProcessor and created unit-test. Reviewed by ecn
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@24827 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/MailProcessor.py
+++ Products/ZenEvents/MailProcessor.py
@@ -10,7 +10,10 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-#! /usr/bin/env python 
+
+__doc__ = """MailProcessor
+Base class module that other servers will subclass.
+"""
 
 import email, socket, rfc822, types
 import calendar
@@ -25,10 +28,15 @@ log = logging.getLogger("zen.mail")
 
 
 class MailEvent(Event):
+    """
+    Defaults for events created by the processor
+    """
     agent="zenmail"
     eventGroup="mail" 
 
 
+# The following is copied from the Python standard library
+# examples page: http://docs.python.org/library/datetime.html
 ZERO = timedelta(0)
 
 # A class building tzinfo objects for fixed-offset time zones.
@@ -53,38 +61,56 @@ class FixedOffset(tzinfo):
 
 
 class MessageProcessor(object):
-    '''Base class for parsing email messages that are retrieved via POP or
-    received via SMTP.'''
+    """
+    Base class for parsing email messages that are retrieved via POP or
+    received via SMTP.
+    """
 
     def __init__(self, zem, defaultSeverity = 2): 
+        """
+        Initializer
+
+        @param zem: class that provides sendEvent() method
+        @type zem: Zenoss event manager object
+        @param defaultSeverity: severity level to use if we can't figure one out
+        @type defaultSeverity: integer
+        """
         self.zem = zem
         self.eventSeverity = defaultSeverity
 
+
     def process(self, messageStr):
+        """
+        Convert an e-mail message into a Zenoss event.
+
+        @param messageStr: e-mail message
+        @type messageStr: string
+        """
         message = email.message_from_string(messageStr)
         self.message = message
         
         fromAddr = message.get('From')
-        log.debug("process(): From addr is %s" % fromAddr)
+        log.debug("Found a 'from' address of %s" % fromAddr)
         if not fromAddr or fromAddr.find('@') == -1:
-            log.warning("cannot process message with from address %s" % fromAddr)
+            log.warning("Unable to process the 'from' address %s -- ignoring mail" \
+                        % fromAddr)
             return
         
         fromAddr = message.get('From').split('@')[1].rstrip('>')
         fromAddr = fromAddr.split(' ')[0]
-        log.debug("process(): From addr after processing is %s" % fromAddr)
+        log.debug("The from address after processing is '%s'" % fromAddr)
         try:
             fromIp = socket.gethostbyname(fromAddr)
         except socket.gaierror:
             fromIp = None
-            log.info('hostname lookup failed for host: %s' % fromAddr)
+            log.info('Hostname lookup failed for host: %s' % fromAddr)
 
         subject = message.get('Subject')
 
-        # this is tricky...  date comes in with an offset value that
+        # This is tricky...  date comes in with an offset value that
         # represents the number of seconds of difference between the
-        # parsed timezone and UTC.  the events database wants all time
-        # as seconds since the epoch and treats it as UTC.  as a
+        # parsed timezone and UTC.  The events database wants all time
+        # as seconds since the epoch and treats it as UTC.  As a
         # result we have to use the datetime class to do the
         # conversion because the functions in the time module do all
         # kinds of covnersions "to be helpful"
@@ -92,14 +118,14 @@ class MessageProcessor(object):
 
         offset_secs = t[-1]
 
-        # convert the offset in seconds to minutes.  calendar wants minutes
+        # Convert the offset in seconds to minutes.  calendar wants minutes
         offset_mins = offset_secs / 60
         tz = FixedOffset(offset_mins, "Unknown")
 
-        # construct dt using the date and time as well as the timezone 
+        # Construct dt using the date and time as well as the timezone 
         dt = datetime(t[0], t[1], t[2], t[3], t[4], t[5], 0, tz)
         secs = calendar.timegm(dt.utctimetuple())
-        log.info('timestamp of event (should be in UTC): %f' % secs)
+        log.info('Timestamp of the event (should be in UTC): %f' % secs)
 
         event = MailEvent(device=fromAddr, rcvtime=secs)
         if fromIp:
@@ -118,18 +144,34 @@ class MessageProcessor(object):
         self.enrich(event, subject)
 
         event = self.buildEventClassKey(event)
-        log.info('sending event...')
+        log.info('Sending event...')
         self.zem.sendEvent(event.__dict__)
-        log.info('event sent.')
+        log.info('The event has been sent.')
 
 
     def enrich(self, event, subject):
+        """
+        Sanitize the event facility and severity fields.
+
+        @param event: event
+        @type event: simple class
+        @param subject: e-mail subject (unused)
+        @type subject: string
+        """
         unused(subject)
         event.facility = "unknown"
         event.severity = self.eventSeverity
         
 
     def buildEventClassKey(self, evt):
+        """
+        Set the Zenoss eventClassKey
+
+        @param evt: event
+        @type evt: simple class
+        @return: modified event
+        @rtype: simple class
+        """
         if hasattr(evt, 'eventClassKey') or hasattr(evt, 'eventClass'):
             return evt
         elif hasattr(evt, 'ntevid'):
@@ -139,26 +181,44 @@ class MessageProcessor(object):
         if hasattr(evt, 'eventClassKey'): 
             log.debug("eventClassKey=%s", evt.eventClassKey)
         else:
-            log.debug("no eventClassKey assigned")
+            log.debug("No eventClassKey assigned")
         return evt
 
 
+
 class POPProcessor(MessageProcessor):
-    '''Extension point for messages received via POP.  If you need to
+    """
+    Extension point for messages received via POP.  If you need to
     override the behavior of "process" you should do so by
-    implementing it here.'''
+    implementing it here.
+    """
 
     def __init__(self, zem, defaultSeverity = 2): 
+        """
+        Initializer
+
+        @param zem: class that provides sendEvent() method
+        @type zem: Zenoss event manager object
+        @param defaultSeverity: severity level to use if we can't figure one out
+        """
         MessageProcessor.__init__(self, zem, defaultSeverity)
+
 
 
 class MailProcessor(MessageProcessor):
-    '''Extension point for messages received via SMTP.  If you need to
+    """
+    Extension point for messages received via SMTP.  If you need to
     override the behavior of "process" you should do so by
-    implementing it here.'''
+    implementing it here.
+    """
 
     def __init__(self, zem, defaultSeverity = 2): 
-        MessageProcessor.__init__(self, zem, defaultSeverity)
-    
+        """
+        Initializer
 
-    
+        @param zem: class that provides sendEvent() method
+        @type zem: Zenoss event manager object
+        @param defaultSeverity: severity level to use if we can't figure one out
+        """
+        MessageProcessor.__init__(self, zem, defaultSeverity)
+
---
+++ Products/ZenEvents/tests/testMailProcessor.py
@@ -0,0 +1,202 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2008, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import logging
+from unittest import TestSuite, makeSuite, TestCase
+
+from Products.ZenEvents.MailProcessor import MessageProcessor
+
+
+# Notes:
+#
+# * Tab characters in any e-mail need to be escaped or the
+#   Subversion hook that checks for tab characters will go
+#   insane with rage.
+
+class TestMailProcessor(TestCase):
+
+    def setUp(self):
+        class zemclass: pass
+        self.zem = zemclass()
+        self.zem.sendEvent = self.sendEvent
+        self.sent = {}
+
+        logging.disable(logging.INFO)
+        logging.disable(logging.WARN)
+        logging.disable(logging.ERROR)
+        logging.disable(logging.CRITICAL)
+
+
+    def sendEvent(self, evt):
+        "Fakeout sendEvent() method"
+        self.sent = evt
+
+
+    def checkMsgToEvent(self, testName ):
+        "Check that an e-mail was translated as expected"
+        self.sent = {}
+
+        mp = MessageProcessor( self.zem, 2 )
+
+        message = mail_data[testName]['msg']
+        mp.process( message )
+
+        expected_evt = mail_data[testName]['event']
+
+        # Actually check for expected values
+        for field, value in expected_evt.items():
+            self.assertEquals( self.sent.get( field, '' ), value )
+
+
+    def testValidEmail(self):
+        "Sanity check of e-mail processor"
+        self.checkMsgToEvent( "simple" )
+
+
+    def testInvalidEmail(self):
+        "Simple sanity checks of e-mail processor for negative testing"
+        self.sent = {}
+        mp = MessageProcessor( self.zem, 2 )
+
+        self.assertRaises( TypeError, mp.process, None )
+
+        mp.process( '' )
+        self.assertEquals( len(self.sent), 0 )
+
+
+    def testNormalEmail(self):
+        "Test of a valid e-mail with extra headers"
+        self.checkMsgToEvent( "normal" )
+
+
+    def testUTC(self):
+        "Sanity check of e-mails using UTC"
+        # Hmmmm....  can't reproduce ticket http://dev.zenoss.org/trac/ticket/3263
+        # I *think* it got fixed in Python
+        self.checkMsgToEvent( "utc" )
+
+
+
+def test_suite():
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestMailProcessor))
+    return suite
+
+# =================  Start of sample mail messages  =======================
+
+# Format of a mail_data entry:
+#  "name": {
+#       'msg': """E-mail text here""",
+#       'event': { expected_field1: expected_value1, ... },
+#  },
+
+mail_data = {
+
+   "simple": {
+       'msg': """Return-Path: example_account@example.com
+Date: Thu, 11 Dec 2008 17:18:26 -0500 (EST)
+From: Test Account <example_account@example.com>
+To: Test Account <example_account@example.com>
+Message-ID: <375303282.37931229033906513.JavaMail.root@zimbra1>
+Subject: Test event message subject
+
+Simple body message
+""",
+       'event': {
+           'severity': 2, 'facility': 'unknown', 'eventClassKey': '',
+           '_action': 'status', 'component': '',
+           'summary': 'Test event message subject',
+           '_clearClasses': [], 'eventKey': '',
+           'device': 'example.com',
+           'message': 'Simple body message\n',
+           '_fields': [], 'ipAddress': '208.77.188.166'
+  }
+},
+
+# ----------------------------
+
+   "utc": {
+       'msg': """Return-Path: example_account@example.com
+Date: Thu, 11 Dec 2008 17:18:26 0000 (UTC)
+From: Test Account <example_account@example.com>
+To: Test Account <example_account@example.com>
+Message-ID: <375303282.37931229033906513.JavaMail.root@zimbra1>
+Subject: Test event message subject
+
+Simple body message
+""",
+       'event': {
+           'severity': 2, 'facility': 'unknown', 'eventClassKey': '',
+           '_action': 'status', 'component': '', 'summary': 'Test event message subject',
+           '_clearClasses': [], 'eventKey': '', 'device': 'example.com',
+           'message': 'Simple body message\n', '_fields': [],
+           'ipAddress': '208.77.188.166'
+  }
+},
+
+# ----------------------------
+
+   "normal": {
+       'msg': """Return-Path: example_account@example.com
+Received: from zimbra1.example.com (LHLO zimbra1.example.com) (74.63.38.26) by
+ zimbra1.example.com with LMTP; Thu, 11 Dec 2008 17:18:31 -0500 (EST)
+Received: from localhost (localhost.localdomain [127.0.0.1])
+\tby zimbra1.example.com (Postfix) with ESMTP id 3D8BF2128004
+\tfor <example_account@example.com>; Thu, 11 Dec 2008 17:18:31 -0500 (EST)
+X-Virus-Scanned: amavisd-new at zimbra1.example.com
+X-Spam-Flag: NO
+X-Spam-Score: -1.431
+X-Spam-Level: 
+X-Spam-Status: No, score=-1.431 tagged_above=-10 required=4 tests=[AWL=0.191,
+\tBAYES_00=-2.599, RCVD_IN_SORBS_DUL=0.877, RDNS_NONE=0.1]
+Received: from zimbra1.example.com ([127.0.0.1])
+\tby localhost (zimbra1.example.com [127.0.0.1]) (amavisd-new, port 10024)
+\twith ESMTP id 4FrgxduZh0Vm; Thu, 11 Dec 2008 17:18:26 -0500 (EST)
+Received: from zimbra1.example.com (zimbra1.example.com [74.63.38.26])
+\tby zimbra1.example.com (Postfix) with ESMTP id 861612128001
+\tfor <example_account@example.com>; Thu, 11 Dec 2008 17:18:26 -0500 (EST)
+Date: Thu, 11 Dec 2008 17:18:26 -0500 (EST)
+From: Test Account <example_account@example.com>
+To: Test Account <example_account@example.com>
+Message-ID: <375303282.37931229033906513.JavaMail.root@zimbra1>
+Subject: Test event message subject
+MIME-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 7bit
+X-Originating-IP: [68.146.118.240]
+X-Mailer: Zimbra 5.0.11_GA_2695.RHEL5_64 (ZimbraWebClient - FF3.0 (Mac)/5.0.11_GA_2695.RHEL5_64)
+
+Body part of the message
+Lots of
+  lovely
+    text
+
+# special case: '.' on at the beginning of a line all on its own 
+#    may terminate message processing, depending on the agent
+
+.
+
+  Spread out all over the e-mail.
+
+sig
+""",
+       'event': {
+           'severity': 2, 'facility': 'unknown', 'eventClassKey': '',
+           '_action': 'status', 'component': '', 'summary': 'Test event message subject',
+           '_clearClasses': [], 'eventKey': '', 'device': 'example.com',
+           'message': "Body part of the message\nLots of\n  lovely\n    text\n\n# special case: '.' on at the beginning of a line all on its own \n#    may terminate message processing, depending on the agent\n\n.\n\n  Spread out all over the e-mail.\n\nsig\n",
+           '_fields': [], 'ipAddress': '208.77.188.166'
+  }
+},
+
+} # End of mail data
