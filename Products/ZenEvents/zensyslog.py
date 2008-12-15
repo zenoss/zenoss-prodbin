512f5c1bb891db653d950de0e945ab424ed34a6c
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-12-15 18:49:11 +0000

    * Refs #4139: fix comments. Reviewed by ecn
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@24857 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/zensyslog.py
+++ Products/ZenEvents/zensyslog.py
@@ -1,8 +1,9 @@
-#! /usr/bin/env python 
-###########################################################################
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# ##########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2007, Zenoss Inc.
+# Copyright (C) 2008, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published by
@@ -10,17 +11,16 @@
 #
 # For complete information please visit: http://www.zenoss.com/oss/
 #
-###########################################################################
+# ##########################################################################
 
-__doc__='''zensyslog
+__doc__ = """zensyslog
 
 Turn syslog messages into events.
 
-$Id$
-'''
+"""
 
 import time
-import socket 
+import socket
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
@@ -40,59 +40,74 @@ try:
 except socket.error:
     pass
 
+
 class ZenSyslog(DatagramProtocol, EventServer):
+    """
+    ZenSyslog
+    """
 
     name = 'zensyslog'
     SYSLOG_DATE_FORMAT = '%b %d %H:%M:%S'
     SAMPLE_DATE = 'Apr 10 15:19:22'
 
-
     def __init__(self):
         EventServer.__init__(self)
-        if not self.options.useFileDescriptor and \
-               self.options.syslogport < 1024:
+        if not self.options.useFileDescriptor\
+             and self.options.syslogport < 1024:
             self.openPrivilegedPort('--listen', '--proto=udp',
-                '--port=%s:%d' % (self.options.listenip,
-                self.options.syslogport))
+                                    '--port=%s:%d'
+                                     % (self.options.listenip,
+                                    self.options.syslogport))
         self.changeUser()
         self.minpriority = self.options.minpriority
         self.processor = None
 
         if self.options.logorig:
             import logging
-            self.olog = logging.getLogger("origsyslog")
+            self.olog = logging.getLogger('origsyslog')
             self.olog.setLevel(20)
             self.olog.propagate = False
-            lname = zenPath("log/origsyslog.log")
+            lname = zenPath('log/origsyslog.log')
             hdlr = logging.FileHandler(lname)
-            hdlr.setFormatter(logging.Formatter("%(message)s"))
+            hdlr.setFormatter(logging.Formatter('%(message)s'))
             self.olog.addHandler(hdlr)
         if self.options.useFileDescriptor is not None:
             self.useUdpFileDescriptor(int(self.options.useFileDescriptor))
         else:
             reactor.listenUDP(self.options.syslogport, self,
-                interface=self.options.listenip)
+                              interface=self.options.listenip)
 
 
     def expand(self, msg, client_address):
-        """expands a syslog message into a string format suitable for writing
+        """
+        Expands a syslog message into a string format suitable for writing
         to the filesystem such that it appears the same as it would
-        had the message been logged by the syslog daemon."""
+        had the message been logged by the syslog daemon.
         
+        @param msg: syslog message
+        @type msg: string
+        @param client_address: IP info of the remote device (ipaddr, port)
+        @type client_address: tuple of (string, number)
+        @return: message
+        @rtype: string
+        """
+
         # pri := facility * severity
         stop = msg.find('>')
-        
+
         # check for a datestamp.  default to right now if date not present
         start = stop + 1
         stop = start + len(ZenSyslog.SAMPLE_DATE)
         dateField = msg[start:stop]
         try:
-            date = time.strptime(dateField, ZenSyslog.SYSLOG_DATE_FORMAT)
+            date = time.strptime(dateField,
+                                 ZenSyslog.SYSLOG_DATE_FORMAT)
             year = time.localtime()[0]
-            date = (year,) + date[1:]
+            date = (year, ) + date[1:]
             start = stop + 1
-        except  ValueError:
-            # date not present, so use today's date
+        except ValueError:
+
+        # date not present, so use today's date
             date = time.localtime()
 
         # check for a hostname.  default to localhost if not present
@@ -110,11 +125,19 @@ class ZenSyslog(DatagramProtocol, EventServer):
         prettyTime = time.strftime(ZenSyslog.SYSLOG_DATE_FORMAT, date)
         message = '%s %s %s' % (prettyTime, hostname, body)
         return message
-        
+
 
     def datagramReceived(self, msg, client_address):
-        """Consume the network packet"""
-        ipaddr, port = client_address
+        """
+        Consume the network packet
+        
+        @param msg: syslog message
+        @type msg: string
+        @param client_address: IP info of the remote device (ipaddr, port)
+        @type client_address: tuple of (string, number)
+        """
+
+        (ipaddr, port) = client_address
         if self.options.logorig:
             if self.options.logformat == 'human':
                 message = self.expand(msg, client_address)
@@ -123,66 +146,101 @@ class ZenSyslog(DatagramProtocol, EventServer):
 
             self.olog.info(message)
 
-        asyncNameLookup(ipaddr).addBoth(self.gotHostname, (msg, ipaddr, time.time()))
+        asyncNameLookup(ipaddr).addBoth(self.gotHostname, (msg, ipaddr,
+                time.time()))
+
 
     def configure(self):
+        """
+        Initialize the daemon
+        
+        @return: Twisted deferred object
+        @rtype: Twisted deferred object
+        """
+
         def inner(driver):
+            """
+            Generator function to gather zProperites and then initialize.
+            
+            @param driver: driver
+            @type driver: string
+            @return: Twisted deferred object
+            @rtype: Twisted deferred object
+            """
+
             yield EventServer.configure(self)
             driver.next()
-            self.log.info("Fetching the default syslog priority")
+            self.log.info('Fetching the default syslog priority')
             yield self.model().callRemote('getDefaultPriority')
-            self.processor = SyslogProcessor(self.sendEvent, 
-                                             self.options.minpriority,
-                                             self.options.parsehost,
-                                             self.options.monitor,
-                                             driver.next())
-            self.log.info("Configuration finished")
+            self.processor = SyslogProcessor(self.sendEvent,
+                    self.options.minpriority, self.options.parsehost,
+                    self.options.monitor, driver.next())
+            self.log.info('Configuration finished')
+
         return drive(inner)
 
 
     def gotHostname(self, response, data):
-        "send the resolved address, if possible, and the event via the thread"
-        msg, ipaddr, rtime = data
+        """
+        Send the resolved address, if possible, and the event via the thread
+        
+        @param response: Twisted response
+        @type response: Twisted response
+        @param data: (msg, ipaddr, rtime)
+        @type data: tuple of (string, string, datetime object)
+        """
+
+        (msg, ipaddr, rtime) = data
         host = ipaddr
         if not isinstance(response, failure.Failure):
             host = response
         if self.processor:
             self.processor.process(msg, ipaddr, host, rtime)
 
+
     def buildOptions(self):
+        """
+        Command-line options
+        """
+
         EventServer.buildOptions(self)
-        self.parser.add_option('--dmdpath',
-            dest='dmdpath', default="/zport/dmd",
-            help="zope path to our dmd /zport/dmd")
-        self.parser.add_option('--parsehost',
-            dest='parsehost', action="store_true",  default=False,
-            help="try to parse the hostname part of a syslog HEADER")
-        self.parser.add_option('--stats',
-            dest='stats', action="store_true",  default=False,
-            help="print stats to log every 2 secs")
-        self.parser.add_option('--logorig',
-            dest='logorig', action="store_true",  default=False,
-            help="log the original message")
-        self.parser.add_option('--logformat',
-            dest='logformat', default="human",
-            help="human (/var/log/messages) or raw (wire)")
-        self.parser.add_option('--minpriority',
-            dest='minpriority', default=6, type="int",
-            help="Minimum priority that syslog will accecpt")
-        self.parser.add_option('--heartbeat',
-            dest='heartbeat', default=60,
-            help="Number of seconds between heartbeats")
-        self.parser.add_option('--syslogport',
-            dest='syslogport', default=SYSLOG_PORT, type='int',
-            help="Port number to use for syslog events")
-        self.parser.add_option('--listenip',
-            dest='listenip', default='0.0.0.0',
-            help="IP address to listen on. Default is 0.0.0.0")
+        self.parser.add_option('--dmdpath', dest='dmdpath',
+                               default='/zport/dmd',
+                               help='Zope path to our DMD /zport/dmd')
+        self.parser.add_option('--parsehost', dest='parsehost',
+                               action='store_true', default=False,
+                               help='Try to parse the hostname part of a syslog HEADER'
+                               )
+        self.parser.add_option('--stats', dest='stats',
+                               action='store_true', default=False,
+                               help='Print statistics to log every 2 secs')
+        self.parser.add_option('--logorig', dest='logorig',
+                               action='store_true', default=False,
+                               help='Log the original message')
+        self.parser.add_option('--logformat', dest='logformat',
+                               default='human',
+                               help='Human-readable (/var/log/messages) or raw (wire)'
+                               )
+        self.parser.add_option('--minpriority', dest='minpriority',
+                               default=6, type='int',
+                               help='Minimum priority message that zensyslog will accept'
+                               )
+        self.parser.add_option('--heartbeat', dest='heartbeat',
+                               default=60,
+                               help='Number of seconds between heartbeats'
+                               )
+        self.parser.add_option('--syslogport', dest='syslogport',
+                               default=SYSLOG_PORT, type='int',
+                               help='Port number to use for syslog events'
+                               )
+        self.parser.add_option('--listenip', dest='listenip',
+                               default='0.0.0.0',
+                               help='IP address to listen on. Default is 0.0.0.0'
+                               )
         self.parser.add_option('--useFileDescriptor',
-                               dest='useFileDescriptor',
-                               type='int',
-                               help="Read from an existing connection rather opening a new port.",
-                               default=None)
+                               dest='useFileDescriptor', type='int',
+                               help='Read from an existing connection rather opening a new port.'
+                               , default=None)
 
 
 if __name__ == '__main__':
