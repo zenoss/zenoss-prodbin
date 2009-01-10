0d18bb699cb12cac9dfd3955600150e5d6eb6c8a
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-01-10 23:39:52 +0000

    Added relation building
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@25126 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/usermsgsportlet.py
+++ Products/ZenModel/migrate/usermsgsportlet.py
@@ -19,6 +19,8 @@ class UserMsgsPortlet(Migrate.Step):
     version = Migrate.Version(2, 4, 0)
 
     def cutover(self, dmd):
+        for us in dmd.ZenUsers.getAllUserSettings():
+            us.buildRelations()
         zpm = dmd.zport.ZenPortletManager
         register_default_portlets(zpm)
 
