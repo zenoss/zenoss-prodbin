36ae173d40c3a9fab7f7a42d89b4fc9b4f93e397
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-01-15 13:13:37 +0000

    fixes #3567  * zenperfsnmp now collects other devices while waiting extra cycles for an unresponsive device  * reviewed by kells
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@25187 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/zenperfsnmp.py
+++ Products/ZenRRD/zenperfsnmp.py
@@ -23,6 +23,7 @@ import time
 import logging
 log = logging.getLogger("zen.zenperfsnmp")
 
+import copy
 from sets import Set
 import cPickle
 
@@ -51,6 +52,7 @@ unused(PerformanceConfig)
 MAX_OIDS_PER_REQUEST = 40
 MAX_SNMP_REQUESTS = 20
 DEVICE_LOAD_CHUNK_SIZE = 2
+CYCLES_TO_WAIT_FOR_RESPONSE = 2
 
 def makeDirs(dir):
     """
@@ -157,58 +159,107 @@ class Status:
     """
     Keep track of the status of many parallel requests
     """
-    _success = _fail = 0
-    _startTime = _stopTime = 0.0
-    _deferred = None
 
     def __init__(self):
         """
         Initializer
         """
-        self._allDevices = Set()
+        self.reset()
+        
+    def reset(self):
+        """
+        Reset instance variables to intial values
+        """
+        
+        # Number of requests issued this cycle that succeeded
+        self._numSucceeded = 0
+        
+        # Number of requests issued prior to this cycle that responded
+        # successfully this cycle
+        self._numPrevSucceeded = 0
+        
+        # Number of requests issued this cycle that failed
+        self._numFailed = 0
+        
+        # Number of requests issues prior to this cycle that responded
+        # unsuccessfully this cycle
+        self._numPrevFailed = 0
+        
+        # timestamp when this cycle was started
+        self._startTime = 0
+        
+        # timestamp when this cycle completed
+        self._stopTime = 0
+        
+        # This deferred gets triggered when this cycle completes.
+        self._deferred = defer.Deferred()
+        
+        # Set of device names that should be queried this cycle
+        self._devicesToQueryThisCycle = Set()
+        
+        # Names of devices with outstanding queries when this cycle was
+        # started.  Values are the timestamp of when the query was made.
+        self._prevQueriesAndAges = {}
+        
+        # Set of device names still to be queried this cycle.  Initially
+        # this is the same as _devicesToQueryThisCycle but devices are
+        # popped from it as they are queried
+        self._queue = Set()
+        
+        # Set of devices names that have reported back this cycle.  The query 
+        # may have been from this cycle or a previous cycle
         self._reported = Set()
-    
-    def start(self, devices):
+
+            
+    def start(self, devicesToQuery, prevQueriesAndAges):
         """
         Record our start time, and return a deferred for our devices
+
+        @type devicesToQuery: iterable
+        @param devicesToQuery: names of devices to poll
+        @type prevQueriesAndAges: dict
+        @param prevQueriesAndAges: devices with outstanding quests
+        @return: deferred
         """
-        self._allDevices = Set(devices)
-        self._reported = Set()
+        self.reset()
         self._startTime = time.time()
-        self._deferred = defer.Deferred()
+                
+        # If there is an outstanding request for a device we don't want to
+        # issue another
+        self._devicesToQueryThisCycle = \
+                Set(devicesToQuery) - Set(prevQueriesAndAges.keys())
+        self._prevQueriesAndAges = prevQueriesAndAges
+        self._queue = copy.copy(self._devicesToQueryThisCycle)
         self._checkFinished()           # count could be zero
         return self._deferred
 
 
-    def record(self, name, successOrFailure):
+    def record(self, name, success):
         """
         Record success or failure
+        
+        @type name: string
+        @param name: name of device reporting results
+        @type success: boolean
+        @param success: True if query succeeded, False otherwise. 
         """
         if name in self._reported:
             log.error("Device %s is reporting more than once", name)
             return
         self._reported.add(name)
-        if successOrFailure:
-            self.success()
+        if name in self._devicesToQueryThisCycle:
+            if success:
+                self._numSucceeded += 1
+            else:
+                self._numFailed += 1
+            self._checkFinished()
+        elif name in self._prevQueriesAndAges:
+            if success:
+                self._numPrevSucceeded += 1
+            else:
+                self._numPrevFailed += 1
         else:
-            self.fail()
-
-        
-    def success(self, *unused):
-        """
-        Record a successful result
-        """
-        self._success += 1
-        self._checkFinished()
-
-
-    def fail(self, *unused):
-        """
-        Record a failed operation
-        """
-        self._fail += 1
-        self._checkFinished()
-
+            log.debug('Unrecognized device reporting: %s' % name)
 
     def _checkFinished(self):
         """
@@ -218,43 +269,87 @@ class Status:
             self._stopTime = time.time()
             if not self._deferred.called:
                 self._deferred.callback(self)
-        log.info("total=%d good=%d bad=%d time=%f", * self.stats())
+        info = self.stats()
+        log.info(
+            'success:%d ' % info['numSucceeded'] +
+            'fail:%d ' % info['numFailed'] +
+            'pending:%d ' % info['numInProcess'] +
+            'todo:%d ' % info['queueSize'])
 
 
     def finished(self):
         """
-        Determine if we have finished everything
+        Determine if we have finished, disregarding devices that were queried
+        in a previous cycle and still haven't reported back.
         """
-        return self.outstanding() <= 0
-
+        return len(self.inQueue()) == 0 and len(self.inProcess()) == 0
 
     def stats(self):
         """
-        Return a tuple with:
-         * number of devices
-         * number of successful operations
-         * number of failed operations
-         * amount of time since our last start() operation
+        Return a dictionary with stats for this cycle:
+            numSucceeded - queries made this cycle and reported back success
+            numPrevSucceeded - queries from a prev cycle reported back success
+            numFailed - queries made this cycle reported back failure
+            numPrevFailed - queries made prev cycle reported back failure
+            startTime - timestamp when this cycle started
+            stopTime - timestamp when this cycle stopped
+            age - time cycle took to run or current age (if still running)
+            queueSize - num of devices not queried yet
+            numInProcess - num queried this cycle not reported back yet
+            numPrevInProcess - num queried prev cycle still not reported back
+            numReported - number reported back from this or previous cycles
         """
-        age = self._stopTime - self._startTime
-        if not self._startTime:
-            age = 0.0
-        elif not self.finished():
-            age = time.time() - self._startTime
-        return (len(self._allDevices), self._success, self._fail, age)
+        return dict(
+            numSucceeded = self._numSucceeded,
+            numPrevSucceeded = self._numPrevSucceeded,
+            numFailed = self._numFailed,
+            numPrevFailed = self._numPrevFailed,
+            startTime = self._startTime,
+            stopTime = self._stopTime,
+            age = self._stopTime and (self._stopTime - self._startTime) \
+                    or (time.time() - self._startTime),
+            queueSize = len(self._queue),
+            numInProcess = len(self.inProcess()),
+            numPrevInProcess = len(self.prevInProcess()),
+            numReported = len(self._reported)
+            )
 
+    def inProcess(self):
+        """
+        Return the name of the devices that have been queried this cycle
+        but from whom no response has been received.
+        """
+        return self._devicesToQueryThisCycle - self._reported - self._queue
 
-    def outstanding(self):
+    def prevInProcess(self):
         """
-        Return the number of unfinished operations
+        Return the names of the devices that were queried prior to this cycle
+        and have not yet reported.
         """
-        return len(self.outstandingNames())
+        return Set(self._prevQueriesAndAges.keys()) - self._reported
 
-    def outstandingNames(self):
+    def inQueue(self):
         """
-        Return the set of unfinished operations
+        Return the names of the devices that have yet to be queried.
         """
-        return self._allDevices - self._reported
+        return self._queue
+
+    def popDevice(self):
+        """
+        Pop a device to be queried from the queue
+        """
+        return self._queue.pop()
+
+    def getQueryAges(self):
+        """
+        Return a dictionary with the device and age of each query from this
+        or previous cycles that has not yet responeded.
+        """
+        waiting = dict([(d,a) for (d, a) in self._prevQueriesAndAges.items()
+                        if d not in self._reported])
+        waiting.update(dict([(d, self._startTime)
+                            for d in self.inProcess()]))
+        return waiting
 
 
 class SnmpStatus:
@@ -328,7 +423,6 @@ class zenperfsnmp(SnmpDaemon):
         SnmpDaemon.__init__(self, 'zenperfsnmp', noopts)
         self.status = None
         self.proxies = {}
-        self.queryWorkList = Set()
         self.unresponsiveDevices = Set()
         self.snmpOidsRequested = 0
 
@@ -410,16 +504,6 @@ class zenperfsnmp(SnmpDaemon):
         Add an RRD file to the list of files to be removed
         """
         self.oldFiles.add(fullPath)
-
-
-    def maybeQuit(self):
-        """
-        Stop if all performance has been fetched, and we aren't cycling
-        """
-        if not self.options.daemon and \
-           not self.options.cycle:
-            reactor.callLater(0, reactor.stop)
-
 
     def remote_updateDeviceList(self, devices):
         """
@@ -688,28 +772,56 @@ class zenperfsnmp(SnmpDaemon):
         """
         Periodically fetch the performance values from all known devices
         """
-        
         self.heartbeat()
 
-        if self.status and not self.status.finished():
-            _, _, _, age = self.status.stats()
-            self.log.warning("There are still %d devices to query",
-                             self.status.outstanding())
-            self.log.warning("Problem devices: %r",
-                             list(self.status.outstandingNames()))
-            if age < self.perfsnmpCycleInterval * 2:
-                self.log.warning("Waiting one more cycle period")
-                return
-            self.log.warning("Devices status is not clearing.  Restarting.")
+        # If self.status then this is not the first cycle
+        if self.status:
+            # pending is a dictionary of devices that haven't responded
+            # and the values are the timestamps of each query
+            pending = self.status.getQueryAges()
+            # doneWaiting is the devices from pending that have exceeded
+            # the time we're willing to wait for them
+            doneWaiting = []
+            for device, age in pending.items():
+                beenWaiting = time.time() - age
+                if beenWaiting >= self.perfsnmpCycleInterval \
+                        * CYCLES_TO_WAIT_FOR_RESPONSE:
+                    self.log.error('No response from %s after %s cycles.'
+                        % (device, CYCLES_TO_WAIT_FOR_RESPONSE))
+                    doneWaiting.append(device)
+                else:
+                    self.log.warning('Continuing to wait for response from'
+                        ' %s after %s seconds' % (device, beenWaiting))
+            for device in doneWaiting:
+                del pending[device]
+            
+            # Report on devices that we didn't have the time to get to
+            queued = self.status.inQueue()
+            if queued:
+                self.log.error('%s devices still queued at end of cycle and did'
+                    ' not get queried.' % len(queued))
+                self.log.debug('Devices not queried: %s' % ', '.join(queued))
+            
+            # If the previous cycle did not complete then report stats
+            # (If it did complete then stats were reports by a
+            # callback on the deferred.)
+            if not self.status._stopTime:
+                self.reportRate()
+        else:
+            pending = {}
 
-        self.queryWorkList =  Set(self.proxies.keys())
-        self.queryWorkList -= self.unresponsiveDevices
+        devicesToQuery =  Set(self.proxies.keys())
+        # Don't query devices that can't be pinged
+        devicesToQuery -= self.unresponsiveDevices
+        # Don't query devices we're still waiting for responses from
+        devicesToQuery -= Set(pending.keys())
         self.status = Status()
-        d = self.status.start(self.queryWorkList)
+        d = self.status.start(devicesToQuery, pending)
         d.addCallback(self.reportRate)
         for unused in range(MAX_SNMP_REQUESTS):
-            if not self.queryWorkList: break
-            d = self.startReadDevice(self.queryWorkList.pop())
+            if not len(self.status.inQueue()):
+                break
+            d = self.startReadDevice(self.status.popDevice())
 
             def printError(reason):
                 """
@@ -727,17 +839,32 @@ class zenperfsnmp(SnmpDaemon):
         """
         Finished reading all the devices, report stats and maybe stop
         """
-        total, success, failed, runtime = self.status.stats()
+        info = self.status.stats()
         oidsRequested, self.snmpOidsRequested = self.snmpOidsRequested, 0
+
+        self.log.info('******** Cycle completed ********')
         self.log.info("Sent %d OID requests", oidsRequested)
-        self.log.info("collected %d of %d devices in %.2f" %
-                      (success, total, runtime))
+        self.log.info('Queried %d devices' % (info['numSucceeded'] \
+                        + info['numFailed'] + info['numInProcess']))
+        self.log.info('  %s in queue still unqueried' % info['queueSize'])
+        self.log.info('  Successes: %d  Failures: %d  Not reporting: %d' %
+                (info['numSucceeded'], info['numFailed'], info['numInProcess']))
+        self.log.info('Waited on %d queries from previous cycles.' %
+                (info['numPrevSucceeded'] + info['numPrevFailed'] \
+                + info['numPrevInProcess']))
+        self.log.info('  Successes: %d  Failures: %d  Not reporting: %d' %
+                (info['numPrevSucceeded'], info['numPrevFailed'],
+                info['numPrevInProcess']))
+        self.log.info('Cycle lasted %.2f seconds' % info['age'])
+        self.log.info('*********************************')
         
         cycle = self.perfsnmpCycleInterval
         self.sendEvents(
-            self.rrdStats.gauge('success', cycle, success) + 
-            self.rrdStats.gauge('failed', cycle, failed) +
-            self.rrdStats.gauge('cycleTime', cycle, runtime) +
+            self.rrdStats.gauge('success', cycle,
+                info['numSucceeded'] + info['numPrevSucceeded']) + 
+            self.rrdStats.gauge('failed', cycle,
+                info['numFailed'] + info['numPrevFailed']) +
+            self.rrdStats.gauge('cycleTime', cycle, info['age']) +
             self.rrdStats.counter('dataPoints', cycle, self.rrd.dataPoints) +
             self.rrdStats.gauge('cyclePoints', cycle, self.rrd.endCycle())
             )
@@ -893,8 +1020,8 @@ class zenperfsnmp(SnmpDaemon):
             for doomed in Set(proxy.oidMap.keys()) - Set(oids):
                 self.badOid(deviceName, doomed)
 
-        if self.queryWorkList:
-            self.startReadDevice(self.queryWorkList.pop())
+        if self.status.inQueue():
+            self.startReadDevice(self.status.popDevice())
 
         if successCount and len(updates) > 0:
             successPercent = successCount * 100 / len(updates)
