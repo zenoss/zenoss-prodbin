bd300343205e083fb9a2770e6ab68df1f774fe14
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-03-26 19:36:46 +0000

    Added unit test for setAqProperty method. Reviewed by EAD.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@26271 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenModel/tests/testDeviceComponent.py
@@ -0,0 +1,68 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+import time
+
+from DateTime import DateTime
+
+from Acquisition import aq_base
+from Products.ZenModel.Exceptions import *
+
+from ZenModelBaseTest import ZenModelBaseTest
+from Products.ZenModel.IpService import IpService
+
+
+class TestDeviceComponent(ZenModelBaseTest):
+
+    def setUp(self):
+        ZenModelBaseTest.setUp(self)
+        self.dev = self.dmd.Devices.createInstance("testdev")
+        tmpo = IpService('ipsvc')
+        self.dev.os.ipservices._setObject('ipsvc',tmpo)
+        tmpo.port = 121
+        tmpo.protocol = 'tcp'
+        self.ipsvc = self.dev.os.ipservices()[0]
+
+        self.sc = self.dmd.Services.manage_addIpServiceClass('tcp_00121')
+        self.sc.addServiceKey('tcp_00121')
+        self.ipsvc.setServiceClass({'protocol':'tcp','port':121})
+
+    def test_setAqProperty(self):
+
+        self.sc.zFailSeverity = 2
+
+        self.ipsvc.setAqProperty('zFailSeverity', 2, 'int')
+        self.assertEqual(self.ipsvc.hasProperty('zFailSeverity'), False)
+
+        self.ipsvc.setAqProperty('zFailSeverity', 5, 'int')
+        self.assertEqual(aq_base(self.ipsvc).zFailSeverity, 5)
+
+        self.ipsvc.setAqProperty('zFailSeverity', 3, 'int')
+        self.assertEqual(aq_base(self.ipsvc).zFailSeverity, 3)
+
+        self.ipsvc.setAqProperty('zFailSeverity', 2, 'int')
+        self.assertEqual(self.ipsvc.hasProperty('zFailSeverity'), False)
+
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestDeviceComponent))
+    return suite
+
+if __name__=="__main__":
+    framework()
