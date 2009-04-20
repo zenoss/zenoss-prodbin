c64a4401cf635619a3ae06c12c65ce929a2cd631
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-04-20 03:27:35 +0000

    Bump version for BC release candidate
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@26635 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -10,4 +10,4 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-VERSION="2.3.298"
+VERSION="2.4.0"
