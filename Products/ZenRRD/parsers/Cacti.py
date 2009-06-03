a5d3fbb4cc573309b69957e5d1f85f14dbfeee55
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-06-03 17:18:52 +0000

    * Fixes #4945: merge from branch, add unit tests, support Cacti single data point. Reviewed by JP
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@27333 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenRRD/parsers/Cacti.py
+++ Products/ZenRRD/parsers/Cacti.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008, Zenoss Inc.
+# Copyright (C) 2008, 2009 Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published by
@@ -11,5 +11,65 @@
 #
 ###########################################################################
 
-from Products.ZenRRD.parsers.Auto import Auto
-class Cacti(Auto): pass
+import re
+# how to parse each value from a Cacti plugin
+CacParser = re.compile(r"""([^ :']+|'(.*)'+):([-0-9.]+)""")
+
+from Products.ZenUtils.Utils import getExitMessage
+from Products.ZenRRD.CommandParser import CommandParser
+
+class Cacti(CommandParser):
+
+    def processResults(self, cmd, result):
+        output = cmd.result.output
+        output = output.split('\n')[0].strip()
+        exitCode = cmd.result.exitCode
+        severity = cmd.severity
+        if output.find('|') >= 0:
+            msg, values = output.split('|', 1)
+            msg, values = output, ''
+
+        elif CacParser.search(output):
+            msg, values = '', output
+
+        elif len(cmd.points) == 1:
+            # Special case for plugins that only return one datapoint
+            try:
+                number = float(output)
+                result.values.append( (cmd.points[0], number) )
+                msg, values = '', output
+            except:
+                msg, values = output, ''
+
+        else:
+            msg, values = output, ''
+
+        msg = msg.strip() or 'Cmd: %s - Code: %s - Msg: %s' % (
+            cmd.command, exitCode, getExitMessage(exitCode))
+        if exitCode == 0:
+            severity = 0
+        elif exitCode == 2:
+            severity = min(severity + 1, 5)
+        result.events.append(dict(device=cmd.deviceConfig.device,
+                                  summary=msg,
+                                  severity=severity,
+                                  message=msg,
+                                  performanceData=values,
+                                  eventKey=cmd.eventKey,
+                                  eventClass=cmd.eventClass,
+                                  component=cmd.component))
+
+        for value in values.split():
+            parts = CacParser.match(value)
+
+            if not parts: continue
+            label = parts.group(1).replace("''", "'")
+            try:
+                value = float(parts.group(3))
+            except:
+                value = 'U'
+            for dp in cmd.points:       # FIXME: linear search
+                if dp.id == label:
+                    result.values.append( (dp, value) )
+                    break
+
--- Products/ZenRRD/tests/testParsers.py
+++ Products/ZenRRD/tests/testParsers.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008, Zenoss Inc.
+# Copyright (C) 2008, 2009 Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published by
@@ -17,11 +17,11 @@ from Products.ZenRRD.tests.BaseParsersTestCase import Object
 from Products.ZenRRD.CommandParser import ParsedResults
 
 
-class TestPsParser(BaseTestCase):
-    """
-    A one-off test of the ps parser that does not use the data files.
-    """
+class TestParsers(BaseTestCase):
     def testPs(self):
+        """
+        A one-off test of the ps parser that does not use the data files.
+        """
         deviceConfig = Object()
         deviceConfig.device = 'localhost'
         cmd = Object()
@@ -80,9 +80,80 @@ class TestPsParser(BaseTestCase):
             else:
                 raise AssertionError("unexpected event")
 
+    def testCacti(self):
+        deviceConfig = Object()
+        deviceConfig.device = 'localhost'
+        cmd = Object()
+        cmd.deviceConfig = deviceConfig
+        p1 = Object()
+        p1.id = 'cacti_single_result'
+        p1.data = dict(processName='someJob a b c',
+                       ignoreParams=False,
+                       alertOnRestart=True,
+                       failSeverity=3)
+        cmd.points = [p1]
+        cmd.result = Object()
+        cmd.result.output = "77"
+        cmd.result.exitCode = 2
+        cmd.severity = 2
+        cmd.command = "testCactiPlugin"
+        cmd.eventKey = "cactiKey"
+        cmd.eventClass = "/Cmd"
+        cmd.component = "zencommand"
+        results = ParsedResults()
+        from Products.ZenRRD.parsers.Cacti import Cacti
+        parser = Cacti()
+        parser.processResults(cmd, results)
+        self.assertEquals( len(results.values), 1)
+        self.assertEquals(77,  int(results.values[0][1]))
+
+        # Now test multiple data points
+        p2 = Object()
+        p2.id = 'cacti_multi_result'
+        p2.data = dict(processName='someJob a b c',
+                       ignoreParams=False,
+                       alertOnRestart=True,
+                       failSeverity=3)
+        cmd.points.append( p2 )
+        cmd.result.output = "cacti_single_result:77 cacti_multi_result:21"
+        results = ParsedResults()
+        parser.processResults(cmd, results)
+        self.assertEquals( len(results.values), 2)
+        values = map(lambda x: x[1], results.values)
+        self.assertTrue(77.0 in values)
+        self.assertTrue(21.0 in values)
+
+    def testNagios(self):
+        deviceConfig = Object()
+        deviceConfig.device = 'localhost'
+        cmd = Object()
+        cmd.deviceConfig = deviceConfig
+        p1 = Object()
+        p1.id = 'np1'
+        p1.data = dict(processName='someJob a b c',
+                       ignoreParams=False,
+                       alertOnRestart=True,
+                       failSeverity=3)
+        cmd.points = [p1]
+        cmd.parser = "Nagios"
+        cmd.result = Object()
+        cmd.result.output = "OK plugin | np1=77;;;"
+        cmd.result.exitCode = 2
+        cmd.severity = 2
+        cmd.command = "testNagiosPlugin"
+        cmd.eventKey = "nagiosKey"
+        cmd.eventClass = "/Cmd"
+        cmd.component = "zencommand"
+        results = ParsedResults()
+        from Products.ZenRRD.parsers.Nagios import Nagios
+        parser = Nagios()
+        parser.processResults(cmd, results)
+        self.assertEquals( len(results.values), 1)
+        self.assertEquals(77.0,  results.values[0][1])
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    suite.addTest(makeSuite(TestPsParser))
+    suite.addTest(makeSuite(TestParsers))
     return suite
