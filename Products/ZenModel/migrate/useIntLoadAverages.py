cf81404cbc91447d46f3cccacb6d574efebd090a
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-06-26 13:29:48 +0000

    Handle case of missing default device classes.
    Fixes #5246. Reviewed by ian.
    
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@27555 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/useIntLoadAverages.py
+++ Products/ZenModel/migrate/useIntLoadAverages.py
@@ -47,7 +47,7 @@ class UseIntLoadAverages(Migrate.Step):
                         '1.3.6.1.4.1.2021.10.1.3',
                         '1.3.6.1.4.1.2021.10.1.5')
 
-            except AttributeError:
+            except AttributeError, KeyError:
                 pass
             except NotFound:
                 log.info( ( 'Could not retrieve datasource at %s. ' +
