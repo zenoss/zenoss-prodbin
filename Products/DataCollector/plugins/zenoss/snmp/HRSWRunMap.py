f9ed0ff11fc9d8bf84cfd97a2ee074c6ea9cf497
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-07-14 16:55:57 +0000

    * Fixes #5307: merge from branch, update HRSWRunMap modeler plugin to handle edge cases. Reviewed by Ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@27697 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/zenoss/snmp/HRSWRunMap.py
+++ Products/DataCollector/plugins/zenoss/snmp/HRSWRunMap.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2007, Zenoss Inc.
+# Copyright (C) 2007, 2009 Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published by
@@ -11,15 +11,17 @@
 #
 ###########################################################################
 
-__doc__="""FileSystemMap
+__doc__="""HRSWRunMap
 
-FileSystemMap maps the interface and ip tables to interface objects
+HRSWRunMap maps the processes running on the system to OSProcess objects.
+Uses the HOST-RESOURCES-MIB OIDs.
 
-$Id: HRFileSystemMap.py,v 1.2 2004/04/07 16:26:53 edahl Exp $"""
+"""
 
-__version__ = '$Revision: 1.2 $'[11:-2]
+from Products.DataCollector.plugins.CollectorPlugin \
+    import SnmpPlugin, GetTableMap
 
-from CollectorPlugin import SnmpPlugin, GetTableMap
+HRSWRUNENTRY = '.1.3.6.1.2.1.25.4.2.1'
 
 class HRSWRunMap(SnmpPlugin):
 
@@ -37,23 +39,30 @@ class HRSWRunMap(SnmpPlugin):
          }
 
     snmpGetTableMaps = (
-        GetTableMap('hrSWRunEntry', '.1.3.6.1.2.1.25.4.2.1', columns),
+        GetTableMap('hrSWRunEntry', HRSWRUNENTRY, columns),
     )
 
     def process(self, device, results, log):
-        """collect snmp information from this device"""
-        log.info('processing %s for device %s', self.name(), device.id)
+        """
+        Process the SNMP information returned from a device
+        """
+        log.info('Processing %s for device %s', self.name(), device.id)
         getdata, tabledata = results
+        log.debug("%s tabledata = %s", device.id, tabledata)
         
         #get the SNMP process data
-        fstable = tabledata.get("hrSWRunEntry")
+        pidtable = tabledata.get("hrSWRunEntry")
+        if pidtable is None:
+            log.error("Unable to get data for %s from hrSWRunEntry %s"
+                          " -- skipping model", HRSWRUNENTRY, device.id)
+            return None
         
-        log.debug("=== process information received ===")
-        for p in fstable.keys():
-            log.debug("snmpidx: %s\tprocess: %s" % (p, fstable[p]))
+        log.debug("=== Process information received ===")
+        for p in sorted(pidtable.keys()):
+            log.debug("snmpidx: %s\tprocess: %s" % (p, pidtable[p]))
         
         rm = self.relMap()
-        for proc in fstable.values():
+        for proc in pidtable.values():
             om = self.objectMap(proc)
             ppath = getattr(om, '_procPath', False) 
             if ppath and ppath.find('\\') == -1:
@@ -63,6 +72,11 @@ class HRSWRunMap(SnmpPlugin):
                 continue
             om.parameters = getattr(om, 'parameters', '')
             rm.append(om)
-        return rm
 
+        if not rm:
+            log.warning("No process information from hrSWRunEntry %s",
+                        HRSWRUNENTRY)
+            return None
+
+        return rm
 
