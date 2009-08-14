5a9d438b891ba04d64a71fc528ac1c5df4e188c8
edahl <edahl@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-08-14 17:08:49 +0000

    very simple script to generate fake syslog events that zensendsyslog.py can use
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28025 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenEvents/zengensyslog.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+NUMB_EVENTS = 120
+
+evts = dict(
+# will hit default rules and be mapped to /Ingore
+#REPEAT = "<165> message repeated 4 times",
+
+# /Security/Login/Fail
+FAIL = "<165> dropbear[23]: exit before auth (user 'root', 3 fails): Max auth tries reached - user root",
+
+# /Security/Login/BadPass
+SSHBADPASS = "<165> ssh[3]: Failed password for user from 10.1.2.3 port 53529 ssh2",
+
+# Cisco Power Loss /HW/Power/PowerLoss (will clear)
+PLOSS = "<165>%C6KPWR-SP-4-PSFAIL: power supply 1 output failed",
+POK = "<165>%C6KPWR-SP-4-PSOK: power supply 1 turned on",
+
+)
+keys = evts.keys()
+import random
+for i in range(NUMB_EVENTS):
+    print evts[random.choice(keys)]
