877c711ca13077d748459691e13f2ef14825a6c5
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-08-28 13:09:32 +0000

    Added ability to link to a grid state. Reviewed by Chet.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28231 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/zenoss.js
+++ Products/ZenUI3/browser/resources/js/zenoss.js
@@ -457,6 +457,39 @@ Ext.onReady(function(){
             }
             return this.view;
         },
+        restoreURLState: function() {
+            var qs = window.location.search.replace(/^\?/, ''),
+                state = Ext.urlDecode(qs).state;
+            if (state) {
+                try {
+                    state = Ext.decode(Zenoss.util.base64.decode(state));
+                    this.applyState(state);
+                } catch(e) { noop(); }
+            }
+        },
+        clearURLState: function() {
+            var qs = window.location.search.replace(/^\?/, ''),
+                qs = Ext.urlDecode(qs);
+            if (qs.state) {
+                delete qs.state;
+                qs = Ext.urlEncode(qs);
+                if (qs) {
+                    window.location.search = '?' + Ext.urlEncode(qs);
+                } else {
+                    window.location.search = ''
+                }
+            }
+        },
+        getPermalink: function() {
+            var l = window.location,
+                path = l.protocol + '//' + l.host + l.pathname;
+                st = Zenoss.util.base64.encode(Ext.encode(this.getState()));
+            return path + '?state=' + st;
+        },
+        initState: function() {
+            Zenoss.FilterGridPanel.superclass.initState.apply(this, arguments);
+            this.restoreURLState();
+        },
         getState: function(){
             var val = Zenoss.FilterGridPanel.superclass.getState.call(this);
             var filterstate = this.getView().getState();
@@ -477,11 +510,13 @@ Ext.onReady(function(){
                     results[results.length] = Ext.decode(r);
                 });
                 var cm = new Ext.grid.ColumnModel(results);
+                this.store.sortInfo = this.store.defaultSort;
                 this.reconfigure(this.store, cm);
                 view.fitColumns();
                 view.showLoadMask(true);
-                view.updateLiveRows(this.rowIndex, true, true);
+                view.nonDisruptiveReset();
                 this.saveState();
+                this.clearURLState();
             }, this);
         },
         hideFilters: function() { this.getView().hideFilters(); },
@@ -683,6 +718,64 @@ Ext.onReady(function(){
             return name;
         }
     }
+
+    Zenoss.util.base64 = {
+        base64s : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+        encode: function(decStr){
+            if (typeof btoa === 'function') {
+                 return btoa(decStr);            
+            }
+            var base64s = this.base64s;
+            var bits;
+            var dual;
+            var i = 0;
+            var encOut = "";
+            while(decStr.length >= i + 3){
+                bits = (decStr.charCodeAt(i++) & 0xff) <<16 | 
+                       (decStr.charCodeAt(i++) & 0xff) <<8 | 
+                       decStr.charCodeAt(i++) & 0xff;
+                encOut += base64s.charAt((bits & 0x00fc0000) >>18) + 
+                          base64s.charAt((bits & 0x0003f000) >>12) + 
+                          base64s.charAt((bits & 0x00000fc0) >> 6) + 
+                          base64s.charAt((bits & 0x0000003f));
+            }
+            if(decStr.length -i > 0 && decStr.length -i < 3){
+                dual = Boolean(decStr.length -i -1);
+                bits = ((decStr.charCodeAt(i++) & 0xff) <<16) | (dual ? (decStr.charCodeAt(i) & 0xff) <<8 : 0);
+                encOut += base64s.charAt((bits & 0x00fc0000) >>18) + 
+                          base64s.charAt((bits & 0x0003f000) >>12) + 
+                          (dual ? base64s.charAt((bits & 0x00000fc0) >>6) : '=') + '=';
+            }
+            return(encOut);
+        },
+        decode: function(encStr){
+            if (typeof atob === 'function') {
+                return atob(encStr); 
+            }
+            var base64s = this.base64s;        
+            var bits;
+            var decOut = "";
+            var i = 0;
+            for(; i<encStr.length; i += 4){
+                bits = (base64s.indexOf(encStr.charAt(i)) & 0xff) <<18 | 
+                       (base64s.indexOf(encStr.charAt(i +1)) & 0xff) <<12 | 
+                       (base64s.indexOf(encStr.charAt(i +2)) & 0xff) << 6 | 
+                       base64s.indexOf(encStr.charAt(i +3)) & 0xff;
+                decOut += String.fromCharCode((bits & 0xff0000) >>16, 
+                                              (bits & 0xff00) >>8, bits & 0xff);
+            }
+            if(encStr.charCodeAt(i -2) == 61){
+                return(decOut.substring(0, decOut.length -2));
+            }
+            else if(encStr.charCodeAt(i -1) == 61){
+                return(decOut.substring(0, decOut.length -1));
+            }
+            else {
+                return(decOut);
+            }
+        }
+    }; 
+    
 
     /**
      * Zenoss date patterns and manipulations
