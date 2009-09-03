b679977de48effa7cd741d3925a035a949045e13
cgibbons <cgibbons@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-09-03 16:50:06 +0000

    move statistics back into core scheduler
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28294 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenCollector/scheduler.py
+++ Products/ZenCollector/scheduler.py
@@ -19,9 +19,14 @@ single device or other monitored object.
 """
 
 import logging
+import math
+import signal
+import time
 
 import zope.interface
+
 from twisted.internet import defer, reactor, task
+from twisted.python.failure import Failure
 
 from Products.ZenCollector.interfaces import IScheduler, IScheduledTask
 from Products.ZenCollector.tasks import TaskStates
@@ -31,6 +36,80 @@ from Products.ZenUtils.Utils import dumpCallbacks
 # creating a logging context for this module to use
 #
 log = logging.getLogger("zen.collector.scheduler")
+
+
+class StateStatistics(object):
+    def __init__(self, state):
+        self.state = state
+        self.reset()
+
+    def addCall(self, elapsedTime):
+        self.totalElapsedTime += elapsedTime
+        self.totalElapsedTimeSquared += elapsedTime ** 2
+        self.totalCalls += 1
+
+        if self.totalCalls == 1:
+            self.minElapsedTime = elapsedTime
+            self.maxElapsedTime = elapsedTime
+        else:
+            self.minElapsedTime = min(self.minElapsedTime, elapsedTime)
+            self.maxElapsedTime = max(self.maxElapsedTime, elapsedTime)
+
+    def reset(self):
+        self.totalElapsedTime = 0.0
+        self.totalElapsedTimeSquared = 0.0
+        self.totalCalls = 0
+        self.minElapsedTime = 0xffffffff
+        self.maxElapsedTime = 0
+
+    @property
+    def mean(self):
+        return float(self.totalElapsedTime) / float(self.totalCalls)
+
+    @property
+    def stddev(self):
+        if self.totalCalls == 1:
+            return 0
+        else:
+            # see http://www.dspguide.com/ch2/2.htm for stddev of running stats
+            return math.sqrt((self.totalElapsedTimeSquared - self.totalElapsedTime ** 2 / self.totalCalls) / (self.totalCalls - 1))
+
+
+class TaskStatistics(object):
+    def __init__(self, task):
+        self.task = task
+        self.totalRuns = 0
+        self.failedRuns = 0
+        self.missedRuns = 0
+        self.states = {}
+        self.stateStartTime = None
+
+    def trackStateChange(self, oldState, newState):
+        now = time.time()
+
+        # record how long we spent in the previous state, if there was one
+        if oldState is not None and self.stateStartTime:
+            # TODO: how do we properly handle clockdrift or when the clock
+            # changes, or is time.time() independent of that?
+            elapsedTime = now - self.stateStartTime
+            previousState = newState
+
+            if not self.states.has_key(oldState):
+                stats = StateStatistics(oldState)
+                self.states[oldState] = stats
+            stats = self.states[oldState]
+            stats.addCall(elapsedTime)
+
+        self.stateStartTime = now
+
+
+class CallableTaskFactory(object):
+    """
+    Our CallableTaskFactory just returns this module's CallableTask, instead
+    of the one in Core.
+    """
+    def getCallableTask(self, newTask, scheduler):
+        return CallableTask(newTask, scheduler)
 
 
 class CallableTask(object):
@@ -48,24 +127,27 @@ class CallableTask(object):
 
         self._scheduler = scheduler
         self.paused = False
+        self.taskStats = None
 
     def running(self):
         """
         Called whenever this task is being run.
         """
-        pass
+        self.taskStats.totalRuns += 1
 
     def finished(self, result):
         """
         Called whenever this task has finished.
         """
-        pass
+        if isinstance(result, Failure):
+            self.taskStats.failedRuns += 1
 
     def late(self):
         """
         Called whenever this task is late and missed its scheduled run time.
         """
-        pass
+        # TODO: report an event
+        self.taskStats.missedRuns += 1
 
     def __call__(self):
         if self.task.state is TaskStates.STATE_PAUSED and not self.paused:
@@ -132,6 +214,7 @@ class Scheduler(object):
         self._loopingCalls = {}
         self._tasks = {}
         self._taskCallback = {}
+        self._taskStats = {}
         self._callableTaskFactory = callableTaskFactory
 
     def addTask(self, newTask, callback=None, now=False):
@@ -180,13 +263,21 @@ class Scheduler(object):
         """
         Called whenever the scheduler adds a task.
         """
-        pass
+        task = taskWrapper.task
+
+        # watch the task's state attribute changes
+        task.attachAttributeObserver('state', self._taskStateChangeListener)
+
+        # create the statistics data for this task
+        self._taskStats[task.name] = TaskStatistics(task)
+        taskWrapper.taskStats = self._taskStats[task.name]
 
     def taskRemoved(self, taskWrapper):
         """
         Called whenever the scheduler removes a task.
         """
-        pass
+        task = taskWrapper.task
+        task.detachAttributeObserver('state', self._taskStateChangeListener)
 
     def taskPaused(self, taskWrapper):
         """
@@ -243,6 +334,81 @@ class Scheduler(object):
         if callable(self._taskCallback[taskName]):
             self._taskCallback[taskName](taskName=taskName)
 
-    def displayStatistics(self, verbose=False):
-        # no statistics in this scheduler
-        pass
+    def _taskStateChangeListener(self, observable, attrName, oldValue, newValue):
+        task = observable
+
+        log.debug("Task %s changing state from %s to %s", task.name, oldValue,
+                  newValue)
+
+        taskStat = self._taskStats[task.name]
+        taskStat.trackStateChange(oldValue, newValue)
+
+    def displayStatistics(self, verbose):
+        totalRuns = 0
+        totalFailedRuns = 0
+        totalMissedRuns = 0
+        totalTasks = 0
+        stateStats = {}
+
+        for taskWrapper in self._tasks.itervalues():
+            task = taskWrapper.task
+            taskStats = self._taskStats[task.name]
+
+            totalTasks += 1
+            totalRuns += taskStats.totalRuns
+            totalFailedRuns += taskStats.failedRuns
+            totalMissedRuns += taskStats.missedRuns
+
+            for state, stats in taskStats.states.iteritems():
+                if not stateStats.has_key(state):
+                    stateStats[state] = StateStatistics(state)
+                totalStateStats = stateStats[state]
+                totalStateStats.totalElapsedTime += stats.totalElapsedTime
+                totalStateStats.totalElapsedTimeSquared += stats.totalElapsedTimeSquared
+                totalStateStats.totalCalls += stats.totalCalls
+                totalStateStats.minElapsedTime = min(totalStateStats.minElapsedTime, stats.minElapsedTime)
+                totalStateStats.maxElapsedTime = max(totalStateStats.maxElapsedTime, stats.maxElapsedTime)
+
+        log.info("Tasks: %d Successful: %d Failed: %d Missed: %d",
+                 totalTasks, totalRuns, totalFailedRuns, totalMissedRuns)
+
+        if verbose:
+            buffer = "Task States Summary:\n"
+            buffer = self._displayStateStatistics(buffer, stateStats)
+
+            buffer += "\nTasks:\n"
+            for taskWrapper in self._tasks.itervalues():
+                task = taskWrapper.task
+                taskStats = self._taskStats[task.name]
+
+                buffer += "%s Current State: %s Successful: %d Failed: %d Missed: %d\n" \
+                    % (task.name, task.state, taskStats.totalRuns, 
+                       taskStats.failedRuns, taskStats.missedRuns)
+
+            buffer += "\nDetailed Task States:\n"
+            for taskWrapper in self._tasks.itervalues():
+                task = taskWrapper.task
+                taskStats = self._taskStats[task.name]
+
+                buffer = self._displayStateStatistics(buffer,
+                                             taskStats.states,
+                                             "%s " % task.name)
+                buffer += "\n"
+
+            log.info("Detailed Scheduler Statistics:\n%s", buffer)
+
+        # TODO: the above logic doesn't print out any stats for the 'current'
+        # state... i.e. enter the PAUSED state and wait there an hour, and 
+        # you'll see no data
+
+        # TODO: should we reset the statistics here, or do it on a periodic
+        # interval, like once an hour or day or something?
+
+    def _displayStateStatistics(self, buffer, stateStats, prefix=''):
+        for state, stats in stateStats.iteritems():
+            buffer += "%sState: %s Total: %d Total Elapsed: %.4f Min: %.4f Max: %.4f Mean: %.4f StdDev: %.4f\n" \
+                % (prefix,
+                   state, stats.totalCalls, stats.totalElapsedTime,
+                   stats.minElapsedTime, stats.maxElapsedTime, stats.mean,
+                   stats.stddev)
+        return buffer
