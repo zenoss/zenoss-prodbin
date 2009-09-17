467a112200233bd8eea917e7cc0487f7fae9314f
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-09-17 20:25:54 +0000

    * Fixes #5517: merge from branch, add XML export option to Event Console. Reviewed by Ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28530 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/eventconsole/export.py
+++ Products/ZenUI3/browser/eventconsole/export.py
@@ -1,3 +1,24 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+__doc__ = """export
+
+Given a list of events to export, format them
+appropriately and then return back a string
+"""
+
+import StringIO
+
 from Products.Five.browser import BrowserView
 
 from Products.ZenModel.ZenModelBase import ZenModelBase
@@ -10,76 +31,76 @@ class EventsExporter(BrowserView):
         body = self.request.form['body']
         state = unjson(body)
         type = state['type']
+
         # Get the events according to grid state
-        events = self._query(**state['params'])
+        fields, events = self._query(**state['params'])
+
         # Send the events to the appropriate formatting method
-        result = getattr(self, type)(state['params']['fields'], events)
+        ctype, filename, result = getattr(self, type)(fields, events)
+
+        # Set the headers appropriately
+        self.request.response.setHeader('Content-Type', ctype)
+        self.request.response.setHeader('Content-Disposition',
+                                        'attachment; filename=' + filename)
         return result
+
 
     def _query(self, fields, sort, dir, params=None):
         evutil = IEventManagerProxy(self)
         zem = evutil.event_manager()
-        if isinstance(params, basestring): params = unjson(params)
+        if isinstance(params, basestring):
+            params = unjson(params)
         if not params:
             params = {}
         args = dict(
-            resultFields=fields,
+            resultFields=['evid'],
             orderby="%s %s" % (sort, dir),
             filters=params
         )
-        data = zem.getEventListME(self.context, **args)
-        data = [evutil.extract_data_from_zevent(ev, fields) for ev in data]
-        return data
+        all_events = zem.getEventListME(self.context, **args)
+        # First item is the list of default fields
+        field_names = set(zem.getFieldList())
+        events = []
+        for evt in all_events:
+            evobj = zem.getEventDetailFromStatusOrHistory(evid=evt.evid)
+            evt_dict = dict(evobj.getEventFields())
+            evt_dict.update(dict(evobj.getEventDetails()))
+            field_names.update(evt_dict.keys())
+            events.append(evt_dict)
+            
+        return list(field_names), events
 
-    def csv(self, fieldsAndLabels, objects, out=None):
+
+    def csv(self, fields, events):
         import csv
-        import StringIO
-        if out:
-            buffer = out
-        else:
-            buffer = StringIO.StringIO()
-        fields = []
-        labels = []
-        if not fieldsAndLabels:
-            fieldsAndLabels = []
-        if not objects:
-            objects = []
-        for p in fieldsAndLabels:
-            if isinstance(p, tuple):
-                fields.append(p[0])
-                labels.append(p[1])
-            else:
-                fields.append(p)
-                labels.append(p)
+        buffer = StringIO.StringIO()
         writer = csv.writer(buffer)
-        writer.writerow(labels)
-        def getDataField(thing, field):
-            if isinstance(thing, dict):
-                value = thing.get(field, '')
-            elif isinstance(thing, list) or isinstance(thing, tuple):
-                value = thing[int(field)]
-            else:
-                value = getattr(thing, field, '')
-            if isinstance(value, ZenModelBase):
-                value = value.id
-            elif callable(value):
-                value = value()
-            if value == None:
-                value = ''
-            return str(value)
-        for o in objects:
-            writer.writerow([getDataField(o,f) for f in fields])
-        if out:
-            result = None
-        else:
-            result = buffer.getvalue()
 
-        # Set the headers appropriately
-        self.request.response.setHeader('Content-Type',
-                                        'application/vns.ms-excel')
-        self.request.response.setHeader('Content-Disposition',
-                                        'attachment; filename=events.csv')
-        return result
+        writer.writerow(fields)
 
-    def xml(self, parameters):
-        pass
+        for evt in events:
+            data = [str(evt.get(field, '')).replace('\n', ' ').strip() for field in fields]
+            writer.writerow(data)
+
+        return 'application/vns.ms-excel', 'events.csv', buffer.getvalue()
+
+
+    def xml(self, fields, events):
+        xml_output = StringIO.StringIO()
+
+        xml_output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        xml_output.write( "<events>\n" )
+
+        for evt in events:
+            xml_output.write('<event ')
+            for field, value in sorted(evt.items()):
+                name = str(field).replace('.', '__DOT__').replace(' ', '__SPACE__')
+                if name[0].isdigit():
+                    name = 'EVENT_PREFIX__' + name
+                xml_output.write('%s="%s" ' % (name, str(value)))
+            xml_output.write(' />\n')
+
+        xml_output.write( "</events>\n" )
+
+        return 'text/xml', 'events.xml', xml_output.getvalue()
+
