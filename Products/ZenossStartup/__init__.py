8312d8ee8ab6c9697f73b569ee9f74241f6e1c89
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-09-21 13:58:44 +0000

    Fixes #5567: Merging branch to trunk. Reviewed by Chad.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28568 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenossStartup/__init__.py
+++ Products/ZenossStartup/__init__.py
@@ -7,8 +7,14 @@
 import logging
 log = logging.getLogger()
 
-from Products.ZenUtils.PkgResources import pkg_resources
+import Products
+from Products.Five import zcml
 from Products.CMFCore.utils import ProductsPath
+from Products.ZenUtils.PkgResources import pkg_resources
+
+# Helpfully load basic directives packs will probably use
+zcml.load_config('meta.zcml', Products.Five)
+zcml.load_config('permissions.zcml', Products.ZenModel)
 
 # Iterate over all ZenPack eggs and load them
 for zpkg  in pkg_resources.iter_entry_points('zenoss.zenpacks'):
@@ -16,7 +22,16 @@ for zpkg  in pkg_resources.iter_entry_points('zenoss.zenpacks'):
         pkg_path = zpkg.load().__path__[0]
         if pkg_path not in ProductsPath:
             ProductsPath.insert(0, pkg_path)
-        __import__(zpkg.module_name)
+
+        fromlist = zpkg.module_name.split('.')[:-1]
+        module = __import__(zpkg.module_name, globals(), locals(), fromlist)
+
+        # Try to load the pack's configuration for Zope 3 goodness
+        try:
+            zcml.load_config('configure.zcml', module)
+        except IOError:
+            # No configure.zcml defined in this pack; move on
+            pass
+
     except Exception, e:
         log.exception(e)
-
