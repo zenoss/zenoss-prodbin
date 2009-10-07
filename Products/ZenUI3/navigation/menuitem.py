770e496fc53210dab563d174d1bf719f837753c7
buildbot <buildbot@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-10-07 18:46:05 +0000

    * Refs #5653: Fix gpl/copyright boilerplate for KC [products]
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@28811 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/navigation/menuitem.py
+++ Products/ZenUI3/navigation/menuitem.py
@@ -1,3 +1,15 @@
+###########################################################################
+#       
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
+#       
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#       
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 import zope.interface
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from Products.Five.viewlet import viewlet
--- Products/ZenUI3/zope2.py
+++ Products/ZenUI3/zope2.py
@@ -1,3 +1,14 @@
-
+###########################################################################
+#       
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
+#       
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#       
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
