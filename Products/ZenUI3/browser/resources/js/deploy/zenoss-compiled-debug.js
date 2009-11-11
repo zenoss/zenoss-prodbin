8d48f562b35f2d356388d07908b233db57657b46
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-11-11 20:42:31 +0000

    Updating compiled Ext files
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@29206 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/deploy/zenoss-compiled-debug.js
+++ Products/ZenUI3/browser/resources/js/deploy/zenoss-compiled-debug.js
@@ -1116,3 +1116,44 @@ Ext.onReady(function(){
     });
 
 });
+(function(){
+
+Ext.ns('Zenoss.i18n');
+
+// Provide a default; this gets filled in later when appropriate.
+Zenoss.i18n._data = {};
+
+Zenoss.i18n.translate = function(s, d) {
+    t = Zenoss.i18n._data[s];
+    return t ? t : (d ? d: s);
+}
+
+// Shortcut
+window._t = Zenoss.i18n.translate
+
+})();
+Ext.ns('Zenoss');
+
+/**
+ * @class Zenoss.HierarchyTreePanel
+ * @extends Ext.tree.TreePanel
+ * The primary way of navigating one or more hierarchical structures. A
+ * more advanced Subsections Tree Panel. Configurable as a drop target.
+ * Accepts array containing one or more trees (as nested arrays). In at
+ * least one case data needs to be asynchronous. Used on screens:
+ *   Device Classification Setup Screen
+ *   Devices
+ *   Device
+ *   Event Classification
+ *   Templates
+ *   Manufacturers
+ *   Processes
+ *   Services
+ *   Report List
+ * @constructor
+ */
+Zenoss.HierarchyTreePanel = Ext.extend(Ext.tree.TreePanel, {
+    
+}); // HierarchyTreePanel
+
+Ext.reg('HierarchyTreePanel', Zenoss.HierarchyTreePanel);
