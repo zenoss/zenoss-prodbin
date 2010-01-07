5c5132b42e418851a5aaf195b982a3c6d269e316
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-01-07 21:59:42 +0000

    initial work on the datasource grid panel. reviewed by Ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@29886 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenUI3/browser/resources/js/zenoss/datasource.js
@@ -0,0 +1,22 @@
+/*
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+*/
+
+Ext.onReady(function(){
+
+Ext.getCmp('center_detail_panel').add({
+    xtype: 'DatasourceGridPanel'
+});
+
+}); // Ext.onReady
