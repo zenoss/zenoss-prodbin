04ec566aa4ed015bab780b1c37721a3e3114b806
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-02-11 04:20:17 +0000

    Reversing accidental commit to trunk.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@30643 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/navigation/menuitem.py
+++ Products/ZenUI3/navigation/menuitem.py
@@ -39,7 +39,7 @@ class PrimaryNavigationMenuItem(viewlet.ViewletBase):
                                          self.__parent__)
         if sec:
             for v in sec.getViewletsByParentName(self.__name__):
-                if v.selected:
+                if requestURL.endswith(v.url):
                     return True
         return False
 
@@ -61,21 +61,12 @@ class SecondaryNavigationMenuItem(PrimaryNavigationMenuItem):
     zope.interface.implements(INavigationItem)
 
     parentItem = ""
-    subviews = ()
-
-    def update(self):
-        super(SecondaryNavigationMenuItem, self).update()
-        if isinstance(self.subviews, basestring):
-            self.subviews = [self.subviews]
 
     @property
     def selected(self):
         requestURL = self.request.getURL()
         if requestURL.endswith(self.url):
             return True
-        for url in self.subviews:
-            if requestURL.endswith(url):
-                return True
         return False
 
 
