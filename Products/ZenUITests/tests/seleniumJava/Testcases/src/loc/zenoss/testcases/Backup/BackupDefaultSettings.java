59852464e4d4c5fa6bd32cdd7a5a646c468b2939
emunoz <emunoz@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-08 23:17:22 +0000

    There are some Test Cases for Backup page
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@31994 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Backup/BackupDefaultSettings.java
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Backup/BackupDefaultSettings.java
@@ -18,7 +18,7 @@ import com.thoughtworks.selenium.SeleneseTestCase;
 public class BackupDefaultSettings {
 
 	private static DefaultSelenium sClient = null;
-
+	private SeleneseTestCase selenese = null;
 	private static int testCaseID = 1790;
 	private static String testCaseResult = "f"; //Fail by default
     
@@ -49,20 +49,53 @@ public class BackupDefaultSettings {
 	public void tearDown() throws Exception {
 	}
 	
-	
 	@Test
-	public void navigationSettings() throws Exception{
+	public void devicesVerified() throws Exception{
 		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
 		Thread.sleep(12000);*/	
-		sClient.open("/zport/dmd/Dashboard");
-		 sClient.waitForPageToLoad("30000");
-		sClient.open("/zport/dmd/backupInfo");
-		 sClient.waitForPageToLoad("30000");
-		 Thread.sleep(12000);
-		 sClient.click("manage_createBackup:method");
-		 sClient.waitForPageToLoad("250000");
-		 SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));
-		
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(5000);
+		sClient.open("/zport/dmd/itinfrastructure");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(8000);
+     	if(sClient.isTextPresent("test-win2003-1d.zenoss.loc")){
+     	 sClient.click("link=Devices");
+     	 sClient.waitForPageToLoad("30000"); 
+     	 sClient.click("link=Advanced");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("link=Settings");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("link=Backups");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("manage_createBackup:method");
+     	 sClient.waitForPageToLoad("30000");
+     	 SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully")); 
+     	 sClient.click("link=Devices");
+     	 sClient.waitForPageToLoad("30000"); 
+     	 sClient.click("link=Advanced");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("link=Settings");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("link=Backups");
+     	 sClient.waitForPageToLoad("30000");
+     	 sClient.click("fileNames:list");
+     	 sClient.click("//table[@id='ext-comp-1046']/tbody/tr[2]/td[2]/em");
+     	 sClient.click("manage_deleteBackups:method");
+     	 sClient.waitForPageToLoad("30000");
+		}else{
+			sClient.open("/zport/dmd/itinfrastructure");
+			sClient.waitForPageToLoad("30000");
+			Thread.sleep(12000);
+			sClient.click("//table[@id='adddevice-button']/tbody/tr[2]/td[2]/em/button[@id='ext-gen60']");
+	     	sClient.click("ext-gen141");
+	     	sClient.type("add-device-name", "test-win2003-1d.zenoss.loc");
+	     	sClient.click("ext-gen206");
+	     	sClient.click("//table[@id='ext-comp-1169']/tbody/tr[3]/td[2]");
+			
+			
+		}	
+	
 	}
 
 }
