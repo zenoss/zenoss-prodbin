55327a4e14a941dc7ed776051228a293d7fb5cfa
achaves <achaves@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-09 03:57:03 +0000

    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@31999 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/ITInfrastructure/Networks/NetworksManagement.java
@@ -0,0 +1,105 @@
+package loc.zenoss.testcases.ITInfrastructure.Networks;
+
+import loc.zenoss.Common;
+import loc.zenoss.TestlinkXMLRPC;
+import loc.zenoss.ZenossConstants;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+
+public class NetworksManagement {
+private static DefaultSelenium sClient = null;
+	
+	private static int testCaseID;
+	private static String testCaseResult; //Fail by default
+		
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,ZenossConstants.browser, ZenossConstants.testedMachine)  {
+        	public void open(String url) {
+        		commandProcessor.doCommand("open", new String[] {url,"true"});
+        	}
+        };
+        sClient.start();
+		sClient.deleteAllVisibleCookies();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		sClient.stop();
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		testCaseResult = "f";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
+	}
+	
+	
+	/**
+	 * Add a network Testcase 3758
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddNetwork() throws Exception{		
+		
+		testCaseID = 3758;
+		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);
+		*/
+		sClient.open("/");
+		Thread.sleep(2000);
+		sClient.open("/zport/dmd/networks");
+		Thread.sleep(6000);
+		sClient.click("link=Networks");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(6000);
+		sClient.click("ext-gen26");
+		Thread.sleep(3000);
+		sClient.type("addNetworkTextfield", "192.168.1.0/24");
+		sClient.click("//table[@id='ext-comp-1052']/tbody/tr[2]/td[2]");
+		Thread.sleep(5000);
+		SeleneseTestCase.assertEquals("192.168.1.0/24", sClient.getText("//ul[@id='ext-gen119']/li/div/a/span"));
+		testCaseResult = "p";
+	}
+	
+	@Test
+	public void testRemoveNetwork() throws Exception
+	{
+		testCaseID = 3760;
+		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);
+		*/
+		sClient.open("/");
+		Thread.sleep(2000);
+		sClient.open("/zport/dmd/networks");
+		Thread.sleep(6000);
+		sClient.click("link=Networks");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(6000);
+		SeleneseTestCase.assertEquals("192.168.1.0/24", sClient.getText("//ul[@id='ext-gen119']/li/div/a/span"));
+		
+		sClient.click("//ul[@id='ext-gen119']/li/div/a/span");		
+		sClient.click("ext-gen28");
+		Thread.sleep(3000);
+		sClient.click("//table[@id='ext-comp-1055']/tbody/tr[2]/td[2]");
+		Thread.sleep(3000);
+		SeleneseTestCase.assertFalse(sClient.isElementPresent("//ul[@id='ext-gen119']/li/div/a/span"));
+		SeleneseTestCase.assertFalse(sClient.isTextPresent("192.168.1.0/24"));
+		testCaseResult = "p";
+	}
+}
