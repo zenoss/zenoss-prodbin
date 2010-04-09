964d0fbabbe41431306630af801385a2b3d83897
arivera <arivera@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-09 23:33:33 +0000

    Created new method to add devices on any class in the Common class file Created new method to wait for one element Also, the DiscoverWindows2003onWindowsClass file was update
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@32038 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/CommonFramework/src/loc/zenoss/Common.java
+++ Products/ZenUITests/tests/seleniumJava/CommonFramework/src/loc/zenoss/Common.java
@@ -1,16 +1,15 @@
 package loc.zenoss;
 import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
 
 
 public class Common {
-
+	
 	public static void openUrl(DefaultSelenium sClient, String url)
 	{
-		sClient.open(url);	
-		
+		sClient.open(url);			
 	}
 	
-
 	public static void Login(DefaultSelenium sClient, String username, String password) throws Exception
 	{
 		sClient.open("/");
@@ -18,5 +17,68 @@ public class Common {
 		sClient.type("__ac_password", password);	
 		sClient.click("submitbutton");
 		sClient.waitForPageToLoad("40000");
+	}
+	
+	public static void waitForElement(String element, DefaultSelenium sClient) throws Exception
+	{
+		byte count = 0;
+		while(!sClient.isElementPresent(element))
+			Thread.sleep(2);
+			count++;
+
+			if(count > 30)
+			  throw new Exception("Element not found");
+	}
+	
+	/*
+	 * Insert and model a Single Device on the specified class
+	 * @author Alvaro Rivera
+	 * @param name Name or IP of the device to add
+	 * @param dClass Class where to add the device
+	 * @param snmpComm SNMP Community of the device
+	 * @param Selenium client connection
+	 * @return Boolean true if the device was successfully added or false in other way
+	 * @throws Generic Exception
+	 */
+	public static boolean addDevice(String name, String dClass, String snmpComm, DefaultSelenium sClient) throws Exception
+	{
+		sClient.click("link=Infrastructure");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(8000);
+		sClient.click("//table[@id='adddevice-button']/tbody/tr[2]/td[2]/em");
+		Thread.sleep(1500);
+		sClient.click("//a[@id='addsingledevice-item']");
+		Thread.sleep(7000);
+		
+		sClient.type("//input[@id='add-device-name']", name);
+		sClient.click("//input[@id='add-device_class']");
+		Thread.sleep(5000);
+		sClient.click("//div//div[31]");
+		sClient.getText("//div//div[text() = '"+dClass+"']");
+		
+		sClient.click("link=More...");
+		Thread.sleep(2000);
+		sClient.type("ext-comp-1156", snmpComm);
+				
+		sClient.click("//table[@id='addsingledevice-submit']/tbody/tr[2]/td[2]");
+		Thread.sleep(5000);
+		sClient.click("//*[contains(text(), 'View Job Log')]");
+		sClient.waitForPageToLoad("120000");
+		
+		waitForElement("Job completed at", sClient);
+		
+		boolean result = true;
+		
+		if (sClient.isTextPresent("Traceback"))
+		{
+			result = false;
+			throw new Exception("There is tracebacks presents on the Model's process log");
+		}
+		else if (sClient.isTextPresent("Error"))
+		{
+			result = false;
+			throw new Exception("There is Error presents on the Model's process log");
+		}		
+		return result;		
 	}
 }
--- Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/ITInfrastructure/Devices/AddDevices/DiscoverWindows2003onWindowsClass.java
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/ITInfrastructure/Devices/AddDevices/DiscoverWindows2003onWindowsClass.java
@@ -3,6 +3,8 @@ package loc.zenoss.testcases.ITInfrastructure.Devices.AddDevices;
 
 import loc.zenoss.TestlinkXMLRPC;
 import loc.zenoss.ZenossConstants;
+import loc.zenoss.testcases.Device.AddDevice;
+import loc.zenoss.Common;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,23 +12,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import loc.zenoss.ZenossConstants;
-import loc.zenoss.TestlinkXMLRPC;
-
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.SeleneseTestCase;
 
 public class DiscoverWindows2003onWindowsClass {
 
 private static DefaultSelenium sClient = null;
-	
 	private static int testCaseID = 2045;
 	private static String testCaseResult = "f"; //Fail by default
 		
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		
-		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,ZenossConstants.browser, ZenossConstants.testedMachine)  {
+		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,ZenossConstants.browser, "http://test-cent4-64-2.zenoss.loc:8080")  {// ZenossConstants.testedMachine)  {
         	public void open(String url) {
         		commandProcessor.doCommand("open", new String[] {url,"true"});
         	}
@@ -53,12 +51,17 @@ private static DefaultSelenium sClient = null;
 	public void AddWindows2003Devices() throws Exception {
 		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
 		Thread.sleep(12000);*/
-		
+				
 		sClient.open("/");
 		sClient.waitForPageToLoad("30000");
 		Thread.sleep(5000);
 		
-		sClient.click("link=IT Infrastructure");
+		boolean status = Common.addDevice("test-win2003-1d.zenoss.loc", "/Server/Windows", "public", sClient); 
+		
+		if (!status)
+			SeleneseTestCase.fail("Problems when add Device");
+		
+		/*sClient.click("link=IT Infrastructure");
 		sClient.waitForPageToLoad("30000");
 		Thread.sleep(8000);
 		sClient.click("//table[@id='adddevice-button']/tbody/tr[2]/td[2]/em");
@@ -70,11 +73,10 @@ private static DefaultSelenium sClient = null;
 		sClient.click("//input[@id='add-device_class']");
 		Thread.sleep(5000);
 		sClient.click("//div//div[31]");
-		
+		System.out.print(sClient.getText("//div//div[31]"));
 		sClient.click("link=More...");
 		Thread.sleep(2000);
 		sClient.type("ext-comp-1146", "public");
-		
 		
 		sClient.click("//table[@id='addsingledevice-submit']/tbody/tr[2]/td[2]");
 		Thread.sleep(5000);
@@ -83,6 +85,9 @@ private static DefaultSelenium sClient = null;
 		
 		SeleneseTestCase.assertFalse(sClient.isTextPresent("Traceback"));
 		SeleneseTestCase.assertFalse(sClient.isTextPresent("Error"));
+			*/
+		
+		
 		
 		
 		/*
