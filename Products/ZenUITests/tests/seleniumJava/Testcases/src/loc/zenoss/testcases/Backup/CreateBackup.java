72c81752016227873155abc92e782913dc5c0899
emunoz <emunoz@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-09 23:02:37 +0000

    There are some Test Cases for Backup page
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@32035 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Backup/CreateBackup.java
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Backup/CreateBackup.java
@@ -16,7 +16,7 @@ public class CreateBackup {
 	
 	private static DefaultSelenium sClient = null;
 
-	private static int testCaseID = 1790;
+	private static int testCaseID;
 	private static String testCaseResult = "f"; //Fail by default
     
 	@BeforeClass
@@ -34,7 +34,7 @@ public class CreateBackup {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		sClient.stop();
-		//TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
+		TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
 	}
 
 	@Before
@@ -48,17 +48,166 @@ public class CreateBackup {
 	
 	
 	@Test
-	public void navigationSettings() throws Exception{
+	public void createBackup() throws Exception{
+		
 		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
 		Thread.sleep(12000);*/	
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
 		sClient.open("/zport/dmd/Dashboard");
-		 sClient.waitForPageToLoad("30000");
+		sClient.waitForPageToLoad("30000");
 		sClient.open("/zport/dmd/backupInfo");
-		 sClient.waitForPageToLoad("30000");
-		 Thread.sleep(12000);
-		 sClient.click("manage_createBackup:method");
-		 sClient.waitForPageToLoad("250000");
-		 SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("manage_createBackup:method");
+		sClient.waitForPageToLoad("250000");
+		SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));
 		
+		/**DELETE THE BACKUP CREATED**/
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("fileNames:list");
+		sClient.click("ext-gen16");
+		sClient.click("BackupFileslistdeleteBackup");
+		Thread.sleep(12000);
+		sClient.click("manage_deleteBackups:method");
+		sClient.waitForPageToLoad("30000");	
 	}
+	/**
+	 * Create a Backup whit DefaultSettings Testcase 1822
+	 * @throws Exception
+	 */
+	@Test
+	public void  createBackupDefaultSettings() throws Exception{
+		//TODO Add devices and run console commands
+		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);*/	
+		testCaseID = 1822;
+		
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("manage_createBackup:method");
+		sClient.waitForPageToLoad("250000");
+		SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));
+		
+		/**DELETE THE BACKUP CREATED**/
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("fileNames:list");
+		sClient.click("ext-gen16");
+		sClient.click("BackupFileslistdeleteBackup");
+		Thread.sleep(12000);
+		sClient.click("manage_deleteBackups:method");
+		sClient.waitForPageToLoad("30000");	
+		
+		testCaseResult = "p";
+     	 
+	}
+	/**
+	 * Create a Backup WithOut Mysql Login Testcase 1826
+	 * @throws Exception
+	 */
+	@Test
+	public void  createBackupWithOutMysqlLogin() throws Exception{
+		//TODO Add devices and run console commands
+		
+		testCaseID = 1826;
+		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);*/	
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("includeMysqlLogin");
+		sClient.click("manage_createBackup:method");
+		sClient.waitForPageToLoad("250000");
+		SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));
+		
+		/**DELETE THE BACKUP CREATED**/
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("fileNames:list");
+		sClient.click("ext-gen16");
+		sClient.click("BackupFileslistdeleteBackup");
+		Thread.sleep(12000);
+		sClient.click("manage_deleteBackups:method");
+		sClient.waitForPageToLoad("30000");	
+		
+		testCaseResult = "p";
+	}
+	/**
+	 * Create Backup Without MySQL events  Testcase 1824
+	 * @throws Exception
+	 */
+	@Test
+	public void  createBackupWithOutMySQLevents () throws Exception{
+		//TODO Add devices and run console commands
+		
+		testCaseID = 1824;
+		/*Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);*/	
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("includeEvents");
+		sClient.click("manage_createBackup:method");
+		sClient.waitForPageToLoad("250000");
+		SeleneseTestCase.assertTrue(sClient.isTextPresent("Backup completed successfully"));	
+		
+		/**DELETE THE BACKUP CREATED**/
+		sClient.open("/zport/dmd?submitted=");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.open("/zport/dmd/Dashboard");
+		sClient.waitForPageToLoad("30000");
+		sClient.open("/zport/dmd/backupInfo");
+		sClient.waitForPageToLoad("30000");
+		Thread.sleep(12000);
+		sClient.click("fileNames:list");
+		sClient.click("ext-gen16");
+		sClient.click("BackupFileslistdeleteBackup");
+		Thread.sleep(12000);
+		sClient.click("manage_deleteBackups:method");
+		sClient.waitForPageToLoad("30000");	
+		
+		testCaseResult = "p";
+	}
+
+
+
+	
 }
