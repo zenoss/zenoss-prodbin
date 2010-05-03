60584723219f742f0c5457777df63469b412af96
dgarcia <dgarcia@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-05-03 18:36:32 +0000

    move css out of python, into stylesheet, get rid of hiding x scroll
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@32839 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/Jobber/browser/LogStreamView.py
+++ Products/Jobber/browser/LogStreamView.py
@@ -27,9 +27,9 @@ class LogStreamView(BrowserView):
         self.request.response.write("""
             <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.0//EN"
             "http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd">
-            <html><body style="overflow-x:hidden;"><pre style="
-                font-family:Monaco,monospace;
-                font-size:14px;">""")
+            <html>
+                <link rel="stylesheet" type="text/css" href="/++resource++zenui/css/zenoss.css" />
+                <body class="log-output"><pre class="log-output">""")
         self._stream()
         self.request.response.write(""" </pre></body></html> """)
         self.request.response.flush()
