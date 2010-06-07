8865acac712468ab9cd8133429d483efc8152372
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-06-07 17:58:33 +0000

    Fixes #6778: Updated trigger image, refactored component somewhat. Reviewed by Danny.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@33728 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/search/resources/zenoss-search.js
+++ Products/ZenUI3/browser/search/resources/zenoss-search.js
@@ -3,70 +3,118 @@ Ext.onReady(function(){
 // If there is no searchbox-container, then we will not attempt to render
 // the searchbox.  No context windows such as the event detail popout have
 // no searchbox-container
+
 if ( Ext.get('searchbox-container') === null ) {
             return;
 }else {
     
-    Ext.create({
-            xtype: 'panel',
-            id: 'saved-searches-button',
-            renderTo: 'searchbox-container',
-            cls: 'searchcombo-left',
-            width: 20,
-            height: 21,
-            handler: function () {
-                // everytime we press this button, we want
-                // to go back to the server to get fresh data
-                delete combo.lastQuery;
-            }
-        }
-    );
-
-    Ext.create({
-            black: true,
-            altCls: 'searchcombo',
-            id: 'searchbox-query',
-            fieldClass: 'searchbox-query',
-            name: 'query',
-            width: 150,
-            xtype: 'searchfield',
-            renderTo: 'searchbox-container'
-        });
-
     var combo,
         router = Zenoss.remote.SearchRouter,
         ManageSavedSearchDialog,     
         ds = new Ext.data.DirectStore({
-        directFn: Zenoss.remote.SearchRouter.getLiveResults,
-        root: 'results',
-        idProperty: 'url',
-        fields: [ 'url', 'content', 'popout', 'category' ]
-    }),
+            directFn: Zenoss.remote.SearchRouter.getLiveResults,
+            root: 'results',
+            idProperty: 'url',
+            fields: [ 'url', 'content', 'popout', 'category' ]
+        }),
+        // Custom rendering Template
+        resultTpl = new Ext.XTemplate(
+            '<tpl for=".">',
+            '<table class="search-result"><tr class="x-combo-list-item">',
+            '<th><tpl if="values.category && (xindex == 1 || parent[xindex - 2].category != values.category)"',
+            '>{category}</tpl></th>',
+            '<td colspan="2">{content}</td>',
+            '</tpl>'),
+        searchfield = new Zenoss.SearchField({
+            black: true,
+            id: 'searchbox-query',
+            fieldClass: 'searchbox-query',
+            name: 'query',
+            width: 150,
+            renderTo: 'searchbox-container'
+        });
 
-    // Custom rendering Template
-    resultTpl = new Ext.XTemplate(
-        '<tpl for=".">',
-        '<table class="search-result"><tr class="x-combo-list-item">',
-        '<th><tpl if="values.category && (xindex == 1 || parent[xindex - 2].category != values.category)">{category}</tpl></th>',
-        '<td colspan="2">{content}</td>',
-        '</tpl>' );
+
 
     Zenoss.env.search = new Ext.form.ComboBox({
         store: ds,
         typeAhead: false,
         loadingText: _t('Searching..'),
-        triggerAction: 'all',
+        //triggerAction: 'all',
         width: 120,
         pageSize: 0,
         minChars: 3,
-        hideTrigger: true,
+        hideTrigger: false,
         tpl: resultTpl,
-        applyTo: Ext.get('searchbox-query'),
+        applyTo: searchfield.getEl(),
         listClass: 'search-result',
         listWidth: 375,
+        initSavedSearch: function() {
+            // Create a dummy target for the saved search combo
+            var container = Ext.get(Ext.select('.searchfield-black-mc').elements[0]),
+                hiddeninput = container.createChild({tag:'input', style:{
+                    position: 'absolute',
+                    visibility: 'hidden',
+                    width: 0
+                }});
+            this.savedSearchCombo = new Ext.form.ComboBox({
+                editable: false,
+                triggerAction: 'all',
+                hideTrigger: true,
+                height: 21,
+                listClass: 'saved-search-item',
+                valueField: 'id',
+                listWidth: 200,
+                displayField: 'name',
+                applyTo: hiddeninput,
+                store: {
+                    xtype: 'directstore',
+                    ref:'store',
+                    directFn: Zenoss.remote.SearchRouter.getAllSavedSearches,
+                    fields: ['id', 'name'],
+                    root: 'data',
+                    baseParams: {
+                        'addManageSavedSearch': true
+                    }
+                },
+                listeners: {
+                    select: function(box, record){
+                        // magic string that means the user wishes to manage
+                        // their saved searches
+                        if (record.id == 'manage_saved_search') {
+                            var decodedUrl = Ext.urlDecode(location.search.substring(1, location.search.length)),
+                                searchId = decodedUrl.search,
+                                win = Ext.create({
+                                    xtype:'managesavedsearchdialog',
+                                    id:'manageSavedSearchesDialog',
+                                    searchId: searchId
+                                });
+                            win.show();
+                        }else {
+                            // otherwise go to the selected search results page
+                            window.location = String.format('/zport/dmd/search?search={0}', record.id);
+                        }
+                    }
+                }
+
+            });
+            this.fireEvent('triggerready');
+        },
+        initTrigger: function() {
+            this.on('triggerready', function(){
+                var combo = this.savedSearchCombo,
+                    triggerclick = combo.onTriggerClick.createInterceptor(function(){
+                        delete combo.lastQuery;
+                    }, this);
+                this.mon(this.trigger, 'click', triggerclick, combo, {preventDefault:true});
+                this.trigger.addClassOnOver('x-form-trigger-over ' + this.triggerClass);
+                this.trigger.addClassOnClick('x-form-trigger-click ' + this.triggerClass);
+            }, this);
+            this.initSavedSearch();
+        },
         listeners: {
             select: function(box,record){
-                if (record.get('url') != '') {
+                if (record.get('url') !== '') {
                     if (record.get('popout')) {
                         window.open( String.format('{0}',record.data.url ),
                                      record.data.url,
@@ -82,47 +130,6 @@ if ( Ext.get('searchbox-container') === null ) {
     });
 
     // drop down box of existing saved searches
-    combo = new Ext.form.ComboBox({
-        editable: false,
-        triggerAction: 'all',
-        height: 21,
-        triggerClass: 'searchcombo-trigger',
-        applyTo: Ext.get('saved-searches-button'),
-        listClass: 'saved-search-item',
-        valueField: 'id',
-        listWidth: 200,
-        displayField: 'name',
-        store: {
-            xtype: 'directstore',
-            ref:'store',
-            directFn: Zenoss.remote.SearchRouter.getAllSavedSearches,
-            fields: ['id', 'name'],
-            root: 'data',
-            baseParams: {
-                'addManageSavedSearch': true
-            }
-        },
-        listeners: {
-            select: function(box, record){
-                // magic string that means the user wishes to manage
-                // their saved searches
-                if (record.id == 'manage_saved_search') {
-                    var decodedUrl = Ext.urlDecode(location.search.substring(1, location.search.length)),
-                        searchId = decodedUrl.search,
-                        win = Ext.create({
-                        xtype:'managesavedsearchdialog',
-                        id:'manageSavedSearchesDialog',
-                        searchId: searchId
-                    });
-                    win.show();
-                }else {
-                    // otherwise go to the selected search results page
-                    window.location = String.format('/zport/dmd/search?search={0}', record.id);
-                }
-            }
-        }
-
-    });
 
     ManageSavedSearchDialog = Ext.extend(Ext.Window, {
         constructor: function(config) {
