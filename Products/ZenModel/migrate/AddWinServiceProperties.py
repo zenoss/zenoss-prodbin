394d55e3b59f920c2503473c5bdedcce04264373
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-06-29 18:34:25 +0000

    added check for services without sevice classes, reviewed by jpl, fixes #6946
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@34315 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/AddWinServiceProperties.py
+++ Products/ZenModel/migrate/AddWinServiceProperties.py
@@ -10,22 +10,23 @@
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-__doc__="""
+__doc__ = """
 Denormalizes serviceName and caption WinService properties to avoid loading
 entire service class objects for common operations.
 """
-
 import Migrate
-from Products.ZenModel.ZenossSecurity import *
+
 
 class AddWinServiceProperties(Migrate.Step):
     version = Migrate.Version(3, 0, 0)
 
     def cutover(self, dmd):
         for b in dmd.Devices.componentSearch(meta_type='WinService'):
-            s = b.getObject()
-            s.serviceName = s.serviceclass().name
-            s.caption = s.serviceclass().description
+            service = b.getObject()
+            serviceClass = service.serviceclass()
+            if serviceClass:
+                service.serviceName = serviceClass.name
+                service.caption = serviceClass.description
 
 
 AddWinServiceProperties()
