4cf6e31c56a956071593484f0789f8fe26fa7da9
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-07-28 17:10:33 +0000

    * Fixes #7116: merge from branch, add a MAC address report. Reviewed by N Zook.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@35100 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenReports/plugins/macaddress.py
@@ -0,0 +1,37 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+from Products.ZenReports.Utils import Record
+
+class macaddress:
+    ignoreInterfaces = ['lo', 'sit0']
+
+    def run(self, dmd, args):
+        report = []
+        catalog = dmd.ZenLinkManager.layer2_catalog
+        for brain in catalog():
+            macaddress = brain.macaddress.upper()
+            if not macaddress or macaddress == '00:00:00:00':
+                continue
+            ifaceName = brain.interfaceId.rsplit('/', 1)[-1]
+            if ifaceName in self.ignoreInterfaces:
+                continue
+            deviceName = brain.deviceId.rsplit('/', 1)[-1]
+            report.append(Record(
+                            devicePath = brain.deviceId,
+                            deviceName = deviceName,
+                            interfacePath = brain.interfaceId,
+                            interfaceName = ifaceName,
+                            macaddress = brain.macaddress,
+            ))
+        return report
+
