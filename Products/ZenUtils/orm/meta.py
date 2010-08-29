a23161a99d9494bde09dd411aea178b831143c7f
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-08-29 12:59:14 +0000

    Adding gpl; reviewed by buildbot
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@35723 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/orm/meta.py
+++ Products/ZenUtils/orm/meta.py
@@ -1,3 +1,13 @@
+#############################################################################
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#############################################################################
 from sqlalchemy import MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
