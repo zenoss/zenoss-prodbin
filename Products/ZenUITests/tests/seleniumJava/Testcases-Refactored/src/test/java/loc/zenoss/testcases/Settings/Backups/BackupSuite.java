1495cc5198a5864dd9b1c290681a4f647399dc75
wquesada <wquesada@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-09-27 15:34:16 +0000

    Fixing Copyrights. Reviewed by crojas.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@35999 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/Testcases-Refactored/src/test/java/loc/zenoss/testcases/Settings/Backups/BackupSuite.java
+++ Products/ZenUITests/tests/seleniumJava/Testcases-Refactored/src/test/java/loc/zenoss/testcases/Settings/Backups/BackupSuite.java
@@ -1,3 +1,15 @@
+/**
+#############################################################################
+# This program is part of Zenoss Core, an open source monitoringplatform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#############################################################################
+ */
 package loc.zenoss.testcases.Settings.Backups;
 
 import org.junit.runner.RunWith;
