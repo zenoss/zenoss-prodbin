f945679c3f5bd0f5862c43e74010d9e7b9d78207
crojas <crojas@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-05 21:11:31 +0000

    New Test Case ID 4089 :: Create new MIBs Reviewed by wquesada
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@36189 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Advanced/MIBs/AddMIB.java
@@ -0,0 +1,83 @@
+/**
+#############################################################################
+# This program is part of Zenoss Core, an open source monitoringplatform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#############################################################################
+*/
+package loc.zenoss.testcases.Advanced.MIBs;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loc.zenoss.Common;
+import loc.zenoss.MIBs;
+import loc.zenoss.ZenossConstants;
+import loc.zenoss.TestlinkXMLRPC;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+public class AddMIB {
+	private static int testCaseID = 4089;
+	private static String testCaseResult = "f"; //Fail by default
+	
+	private static SeleneseTestCase selenese = null;
+	private static DefaultSelenium sClient = null;
+	
+	@BeforeClass
+	 public static void setUpBeforeClass() throws Exception {
+		selenese = new SeleneseTestCase();  
+		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,
+	 			ZenossConstants.browser, ZenossConstants.testedMachine)  {
+	        		public void open(String url) {
+	        			commandProcessor.doCommand("open", new String[] {url,"true"});
+	        		}     	};
+	        		sClient.start();
+			sClient.deleteAllVisibleCookies();
+		}
+
+		@AfterClass
+		public static void tearDownAfterClass() throws Exception {
+			sClient.stop();
+			TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
+		}
+	
+		@Before
+		public void setUp() throws Exception {
+			 
+		}
+
+		@After
+		public void tearDown() throws Exception {
+		}
+		
+		@Test
+		public void addMIB() throws Exception{
+			String mib = "NewMIB";
+			
+			Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+			Thread.sleep(12000);
+							
+			// Click on Advanced page
+			sClient.click("link=Advanced");
+			sClient.waitForPageToLoad("30000");
+			
+			// Click on MIBs
+			sClient.click("link=MIBs");
+			sClient.waitForPageToLoad("30000");
+			// Add new MIB
+			MIBs.addMIB(sClient, mib);
+			
+			testCaseResult = "p";
+			
+		}
+}
