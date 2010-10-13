cf2680c1effe69ac49e3fccab71e1f8d6ff86634
crojas <crojas@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-13 16:48:21 +0000

    Update code - ID 2493 :: Test Case Using the UI to submit an event Reviewed by crojas
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@36354 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Events/CreateEvent.java
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/Events/CreateEvent.java
@@ -9,7 +9,7 @@
 #
 # For complete information please visit: http://www.zenoss.com/oss/
 #############################################################################
-*/
+ */
 package loc.zenoss.testcases.Events;
 
 import org.junit.After;
@@ -18,57 +18,62 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import loc.zenoss.*;
+
 import com.thoughtworks.selenium.SeleneseTestCase;
 import com.thoughtworks.selenium.DefaultSelenium;
 
 public class CreateEvent {
-	
-		private static DefaultSelenium sClient = null;
-		
-		
-		@BeforeClass
-		public static void setUpBeforeClass() throws Exception {
-			
-			sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,ZenossConstants.browser, ZenossConstants.testedMachine)   {
-	        	public void open(String url) {
-	        		commandProcessor.doCommand("open", new String[] {url,"true"});
-	        	}
-	        };
-			sClient.start();
-			sClient.deleteAllVisibleCookies();
-		}
 
-		@AfterClass
-		public static void tearDownAfterClass() throws Exception {
-			sClient.stop();
-		}
+	private static DefaultSelenium sClient = null;
+	private static String testCaseResult = "f"; //Fail by default
+	private static int testCaseID = 2493;
 
-		@Before
-		public void setUp() throws Exception {
-		}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 
-		@After
-		public void tearDown() throws Exception {
-		}
-		
-		@Test
-		public void createEvent() throws Exception{
-		
-			boolean result = false;
-			Common.Login(sClient, ZenossConstants.adminUserName, ZenossConstants.adminPassword);
-			sClient.click("link=Events");
-			sClient.waitForPageToLoad("30000");
-			// Add one Event
-			String summary = "MySummary";
-			String severity = "Critical";
-			String device = "MyDevice";
-			String component = "MyComponent";
-			String eventclass = "/Status";
-			String eventclassKey = "ClassKey";
-			result = Event.addSingleEvent(sClient, summary, severity, device, component, eventclass, eventclassKey);
-			SeleneseTestCase.assertEquals(true, result);			
-		}		
-
+		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,ZenossConstants.browser, ZenossConstants.testedMachine)   {
+			public void open(String url) {
+				commandProcessor.doCommand("open", new String[] {url,"true"});
+			}
+		};
+		sClient.start();
+		sClient.deleteAllVisibleCookies();
 	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		sClient.stop();
+		TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void createEvent() throws Exception{
+
+		boolean result = false;
+		Common.Login(sClient, ZenossConstants.adminUserName, ZenossConstants.adminPassword);
+		sClient.click("link=Events");
+		sClient.waitForPageToLoad("30000");
+		// Add one Event
+		String summary = "MySummary";
+		String severity = "Critical";
+		String device = "MyDevice";
+		String component = "MyComponent";
+		String eventclass = "/Status";
+		String eventclassKey = "ClassKey";
+		result = Event.addSingleEvent(sClient, summary, severity, device, component, eventclass, eventclassKey);
+		SeleneseTestCase.assertEquals(true, result);	
+
+		testCaseResult = "p";
+	}		
+
+}
 
 
