f937e0e680a8be191091b2832d483ca85d6cd1bd
crojas <crojas@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-19 17:39:11 +0000

    ID 2129 :: Test Case Adding Products to a Manufacturer - New common methods Reviewed by wquesada
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@36537 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUITests/tests/seleniumJava/CommonFramework/src/loc/zenoss/Manufacturers.java
+++ Products/ZenUITests/tests/seleniumJava/CommonFramework/src/loc/zenoss/Manufacturers.java
@@ -90,5 +90,116 @@ public class Manufacturers {
 		}
 		return result;		
 	}
-	
+
+	/*
+	 * This method assumes that manufacturers page is already loaded
+	 * and the manufacturer was created and the overview page is displayed
+	 * Create Sofware manufacturers
+	 * @author Catalina Rojas 
+	 * @param sClient Selenium client connection
+	 * @return Boolean true if the new SW was successfully created or false in other way
+	 * @throws Generic Exception
+	 */
+	public static boolean addNewSoftware(DefaultSelenium sClient, String software ) throws Exception{
+		// Add SW product
+		// Click gear menu
+		sClient.click("//table[@id='ext-comp-1081']/tbody/tr[2]/td[2]/em");
+		// Click Add Software
+		Thread.sleep(2000);
+		sClient.click("ProductlistaddSoftware");
+		// Enter SW name
+		Thread.sleep(1000);
+		sClient.type("new_id", software);
+		// Click Ok button
+		Thread.sleep(1000);
+		sClient.click("//input[@value='OK']");
+		// Verify if SW is created
+		Thread.sleep(4000);
+
+		boolean result = true;
+		if(sClient.isElementPresent("//tbody[@id='Products']/tr/td/a[text()='"+software+"']") && sClient.isElementPresent("//tbody[@id='Products']/tr/td[text()='Software']")){
+			result = true;
+		}
+		else
+		{
+			result = false;
+			throw new Exception("The new Software was not created");
+		}
+		return result;
+	}
+
+	/*
+	 * This method assumes that manufacturers page is already loaded
+	 * and the manufacturer was created and the overview page is displayed
+	 * Create Hardware manufacturers
+	 * @author Catalina Rojas 
+	 * @param sClient Selenium client connection
+	 * @return Boolean true if the new HW was successfully created or false in other way
+	 * @throws Generic Exception
+	 */
+	public static boolean addNewHarware(DefaultSelenium sClient, String hardware ) throws Exception{
+		// Add HW product
+		// Click gear menu
+		sClient.click("//table[@id='ext-comp-1081']/tbody/tr[2]/td[2]/em");
+		// Click Add Hardware
+		Thread.sleep(2000);
+		sClient.click("ProductlistaddHardware");
+		// Enter SW name
+		Thread.sleep(1000);
+		sClient.type("new_id", hardware);
+		// Click Ok button
+		Thread.sleep(1000);
+		sClient.click("//input[@value='OK']");
+		// Verify if HW is created
+		Thread.sleep(4000);
+
+		boolean result = true;
+		if(sClient.isElementPresent("//tbody[@id='Products']/tr/td/a[text()='"+hardware+"']") && sClient.isElementPresent("//tbody[@id='Products']/tr/td[text()='Hardware']")){
+			result = true;
+		}
+		else
+		{
+			result = false;
+			throw new Exception("The new Hardware was not created");
+		}
+		return result;
+	}
+
+	/*
+	 * This method assumes that manufacturers page is already loaded
+	 * and the manufacturer was created and the overview page is displayed
+	 * Create OS manufacturers
+	 * @author Catalina Rojas 
+	 * @param sClient Selenium client connection
+	 * @return Boolean true if the new OS was successfully created or false in other way
+	 * @throws Generic Exception
+	 */
+	public static boolean addNewOS(DefaultSelenium sClient, String os ) throws Exception{
+		// Add OS product
+		// Click gear menu
+		sClient.click("//table[@id='ext-comp-1081']/tbody/tr[2]/td[2]/em");
+		// Click Add OS
+		Thread.sleep(2000);
+		sClient.click("ProductlistaddOS");
+		// Enter OS name
+		Thread.sleep(1000);
+		sClient.type("new_id", os);
+		// Click Ok button
+		Thread.sleep(1000);
+		sClient.click("//input[@value='OK']");
+		// Verify OS is created
+		Thread.sleep(4000);
+
+		boolean result = true;	
+		if(sClient.isElementPresent("//tbody[@id='Products']/tr/td/a[text()='"+os+"']") && sClient.isElementPresent("//tbody[@id='Products']/tr/td[text()='Operating System']") ){
+			result = true;
+		}
+		else
+		{
+			result = false;
+			throw new Exception("The new Operating System was not created");
+		}
+		return result;
+	}
+
 }
