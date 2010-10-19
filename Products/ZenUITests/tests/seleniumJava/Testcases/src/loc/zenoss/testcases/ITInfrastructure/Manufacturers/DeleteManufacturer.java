15174eb7db4230758e1fe587959b93736efc46d2
crojas <crojas@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-19 15:55:14 +0000

    ID 2131 :: Test Case Delete a Manufacturer Reviewed by wquesada
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@36530 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenUITests/tests/seleniumJava/Testcases/src/loc/zenoss/testcases/ITInfrastructure/Manufacturers/DeleteManufacturer.java
@@ -0,0 +1,86 @@
+/**
+#############################################################################
+# This program is part of Zenoss Core, an open source monitoringplatform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#############################################################################
+ */
+package loc.zenoss.testcases.ITInfrastructure.Manufacturers;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loc.zenoss.Common;
+import loc.zenoss.Manufacturers;
+import loc.zenoss.ZenossConstants;
+import loc.zenoss.TestlinkXMLRPC;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+public class DeleteManufacturer {
+	private static int testCaseID = 2123;
+	private static String testCaseResult = "f"; //Fail by default
+
+	private static SeleneseTestCase selenese = null;
+	private static DefaultSelenium sClient = null;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		selenese = new SeleneseTestCase();  
+		sClient = new DefaultSelenium(ZenossConstants.SeleniumHubHostname, 4444,
+				ZenossConstants.browser, ZenossConstants.testedMachine)  {
+			public void open(String url) {
+				commandProcessor.doCommand("open", new String[] {url,"true"});
+			}     	};
+			sClient.start();
+			sClient.deleteAllVisibleCookies();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		sClient.stop();
+		TestlinkXMLRPC.UpdateTestCaseResult(testCaseID, ZenossConstants.testPlanID, testCaseResult);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void createManufacturer() throws Exception{
+		String manufacturer = "deleteManufacturer";
+
+		Common.Login(sClient, ZenossConstants.adminUserName,ZenossConstants.adminPassword);
+		Thread.sleep(12000);
+
+		//Click Infractructure
+		sClient.click("link=Infrastructure");
+		Thread.sleep(8000);
+
+		// Click on Manufacturers
+		sClient.click("link=Manufacturers");
+		sClient.waitForPageToLoad("30000");
+
+		//Create new manufacturer
+		Manufacturers.createManufacturers(sClient, manufacturer);
+		Thread.sleep(4000);
+		//Delete manufacturer
+		Manufacturers.deleteManufacturers1(sClient, manufacturer);
+
+		testCaseResult = "p";
+	}
+}
