8f3b30bade2a94ed570ea808a25d0c1a0dd71b04
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-11-09 22:33:44 +0000

    add copyrights; reviewed by Paul
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@37176 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/propertyMonitor.py
+++ Products/ZenEvents/propertyMonitor.py
@@ -1,3 +1,15 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 class PropertyMonitor(type):
     """Utility metaclass for defining monitored properties in a Python object.
        Target class must define a class attribute 'fields' containing the names 
