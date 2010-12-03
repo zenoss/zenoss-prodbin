1db94d1c191335291f2f66b512d651abeda807a3
dgarcia <dgarcia@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-12-03 17:53:28 +0000

    * fixes #7580: Don't apply component templates to devices. reviewed by kkearney.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@37886 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/Zuul/facades/templatefacade.py
+++ Products/Zuul/facades/templatefacade.py
@@ -377,7 +377,9 @@ class TemplateFacade(ZuulFacade):
             # copying to and from a DeviceClass
             source = template.deviceClass()
             source.manage_copyAndPasteRRDTemplates((template.id,), targetUid)
-            target.bindTemplates(target.zDeviceTemplates + [template.id])
+            # Do not bind component templates to a DeviceClass
+            if issubclass(template.getTargetPythonClass(), (Device, DeviceClass)): 
+                target.bindTemplates(target.zDeviceTemplates + [template.id])
         else:
             if isinstance(target, DeviceClass):
                 # copying from a Device to a DeviceClass
@@ -391,7 +393,9 @@ class TemplateFacade(ZuulFacade):
                 raise Exception(msg % args)
             copy = template._getCopy(container)
             container._setObject(copy.id, copy)
-            target.bindTemplates(target.zDeviceTemplates + [copy.id])
+            # Do not bind component templates to a Device
+            if issubclass(template.getTargetPythonClass(), (Device, DeviceClass)): 
+                target.bindTemplates(target.zDeviceTemplates + [copy.id])
 
     def addGraphDefinition(self, templateUid, graphDefinitionId):
         template = self._getTemplate(templateUid)
