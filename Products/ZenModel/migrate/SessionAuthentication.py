fd4fee026bbe89afb155def578f602659f8ba70a
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-12-30 17:41:01 +0000

    fixes tracint 2615 reviewed by ian stores update our PAS to use plone.session so as to not leak credentials
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@38406 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenModel/migrate/SessionAuthentication.py
@@ -0,0 +1,30 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+import Migrate
+
+from Products.ZenUtils.Security import setupSessionHelper, setupCookieHelper
+
+
+class SessionAuthentication(Migrate.Step):
+    version = Migrate.Version(3, 1, 0)
+
+    def __init__(self):
+        Migrate.Step.__init__(self)
+
+    def cutover(self, dmd):
+        zport = dmd.zport
+        setupCookieHelper(zport)
+        setupSessionHelper(zport)
+
+SessionAuthentication()
+
