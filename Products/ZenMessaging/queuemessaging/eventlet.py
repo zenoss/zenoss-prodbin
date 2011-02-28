4a9dfe6de217f1a2bb5804af9cba678fa104b2e0
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-02-28 03:27:03 +0000

    Added copyright, removed unused file. Reviewed by me.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@39296 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenMessaging/queuemessaging/eventlet.py
+++ Products/ZenMessaging/queuemessaging/eventlet.py
@@ -1,6 +1,6 @@
 ###########################################################################
 #
-# This program is part of Zenoss Core, an open source monitoring platform
+# This program is part of Zenoss Core, an open source monitoring platform.
 # Copyright (C) 2010, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
