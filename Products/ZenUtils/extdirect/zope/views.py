3283e1f580e091ebe48ef47acf36d4d8954a55d5
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-03-15 19:27:45 +0000

    Console warning about invalid extdirect.js content type. Fixes #3319. Reviewed by ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@39689 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenUtils/extdirect/zope/views.py
@@ -0,0 +1,22 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2011, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class ExtDirectJsView(BrowserView):
+    template = ViewPageTemplateFile('extdirect.js.pt')
+
+    def __call__(self, *args, **kwargs):
+        self.request.response.setHeader('Content-Type', 'text/javascript')
+        return self.template()
