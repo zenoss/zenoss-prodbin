fe1444280cb5ca99a3be939fe6df7d8f85ba1eb9
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-03-21 21:28:15 +0000

    Make it easier to make unit tests. Reviewed by Philip
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@39950 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/DataCollector/plugins/DataMaps.py
+++ Products/DataCollector/plugins/DataMaps.py
@@ -35,6 +35,13 @@ class RelationshipMap(PBSafe):
     def append(self, obj):
         self.maps.append(obj)
 
+    def asUnitTest(self):
+        """
+        Return the results of the relationship map as something that can
+        be used directly for unit tests.
+        """
+        return pformat(dict([(map.id, map.asUnitTest()) for map in self.maps]))
+
 pb.setUnjellyableForClass(RelationshipMap, RelationshipMap)
 
 
@@ -51,7 +58,6 @@ class ObjectMap(PBSafe):
     _blockattrs = ('compname', 'modname', 'classname')
     _attrs = []
 
-
     def __init__(self, data={}, compname="", modname="", classname=""):
         self._attrs = []
         self.updateFromDict(data)
@@ -59,12 +65,10 @@ class ObjectMap(PBSafe):
         if modname: self.modname = modname
         if classname: self.classname = classname
 
-
     def __setattr__(self, name, value):
         if name not in self._attrs and not name.startswith("_"):
             self._attrs.append(name)
         self.__dict__[name] = value
-        
         
     def __repr__(self):
         map = {}
@@ -72,19 +76,32 @@ class ObjectMap(PBSafe):
         del map["_attrs"]
         return '<%s %s>' % (self.__class__.__name__, pformat(map))
 
-
     def items(self):
         """Return the name value pairs for this ObjectMap.
         """
         return [ (n, v) for n, v in self.__dict__.items() \
                 if n not in self._blockattrs and n in self._attrs ]
 
-
     def updateFromDict(self, data):
         """Update this ObjectMap from a dictionary's values.
         """
         for key, value in data.items():
             setattr(self, key, value)
+
+    def asUnitTest(self):
+        """
+        Return the results of the object map as something that can
+        be used directly for unit tests.
+        """
+        map = {}
+        map.update(self.__dict__)
+        del map["_attrs"]
+        if not map["classname"]:
+            del map["classname"]
+        if not map["compname"]:
+            del map["compname"]
+        return map
+
 
 pb.setUnjellyableForClass(ObjectMap, ObjectMap)
 
