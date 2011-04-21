36ed7d8da898582aa33e457af4f03866e4df576a
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-04-21 10:44:27 +0000

    augment GPL source headers to be GPL > 2 friendly per Addie; reviewed by tsanders and emiller
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@41634 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/BetterEventDetail.py
+++ Products/ZenEvents/BetterEventDetail.py
@@ -4,16 +4,16 @@
 # Copyright (C) 2007, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
 #
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
 # This program is part of Zenoss Core, an open source monitoring platform.
 # This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
 # For complete information please visit: http://www.zenoss.com/oss/
 from EventDetail import EventDetail
 from Globals import InitializeClass
--- Products/ZenModel/tests/testMigrate.py
+++ Products/ZenModel/tests/testMigrate.py
@@ -4,8 +4,8 @@
 # Copyright (C) 2008, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
 #
 # For complete information please visit: http://www.zenoss.com/oss/
 #
