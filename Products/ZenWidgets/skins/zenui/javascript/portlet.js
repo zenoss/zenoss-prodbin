bbffe794c7d05bd36484380dd5b05a3e74a8cc85
emiller <emiller@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-06-15 21:02:52 +0000

    unbreaking ie dashboard state. reviewed by JosephH
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@44170 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenWidgets/skins/zenui/javascript/portlet.js
+++ Products/ZenWidgets/skins/zenui/javascript/portlet.js
@@ -15,7 +15,7 @@ function register_portlet(klass, name) {
 YZP.register_portlet = register_portlet;
 
 YZP.DEFAULT_SITEWINDOW_URL = YZP.DEFAULT_SITEWINDOW_URL || 
-                             "http://www2.zenoss.com/in-app-welcome-sd?v=4.0.0";
+                             "http://www2.zenoss.com/in-app-welcome-sd";
 
 var isIE//@cc_on=1;
 
