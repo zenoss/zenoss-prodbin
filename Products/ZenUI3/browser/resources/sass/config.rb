20b456897c7181948f54045498ae561d3e0daf0d
tsanders <tsanders@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-10-20 15:13:39 +0000

    flares updated for ext 4 theme. reviewed by jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@49401 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/sass/config.rb
+++ Products/ZenUI3/browser/resources/sass/config.rb
@@ -3,6 +3,7 @@ $ext_path = "/++resource++extjs/"
 ext_filesystem = `python -c "import pkg_resources; import zenoss.extjs; print zenoss.extjs.__path__[0]"`.strip
 load File.join(ext_filesystem, 'src', 'resources', 'themes')
 
+#line_comments = false
 sass_path = dir
 css_path = File.join(dir, "..", "css")
 images_dir = File.join(dir, "..", "img")
@@ -10,4 +11,5 @@ javascripts_dir = File.join(dir, "..", 'js')
 
 http_path = "/"
 output_style = :normal
-environment = :development
+environment = :production
+#development
