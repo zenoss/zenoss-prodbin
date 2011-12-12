c03f53a0a4f83765a327765e9fb8789b2be93030
wgerber <wgerber@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-12-12 14:39:24 +0000

    Fixes #29258:  [ExtJs4] Datagrid shift-select issue by overriding a ExtJS method  (reviewed by Joseph Hanson)
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@51703 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/zenoss/ExtOverrides.js
+++ Products/ZenUI3/browser/resources/js/zenoss/ExtOverrides.js
@@ -103,7 +103,105 @@
         }
     };
 
+    /**
+     * The Ext.grid.Panel component row selection has a flaw in it:
 
+     Steps to recreate:
+     1. Create a standard Ext.grid.Panel with multiple records in it and turn "multiSelect: true"
+     Note that you can just go to the documentation page
+     http://docs.sencha.com/ext-js/4-0/#!/api/Ext.grid.Panel and insert the multiSelect:
+     true line right into there and flip to live preview.
+
+     2. Select the top row, then press and hold shift and click on the second row, then the third row,
+     then the fourth. You would expect to see all 4 rows selected but instead you just get the last two.
+
+     3. For reference, release the shift and select the bottom row (4th row). Now press and hold shift
+     and select the 3rd row, then the 2nd row, then the 1st row. You now see all four rows selected.
+
+     To Fix this I have to override the Ext.selection.Model to handle the top down versus bottom up selection.
+     *
+     */
+    Ext.override(Ext.selection.Model, {
+        /**
+         * Selects a range of rows if the selection model {@link #isLocked is not locked}.
+         * All rows in between startRow and endRow are also selected.
+         * @param {Ext.data.Model/Number} startRow The record or index of the first row in the range
+         * @param {Ext.data.Model/Number} endRow The record or index of the last row in the range
+         * @param {Boolean} keepExisting (optional) True to retain existing selections
+         */
+        selectRange : function(startRow, endRow, keepExisting, dir){
+            var me = this,
+                store = me.store,
+                selectedCount = 0,
+                i,
+                tmp,
+                dontDeselect,
+                records = [];
+
+            if (me.isLocked()){
+                return;
+            }
+
+            if (!keepExisting) {
+                me.deselectAll(true);
+            }
+
+            if (!Ext.isNumber(startRow)) {
+                startRow = store.indexOf(startRow);
+            }
+            if (!Ext.isNumber(endRow)) {
+                endRow = store.indexOf(endRow);
+            }
+
+            // WG: create a flag to see if we are swapping
+            var swapped = false;
+            // ---
+
+            // swap values
+            if (startRow > endRow){
+                // WG:  set value to true for my flag
+                swapped = true;
+                // ----
+                tmp = endRow;
+                endRow = startRow;
+                startRow = tmp;
+            }
+
+            for (i = startRow; i <= endRow; i++) {
+                if (me.isSelected(store.getAt(i))) {
+                    selectedCount++;
+                }
+            }
+
+            if (!dir) {
+                dontDeselect = -1;
+            } else {
+                dontDeselect = (dir == 'up') ? startRow : endRow;
+            }
+
+            for (i = startRow; i <= endRow; i++){
+                if (selectedCount == (endRow - startRow + 1)) {
+                    if (i != dontDeselect) {
+                        me.doDeselect(i, true);
+                    }
+                } else {
+                    records.push(store.getAt(i));
+                }
+            }
+
+            //WG:  START  CHANGE
+            // This is my fix, we need to flip the order
+            // for it to correctly track what was selected first.
+            if(!swapped){
+                records.reverse();
+            }
+            //WG:  END CHANGE
+
+
+
+            me.doMultiSelect(records, true);
+        }
+    });
 
 
 }());
