8d8bd985738c5fbd1c9aff0146a80fd256e4127f
jpeacock <jpeacock@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-01-11 20:36:09 +0000

    (reviewed by pwarren) remove reactor replacement in zenhub.py
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@52800 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenHub/zenhub.py
+++ Products/ZenHub/zenhub.py
@@ -18,41 +18,6 @@ Provide remote, authenticated, and possibly encrypted two-way
 communications with the Model and Event databases.
 """
 
-import sys
-
-def _installReactor(desiredReactor):
-    """
-    Install the given reactor as twisted's reactor.
-    """
-    currentReactor = sys.modules.get("twisted.internet.reactor")
-    if currentReactor is desiredReactor:
-        return
-    if currentReactor:
-        # Raise this exception having a more useful message than the
-        # exception raised by twisted.
-        from twisted.internet import error
-        raise error.ReactorAlreadyInstalledError(
-                "Do not import module %s if twisted.internet.reactor "
-                "is already imported." % __name__
-            )
-    desiredReactor.install()
-
-# Install the 'best' reactor available.  If the desired reactor is not
-# available, an ImportError is raised.  Tries to install epoll first, then
-# poll, and if neither are available, the default select reactor will
-# install when twisted.internet.reactor is imported later in this module.
-try:
-    from select import epoll
-    from twisted.internet import epollreactor
-    _installReactor(epollreactor)
-except ImportError:
-    try:
-        from select import poll
-        from twisted.internet import pollreactor
-        _installReactor(pollreactor)
-    except ImportError:
-        pass
-
 import Globals
 
 from XmlRpcService import XmlRpcService
