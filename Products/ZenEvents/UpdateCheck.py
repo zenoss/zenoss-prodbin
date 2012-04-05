93a26a06d04c0ec05cee643ba8249f5cd47b4159
epowell <epowell@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-04-05 21:20:49 +0000

    Fixes ZEN-983: Add an empty module for zenpack compatibility on upgrade. Reviewed by jcausey.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@56371 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ Products/ZenEvents/UpdateCheck.py
@@ -0,0 +1,18 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2012, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+# TODO: This is a placeholder for zenpack compatibility in the upgrade
+# scenario. Once we no longer need to support upgrades from any time before
+# v4.2, this can be removed.
+class UpdateCheck(object):
+    pass
