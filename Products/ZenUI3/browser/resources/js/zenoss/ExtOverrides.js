61d5470663b62ff1f93d569d856c749945761590
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-05-03 22:44:31 +0000

    ZEN-97: Fix Ext bug that led to miscalculation of grid body height. Reviewed by Tim.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@57729 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/zenoss/ExtOverrides.js
+++ Products/ZenUI3/browser/resources/js/zenoss/ExtOverrides.js
@@ -242,6 +242,115 @@
         }
     });
 
+    /*
+    * This is a fix to a bug in Ext. When the scrollbar height is determined,
+    * it takes into account the toolbar, column headers and filter row, for
+    * lord knows what reason. This overrides in order to use the height of the
+    * grid /body/ instead of the entire grid. The function is copied except one
+    * line
+    */
+    Ext.override(Ext.panel.Table, {
+       determineScrollbars: function() {
+           var me = this,
+               box,
+               tableEl,
+               scrollWidth,
+               clientWidth,
+               scrollHeight,
+               clientHeight,
+               verticalScroller = me.verticalScroller,
+               horizontalScroller = me.horizontalScroller,
+               curScrollbars = (verticalScroller   && verticalScroller.ownerCt === me ? 1 : 0) |
+                               (horizontalScroller && horizontalScroller.ownerCt === me ? 2 : 0),
+               reqScrollbars = 0; // 1 = vertical, 2 = horizontal, 3 = both
+
+           // If we are not collapsed, and the view has been rendered AND filled, then we can determine scrollbars
+           if (!me.collapsed && me.view && me.view.el && me.view.el.dom.firstChild && !me.changingScrollBars) {
+
+               // Calculate maximum, *scrollbarless* space which the view has available.
+               // It will be the Fit Layout's calculated size, plus the widths of any currently shown scrollbars
+
+               /********************************
+               *
+               * BEGIN ZENOSS CHANGE
+               *
+               *********************************/
+               // box = me.getSize();
+               box = me.view.getSize();
+               /********************************
+               *
+               * END ZENOSS CHANGE
+               *
+               *********************************/
+
+
+               clientWidth  = box.width  + ((curScrollbars & 1) ? verticalScroller.width : 0);
+               clientHeight = box.height + ((curScrollbars & 2) ? horizontalScroller.height : 0);
+
+               // Calculate the width of the scrolling block
+               // There will never be a horizontal scrollbar if all columns are flexed.
+
+               scrollWidth = (me.headerCt.query('[flex]').length && !me.headerCt.layout.tooNarrow) ? 0 : me.headerCt.getFullWidth();
+
+               // Calculate the height of the scrolling block
+               if (verticalScroller && verticalScroller.el) {
+                   scrollHeight = verticalScroller.getSizeCalculation().height;
+               } else {
+                   tableEl = me.view.el.child('table', true);
+                   scrollHeight = tableEl ? tableEl.offsetHeight : 0;
+               }
+
+               // View is too high.
+               // Definitely need a vertical scrollbar
+               if (scrollHeight > clientHeight) {
+                   reqScrollbars = 1;
+
+                   // But if scrollable block width goes into the zone required by the vertical scrollbar, we'll also need a horizontal
+                   if (horizontalScroller && ((clientWidth - scrollWidth) < verticalScroller.width)) {
+                       reqScrollbars = 3;
+                   }
+               }
+
+               // View height fits. But we stil may need a horizontal scrollbar, and this might necessitate a vertical one.
+               else {
+                   // View is too wide.
+                   // Definitely need a horizontal scrollbar
+                   if (scrollWidth > clientWidth) {
+                       reqScrollbars = 2;
+
+                       // But if scrollable block height goes into the zone required by the horizontal scrollbar, we'll also need a vertical
+                       if (verticalScroller && ((clientHeight - scrollHeight) < horizontalScroller.height)) {
+                           reqScrollbars = 3;
+                       }
+                   }
+               }
+
+               // If scrollbar requirements have changed, change 'em...
+               if (reqScrollbars !== curScrollbars) {
+
+                   // Suspend component layout while we add/remove the docked scrollers
+                   me.suspendLayout = true;
+                   if (reqScrollbars & 1) {
+                       me.showVerticalScroller();
+                   } else {
+                       me.hideVerticalScroller();
+                   }
+                   if (reqScrollbars & 2) {
+                       me.showHorizontalScroller();
+                   } else {
+                       me.hideHorizontalScroller();
+                   }
+                   me.suspendLayout = false;
+               }
+               // Lay out the Component.
+               // Set a flag so that afterComponentLayout does not recurse back into here.
+               me.changingScrollBars = true;
+               me.doComponentLayout();
+               me.changingScrollBars = false;
+           }
+       }
+    });
+
     /**
      * This is a workaround to make scrolling smoother
      **/
@@ -249,7 +358,6 @@
     Ext.override(Ext.grid.PagingScroller, {
         // The default fetch percent is 35% this is a little too early for a smooth scrolling experience
        percentageFromEdge: 0.15
-
    }); 
    
    /**
