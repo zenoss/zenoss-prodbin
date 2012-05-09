b8fcb7c5f66f313ff6f5e289a56e783fa4f61e89
jplouis <jplouis@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-05-09 19:44:54 +0000

    fixes ZEN-1636. restore class for backwards compatibility during migrate. Reviewed by jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@58034 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/Jobber/status.py
+++ Products/Jobber/status.py
@@ -2,4 +2,12 @@
 Retained for backwards compatibility. Will be removed in the release following
 4.2.
 """
+from Products.ZenModel.ZenModelRM import ZenModelRM
+
 SUCCESS = None
+
+class JobStatus(ZenModelRM):
+    """
+    Here for backwards compatibility
+    """
+    pass
