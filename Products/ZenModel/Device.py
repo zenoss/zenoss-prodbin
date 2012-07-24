7e01779e83003c5f65b87b1dc02b5779d9db1314
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 13:56:34 +0000

    fixes ZEN-2724 change to the old style field index instead of the manageindexes field index reviewed by ian
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@61831 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/Device.py
+++ Products/ZenModel/Device.py
@@ -1,11 +1,11 @@
 
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -552,7 +552,7 @@ class Device(ManagedEntity, Commandable, Lockable, MaintenanceWindowable,
         cat.addIndex('meta_type', makeCaseInsensitiveFieldIndex('meta_type'))
         cat.addIndex('getCollectors',
             makeCaseInsensitiveKeywordIndex('getCollectors'))
-        zcat.addIndex('monitored', FieldIndex('monitored'))
+        zcat.addIndex('monitored', 'FieldIndex')
         zcat.addColumn('meta_type')
         zcat.addColumn('getUUID')
         for c in self.getDeviceComponentsNoIndexGen():
