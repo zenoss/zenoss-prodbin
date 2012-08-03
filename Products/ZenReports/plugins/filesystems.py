085499daa4fbad6e993bf6dbe0e406f81c6206fe
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-03 19:13:22 +0000

    reviewed by jcausey fixes ZEN-2863 do not show errors if there isn't enough data for the filesystem report
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@62319 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenReports/plugins/filesystems.py
+++ Products/ZenReports/plugins/filesystems.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -22,12 +22,12 @@ class filesystems( AliasPlugin ):
                  Column( 'mount', PythonColumnHandler( 'component.mount' ) ),
                  Column( 'usedBytes', RRDColumnHandler( 'usedFilesystemSpace__bytes' ) ),
                  Column( 'totalBytes', PythonColumnHandler( 'component.totalBytes()' ) ) ]
-    
+
     def getCompositeColumns(self):
-        return [ Column( 'availableBytes', PythonColumnHandler('totalBytes - usedBytes') ),
+        return [ Column( 'availableBytes', PythonColumnHandler('totalBytes - usedBytes if totalBytes is not None and usedBytes is not None else 0') ),
                  Column( 'percentFull', PythonColumnHandler( 'totalBytes and ' +
                         '( 100 - float(availableBytes) * 100 / float(totalBytes) )' +
                         ' or None' ) ) ]
-    
+
     def getComponentPath(self):
         return 'os/filesystems'
