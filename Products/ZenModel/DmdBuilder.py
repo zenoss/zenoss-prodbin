64c3a186207f5d15d575192ddbcabea545de0d1b
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-13 13:59:44 +0000

    fixes ZEN-3008 user commands changes were not reflected in dmdbuilder reviewed by jpeacock
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@62705 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/DmdBuilder.py
+++ Products/ZenModel/DmdBuilder.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -125,15 +125,15 @@ class DmdBuilder(object):
 
     def buildUserCommands(self):
         for id, cmd, desc in (
-                ('ping', 'dcsh --collector=${device/getPerformanceServerName} -n "${device/pingCommand} -c2 ${device/manageIp}"',
+                ('ping', '${device/pingCommand} -c2 ${device/manageIp}',
                  "Is the device responding to ping?"),
-                ('traceroute', 'dcsh --collector=${device/getPerformanceServerName} -n "${device/tracerouteCommand} -q 1 -w 2 ${device/manageIp}"',
+                ('traceroute', '${device/tracerouteCommand} -q 1 -w 2 ${device/manageIp}',
                  "Show the route to the device"),
-                ('DNS forward', 'dcsh --collector=${device/getPerformanceServerName} -n "host ${device/id}"',
+                ('DNS forward', 'host ${device/id}',
                  "Name to IP address lookup"),
-                ('DNS reverse', 'dcsh --collector=${device/getPerformanceServerName} -n "host ${device/manageIp}"',
+                ('DNS reverse', 'host ${device/manageIp}',
                  "IP address to name lookup"),
-                ('snmpwalk', 'dcsh --collector=${device/getPerformanceServerName} -n "snmpwalk -${device/zSnmpVer} -c${device/zSnmpCommunity} ${device/snmpwalkPrefix}${here/manageIp} system"',
+                ('snmpwalk', 'snmpwalk -${device/zSnmpVer} -c${device/zSnmpCommunity} ${device/snmpwalkPrefix}${here/manageIp} system',
                  "Display the OIDs available on a device"),
                 ):
             self.dmd.manage_addUserCommand(id, cmd=cmd, desc=desc)
--- Products/ZenModel/migrate/removedcshFromCommands.py
+++ Products/ZenModel/migrate/removedcshFromCommands.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2011, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -20,7 +20,7 @@ import Migrate
 
 
 class RemoveDCSHFromCommand(Migrate.Step):
-    version = Migrate.Version(4, 2, 0)
+    version = Migrate.Version(4, 2, 70)
 
     def cutover(self, dmd):
         # make all of the default commands work over different collectors
