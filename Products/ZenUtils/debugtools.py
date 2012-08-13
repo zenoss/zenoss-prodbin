f21243486a592c41dc2030ace96515b325093390
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-13 15:05:05 +0000

    typo in print_stats reviewed by pwarren
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@62713 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/debugtools.py
+++ Products/ZenUtils/debugtools.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -22,7 +22,7 @@ class Profiler(object):
         return self._stats
 
     def print_stats(self, limit=20):
-        if self_stats:
+        if self._stats:
             self._stats.sort_stats('time', 'calls')
             self._stats.print_stats(limit)
 
