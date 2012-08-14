8841ed36cf37b26b92f910bdce60c267c365f1fe
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-14 13:06:28 +0000

    fixes ZEN-3029 reviewed by ian make sure there is a valid not in organizer response before flaring
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@62765 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/zenoss/itinfrastructure.js
+++ Products/ZenUI3/browser/resources/js/zenoss/itinfrastructure.js
@@ -1,10 +1,10 @@
 /*****************************************************************************
- * 
+ *
  * Copyright (C) Zenoss, Inc. 2009, all rights reserved.
- * 
+ *
  * This content is made available according to terms specified in
  * License.zenoss under the directory where your Zenoss product is installed.
- * 
+ *
  ****************************************************************************/
 
 
@@ -248,7 +248,7 @@ function deleteDevicesWithProgressBar(grid, uids, opts, callback){
         total = remaining.length;
 
     function deleteDevice(response) {
-        if (response && response.notRemovedUids) {
+        if (response && !Ext.isEmpty(response.notRemovedUids)) {
             var notRemovedId = response.notRemovedUids[0].replace("/zport/dmd/", "/");
 
             // if we weren't able to remove this device id then warn about it. This can happen
