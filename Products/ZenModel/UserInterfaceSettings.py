d1dadb966bd1a65ee600fc467358de1e4685631c
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-16 20:55:10 +0000

    Fixes ZEN-3078: Make the live search default to true. Reviewed by Joseph.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@62914 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/UserInterfaceSettings.py
+++ Products/ZenModel/UserInterfaceSettings.py
@@ -40,7 +40,7 @@ class UserInterfaceSettings(ZenModelRM):
 
     # information about the properties that is used for the UI
     _propertyMetaData = {
-        'enableLiveSearch': {'xtype': 'checkbox', 'name': _t('Enable Live Filters'), 'defaultValue': False},
+        'enableLiveSearch': {'xtype': 'checkbox', 'name': _t('Enable Live Filters'), 'defaultValue': True},
         'incrementalTreeLoad': {'xtype': 'checkbox', 'name': _t('Enable Incremental Tree Loading on the Infrastructure Page'), 'defaultValue': True},
         'enableTreeFilters': {'xtype': 'checkbox', 'name': _t('Enable Tree Filters'), 'defaultValue': True},
         'deviceGridBufferSize': {'xtype': 'numberfield', 'name': _t('Device Grid Buffer Size'), 'defaultValue': 100, 'minValue': 50, 'maxValue': 1000, 'allowBlank': False},
