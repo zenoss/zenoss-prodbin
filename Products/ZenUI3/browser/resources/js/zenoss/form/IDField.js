be3a756bc1738c47ef9df9c8d57d429de43d49b8
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-11-12 19:34:12 +0000

    reviewed by ian fixes ZEN-4117 allow commas in the device id without breaking the device details page
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@65964 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUI3/browser/resources/js/zenoss/form/IDField.js
+++ Products/ZenUI3/browser/resources/js/zenoss/form/IDField.js
@@ -1,10 +1,10 @@
 /*****************************************************************************
- * 
+ *
  * Copyright (C) Zenoss, Inc. 2010, all rights reserved.
- * 
+ *
  * This content is made available according to terms specified in
  * License.zenoss under the directory where your Zenoss product is installed.
- * 
+ *
  ****************************************************************************/
 
 
