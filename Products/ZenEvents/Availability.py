42ee4b7560429ce8e4ef237cbedde0e214f34a39
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-11-15 13:25:55 +0000

    fixes ZEN-4212 reviewed by glenn account for events that come in tagged with the name instead of the id as the identifier
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@66125 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenEvents/Availability.py
+++ Products/ZenEvents/Availability.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -13,6 +13,7 @@ import logging
 from itertools import takewhile, chain
 
 from Globals import InitializeClass
+from collections import defaultdict
 from Products.ZenUtils import Map
 from Products.ZenUtils.guid.interfaces import IGlobalIdentifier
 from Products.ZenEvents.ZenEventClasses import Status_Ping, Status_Snmp
@@ -107,7 +108,7 @@ class Availability(object):
 
     def __float__(self):
         return float(self.availability)
-    
+
     def __int__(self):
         return int(self.availability * 100)
 
@@ -213,7 +214,7 @@ class Report(object):
 
         deviceList = {}
         tag_uuids = []
-        accumulator = {}
+        accumulator = defaultdict(int)
         for brain in results:
             try:
                 obj = brain.getObject()
