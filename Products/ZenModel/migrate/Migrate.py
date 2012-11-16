f0e1f6de963729cd90c4ed7289b6798c4f9e33d5
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-11-16 14:45:47 +0000

    fixes ZEN-4275 reviewed by jhuie when migrating make sure we force the reports to be loaded this will help when we change rpt files for dumping the database in when upgrading
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@66189 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenModel/migrate/Migrate.py
+++ Products/ZenModel/migrate/Migrate.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -295,6 +295,8 @@ class Migration(ZenScriptBase):
         if not self.options.steps:
             self.message('Loading Reports')
             rl = ReportLoader(noopts=True, app=self.app)
+            # when reports change make sure the new version is loaded during the migrate
+            rl.options.force = True
             rl.options.logseverity = self.options.logseverity + 10
             rl.setupLogging()
             rl.loadDatabase()
