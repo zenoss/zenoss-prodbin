d63ee9a33bcae439f92c94f84d58187764e9939a
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-11-27 14:36:19 +0000

    fixes ZEN-4357 reviewed by jhuie was sending the string back to the client when the number was expected
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@66444 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/Zuul/infos/template.py
+++ Products/Zuul/infos/template.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2010, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -271,7 +271,7 @@ class RRDDataSourceInfo(InfoBase):
         self._object.severity = value
 
     def _getSeverity(self):
-        return self._object.getSeverityString()
+        return self._object.severity
 
     @property
     def newId(self):
