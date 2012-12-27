1eeaf21b8291af71a8fd842edb8774b76d9a73a1
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-12-27 19:37:26 +0000

    Fixes ZEN-4855 Add internationalization wrappers around components, add IHardDiskInfo. Reviewed by Evan Powell
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@67743 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/Zuul/interfaces/component.py
+++ Products/Zuul/interfaces/component.py
@@ -11,6 +11,8 @@
 from zope.interface import Attribute, Interface
 from Products.Zuul.interfaces import IInfo
 from Products.Zuul.form import schema
+from Products.Zuul.utils import ZuulMessageFactory as _t
+
 
 class IComponent(Interface):
     """
@@ -29,7 +31,7 @@ class IComponentInfo(IInfo):
     components are OSProcesses, IPServices and WinServices.
     """
     device = Attribute("Parent Device")
-    status = schema.TextLine(title=u"Status",
+    status = schema.TextLine(title=_t(u"Status"),
                              description=u"Are there any active status events"
                                          u" for this component?", group="Overview",
                              order=1,
@@ -46,54 +48,54 @@ class IIpInterfaceInfo(IComponentInfo):
     Info adapter for IPInterface components.
     """
     interfaceName = schema.TextLine(
-        title=u"Interface Name", group="Overview",
+        title=_t(u"Interface Name"), group="Overview",
         order=1)
 
     description = schema.Text(
-        title=u"Description", group="Overview",
+        title=_t(u"Description"), group="Overview",
         order=2)
 
     adminStatus = schema.TextLine(
-        title=u"Administrative Status", group="Overview",
+        title=_t(u"Administrative Status"), group="Overview",
         readonly=True, order=3)
 
     operStatus = schema.TextLine(
-        title=u"Operational Status", group="Overview",
+        title=_t(u"Operational Status"), group="Overview",
         readonly=True, order=4)
 
     status = schema.TextLine(
-        title=u"Status", group="Overview",
+        title=_t(u"Status"), group="Overview",
         description=u"Are there any active status events for this component?"
                     u" for this component?",
         readonly=True, order=5)
 
     ipAddress = schema.Entity(
-        title=u"IP Address (Primary)", group="Overview",
+        title=_t(u"IP Address (Primary)"), group="Overview",
         description=u"Primary IP address",
         order=6)
 
     ipAddresses = schema.List(
-        title=u'IP Addresses (All)', group="Details",
+        title=_t(u'IP Addresses (All)'), group="Details",
         order=7)
 
     macaddress = schema.TextLine(
-        title=u"MAC Address", group="Details",
+        title=_t(u"MAC Address"), group="Details",
         order=8)
 
     type = schema.TextLine(
-        title=u"Type", group="Details",
+        title=_t(u"Type"), group="Details",
         order=9)
 
     speed = schema.TextLine(
-        title=u"Speed", group="Details", readonly=True,
+        title=_t(u"Speed"), group="Details", readonly=True,
         order=10)
 
     duplex = schema.TextLine(
-        title=u"Duplex Mode", group="Details",
+        title=_t(u"Duplex Mode"), group="Details",
         readonly=True, order=11)
 
     mtu = schema.TextLine(
-        title=u"MTU", group="Details",
+        title=_t(u"MTU"), group="Details",
         order=12)
 
 
@@ -101,39 +103,39 @@ class IFileSystemInfo(IComponentInfo):
     """
     Info adapter for FileSystem components.
     """
-    mount = schema.TextLine(title=u"Mount Point", group="Overview", order=-1)
-    storageDevice = schema.TextLine(title=u"Storage Device", group="Details")
-    type = schema.TextLine(title=u"Type", group="Details")
-    blockSize = schema.Int(title=u"Block Size", group="Details")
+    mount = schema.TextLine(title=_t(u"Mount Point"), group="Overview", order=-1)
+    storageDevice = schema.TextLine(title=_t(u"Storage Device"), group="Details")
+    type = schema.TextLine(title=_t(u"Type"), group="Details")
+    blockSize = schema.Int(title=_t(u"Block Size"), group="Details")
     totalBlocks = Attribute("Total Blocks")
-    totalBytes = schema.Int(title=u"Total Bytes", readonly=True,
+    totalBytes = schema.Int(title=_t(u"Total Bytes"), readonly=True,
                             group="Details")
-    usedBytes = schema.Int(title=u"Used Bytes", readonly=True, group="Details")
-    availableBytes = schema.Int(title=u"Available Bytes", readonly=True,
+    usedBytes = schema.Int(title=_t(u"Used Bytes"), readonly=True, group="Details")
+    availableBytes = schema.Int(title=_t(u"Available Bytes"), readonly=True,
                                 group="Details")
-    capacityBytes = schema.Int(u"Capacity Bytes", readonly=True,
+    capacityBytes = schema.Int(title=_t(u"Capacity Bytes"), readonly=True,
                                group="Details")
-    totalFiles = schema.Int(title=u"Total Files", group="Details")
-    availableFiles = schema.Int(title=u"Available Files", readonly=True,
+    totalFiles = schema.Int(title=_t(u"Total Files"), group="Details")
+    availableFiles = schema.Int(title=_t(u"Available Files"), readonly=True,
                                 group="Details")
-    capacityFiles = schema.Int(title=u"Capacity Files", readonly=True,
+    capacityFiles = schema.Int(title=_t(u"Capacity Files"), readonly=True,
                                group="Details")
-    maxNameLength = schema.Int(title=u"Maximum Name Length", group="Details")
+    maxNameLength = schema.Int(title=_t(u"Maximum Name Length"), group="Details")
 
 
 class IOSProcessInfo(IComponentInfo):
     """
     Info adapter for OSProcess components.
     """
-    processClass = schema.Entity(title=u"Process Class", group="Overview",
+    processClass = schema.Entity(title=_t(u"Process Class"), group="Overview",
                                  order=1)
-    processName = schema.TextLine(title=u"Process Name", group="Overview",
+    processName = schema.TextLine(title=_t(u"Process Name"), group="Overview",
                                   readonly=True, order=-1)
-    description = schema.Text(title=u"Description", group="Overview",
+    description = schema.Text(title=_t(u"Description"), group="Overview",
                               readonly=True, order=2)
-    alertOnRestart = schema.Bool(title=u"Alert on Restart", group="Details",
+    alertOnRestart = schema.Bool(title=_t(u"Alert on Restart"), group="Details",
                                  alwaysEditable=True)
-    failSeverity = schema.Int(title=u"Fail Severity", xtype="severity",
+    failSeverity = schema.Int(title=_t(u"Fail Severity"), xtype="severity",
                               group="Details", alwaysEditable=True)
 
 
@@ -141,36 +143,36 @@ class IWinServiceInfo(IComponentInfo):
     """
     Info adapter for WinService components.
     """
-    serviceName = schema.Text(title=u"Name", group="Overview")
-    serviceClass = schema.Entity(title=u"Service Class", group="Overview")
-    caption = schema.TextLine(title=u"Caption", group="Overview")
-    command = schema.TextLine(title=u"Command", group="Overview")
-    failSeverity = schema.Int(title=u"Fail Severity", xtype="severity",
+    serviceName = schema.Text(title=_t(u"Name"), group="Overview")
+    serviceClass = schema.Entity(title=_t(u"Service Class"), group="Overview")
+    caption = schema.TextLine(title=_t(u"Caption"), group="Overview")
+    command = schema.TextLine(title=_t(u"Command"), group="Overview")
+    failSeverity = schema.Int(title=_t(u"Fail Severity"), xtype="severity",
                               group="Details", alwaysEditable=True)
-    serviceType = schema.TextLine(title=u"Service Type", group="Details")
-    startMode = schema.TextLine(title=u"Start Mode", group="Details")
-    startName = schema.TextLine(title=u"Start Name", group="Details")
-    pathName = schema.TextLine(title=u"Path Name", group="Details")
+    serviceType = schema.TextLine(title=_t(u"Service Type"), group="Details")
+    startMode = schema.TextLine(title=_t(u"Start Mode"), group="Details")
+    startName = schema.TextLine(title=_t(u"Start Name"), group="Details")
+    pathName = schema.TextLine(title=_t(u"Path Name"), group="Details")
 
 
 class IIpServiceInfo(IComponentInfo):
     """
     Info adapter for IpService components
     """
-    description = schema.Text(title=u"Description",
+    description = schema.Text(title=_t(u"Description"),
                               group="Overview")
-    serviceClass = schema.Entity(title=u"Service Class", group="Overview")
-    port = schema.Int(title=u"Port", group="Overview")
-    protocol = schema.TextLine(title=u"Protocol", group="Details")
-    ipaddresses = schema.List(title=u"IP Addresses", group="Details")
-    manageIp = schema.Choice(title=u"Management IP Address",
+    serviceClass = schema.Entity(title=_t(u"Service Class"), group="Overview")
+    port = schema.Int(title=_t(u"Port"), group="Overview")
+    protocol = schema.TextLine(title=_t(u"Protocol"), group="Details")
+    ipaddresses = schema.List(title=_t(u"IP Addresses"), group="Details")
+    manageIp = schema.Choice(title=_t(u"Management IP Address"),
                              vocabulary="serviceIpAddresses",
                              group="Overview")
-    discoveryAgent = schema.TextLine(title=u"Discovery Agent", group="Details")
-    failSeverity = schema.Int(title=u"Fail Severity", xtype="severity",
+    discoveryAgent = schema.TextLine(title=_t(u"Discovery Agent"), group="Details")
+    failSeverity = schema.Int(title=_t(u"Fail Severity"), xtype="severity",
                               group="Details", alwaysEditable=True)
-    sendString = schema.Text(title=u"Send String", group="Details")
-    expectRegex = schema.TextLine(title=u"Expect Regex", group="Details")
+    sendString = schema.Text(title=_t(u"Send String"), group="Details")
+    expectRegex = schema.TextLine(title=_t(u"Expect Regex"), group="Details")
 
 
 
@@ -178,63 +180,73 @@ class IIpRouteEntryInfo(IComponentInfo):
     """
     Info adapter for IpRouteEntry components.
     """
-    destination = schema.Entity(title=u'Destination', readonly=True,
+    destination = schema.Entity(title=_t(u'Destination'), readonly=True,
                                 group="Overview")
-    nextHop = schema.Entity(title=u"Next Hop", readonly=True, group="Overview")
-    interface = schema.Entity(title=u"Interface", readonly=True,
+    nextHop = schema.Entity(title=_t(u"Next Hop"), readonly=True, group="Overview")
+    interface = schema.Entity(title=_t(u"Interface"), readonly=True,
                               group="Overview")
-    protocol = schema.TextLine(title=u"Protocol", readonly=True, group="Overview")
-    type = schema.TextLine(title=u"Type", readonly=True, group="Overview")
+    protocol = schema.TextLine(title=_t(u"Protocol"), readonly=True, group="Overview")
+    type = schema.TextLine(title=_t(u"Type"), readonly=True, group="Overview")
 
 
 class ICPUInfo(IComponentInfo):
     """
-    Info adapter for IpRouteEntry components.
+    Info adapter for CPU components.
     """
-    socket = schema.Int(title=u"Socket", readonly=True)
-    clockspeed = schema.Int(title=u"Clock Speed", readonly=True)
-    extspeed = schema.Int(title=u"Ext Speed", readonly=True)
-    voltage = schema.Int(title=u"Voltage", readonly=True)
-    cacheSizeL1 = schema.Int(title=u"L1", readonly=True)
-    cacheSizeL2 = schema.Int(title=u"L2", readonly=True)
-    product = schema.Entity(title=u"Model", readonly=True)
-    manufacturer = schema.Entity(title=u"Manufacturer", readonly=True)
+    socket = schema.Int(title=_t(u"Socket"), readonly=True)
+    clockspeed = schema.Int(title=_t(u"Clock Speed"), readonly=True)
+    extspeed = schema.Int(title=_t(u"Ext Speed"), readonly=True)
+    voltage = schema.Int(title=_t(u"Voltage"), readonly=True)
+    cacheSizeL1 = schema.Int(title=_t(u"L1"), readonly=True)
+    cacheSizeL2 = schema.Int(title=_t(u"L2"), readonly=True)
+    product = schema.Entity(title=_t(u"Model"), readonly=True)
+    manufacturer = schema.Entity(title=_t(u"Manufacturer"), readonly=True)
 
 
 class IExpansionCardInfo(IComponentInfo):
     """
     Info adapter for ExpansionCard components.
     """
-    slot = schema.TextLine(title=u'Slot', group='Overview', readonly=True)
-    serialNumber = schema.TextLine(title=u'Serial Number', readonly=True)
-    product = schema.Entity(title=u'Model', readonly=True)
-    manufacturer = schema.Entity(title=u'Manufacturer', readonly=True)
+    slot = schema.TextLine(title=_t(u'Slot'), group='Overview', readonly=True)
+    serialNumber = schema.TextLine(title=_t(u'Serial Number'), readonly=True)
+    product = schema.Entity(title=_t(u'Model'), readonly=True)
+    manufacturer = schema.Entity(title=_t(u'Manufacturer'), readonly=True)
 
 
 class IPowerSupplyInfo(IComponentInfo):
     """
     Info adapter for PowerSupply components.
     """
-    watts = schema.Int(title=u'Watts', group='Overview', readonly=True)
-    type = schema.TextLine(title=u'Type', group='Overview', readonly=True)
-    state = schema.TextLine(title=u'State', group='Overview', readonly=True)
+    watts = schema.Int(title=_t(u'Watts'), group='Overview', readonly=True)
+    type = schema.TextLine(title=_t(u'Type'), group='Overview', readonly=True)
+    state = schema.TextLine(title=_t(u'State'), group='Overview', readonly=True)
     millivolts = schema.Int(
-        title=u'Millivolts', group='Overview', readonly=True)
+        title=_t(u'Millivolts'), group='Overview', readonly=True)
 
 
 class ITemperatureSensorInfo(IComponentInfo):
     """
     Info adapter for TemperatureSensor components.
     """
-    state = schema.TextLine(title=u'State', group='Overview', readonly=True)
+    state = schema.TextLine(title=_t(u'State'), group='Overview', readonly=True)
     temperature = schema.Int(
-        title=u'Temperature (Fahrenheit)', group='Overview', readonly=True)
+        title=_t(u'Temperature (Fahrenheit)'), group='Overview', readonly=True)
 
 
 class IFanInfo(IComponentInfo):
     """
     Info adapter for Fan components.
     """
-    state = schema.TextLine(title=u'State', group='Overview', readonly=True)
-    type = schema.TextLine(title=u'Type', group='Overview', readonly=True)
-    rpm = schema.Int(title=u'RPM', group='Overview', readonly=True)
+    state = schema.TextLine(title=_t(u'State'), group='Overview', readonly=True)
+    type = schema.TextLine(title=_t(u'Type'), group='Overview', readonly=True)
+    rpm = schema.Int(title=_t(u'RPM'), group='Overview', readonly=True)
+
+
+class IHardDiskInfo(IComponentInfo):
+    """
+    Info adapter for HardDisk components.
+    """
+    description = schema.Text(
+        title=_t(u"Description"), group="Overview",
+        order=2)
+
