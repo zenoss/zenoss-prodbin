013fda5d49cbe156b24b35bacf5b21adb9f7fad1
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2013-01-22 20:32:25 +0000

    Fix log_tb after initial call. Reviewed by Phil B.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core@68654 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- Products/ZenUtils/tbdetail.py
+++ Products/ZenUtils/tbdetail.py
@@ -1,12 +1,11 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2012, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
-
 
 import os
 import re
@@ -17,13 +16,20 @@ from Products.ZenUtils.Utils import unused, zenPath
 
 unused(Globals)
 
-pattern = re.compile(r'<p> (?P<message>.* contains the description of this error.)')
+_PATTERN = re.compile(
+            r'<p> (?P<message>.* contains the description of this error.)')
+
+_LOG_DIR = zenPath('log', 'tracebacks')
+
 
 def log_tb(exc_info):
+    """log a detailed traceback to $ZENHOME/log/tracebacks. This traceback
+    include the value of parameters in addition to the call stack."""
     file_ = StringIO()
-    hook = cgitb.Hook(display=0, logdir=zenPath('log', 'tracebacks'), file=file_, format='text')
-    os.makedirs(hook.logdir)
+    hook = cgitb.Hook(display=0, logdir=_LOG_DIR, file=file_, format='text')
+    if not os.path.isdir(hook.logdir):
+        os.makedirs(hook.logdir)
     hook.handle(exc_info)
     output = file_.getvalue()
-    match = pattern.search(output)
+    match = _PATTERN.search(output)
     return match.group('message')
---
+++ Products/ZenUtils/tests/test_tbdetail.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2012, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""runtests -m test_tbdetail -n test_log_tb Products.ZenUtils"""
+
+from sys import exc_info
+from os import stat
+from shutil import rmtree
+from unittest import TestCase, makeSuite
+from ..tbdetail import _LOG_DIR, log_tb
+
+
+class TestTbdetail(TestCase):
+
+    def test_log_tb(self):
+        """call log_tb twice to make sure it handles the case where
+        $ZENHOME/log/traceback directory is already created"""
+        rmtree(_LOG_DIR)
+        for i in range(2):
+            try:
+                self._raise()
+                self.fail("raise method didn't raise")
+            except:
+                message = log_tb(exc_info())
+                path = message.split()[0]
+                self.assert_(0 < stat(path).st_size, 'log file is empty')
+
+    def _raise(self):
+        """raise an exception"""
+        raise Exception("Testing log_tb")
+
+
+def test_suite():
+    return makeSuite(TestTbdetail)
