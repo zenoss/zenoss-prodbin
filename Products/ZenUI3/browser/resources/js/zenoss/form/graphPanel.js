fe1860e7bc6fc4cf6de53714e5153e9d89e0a030
Joseph Hanson <jhanson@zenoss.com>
2013-08-12 11:12:57 -0500

    set the default drange to last hour and move the color to another class

--- Products/ZenUI3/browser/resources/js/zenoss/form/graphPanel.js
+++ Products/ZenUI3/browser/resources/js/zenoss/form/graphPanel.js
@@ -17,19 +17,19 @@
         json = JSON.stringify(json, undefined, 4);
         json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
         return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-            var cls = 'brown';
+            var cls = 'syntax-number';
             if (/^"/.test(match)) {
                 if (/:$/.test(match)) {
-                    cls = 'green';
+                    cls = 'syntax-string';
                 } else {
-                    cls = 'white';
+                    cls = 'syntax-text';
                 }
             } else if (/true|false/.test(match)) {
-                cls = 'blue';
+                cls = 'syntax-boolean';
             } else if (/null/.test(match)) {
-                cls = 'null';
+                cls = 'syntax-null';
             }
-            return '<span style="color:' + cls + '">' + match + '</span>';
+            return '<span class="' + cls + '">' + match + '</span>';
         });
     }
 
@@ -553,7 +553,7 @@
             }
 
             Ext.applyIf(config, {
-                drange: 129600,
+                drange: 3600000,
                 isLinked: true,
                 newWindowButton: true,
                 // images show up after Ext has calculated the
