a129fd86f9ed9e2712975c190f59976e77d5b8c7
Joseph Hanson <jhanson@zenoss.com>
2013-08-14 08:12:48 -0500

    unit tests passing

--- Products/Zuul/tests/test_metricfacade.py
+++ Products/Zuul/tests/test_metricfacade.py
@@ -1,16 +1,16 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2009, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
 import unittest
 from Products import Zuul
-from Products.Zuul.tests.base import ZuulFacadeTestCase
+from Products.ZenTestCase.BaseTestCase import BaseTestCase
 from zenoss.protocols.services import JsonRestServiceClient
 
 class MockRestServiceClient(JsonRestServiceClient):
@@ -18,7 +18,7 @@ class MockRestServiceClient(JsonRestServiceClient):
     def _executeRequest(self, request):
         return 200, '[2]'
 
-class MetricFacadeTest(ZuulFacadeTestCase):
+class MetricFacadeTest(BaseTestCase):
 
     def afterSetUp(self):
         super(MetricFacadeTest, self).afterSetUp()
@@ -45,9 +45,10 @@ class MetricFacadeTest(ZuulFacadeTestCase):
     def testRequestBuilder(self):
         metric = ["laLoadInt1_laLoadInt1"]
         dev = self.dmd.Devices.createInstance('device1')
-        request = self.facade._buildRequest(dev, metric, cf="AVERAGE")
-        self.assertEquals(request['metrics'][0]['aggregator'], 'avg')
-        
+        request = self.facade._buildRequest(dev, metric)
+        self.assertEquals(request['returnset'], 'LAST')
+
+
 def test_suite():
     return unittest.TestSuite((unittest.makeSuite(MetricFacadeTest),))
 
