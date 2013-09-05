3812ca2232fd64fda7f21228d0df80b6eeec9dbf
Seth Cleveland <scleveland@zenoss.com>
2013-09-05 11:13:51 -0500

    improve installation instructions to manage incongruent requests version with europa

--- tests/Authorization.py
+++ tests/Authorization.py
@@ -5,7 +5,8 @@ from locust import Locust, TaskSet, task
 ## Authorization.py defines test cases to benchmark the ZAuth service. The test
 #  cases are written using Locust IO (http://locust.io/).  Execute the following
 #  to  run the test cases:
-#    workon europa
+#    mkvirtualenv europa.testing
+#    workon europa.testing
 #    pip install locustio
 #    locust -H http://localhost:9080 -f Authorization.py
 
