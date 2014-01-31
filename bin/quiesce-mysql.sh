edfc93aec90a2efbea9c308fe7d884925c0c30e4
Phil Lu <plu@zenoss.com>
2014-01-31 08:54:45 -0600

    added -A to mysql; added comment about locking ZEP; code cleanup

--- bin/quiesce-mysql.sh
+++ bin/quiesce-mysql.sh
@@ -11,22 +11,46 @@
 
 function help()
 {
-    echo "Usage: $0 {status|pause|resume|help}" >&2
+    cat <<CAT_EOF >&2
+Usage: $0 {status|pause|resume|help}
+
+    MYSQL_LOCK_TIMEOUT env var may be overridden from default of 60 seconds
+        used in call to mysql GET_LOCK()
+
+CAT_EOF
+}
+
+
+function datetimestamp()
+{
+    TZ=UTC date +"%Y-%m-%dT%Hh%Mm%SsZ"
 }
 
 
 function die
 {
-    echo "ERROR: ${*}" >&2
+    echo "$(datetimestamp): ERROR: ${*}" >&2
     exit 1
+}
+
+
+function logError
+{
+    echo "$(datetimestamp): ERROR: ${*}" >&2
+}
+
+
+function logInfo
+{
+    echo "$(datetimestamp): INFO: ${*}" >&2
 }
 
 
 function getConfValue
 {
-    local conf="$1"
-    local key="$2"
-    local prepend="$3"
+    local conf="$1"     # configuration file to use (full path)
+    local key="$2"      # config value to check for
+    local prepend="$3"  # optional: prepend this to value if value is set
     value=$(awk -v key="$key" '$1 == key {print $2; exit(0);}' "$conf")
     if [[ -n $prepend ]]; then
         if [[ -n $value ]]; then
@@ -43,7 +67,7 @@ function run_mysql_cmd()
     local database=$1
     shift
     local statement="$@"
-    local cmd="mysql $MY_MYSQL_CONNECTION -e \"$statement \G\" $database"
+    local cmd="mysql -A $MY_MYSQL_CONNECTION -e \"$statement\" $database"
     eval $cmd
     local rc=$?
 
@@ -60,9 +84,11 @@ function do_status()
     local database=$1
     local lockname=$2
 
+    logInfo "status database:$database lockname:$lockname"
+
     local fxn="IS_FREE_LOCK('$lockname')"
     local cmd="SELECT $fxn"
-    local output=$(run_mysql_cmd "$database" "$cmd" | tail -1)
+    local output=$(run_mysql_cmd "$database" "$cmd \G" | tail -1)
     case "$output" in
         "$fxn: 0")
             echo "IS_LOCKED: $database database with $lockname lockname"
@@ -88,6 +114,11 @@ function do_hold_lock()
     local lockname=$2
     local timeout=$3
 
+    # purposely not executing: FLUSH TABLES WITH READ LOCK
+    #   http://www.mysqlperformanceblog.com/2012/03/23/how-flush-tables-with-read-lock-works-with-innodb-tables/
+    # despite:
+    #   http://www.mysqlperformanceblog.com/2006/08/21/using-lvm-for-mysql-backup-and-replication-setup/
+    # ZEP is locked by locking ZODB since it attempts a ZODB lock before writes
     {
         echo "SELECT GET_LOCK('$lockname', $timeout) \G;"
         while true; do sleep 1; done
@@ -103,12 +134,14 @@ function do_pause()
     local lockname=$2
     local timeout=$3
 
+    logInfo "pause database:$database lockname:$lockname timeout:$timeout"
+
     $0 hold-lock &
     disown
     for numtries in $(eval echo {1..$timeout}); do
-        echo "#######======= checking lock status (try: $numtries/$timeout):"
+        logInfo "Checking lock status (try: $numtries/$timeout):"
         local output=$($0 status)
-        echo $output
+        logInfo $output
         if [[ $output =~ "IS_LOCKED:" ]]; then
             return 0
         fi
@@ -122,6 +155,8 @@ function do_resume()
 {
     local database=$1
     local lockname=$2
+
+    logInfo "resume database:$database lockname:$lockname"
 
     pkill -f "$0 hold-lock"
     sleep 1
@@ -139,6 +174,7 @@ function main()
     MY_MYSQL_CONNECTION+=" "$(getConfValue "$CFGDIR/global.conf" "zodb-admin-password" "--password=")
     MY_MYSQL_CONNECTION+=" "$(getConfValue "$CFGDIR/global.conf" "zodb-host" "--host=")
     MY_MYSQL_CONNECTION+=" "$(getConfValue "$CFGDIR/global.conf" "zodb-port" "--port=")
+    [[ $MY_MYSQL_CONNECTION =~ "--user=" ]] || die "required zodb-admin-user not found in $CFGDIR/global.conf"
 
     local database=$(getConfValue "$CFGDIR/global.conf" "zodb-db")
     local lockname="$database.quiesce"
@@ -173,6 +209,8 @@ function main()
 
 
 if [[ "$(basename $0)" == "quiesce-mysql.sh" ]]; then
+    [[ -n "$ZENHOME" ]] || die "ZENHOME env var is not set"
+
     # Needs these env vars provided from zenfunctions:
     #   CFGDIR=$ZENHOME/etc
     source $ZENHOME/bin/zenfunctions
