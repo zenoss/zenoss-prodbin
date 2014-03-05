500562193766e769bbbdcb109b1b2d72d5206bb0
Joseph Hanson <jhanson@zenoss.com>
2014-03-05 09:50:28 -0600

    fixed undeclared methods and changed to use index_object

--- Products/ZenModel/migrate/maintenanceWindowHooks.py
+++ Products/ZenModel/migrate/maintenanceWindowHooks.py
@@ -15,29 +15,31 @@ Create hooks for finding and displaying maintenance windows
 
 '''
 import Migrate
-
+import logging 
 from Products.ZenModel.MaintenanceWindow import createMaintenanceWindowCatalog
 from Products.ZenUtils.Search import makePathIndex
+log = logging.getLogger('zen.migrateMaintWindows')
+
 
 class MaintenanceWindowHooks(Migrate.Step):
     version = Migrate.Version(5, 0, 0)
     
-    def _createNewIndex( mwcat ):
+    def _createNewIndex(self, mwcat ):
         log.info("Adding new columns to the dmd.maintenanceWindowSearch catalog.")
         mwcat._catalog.addIndex('getPhysicalPath', makePathIndex('getPhysicalPath'))
         mwcat.addColumn('getPhysicalPath')
  
-    def _reindexWindows( mwcat ):
+    def _reindexWindows(self, mwcat ):
         log.info("Re-indexing the dmd.maintenanceWindowSearch catalog....")
         for brain in mwcat():
             obj = brain.getObject()
-            obj.reindex_all()
-        log.info("Completed re-indexing the dmd.maintenanceWindowSearch catalog!")
+            obj.index_object(idxs=('getPhysicalPath',))
+        log.info("Completed re-indexing the dmd.maintenanceWindowSearch catalog.")
     
     def cutover(self, dmd):
         mwcat = dmd.maintenanceWindowSearch
         if 'getPhysicalPath' not in mwcat.indexes():
-            _createNewIndex( mwcat )
-            _reindexWindows( mwcat )
+            self._createNewIndex( mwcat )
+            self._reindexWindows( mwcat )
 
 MaintenanceWindowHooks()
