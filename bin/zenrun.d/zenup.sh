f93ede1cbb2b36af43ddb8e020399cd52a15ab34
Andrew Lapidas <alapidas@gmail.com>
2014-07-17 15:30:07 -0500

    Get zenup run script to change context to find artifact

--- bin/zenrun.d/zenup.sh
+++ bin/zenrun.d/zenup.sh
@@ -22,17 +22,43 @@ __nocommit__() {
     return 1
 }
 
+mapPath() {
+    local hostArtifactPath=$1
+    local __resultvar=$2
+
+    if [[ "${hostArtifactPath:0:1}" = "/" ]]; then
+        local containerArtifactPath=$hostArtifactath
+    else
+        local containerArtifactPath="/mnt/pwd/"$hostArtifactPath
+    fi
+
+    #Test for presence of artifact
+    if [[ ! -r "$containerArtifactPath" ]]; then
+        echo "Unable to read file: '$hostArtifactPath'"
+        echo "The specified file must be located in the current working directory and must be specified with a relative path."
+        exit 1
+    fi
+
+    eval $__resultvar="$containerArtifactPath"
+}
+
 install() {
+    mapPath ${!#} artifactPath
+    set -- "${@:1:$(($#-1))}" "$artifactPath"
     zenup install "$@"
     return $?
 }
 
 init() {
+    mapPath ${!#} artifactPath
+    set -- "${@:1:$(($#-1))}" "$artifactPath"
     zenup init "$@"
     return $?
 }
 
 patch() {
+    mapPath ${!#} artifactPath
+    set -- "${@:1:$(($#-1))}" "$artifactPath"
     zenup patch "$@"
     return $?
 }
@@ -46,3 +72,4 @@ revert() {
     zenup revert "$@"
     return $?
 }
+
