2c7e85763a2775e98fbd8cfe1bbba6258c46eab9
Joseph Hanson <jhanson@zenoss.com>
2014-08-27 13:05:26 -0500

    on chrome make sure we are not exporting when we submitviaenter

--- Products/ZenModel/skins/zenmodel/submitViaEnter.js
+++ Products/ZenModel/skins/zenmodel/submitViaEnter.js
@@ -1,10 +1,15 @@
-function submitViaEnter(evt) {
+function submitViaEnter(evt) {    
     evt = (evt) ? evt : event;
     var target = (evt.target) ? evt.target : evt.srcElement;
     var form = target.form;
     var charCode = (evt.charCode) ? evt.charCode : 
         ((evt.which) ? evt.which : evt.keyCode);
     if (charCode == 13 || charCode == 3) {
+        // if we are on a zentable and pressing enter we do not want to export
+        // we want to filter
+        if (document.getElementById('exportInput')) {
+            document.getElementById('exportInput').name = "";
+        }
         form.submit();
         return false;
     } else if (evt.type == "change") {
