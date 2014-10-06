5f0b80ce99e5c4f4a34084a0cfe7905d9fbf6add
Ian McCracken <ian.mccracken@gmail.com>
2014-10-06 16:11:36 -0500

    Add copyright header to new file

--- Products/ZenUtils/metrics.py
+++ Products/ZenUtils/metrics.py
@@ -1,3 +1,11 @@
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
 
 def ensure_prefix(prefix, metric):
     """
