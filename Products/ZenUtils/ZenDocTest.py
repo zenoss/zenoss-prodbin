549b04783b93a1e5d3a21ff7a2ce5b899db83dcc
Ian McCracken <ian.mccracken@gmail.com>
2014-10-22 13:14:53 -0500

    Force the provision of the 'unittests' feature when loading

--- Products/ZenUtils/ZenDocTest.py
+++ Products/ZenUtils/ZenDocTest.py
@@ -1,26 +1,65 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
-
-
 import unittest
 import doctest
 import transaction
 import socket
 import Globals
+import os.path
 
+from Zope2.App import zcml as _zcml
 import zope.component
+from zope.testing.cleanup import cleanUp
 from zope.traversing.adapters import DefaultTraversable
+from zope.configuration import xmlconfig
 
 from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SecurityManagement import noSecurityManager
 
 from Products.ZenUtils.ZeoConn import ZeoConn
+
+
+unused = lambda x: None
+unused(Globals)
+
+
+def load_unittest_site(force=False):
+    """
+    Custom version of zcml.load_site() that will force the provision of the
+    "unittests" feature.
+    """
+    if _zcml._initialized and not force:
+        return
+    _zcml._initialized = True
+
+    import Globals
+    Globals.INSTANCE_HOME
+
+    # load instance site configuration file
+    site_zcml = os.path.join(Globals.INSTANCE_HOME, "etc", "site.zcml")
+
+    if not os.path.exists(site_zcml):
+        # check for zope installation home skel during running unit tests
+        import Zope2.utilities
+        zope_utils = os.path.dirname(Zope2.utilities.__file__)
+        site_zcml = os.path.join(zope_utils, "skel", "etc", "site.zcml")
+
+    # Blow away existing context (this normally happens in a call to load_site)
+    _zcml._context = None
+    # Load the snippet claiming the unittests feature is provided
+    _zcml.load_string("""
+        <configure xmlns="http://namespaces.zope.org/meta">
+            <provides feature="unittests"/>
+        </configure>
+    """)
+    # Now load_site as usual, except keep the existing context
+    _zcml._context = xmlconfig.file(site_zcml, None, _zcml._context)
 
 
 class TestSuiteWithHooks(unittest.TestSuite):
@@ -61,20 +100,20 @@ class ZenDocTestRunner(object):
                 return db.config.storage.config.adapter.config
 
     def setUp(self):
+        zope.component.testing.setUp(self.__class__)
         import Products.ZenossStartup
-        from Products.Five import zcml
-        zcml.load_site()
-
+        load_unittest_site(force=True)
         zope.component.provideAdapter(DefaultTraversable, (None,))
+
         if not self.conn:
             adapter_config = self._find_relstorage_adapter_config()
             if adapter_config:
                 self.conn = ZeoConn(zodb_host=adapter_config.host,
-                        zodb_port=adapter_config.port,
-                        zodb_user=adapter_config.user,
-                        zodb_password=adapter_config.passwd,
-                        zodb_db=adapter_config.db,
-                        zodb_socket=adapter_config.unix_socket)
+                                    zodb_port=adapter_config.port,
+                                    zodb_user=adapter_config.user,
+                                    zodb_password=adapter_config.passwd,
+                                    zodb_db=adapter_config.db,
+                                    zodb_socket=adapter_config.unix_socket)
             else:
                 self.conn = ZeoConn()
 
@@ -83,27 +122,29 @@ class ZenDocTestRunner(object):
         self.dmd = self.app.zport.dmd
         find = self.dmd.Devices.findDevice
         self.globals = dict(
-            app = self.app,
-            zport = self.app.zport,
-            dmd = self.dmd,
-            find = find,
-            devices = self.dmd.Devices,
-            sync = self.dmd._p_jar.sync,
-            commit = transaction.commit,
-            abort = transaction.abort,
-            me = find(socket.getfqdn())
+            app=self.app,
+            zport=self.app.zport,
+            dmd=self.dmd,
+            find=find,
+            devices=self.dmd.Devices,
+            sync=self.dmd._p_jar.sync,
+            commit=transaction.commit,
+            abort=transaction.abort,
+            me=find(socket.getfqdn())
         )
 
     def tearDown(self):
         self.logout()
         self.conn.closedb()
+        cleanUp()
 
     def login(self, name='admin', userfolder=None):
         '''Logs in.'''
         if userfolder is None:
             userfolder = self.app.acl_users
         user = userfolder.getUserById(name)
-        if user is None: return
+        if user is None:
+            return
         if not hasattr(user, 'aq_base'):
             user = user.__of__(userfolder)
         newSecurityManager(None, user)
@@ -128,7 +169,8 @@ class ZenDocTestRunner(object):
         @param mods: One or more module objects or dotted names.
         @type mods: module or list
         """
-        if not isinstance(mods, list): mods = [mods]
+        if not isinstance(mods, list):
+            mods = [mods]
         self.modules.extend(mods)
 
     def get_suites(self):
@@ -146,8 +188,8 @@ class ZenDocTestRunner(object):
                 dtsuite = doctest.DocTestSuite(
                     mod,
                     optionflags=doctest.NORMALIZE_WHITESPACE,
-                    setUp = self.doctest_setUp,
-                    tearDown = self.doctest_tearDown
+                    setUp=self.doctest_setUp,
+                    tearDown=self.doctest_tearDown
                 )
             except ValueError:
                 pass
