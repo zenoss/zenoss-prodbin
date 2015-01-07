557e088b1d3cfacee306976bde9a51f891239e12
Joseph Hanson <jhanson@zenoss.com>
2015-01-07 10:02:47 -0600

    reviewed by jhuie possibly fixes ZEN-15719 was not paginating correctly because the buffered flag was explictly set to false

--- Products/ZenUI3/browser/resources/js/zenoss/joblist.js
+++ Products/ZenUI3/browser/resources/js/zenoss/joblist.js
@@ -1,10 +1,10 @@
 /*****************************************************************************
- * 
+ *
  * Copyright (C) Zenoss, Inc. 2012, all rights reserved.
- * 
+ *
  * This content is made available according to terms specified in
  * License.zenoss under the directory where your Zenoss product is installed.
- * 
+ *
  ****************************************************************************/
 
 
@@ -41,7 +41,6 @@ Ext.getCmp('center_panel').add({
             root: 'jobs',
             autoLoad: true,
             pageSize: Zenoss.settings.zenjobsGridBufferSize,
-            buffered: false,
             initialSortColumn: 'scheduled',
             initialSortDirection: 'DESC',
             totalProperty: 'totalCount',
