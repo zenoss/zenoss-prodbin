c8952385b7a133e13fc589393f04f33e62ea7acd
Roman Bakaleyko <rbakaleyko@zenoss.com>
2015-01-19 15:54:24 +0200

    ZEN-15995 When MultiRealm Zenpack is installed, event transform to change device does not change the rest of the device context.

--- Products/ZenEvents/events2/processing.py
+++ Products/ZenEvents/events2/processing.py
@@ -195,7 +195,7 @@ class Manager(object):
             # don't search interfaces for the ::1 IPv6 address
             elif ipToDecimal('::1') == ip_decimal:
                 ip_decimal = None
-        if ip_decimal is None:
+        if ip_decimal is None and not device_brains:
             return [], []
 
         net_cat = ICatalogTool(self._networks)
