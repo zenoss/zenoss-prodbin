256cda1fa20e71dff8c54bf0707f7e031e32aad9
Joseph Hanson <jhanson@zenoss.com>
2015-04-13 14:48:57 -0500

    redumped the database for ZEN-17109

--- Products/ZenModel/zenbuild.py
+++ Products/ZenModel/zenbuild.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -55,7 +55,7 @@ class zenbuild(CmdBase):
         self.parser.add_option('-t','--evtport', dest="evtport",
                 type='int', default=3306,
                 help="port used to connect to the events database")
-        self.parser.add_option('--smtphost', dest="smtphost", default="localhost",
+        self.parser.add_option('--smtphost', dest="smtphost", default="",
                 help="smtp host")
         self.parser.add_option('--smtpport', dest="smtpport", default=25,
                 type='int', help="smtp port")
