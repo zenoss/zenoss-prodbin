d0743bfd0652e949139280f1c30d286ca5cc0b56
Vadym Mashkov <vmashkov@zenoss.com>
2015-05-20 09:41:48 -0400

    Port chenges https://jira.zenoss.com/browse/ZEN-17625 to 5.1 	modified:   Products/DataCollector/plugins/zenoss/cmd/linux/ifconfig.py

--- Products/DataCollector/plugins/zenoss/cmd/linux/ifconfig.py
+++ Products/DataCollector/plugins/zenoss/cmd/linux/ifconfig.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # 
-# Copyright (C) Zenoss, Inc. 2009, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2009, 2015, all rights reserved.
 # 
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -28,7 +28,7 @@ def parseDmesg(dmesg, relMap):
             for objMap in relMap.maps:
                 if objMap.interfaceName in line:
                     if 'Gbps' in speedMatch.group(0): 
-                        objMap.speed = int(speedMatch.group(1)) * 1e9 
+                        objMap.speed = int(speedMatch.group(1)) * 1e9
                     else: 
                         objMap.speed = int(speedMatch.group(1)) * 1e6
                     break
@@ -36,27 +36,50 @@ def parseDmesg(dmesg, relMap):
 
 class ifconfig(LinuxCommandPlugin):
     # echo __COMMAND__ is used to delimit the results
-    command = '/sbin/ifconfig -a && echo __COMMAND__ && /bin/dmesg'
+    command = 'if [ -x /usr/sbin/ip ]; then \
+                   echo "### ip addr output"; /usr/sbin/ip addr; \
+               elif [ -x /sbin/ifconfig ]; then \
+                   /sbin/ifconfig -a; \
+               else \
+                   echo "No ifconfig or ip utilities were found."; exit 127; \
+               fi  && echo __COMMAND__ && /bin/dmesg'
     compname = "os"
     relname = "interfaces"
     modname = "Products.ZenModel.IpInterface"
     deviceProperties = LinuxCommandPlugin.deviceProperties + (
            'zInterfaceMapIgnoreNames', 'zInterfaceMapIgnoreTypes')
 
-    ifstart = re.compile(r"^(\S+)\s+Link encap:(.+)HWaddr (\S+)"
-                         "|^(\S+)\s+Link encap:(.+)")
-    v4addr = re.compile(r"inet addr:(\S+).*Mask:(\S+)")
-    v6addr = re.compile(r"inet6 addr: (\S+).*")
-    flags = re.compile(r"^(.*) MTU:(\d+)\s+Metric:.*")
-    
+    # variables for ifconfig
+    ifstart = re.compile(r"^(\w+):?\s+")
+    oldiftype = re.compile(r"^\S+\s+Link encap:(.+)HWaddr (\S+)"
+                           r"|^\S+\s+Link encap:(.+)")
+    v4addr = re.compile(r"inet addr:(\S+).*Mask:(\S+)"
+                        r"|inet\s+(\S+)\s+netmask\s+(\S+)")
+    v6addr = re.compile(r"inet6 addr: (\S+).*"
+                        r"|inet6\s+(\S+)\s+prefixlen\s+(\d+)")
+    flags = re.compile(r"^(.*) MTU:(\d+)\s+Metric:.*"
+                       r"|^\S+:\s+flags=\d+<(\S+)>\s+mtu\s+(\d+)")
+    newether = re.compile(r"^\s+ether\s+(\S+)")
+    newifctype = re.compile(r"txqueuelen\s+\d+\s+\(([^)]+)\)")
+
+    # variables for ip tool (ip addr)
+    ip_ifstart = re.compile(r"^(\d+):\s(\w+):\s(.*)mtu\s(\d+)(.*)")
+    ip_v4addr = re.compile(r"inet (\S+)")
+    ip_v6addr = re.compile(r"inet6 (\S+)")
+    ip_hwaddr = re.compile(r"link/(\S+)\s(\S+)") #for further purpose to get HWADDRESS
+
+
     def process(self, device, results, log):
         log.info('Modeler %s processing data for device %s', self.name(), device.id)
         self.log = log
         ifconfig, dmesg = results.split('__COMMAND__')
-        relMap = self.parseIfconfig(ifconfig, device, self.relMap())
+        if '###' in ifconfig:
+            relMap = self.parseIpconfig(ifconfig, device, self.relMap())
+        else:
+            relMap = self.parseIfconfig(ifconfig, device, self.relMap())
         relMap = parseDmesg(dmesg.lstrip(), relMap)
         return relMap
-        
+
     def parseIfconfig(self, ifconfig, device, relMap):
         """
         Parse the output of the ifconfig -a command.
@@ -66,43 +89,48 @@ class ifconfig(LinuxCommandPlugin):
         for line in rlines:
 
             # reset state to no interface
-            if not line.strip(): 
+            if not line.strip():
                 iface = None
 
             # new interface starting
-            miface = self.ifstart.search(line) 
+            miface = self.ifstart.search(line)
             if miface:
                 # start new interface and get name, type, and macaddress
                 iface = self.objectMap()
-                if miface.lastindex == 3:
-                    name, itype, iface.macaddress=miface.groups()[:3]
-                else:
-                    name, itype = miface.groups()[3:]
-                if itype.startswith("Ethernet"): itype = "ethernetCsmacd"
-                iface.type = itype.strip()
+                name = miface.group(1)
 
                 iface.interfaceName = name
                 iface.id = self.prepId(name)
-                dontCollectIntNames = getattr(device, 'zInterfaceMapIgnoreNames', None)
-                if dontCollectIntNames and re.search(dontCollectIntNames, iface.interfaceName):
-                    self.log.debug("Interface %s matched the zInterfaceMapIgnoreNames zprop '%s'",
-                              iface.interfaceName, dontCollectIntNames)
-                    continue
-
-                dontCollectIntTypes = getattr(device, 'zInterfaceMapIgnoreTypes', None)
-                if dontCollectIntTypes and re.search(dontCollectIntTypes, iface.type):
-                    self.log.debug("Interface %s type %s matched the zInterfaceMapIgnoreTypes zprop '%s'",
-                      iface.interfaceName, iface.type, dontCollectIntTypes)
+                if self.isIgnoredName(device, iface.interfaceName):
                     continue
 
                 relMap.append(iface)
-                continue
+
+            mtype = self.oldiftype.search(line)
+            if mtype:
+                if mtype.lastindex == 2:
+                    itype, iface.macaddress = mtype.groups()[:2]
+                else:
+                    itype = mtype.group(3)
+
+                if itype.startswith("Ethernet"):
+                    itype = "ethernetCsmacd"
+                iface.type = itype.strip()
+
+                if self.isIgnoredType(device, iface.interfaceName, iface.type):
+                    relMap.maps.remove(iface)
+                    iface = None
+
+                    continue
 
             # get the IP addresses of an interface
             maddr = self.v4addr.search(line)
             if maddr and iface:
                 # get IP address and netmask
-                ip, netmask = maddr.groups()
+                if maddr.lastindex == 2:
+                    ip, netmask = maddr.groups()[:2]
+                else:
+                    ip, netmask = maddr.groups()[2:]
                 netmask = self.maskToBits(netmask)
                 if not hasattr(iface, 'setIpAddresses'):
                     iface.setIpAddresses = []
@@ -111,16 +139,49 @@ class ifconfig(LinuxCommandPlugin):
             maddr = self.v6addr.search(line)
             if maddr and iface:
                 # get IP address
-                ip = maddr.groups()[0]
+                if maddr.lastindex == 3:
+                    ip = "%s/%s" % maddr.groups()[1:]
+                else:
+                    ip = maddr.group(1)
                 if not hasattr(iface, 'setIpAddresses'):
                     iface.setIpAddresses = []
                 iface.setIpAddresses.append(ip)
+
+            mether = self.newether.search(line)
+            if mether and iface:
+                # get MAC address (new style)
+                macaddress = mether.group(1)
+                iface.macaddress = macaddress
+
+                iface.type = "ethernetCsmacd"
+
+                if self.isIgnoredType(device, iface.interfaceName, iface.type):
+                    relMap.maps.remove(iface)
+                    iface = None
+
+                continue
+
+            mtype = self.newifctype.search(line)
+            if mtype and iface:
+                # get MAC address (new style)
+                ifctype = mtype.group(1)
+
+                iface.type = ifctype.strip()
+
+                if self.isIgnoredType(device, iface.interfaceName, iface.type):
+                    relMap.maps.remove(iface)
+                    iface = None
+
+                    continue
 
             # get the state UP/DOWN of the interface
             mstatus = self.flags.search(line)
             if mstatus and iface:
                 # get adminStatus, operStatus, and mtu
-                flags, mtu = mstatus.groups()
+                if mstatus.lastindex == 2:
+                    flags, mtu = mstatus.groups()[:2]
+                else:
+                    flags, mtu = mstatus.groups()[2:]
                 if "UP" in flags: iface.operStatus = 1
                 else: iface.operStatus = 2
                 if "RUNNING" in flags: iface.adminStatus = 1
@@ -128,3 +189,96 @@ class ifconfig(LinuxCommandPlugin):
                 iface.mtu = int(mtu)
 
         return relMap
+
+    def parseIpconfig(self, ifconfig, device, relMap):
+        """
+        Parse the output of the ip addr command.
+        """
+        rlines = ifconfig.splitlines()
+        iface = None
+        for line in rlines:
+            # reset state to no interface
+            if not line.strip():
+                iface = None
+
+            # new interface starting
+            miface = self.ip_ifstart.search(line)
+            if miface:
+                # start new interface and get name, type, and macaddress
+                iface = self.objectMap()
+                no,name, flags, mtu = miface.groups()[:4]
+                iface.interfaceName = name
+                iface.id = self.prepId(name)
+                dontCollectIntNames = getattr(device, 'zInterfaceMapIgnoreNames', None)
+                if dontCollectIntNames and re.search(dontCollectIntNames, iface.interfaceName):
+                    self.log.debug("Interface %s matched the zInterfaceMapIgnoreNames zprop '%s'",
+                        iface.interfaceName, dontCollectIntNames)
+                    continue
+
+                dontCollectIntTypes = getattr(device, 'zInterfaceMapIgnoreTypes', None)
+                if dontCollectIntTypes and re.search(dontCollectIntTypes, iface.type):
+                    self.log.debug("Interface %s type %s matched the zInterfaceMapIgnoreTypes zprop '%s'",
+                        iface.interfaceName, iface.type, dontCollectIntTypes)
+                    continue
+
+                relMap.append(iface)
+
+                if "UP" in flags:
+                    iface.operStatus = 1
+                else:
+                    iface.operStatus = 2
+                if "RUNNING" in flags:
+                    iface.adminStatus = 1
+                else:
+                    iface.adminStatus = 2
+
+                if mtu:
+                    iface.mtu = int(mtu)
+                continue
+
+            # get the IP addresses of an interface
+            maddr = self.ip_v4addr.search(line)
+            if maddr and iface:
+                # get IP address and netmask
+                _ip = str(maddr.groups()[:1]).translate(None, '\(\)\', ')
+                _ip = _ip.split("/")
+                ip = _ip[0]
+                netmask = _ip[1]
+                if not hasattr(iface, 'setIpAddresses'):
+                    iface.setIpAddresses = []
+                iface.setIpAddresses.append("%s/%s" % (ip, netmask))
+
+            maddr = self.ip_v6addr.search(line)
+            if maddr and iface:
+                # get IP address
+                ip = maddr.groups()[0]
+                if not hasattr(iface, 'setIpAddresses'):
+                    iface.setIpAddresses = []
+                iface.setIpAddresses.append(ip)
+
+            # get macaddress
+            maddr = self.ip_hwaddr.search(line)
+            if  maddr and iface:
+                iface.macaddress = maddr.groups()[1]
+
+        return relMap
+
+    def isIgnoredName(self, device, name):
+        """Checks whether interface name in device's ignore list."""
+        dontCollectIntNames = getattr(device, 'zInterfaceMapIgnoreNames', None)
+        if dontCollectIntNames and re.search(dontCollectIntNames, name):
+            self.log.debug("Interface %s matched the "
+                           "zInterfaceMapIgnoreNames zprop '%s'", name,
+                           dontCollectIntNames)
+            return True
+        return False
+
+    def isIgnoredType(self, device, name, ifcType):
+        """Checks whether interface type in device's ignore list."""
+        dontCollectIntTypes = getattr(device, 'zInterfaceMapIgnoreTypes', None)
+        if dontCollectIntTypes and re.search(dontCollectIntTypes, ifcType):
+            self.log.debug("Interface %s type %s matched the "
+                           "zInterfaceMapIgnoreTypes zprop '%s'", name,
+                           ifcType, dontCollectIntTypes)
+            return True
+        return False
