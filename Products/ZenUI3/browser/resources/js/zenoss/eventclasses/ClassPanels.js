c953519e9a410bcb4113a2b2b196f18ae1fc2b88
Roman Bakaleyko <rbakaleyko@zenoss.com>
2015-06-01 17:49:44 +0300

    Fixes ZEN-17898
    cherry-pick commit 0f49a71228e0714ab847b5685e8b435558766043
    (committed by "zendev port cherry-pick")

--- Products/ZenUI3/browser/resources/js/zenoss/eventclasses/ClassPanels.js
+++ Products/ZenUI3/browser/resources/js/zenoss/eventclasses/ClassPanels.js
@@ -872,7 +872,7 @@ Ext.onReady(function(){
                         var uid = me.uid;
                         Zenoss.remote.EventClassesRouter.getTransform({'uid':uid}, function(response){
                             if(response.success){
-                                var str = me.setDefaultString(response.data)
+                                var str = me.getDefaultString(response.data)
                                 me.items.items[0].setValue(str);
                             }
                         });
@@ -908,7 +908,7 @@ Ext.onReady(function(){
         setValue: function(value) {
             this.setTitle(_t("Transform for")+": "+value.transid);
             this.uid = "/zport/dmd"+value.transid;
-            var str = this.setDefaultString(value.trans)
+            var str = this.getDefaultString(value.trans)
             this.down('codeeditorfield').setValue(str);
         },
         reset: function() {
