28eff23b664793e271526e6015e05896ee344684
Jason Peacock <jpeacock@zenoss.com>
2015-06-18 08:15:05 -0500

    Cherry-picked #925
    ZEN-18339: Linux_ifconfig Command Plugin fails on CentOS 7 [modeling …
    Conflicts:
    	Products/DataCollector/plugins/zenoss/cmd/linux/ifconfig.py

--- Products/DataCollector/plugins/zenoss/cmd/linux/netstat_an.py
+++ Products/DataCollector/plugins/zenoss/cmd/linux/netstat_an.py
@@ -1,36 +1,190 @@
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2007, 2015, all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
 __doc__ = """netstat_an
-Collect running IP services using netstat -a
+Collect running IP services using `netstat -an` or `ss -lntu`
 """
 
+import abc
 from Products.DataCollector.plugins.CollectorPlugin import LinuxCommandPlugin
-from Products.ZenUtils import IpUtil
+
+
+class BaseParser(object):
+
+    """Base class for command output parsers."""
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, log):
+        self.log = log
+
+    @abc.abstractmethod
+    def parse(self, results):
+        """Parses command output in `results` string and returns services
+        information in format:
+
+            [(proto, port, addr), ...]
+        """
+
+
+class SsServicesParser(BaseParser):
+
+    """Parser class for `/usr/sbin/ss -lntu` command output.
+
+    Supported format:
+
+        Netid State      Recv-Q Send-Q  Local Address:Port   Peer Address:Port
+        tcp   UNCONN     0      0                   *:68                *:*
+        tcp   UNCONN     0      0           127.0.0.1:323               *:*
+        tcp   UNCONN     0      0                  :::38496            :::*
+        tcp   UNCONN     0      0                  :::111              :::*
+        tcp   LISTEN     0      128                 *:111               *:*
+        tcp   LISTEN     0      128                 *:22                *:*
+        tcp   LISTEN     0      100         127.0.0.1:25                *:*
+        tcp   LISTEN     0      128                :::111              :::*
+        tcp   LISTEN     0      128                :::22               :::*
+        tcp   LISTEN     0      100               ::1:25               :::*
+    """
+
+    def parse(self, results):
+        """Parses command output in `results` string and returns services
+        information in format:
+
+            [(proto, port, addr), ...]
+        """
+        services = []
+
+        for line in results.split("\n"):
+            # tcp    UNCONN     0      0      *:68      *:*
+            fields = line.split()
+            if len(fields) != 6:
+                continue
+            try:
+                proto = fields[0]
+                # please note protocol alway be tcp due a bug in RH;
+                # see https://bugzilla.redhat.com/show_bug.cgi?id=1063927
+                if fields[1] == 'UNCONN':
+                    proto = 'udp'
+                listar = fields[4].split(":")
+                addr = port = ""
+                if len(listar) == 2:
+                    addr, port = listar
+                    if addr == '*':
+                        addr = '0.0.0.0'
+                else:
+                    addr = "0.0.0.0"
+                    port = listar[-1]
+                    proto = proto + '6'
+
+                self.log.debug("Got %s %s port %s", addr, proto, port)
+                port = int(port)
+            except ValueError:
+                self.log.exception("Failed to parse IPService information '%s'",
+                                   line)
+                continue
+
+            services.append((proto, port, addr))
+
+        return services
+
+
+class NetstatServicesParser(BaseParser):
+
+    """Parser class for `/usr/bin/netstat -an | grep ":\\*";` command output.
+
+    Supported format:
+
+        tcp        0      0 0.0.0.0:111         0.0.0.0:*        LISTEN
+        tcp        0      0 0.0.0.0:22          0.0.0.0:*        LISTEN
+        tcp        0      0 127.0.0.1:25        0.0.0.0:*        LISTEN
+        tcp6       0      0 :::111              :::*             LISTEN
+        tcp6       0      0 :::22               :::*             LISTEN
+        tcp6       0      0 ::1:25              :::*             LISTEN
+        udp        0      0 0.0.0.0:68          0.0.0.0:*
+        udp        0      0 127.0.0.1:323       0.0.0.0:*
+        udp        0      0 0.0.0.0:21944       0.0.0.0:*
+        udp6       0      0 :::111              :::*
+        udp6       0      0 :::123              :::*
+        udp6       0      0 :::37339            :::*
+    """
+
+    def parse(self, results):
+        """Parses command output in `results` string and returns services
+        information in format:
+
+            [(proto, port, addr), ...]
+        """
+        services = []
+
+        for line in results.split("\n"):
+            fields = line.split()
+            if len(fields) < 5:
+                continue
+            try:
+                proto = fields[0]
+                if proto == "raw":
+                    continue
+                listar = fields[3].split(":")
+                addr = port = ""
+                if len(listar) == 2:
+                    addr, port = listar
+                elif len(listar) == 4:
+                    addr = "0.0.0.0"
+                    port = listar[-1]
+                else:
+                    continue
+
+                self.log.debug("Got %s %s port %s", addr, proto, port)
+
+                port = int(port)
+            except ValueError:
+                self.log.exception("Failed to parse IPService information '%s'",
+                                   line)
+                continue
+
+            services.append((proto, port, addr))
+
+        return services
+
 
 class netstat_an(LinuxCommandPlugin):
-    maptype = "IpServiceMap" 
-    command = "netstat -an | grep ':\\*'"
+
+    maptype = "IpServiceMap"
+    command = 'export PATH=$PATH:/sbin:/usr/sbin; \
+                 if which netstat >/dev/null 2>&1; then \
+                     netstat -an | grep ":\\*"; \
+                 elif which ss >/dev/null 2>&1; then \
+                     echo "### ss output"; ss -lntu; \
+                 else \
+                     echo "No netstat or ss utilities were found."; exit 127; \
+                 fi'
+
     compname = "os"
     relname = "ipservices"
     modname = "Products.ZenModel.IpService"
-    deviceProperties = LinuxCommandPlugin.deviceProperties + (
-           'zIpServiceMapMaxPort', )
-
+    deviceProperties = \
+        LinuxCommandPlugin.deviceProperties + ('zIpServiceMapMaxPort', )
 
     def process(self, device, results, log):
-        log.info('Modeler %s processing data for device %s', self.name(), device.id)
-
+        log.info('Modeler %s processing ip services data for device %s',
+                 self.name(), device.id)
+        self.log = log
+        rm = self.relMap()
         if not results.strip(): # No output
             log.error("No output from the command: %s", self.command)
             return
+
+        if '### ss output' not in results:
+            parser = NetstatServicesParser(log)
+        else:
+            parser = SsServicesParser(log)
 
         try:
             maxport = getattr(device, 'zIpServiceMapMaxPort', 1024)
@@ -40,45 +194,31 @@ class netstat_an(LinuxCommandPlugin):
 
         rm = self.relMap()
         ports = {}
-        for line in results.splitlines():
-            aline = line.split()
-            if len(aline) < 5: continue
-            try:
-                proto, local = aline[0], aline[3]
-                if proto == "raw":
-                    continue
-                addr, port = local.rsplit(":", 1) 
-                log.debug("Got %s %s port %s", addr, proto, port)
-                if not IpUtil.isRemotelyReachable(addr) or not port:
-                    # Can't monitor things we can't reach
-                    continue
-                port = int(port)
-                if port > maxport:
-                    log.debug("Ignoring entry greater than " \
-                              "zIpServiceMapMaxPort (%s): %s %s %s",
-                              maxport, addr, proto, port)
-                    continue
-            except ValueError:
-                log.exception("Failed to parse IPService information '%s'",
-                              line)
+        for proto, port, addr in parser.parse(results):
+            if addr == "127.0.0.1" or not port:  # Can't monitor things we can't reach
+                continue
+
+            if port > maxport:
+                log.debug("Ignoring entry greater than zIpServiceMapMaxPort "
+                          "(%s): %s %s %s", maxport, addr, proto, port)
                 continue
 
             om = ports.get((proto, port), None)
             if om:
                 if addr in om.ipaddresses:
                     continue
-                log.debug("Adding %s to the list of addresses listening to %s port %s",
-                          addr, proto, port)
+                log.debug("Adding %s to the list of addresses listening "
+                          "to %s port %s", addr, proto, port)
                 om.ipaddresses.append(addr)
             else:
                 om = self.objectMap()
                 om.protocol = proto
                 om.port = int(port)
-                om.id = '%s_%05d' % (om.protocol,om.port)
+                om.id = '%s_%05d' % (om.protocol, om.port)
                 log.debug("Found %s listening to %s port %s (%s)",
                           addr, proto, port, om.id)
-                om.setServiceClass = {'protocol': proto, 'port':om.port}
-                om.ipaddresses = [addr,]
+                om.setServiceClass = {'protocol': proto, 'port': om.port}
+                om.ipaddresses = [addr]
                 om.discoveryAgent = self.name()
                 ports[(proto, port)] = om
                 rm.append(om)
--- Products/DataCollector/plugins/zenoss/cmd/linux/netstat_rn.py
+++ Products/DataCollector/plugins/zenoss/cmd/linux/netstat_rn.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # 
-# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2007, 2015 all rights reserved.
 # 
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -9,21 +9,197 @@
 
 
 __doc__ = """netstat_rn
-Collect routing information using the netstat -rn command.
+Collect routing information using the `netstat -rn` or `ip route show`
+commands.
 """
 
-TARGET=0
-GATEWAY=1
-NETMASK=2
-FLAGS=3
-INTERFACE=7
-
+import abc
+import re
 from Products.DataCollector.plugins.CollectorPlugin import LinuxCommandPlugin
+
+
+# vars for netstat
+IFCFG_TARGET=0
+IFCFG_GATEWAY=1
+IFCFG_NETMASK=2
+IFCFG_FLAGS=3
+IFCFG_INTERFACE=7
+
+# vars for ip tool
+# example
+# default via 10.111.23.1 dev eth0
+IP_GATEWAY = re.compile(r"^default via (\S+) dev (\S+)(.*)")
+IP_GATEWAY_PROTO = re.compile(r"proto (\S+)")
+# example: 10.111.23.0/24 dev eth0  proto kernel  scope link  src 10.111.23.72
+# 10.111.23.0/24 dev eth0  proto kernel  scope link  src 10.111.23.72
+#          192.168.99.0/24 dev eth0  scope link
+IP_ROUTE_RECORD = \
+    re.compile(r"^(\S+)\s+dev\s+(\S+)\s+proto\s+(\S+)\s+scope\s+link\s+")
+IP_ROUTE_RECORD_SHORT = re.compile(r"^(\S+)\s+dev\s+(\S+)\s+scope\s+link\s+")
+
+
+class BaseParser(object):
+
+    """Base class for command output parsers."""
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, plugin, log):
+        self.plugin = plugin
+        self.log = log
+
+    @abc.abstractmethod
+    def parse(self, results):
+        """Parses command output in `results` string and returns list of
+        ObjectMaps with routes information.
+        """
+
+
+class IpRoutesParser(BaseParser):
+
+    """Parser class for `/usr/sbin/ip route show` command output.
+
+    Supported format:
+
+        default via 10.111.3.1 dev eth0
+        10.111.3.0/24 dev eth0  proto kernel  scope link  src 10.111.3.244
+    """
+
+    def parse(self, results):
+        """Parses command output in `results` string and returns list of
+        ObjectMaps with routes information.
+        """
+        routes = []
+
+        rlines = results.split("\n")
+        for line in rlines:
+            if '###' in line or not line:
+                continue
+
+            route = self.plugin.objectMap()
+            route.id = None
+
+            isgate = IP_GATEWAY.search(line)
+            if isgate:
+                # default via 10.111.23.1 dev eth0
+                route.id = "0.0.0.0_0"
+                route.mask = '0'
+                route.setTarget = '0.0.0.0/0'  # rfc1519
+                route.setNextHopIp = isgate.group(1)
+                route.setInterfaceName = isgate.group(2)
+                route.routetype = 'indirect'
+
+                route.routeproto = 'local'
+                protomatch = IP_GATEWAY_PROTO.search(line)
+                if protomatch:
+                    route.routeproto = self.convertProto(protomatch.group(1))
+
+            isroute = IP_ROUTE_RECORD.search(line)
+            if isroute:
+                # 192.168.99.0/24 dev eth0  scope link
+                route.id = isroute.group(1).replace('/', '_')
+                route.mask = isroute.group(1).split('/', 1)[1]
+                if route.mask == 32:
+                    continue
+                route.setTarget = isroute.group(1)
+                route.setNextHopIp = '0.0.0.0'
+                route.setInterfaceName = isroute.group(2)
+                route.routetype = 'direct'
+                route.routeproto = self.convertProto(isroute.group(3))
+
+            isroute = IP_ROUTE_RECORD_SHORT.search(line)
+            if isroute:
+                route.id = isroute.group(1).replace('/', '_')
+                route.mask = isroute.group(1).split('/', 1)[1]
+                if route.mask == 32:
+                    continue
+                route.setTarget = isroute.group(1)
+                route.setNextHopIp = '0.0.0.0'
+                route.setInterfaceName = isroute.group(2)
+                route.routetype = 'direct'
+                route.routeproto = 'local'
+
+            if route.id is not None:
+                self.log.debug('Got route %s %s %s %s %s %s %s', route.id,
+                               route.setTarget, route.mask,
+                               route.setInterfaceName, route.setNextHopIp,
+                               route.routetype, route.routeproto)
+                routes.append(route)
+
+        return routes
+
+    def convertProto(self, routeproto):
+        if routeproto.strip().lower() == 'redirect':
+            return 'dynamic'
+        return 'local'
+
+
+class NetstatRoutesParser(BaseParser):
+
+    """Parser class for `/usr/bin/netstat -rn` command output.
+
+    Supported format:
+
+        Kernel IP routing table
+        Destination  Gateway     Genmask         Flags   MSS Window  irtt Iface
+        0.0.0.0      10.111.3.1  0.0.0.0         UG        0 0          0 eth0
+        10.111.3.0   0.0.0.0     255.255.255.0   U         0 0          0 eth0
+    """
+
+    def parse(self, results):
+        """Parses command output in `results` string and returns list of
+        ObjectMaps with routes information.
+        """
+        routes = []
+
+        rlines = results.splitlines()
+        for line in rlines:
+            fields = line.split()
+            if len(fields) != 8 or not self.plugin.isip(fields[0]):
+                continue
+
+            route = self.plugin.objectMap()
+
+            route.routemask = self.plugin.maskToBits(fields[IFCFG_NETMASK])
+            if route.routemask == 32:
+                continue
+
+            if "G" in fields[IFCFG_FLAGS]:
+                route.routetype = 'indirect'
+            else:
+                route.routetype = 'direct'
+
+            if "D" in fields[IFCFG_FLAGS]:
+                route.routeproto = "dynamic"
+            else:
+                route.routeproto = "local"
+
+            route.id = '%s_%s' % (fields[IFCFG_TARGET], route.routemask)
+            route.setTarget = '%s/%s' % (fields[IFCFG_TARGET], route.routemask)
+            route.setInterfaceName = fields[IFCFG_INTERFACE]
+            route.setNextHopIp = fields[IFCFG_GATEWAY]
+
+            self.log.debug('Got route %s %s %s %s %s %s %s', route.id,
+                           route.setTarget, route.routemask,
+                           route.setInterfaceName, route.setNextHopIp,
+                           route.routetype, route.routeproto)
+            routes.append(route)
+
+        return routes
+
+
 
 class netstat_rn(LinuxCommandPlugin):
     
     maptype = "RouteMap" 
-    command = 'netstat -rn'
+    command = 'export PATH=$PATH:/sbin:/usr/sbin; \
+                 if which netstat >/dev/null 2>&1; then \
+                     netstat -rn; \
+                 elif which ip >/dev/null 2>&1; then \
+                     echo "### ip addr output"; ip route show; echo "";\
+                 else \
+                     echo "No netstat or ip utilities were found."; exit 127; \
+                 fi'
     compname = "os"
     relname = "routes"
     modname = "Products.ZenModel.IpRouteEntry"
@@ -31,35 +207,23 @@ class netstat_rn(LinuxCommandPlugin):
         'zRouteMapCollectOnlyIndirect',
         )
         
-        
+
     def process(self, device, results, log):
-        log.info('Collecting routes for device %s' % device.id)
+        log.info('Modeler %s collecting routes for device %s', self.name(),
+                 device.id)
+        self.log = log
+        if '### ip addr' not in results:
+            parser = NetstatRoutesParser(self, log)
+        else:
+            parser = IpRoutesParser(self, log)
+
         indirectOnly = getattr(device, 'zRouteMapCollectOnlyIndirect', False)
         rm = self.relMap()
-        rlines = results.split("\n")
-        for line in rlines:
-            aline = line.split()
-            if len(aline) != 8 or not self.isip(aline[0]):
-                continue
-            route = self.objectMap()
-            
-            route.routemask = self.maskToBits(aline[NETMASK])
-            if route.routemask == 32: continue
-            
-            if "G" in aline[FLAGS]:
-                route.routetype = 'indirect'
-            else:
-                route.routetype = 'direct'
+
+        for route in parser.parse(results):
             if indirectOnly and route.routetype != 'indirect':
                 continue
-            
-            if "D" in aline[FLAGS]: route.routeproto = "dynamic"
-            else: route.routeproto = "local"
-            
-            route.id = aline[TARGET]
-            route.setTarget = route.id + "/" + str(route.routemask)
-            route.id = route.id + "_" + str(route.routemask)
-            route.setInterfaceName = aline[INTERFACE]
-            route.setNextHopIp = aline[GATEWAY]
+
             rm.append(route)
+
         return rm
