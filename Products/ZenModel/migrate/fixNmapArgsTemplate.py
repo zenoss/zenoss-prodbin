79946cd5455717b280e70d171bfade696f8b451c
Roman Bakaleyko <rbakaleyko@zenoss.com>
2015-09-14 11:34:28 +0300

    ZEN-17811 Default zNmapPortscanOptions are invalid

---
+++ Products/ZenModel/migrate/fixNmapArgsTemplate.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2015, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import Migrate
+
+
+
+
+class fixNmapArgsTemplate(Migrate.Step):
+    " Update default args for nmap command "
+
+    version = Migrate.Version(5,0,70)
+
+    def cutover(self, dmd):
+        
+        if hasattr(dmd.Devices, 'zNmapPortscanOptions'): dmd.Devices.zNmapPortscanOptions = '-p 1-1024 -sT --open -oG -'
+
+
+
+fixNmapArgsTemplate()
