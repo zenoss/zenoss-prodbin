4ac69e46442c653101cb775b301ace565edd98f8
Sergey Fedchenko <himixoidus@gmail.com>
2015-10-29 16:57:53 +0200

    fixes ZEN-20495: Central Query logs unknown metric errors too frequently

--- Products/ZenModel/migrate/switchLoggingLevel.py
+++ Products/ZenModel/migrate/switchLoggingLevel.py
@@ -21,7 +21,7 @@ sm.require("1.0.0")
 
 class SwitchLoggingLevel(Migrate.Step):
 
-    version = Migrate.Version(5, 1, 70)
+    version = Migrate.Version(5, 0, 8)
 
     def cutover(self, dmd):
         try:
