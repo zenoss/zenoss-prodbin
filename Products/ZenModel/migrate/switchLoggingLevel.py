803a5d16c452c790a7c15c748102c8d7d2fe037c
Sergey Fedchenko <himixoidus@gmail.com>
2015-11-09 19:13:34 +0200

    fixes ZEN-20859: ImportError: No module named servicemigration when doing a 5.0.8 upgrade

--- Products/ZenModel/migrate/switchLoggingLevel.py
+++ Products/ZenModel/migrate/switchLoggingLevel.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 ##############################################################################
 #
 # Copyright (C) Zenoss, Inc. 2015, all rights reserved.
@@ -12,6 +11,7 @@ __doc__ = """
 Switch default logging level to WARN
 """
 
+import Migrate
 import logging
 log = logging.getLogger("zen.migrate")
 import os
@@ -23,24 +23,27 @@ import subprocess
 DEVNULL = open(os.devnull, 'w')
 
 
-def main():
-    log.info("Looking up service CentralQuery")
-    try:
-        svc_json = subprocess.check_output(["serviced", "service", "list", "CentralQuery"])
-        parsed = json.loads(svc_json)
-    except subprocess.CalledProcessError as e:
-        sys.exit(e.returncode)
-    except ValueError:
-        sys.exit(1)
-    log.info("Updating default logging level")
-    parsed['logging']['level'] = 'WARN'
-    parsed['logging']['loggers']['org.zenoss'] = 'WARN'
-    try:
-        proc = subprocess.Popen(["serviced", "service", "edit", "CentralQuery"], stdin=subprocess.PIPE, stdout=DEVNULL)
-        proc.communicate(json.dumps(parsed))
-    except subprocess.CalledProcessError as e:
-        sys.exit(e.returncode)
+class SwitchLoggingLevel(Migrate.Step):
+
+    version = Migrate.Version(5, 0, 8)
+
+    def cutover(self, dmd):
+        log.info("Looking up service CentralQuery")
+        try:
+            svc_json = subprocess.check_output(["serviced", "service", "list", "CentralQuery"])
+            parsed = json.loads(svc_json)
+        except subprocess.CalledProcessError as e:
+            sys.exit(e.returncode)
+        except ValueError:
+            sys.exit(1)
+        log.info("Updating default logging level")
+        parsed['logging']['level'] = 'WARN'
+        parsed['logging']['loggers']['org.zenoss'] = 'WARN'
+        try:
+            proc = subprocess.Popen(["serviced", "service", "edit", "CentralQuery"], stdin=subprocess.PIPE, stdout=DEVNULL)
+            proc.communicate(json.dumps(parsed))
+        except subprocess.CalledProcessError as e:
+            sys.exit(e.returncode)
 
 
-if __name__ == "__main__":
-    main()
+SwitchLoggingLevel()
