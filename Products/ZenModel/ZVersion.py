76148788d655a93e9bae1dcd1ae45ebc9b0d9a9d
Andrew Lapidas <alapidas@gmail.com>
2015-12-07 13:04:18 -0600

    Bump version to 5.1.0

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -1,11 +1,11 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2008, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 # If you change this, also change inst/zenoss-version/setup.py
-VERSION="5.0.70"
+VERSION="5.1.0"
