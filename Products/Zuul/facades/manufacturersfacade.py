b486ad227d5242e25de72a99d45d491b816371e1
Roman Bakaleyko <rbakaleyko@zenoss.com>
2016-01-08 17:37:06 +0200

    Fixes ZEN-21558
    cherry-pick commit f454dbbf33d5b02ef37d94dc3b699def4fb2870e
    (committed by "zendev port cherry-pick")

--- Products/Zuul/facades/manufacturersfacade.py
+++ Products/Zuul/facades/manufacturersfacade.py
@@ -194,7 +194,7 @@ class ManufacturersFacade(TreeFacade):
         """
         edit the information for a given manufacturer
         """
-        manufacturer = self._getObject("/zport/dmd/Manufacturers/"+params['name'])
+        manufacturer = self._getObject("/zport/dmd/Manufacturers/"+params['oldname'])
         manufacturer.url = params['URL']
         manufacturer.supportNumber = params['phone']
         manufacturer.address1 = params['address1']
@@ -204,7 +204,7 @@ class ManufacturersFacade(TreeFacade):
         manufacturer.zip = params['zip']
         manufacturer.country = params['country']
         manufacturer.regexes = params['regexes']
-        if (manufacturer.title != params['name']):
+        if (manufacturer.id != params['name']):
             manufacturer.rename(params['name'])
 
     def moveProduct(self, moveFrom, moveTarget, ids):
