2fec934a5917c6379b54ccdc691259f565e21444
jplouis <jplouis@gmail.com>
2016-02-22 11:05:00 -0600

    don't run zenpack install twice

--- Products/ZenUtils/zenpack.py
+++ Products/ZenUtils/zenpack.py
@@ -550,11 +550,9 @@ class ZenPackCmd(ZenScriptBase):
                     self.log.debug("running cmd `%s`", cmd.join(" "))
                     with open(os.devnull, 'w') as fnull:
                         # the first time fixes the easy-install path
-                        subprocess.check_call(cmd, stdout=fnull, stderr=fnull)
+                        subprocess.check_call(cmd)
                 except Exception:
                     pass
-                # the second time runs the loaders
-                subprocess.check_call(cmd)
             finally:
                 try:
                     os.remove(os.path.join(tempfile.gettempdir(), os.path.basename(candidate)))
@@ -864,7 +862,7 @@ class ZenPackCmd(ZenScriptBase):
         self.parser.defaults['logpath'] = zenPath('log')
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s")
     log = logging.getLogger('zen.ZenPackCmd')
     try:
         zp = ZenPackCmd()
