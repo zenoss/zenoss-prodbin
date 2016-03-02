1819e8a8eebab192655a0268db665ce3b4b7f5f5
Serhiy Shvorob <sshvorob@zenoss.com>
2016-03-02 17:22:30 +0000

    ZEN-22066: Migrate Script: ZProxy Logging Every Connection to Logstash

--- Products/ZenModel/migrate/disableZproxyAccessLog.py
+++ Products/ZenModel/migrate/disableZproxyAccessLog.py
@@ -20,7 +20,7 @@ sm.require("1.0.0")
 class DisableZproxyAccessLog(Migrate.Step):
     "Disable zproxy nginx access logging by default"
 
-    version = Migrate.Version(5,0,70)
+    version = Migrate.Version(5,2,0)
 
     def cutover(self, dmd):
 
