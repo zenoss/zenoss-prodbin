28b8f7a64ece7545d0cefc9b735ba91b6e19a063
morr-zenoss <morr@zenoss.com>
2016-05-18 10:30:09 -0500

    adapt migration tests for support branch

--- Products/ZenModel/migrate/tests/test_defaultBeakerToSecure.py
+++ Products/ZenModel/migrate/tests/test_defaultBeakerToSecure.py
@@ -11,8 +11,8 @@ class Test_UseBeakerInZope(unittest.TestCase, common.ServiceMigrationTestCase):
     """
     Test that beaker is added to zope's config file.
     """
-    initial_servicedef = 'zenoss-resmgr-5.1.2.json'
-    expected_servicedef = 'zenoss-resmgr-5.1.2-defaultBeakerToSecure.json'
+    initial_servicedef = 'zenoss-resmgr-5.0.6_1-useBeakerInZope.json'
+    expected_servicedef = 'zenoss-resmgr-5.0.6_1-defaultBeakerToSecure.json'
     migration_module_name = 'defaultBeakerToSecure'
     migration_class_name = 'DefaultBeakerToSecure'
        
