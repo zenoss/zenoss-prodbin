dbe406f7298463603499a4a95af1497cd73160c0
Sergii Gromovyi <sgromovyi@zenoss.com>
2016-05-25 11:42:09 +0300

    added a link creation to ZPLBin

--- Products/ZenUtils/ZenPackCmd.py
+++ Products/ZenUtils/ZenPackCmd.py
@@ -381,8 +381,6 @@ def InstallDistAsZenPack(dmd, dist, eggPath, link=False, filesOnly=False,
             else:
                 zenPack = ZenPack(packName)
             zenPack.eggPack = True
-            if hasattr(zenPack, 'install_bin_files'):
-                zenPack.install_bin_files()
             CopyMetaDataToZenPackObject(dist, zenPack)
             if filesOnly:
                 for loader in (ZPL.ZPLDaemons(), ZPL.ZPLBin(), ZPL.ZPLLibExec()):
