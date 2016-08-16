ea810d20b702ad74967577cbacdc4e9053600cfb
Supriya Ambati <svambati@gmail.com>
2016-08-16 15:06:35 -0500

    Updated filename in unit test

--- Products/ZenModel/migrate/tests/test_updateZepLogbackConfig.py
+++ Products/ZenModel/migrate/tests/test_updateZepLogbackConfig.py
@@ -8,7 +8,7 @@ class Test_UpdateZepLogbackConfig(unittest.TestCase, common.ServiceMigrationTest
     """Test that the logback.xml file is updated."""
 
     initial_servicedef = 'zenoss-resmgr-5.0.6_1.json'
-    expected_servicedef = 'zenoss-resmgr-updateLogbackConfig.json'
+    expected_servicedef = 'zenoss-resmgr-5.0.6_1-updateZepLogbackConfig.json'
     migration_module_name = 'updateZepLogbackConfig'
     migration_class_name = 'UpdateZepLogbackConfig'
 
