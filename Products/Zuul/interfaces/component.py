8ee0bf28853d36742f1d8ad729f6e89bacafb930
alam-zenoss <alam@zenoss.com>
2016-09-14 10:33:27 -0500

    Adds Parent Device to Component Details View

--- Products/Zuul/interfaces/component.py
+++ Products/Zuul/interfaces/component.py
@@ -30,12 +30,14 @@ class IComponentInfo(IInfo):
     An info adapter that wraps a device component.  Examples of device
     components are OSProcesses, IPServices and WinServices.
     """
-    device = Attribute("Parent Device")
     status = schema.TextLine(title=_t(u"Status"),
                              description=u"Are there any active status events"
                                          u" for this component?", group="Overview",
                              order=1,
                              readonly=True)
+    device = schema.Entity(title=_t(u"Device"),
+                           description=u"Parent Device", group="Overview",
+                           order=2)
     usesMonitorAttribute = Attribute("Should the user be able to set the monitor attribute")
     monitor = Attribute("Has monitoring been enabled on the component")
     monitored = Attribute(u"Is the component being monitored"
