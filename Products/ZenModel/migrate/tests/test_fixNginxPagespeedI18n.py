06368a5fa797e95d8c8ce8f86610a38b3f2baec3
Cornelius Toole <ctoole@zenoss.com>
2016-09-23 13:19:33 -0500

    WIP: renaming migrations, test and test data more appropriately

--- Products/ZenModel/migrate/tests/test_fixNginxPagespeedI18n.py
+++ Products/ZenModel/migrate/tests/test_fixNginxPagespeedI18n.py
@@ -5,14 +5,14 @@ import unittest
 import Globals
 import common
 
-class Test_fixNginxPagespeedI18n(unittest.TestCase, common.ServiceMigrationTestCase):
+class Test_EnableNginxPagespeed(unittest.TestCase, common.ServiceMigrationTestCase):
     """
-    Test that 'pagespeed on' pagespeed Disallow '*i18n*' is in zproxy's config file.
+    Test that 'pagespeed on' is in zproxy's config file.
     """
-    initial_servicedef = 'zenoss-resmgr-5.1.5.json'
-    expected_servicedef = 'zenoss-resmgr-5.1.5-fixNginxPagespeedI18n.json'
-    migration_module_name = 'fixNginxPagespeedI18n'
-    migration_class_name = 'FixNginxPagespeedI18n'
+    initial_servicedef = 'zenoss-resmgr-5.1.5-pagespeedoff.json'
+    expected_servicedef = 'zenoss-resmgr-5.1.5.json-enableNginxPagespeed.json'
+    migration_module_name = 'enableNginxPagespeed'
+    migration_class_name = 'EnableNginxPagespeed'
 
 
 if __name__ == '__main__':
