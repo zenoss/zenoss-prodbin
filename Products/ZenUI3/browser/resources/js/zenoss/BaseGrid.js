0019e7d4ea355e3e8dfe1a41b7c45fa646e47b7a
Cornelius Toole <ctoole@zenoss.com>
2016-11-07 17:02:11 -0600

    convert {first|last}Time event filter to UTC before issuing query

--- Products/ZenUI3/browser/resources/js/zenoss/BaseGrid.js
+++ Products/ZenUI3/browser/resources/js/zenoss/BaseGrid.js
@@ -451,6 +451,15 @@
             }
             // this will make sure that all subsequent buffer loads have the parameters
             this.grid.store.proxy.extraParams.params = values;
+            // ZEN-23418 Convert firstSeen and lastSeen filter params to UTC before making query request
+            if (this.grid.store.proxy.extraParams.params.firstTime) {
+                var timeRange = this.grid.store.proxy.extraParams.params.firstTime;
+                this.grid.store.proxy.extraParams.params.firstTime = moment(timeRange).utc().format("YYYY-MM-DD hh:mm:ss");
+            }
+            if (this.grid.store.proxy.extraParams.params.lastTime) {
+                var timeRange = this.grid.store.proxy.extraParams.params.lastTime;
+                this.grid.store.proxy.extraParams.params.lastTime = moment(timeRange).utc().format("YYYY-MM-DD hh:mm:ss");
+            }
 
             // reset their scrolling when the filters change
             this.grid.scrollToTop();
