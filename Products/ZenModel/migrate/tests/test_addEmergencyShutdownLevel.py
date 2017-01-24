bb686c5e5d344f4691572901b3a2eafe3d68847c
Young Lee <ylee@zenoss.com>
2017-01-24 15:24:26 -0600

    Add emergency shutdown level and start level to service definitions

---
+++ Products/ZenModel/migrate/tests/test_addEmergencyShutdownLevel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+
+import Globals
+import common
+
+
+class Test_AddEmergencyShutdownLevel(unittest.TestCase, common.ServiceMigrationTestCase):
+    """
+    Test that the start and emergency shutdown levels are added to service definitions.
+    """
+    initial_servicedef = 'zenoss-resmgr-5.1.5.json'
+    expected_servicedef = 'zenoss-resmgr-5.1.5-addEmergencyShutdownLevel.json'
+    migration_module_name = 'addEmergencyShutdownLevel'
+    migration_class_name = 'AddEmergencyShutdownLevel'
+
+
+if __name__ == '__main__':
+    unittest.main()
