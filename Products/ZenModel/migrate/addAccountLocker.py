86b20490f74d70af833a0caec857f1e91797b643
romanbakaleyko <roman.bakaleyko@gmail.com>
2017-02-03 18:52:57 +0200

    ZEN-22244 Improper Restriction of Excessive Authentication Attempts

---
+++ Products/ZenModel/migrate/addAccountLocker.py
@@ -0,0 +1,32 @@
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2017, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
+
+__doc__='''
+This migration script adds AccountLocker plugin to PAS.
+''' 
+
+import logging
+log = logging.getLogger("zen.migrate")
+
+import Migrate
+import os
+from Products.ZenUtils.AccountLocker.AccountLocker import setup
+
+
+class AddAccountLocker(Migrate.Step):
+
+    version = Migrate.Version(109, 0, 0)
+
+    def cutover(self, dmd):
+        
+        setup(dmd)
+        log.info("Account locker is installed.")
+
+AddAccountLocker()
