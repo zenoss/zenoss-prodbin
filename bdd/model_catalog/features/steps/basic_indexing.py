d27005324f8de01b16f458f3a357289e3dd24424
Paco Ibanez-Castillo <fraibacas@gmail.com>
2017-04-21 17:38:57 -0500

    ZEN-27330: add test for devices

--- bdd/model_catalog/features/steps/basic_indexing.py
+++ bdd/model_catalog/features/steps/basic_indexing.py
@@ -1,15 +1,6 @@
 from behave import given, when, then
 
 
-@given('I can connect to "{connectable}"')
-def step_impl(context, connectable):
-    connectable = connectable.lower()
-    if connectable == "zodb":
-        assert context.zen_context.dmd.Devices is not None
-    elif connectable == "model_catalog" or connectable == "model catalog":
-        assert context.zen_context.solr_client.ping() is True
-
-
 @given('all "{object_type}" have been indexed')
 def step_impl(context, object_type):
     # nothing to do
---
+++ bdd/model_catalog/features/steps/devices.py
@@ -0,0 +1,47 @@
+
+from behave import given, when, then
+
+import pickle
+import os
+
+def _load_device_maps(maps_path):
+    maps = []
+    file_names = os.listdir(maps_path)
+    for file_name in file_names:
+        path = "{}/{}".format(maps_path, file_name)
+        with open(path, "rb") as f:
+            maps.append(pickle.load(f))
+    return maps
+
+@given('the mock "{device_class}" device with ip "{ip}" is in Zenoss')
+def step_impl(context, device_class, ip):
+    device = context.zen_context.dmd.Devices.findDeviceByIdOrIp(ip)
+    if not device:
+        maps = _load_device_maps("./model_catalog/features/data/snmp_device")
+        device = context.zen_context.zodb_helper.create_device_from_maps(ip, device_class, maps)
+    context.device = device
+
+
+@when('I search for all the device\'s "{object_type}" in model catalog')
+def step_impl(context, object_type):
+    object_type = object_type.lower()
+    catalog_helper = context.zen_context.model_catalog_helper
+
+    if object_type == "components":
+        components = catalog_helper.get_device_components(context.device)
+        context.indexed_device_components = components
+    else:
+        raise NotImplementedError
+
+
+@then('I get all the device\'s "{object_type}"')
+def step_impl(context, object_type):
+    object_type = object_type.lower()
+    zodb_helper = context.zen_context.zodb_helper
+
+    if object_type == "components":
+        device_components = zodb_helper.get_device_components(context.device)
+        assert set(device_components) == set(context.indexed_device_components)
+    else:
+        raise NotImplementedError
+
