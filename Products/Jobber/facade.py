daf73db9f3409b149d0c3a34808f3715f9845ca4
Young Lee <ylee@zenoss.com>
2017-06-05 11:24:16 -0500

    Log msgs from facade method job in job log

--- Products/Jobber/facade.py
+++ Products/Jobber/facade.py
@@ -8,6 +8,11 @@
 ##############################################################################
 
 
+import os
+import errno
+import logging
+
+from Products.ZenUtils.celeryintegration import get_task_logger
 from zope.dottedname.resolve import resolve
 from celery.utils import fun_takes_kwargs
 from .jobs import Job
@@ -27,7 +32,31 @@ class FacadeMethodJob(Job):
         facade = facadefqdn.split('.')[-1]
         return "%s.%s %s" % (facade, method, args[0] if args else '')
 
+    @property
+    def log(self):
+        if self._log is None:
+            # Get log directory, ensure it exists
+            logdir = self._get_config('job-log-path')
+            try:
+                os.makedirs(logdir)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            # Make the logfile path and store it in the backend for later
+            # retrieval
+            logfile = os.path.join(logdir, '%s.log' % self.request.id)
+            self.setProperties(logfile=logfile)
+            self._log = get_task_logger(self.request.id)
+            self._log.setLevel(self._get_config('logseverity'))
+            handler = logging.FileHandler(logfile)
+            handler.setFormatter(logging.Formatter(
+                "%(asctime)s %(levelname)s zen.Job: %(message)s"))
+            self._log.handlers = [handler]
+        return self._log
+
     def _run(self, facadefqdn, method, *args, **kwargs):
+        # Pass the job log to the facade method so that it can log to the job log.
+        kwargs['joblog'] = self.log
         self.args = args
         self.kwargs = kwargs
         facadeclass = resolve(facadefqdn)
