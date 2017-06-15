730b190598166aac5aef7298be18d57a230d8e42
ostyhar <ostyhar@zenoss.com>
2017-06-15 13:32:01 -0400

    ZEN-27731: vSphere devices are not filtered with Down status on Infrastructure page

--- Products/Zuul/facades/__init__.py
+++ Products/Zuul/facades/__init__.py
@@ -279,12 +279,9 @@ class TreeFacade(ZuulFacade):
         devices = list(imap(IInfo, imap(unbrain, brains)))
         if isinstance(params, dict):
             statuses = params.pop('status', None)
-            # Don't filter if we want to see devices with both states UP and DOWN what is set by default
+            # Don't filter if we want to see devices with all statuses UP, DOWN and UNKNOWN what is set by default
             if statuses is not None and len(statuses) < 3:
-                if None in statuses:
-                    devices = [d for d in devices if (d.status in statuses or d.status is None)]
-                else:
-                    devices = [d for d in devices if d.status in statuses]
+                devices = [d for d in devices if d.status in statuses]
 
         uuids = set(dev.uuid for dev in devices)
         if uuids:
