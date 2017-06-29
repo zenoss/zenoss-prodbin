23bf878ec90559ee42e1bae13f6e4a8f86e62f49
Paco Ibanez-Castillo <fraibacas@gmail.com>
2017-06-28 23:04:45 -0500

    model_catalog_tool.uid_field_name is no longer available. All search include the uid as field to return

--- Products/Zuul/facades/templatefacade.py
+++ Products/Zuul/facades/templatefacade.py
@@ -44,7 +44,7 @@ class TemplateFacade(ZuulFacade):
 
     def _getTemplateNodes(self):
         catalog = self._getCatalog('/zport/dmd/Devices')
-        fields_to_return = [ catalog.uid_field_name, "id" ]
+        fields_to_return = [ "id" ]
         brains = catalog.search(types=RRDTemplate, fields=fields_to_return)
         nodes = {}
         # create 1 node for each template type
