a14a7ea70417abf951b25d3b3b5b53f06c8d39b3
jplouis <jplouis@gmail.com>
2017-07-26 15:14:38 -0500

    Convert worklist metrics to metrology gauges

--- Products/ZenHub/zenhub.py
+++ Products/ZenHub/zenhub.py
@@ -27,6 +27,7 @@ import collections
 import heapq
 import logging
 from metrology import Metrology
+from metrology.instruments import Gauge
 import time
 import signal
 import cPickle as pickle
@@ -445,6 +446,32 @@ class ZenHub(ZCmdBase):
         self.shutdown = False
         self.counters = collections.Counter()
         self._invalidations_paused = False
+
+
+        wl = self.workList
+        class EventWorkList(Gauge):
+            @property
+            def value(self):
+                return len(wl.eventworklist)
+        Metrology.gauge("zenhub.eventWorkList", EventWorkList())
+
+        class ADMWorkList(Gauge):
+            @property
+            def value(self):
+                return len(wl.applyworklist)
+        Metrology.gauge("zenhub.admWorkList", ADMWorkList())
+
+        class OtherWorkList(Gauge):
+            @property
+            def value(self):
+                return len(wl.otherworklist)
+        Metrology.gauge("zenhub.otherWorkList", OtherWorkList())
+
+        class WorkListTotal(Gauge):
+            @property
+            def value(self):
+                return len(wl)
+        Metrology.gauge("zenhub.workList", WorkListTotal())
 
         ZCmdBase.__init__(self)
         import Products.ZenHub
@@ -1032,9 +1059,7 @@ class ZenHub(ZCmdBase):
         r.gauge('services', len(self.services))
         r.counter('totalCallTime', totalTime)
         r.gauge('workListLength', len(self.workList))
-        r.gauge('eventWorkList', len(self.workList.eventworklist))
-        r.gauge('applyWorkList', len(self.workList.applyworklist))
-        r.gauge('otherWorkList', len(self.workList.otherworklist))
+
         for name, value in self.counters.items():
             r.counter(name, value)
 
