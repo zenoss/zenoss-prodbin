731d1a05ec8590e6acbfb8c9676b7e9c77b0b359
Erik Dieckert <edieckert@zenoss.com>
2017-08-17 11:43:50 -0500

    Added in the migration scripts and test for adding the rabbitmq log filters

---
+++ Products/ZenModel/migrate/tests/test_addRabbitMQLogFilters.py
@@ -0,0 +1,28 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import common
+import unittest
+from Products.ZenUtils.Utils import zenPath
+
+class test_addRabbitMQLogFilters(unittest.TestCase, common.ServiceMigrationTestCase):
+    """
+    Test adding LogFilters for RabbitMQ logs issue addressed by ZEN-28095
+    """
+    initial_servicedef = 'zenoss-resmgr-5.1.3-updateRabbitMQLogPaths.json'
+    expected_servicedef = 'zenoss-resmgr-5.1.3-addRabbitMQLogFilters.json'
+    migration_module_name = 'addRabbitMQLogFilters'
+    migration_class_name = 'AddRabbitMQLogFilters'
+    expected_log_filters = dict()
+
+    filterName = "rabbitmq"
+    filename = 'Products/ZenModel/migrate/data/%s-6.0.0.conf' % filterName
+    with open(zenPath(filename)) as filterFile:
+        filterDef = filterFile.read()
+        expected_log_filters[filterName] = filterDef
