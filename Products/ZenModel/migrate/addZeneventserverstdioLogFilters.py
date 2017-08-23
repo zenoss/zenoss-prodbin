99795397e5045af46945503fa7b0a78a5d4d0824
Erik Dieckert <edieckert@zenoss.com>
2017-08-23 09:38:08 -0500

    Created migrations and tests of migrations for new zeneventserver-stdio log filter and the service def change

---
+++ Products/ZenModel/migrate/addZeneventserverstdioLogFilters.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2017, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import logging
+import Migrate
+import os
+import servicemigration as sm
+from Products.ZenUtils.Utils import zenPath
+from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION
+
+log = logging.getLogger("zen.migrate")
+sm.require("1.1.10")
+
+class AddZeneventserverstdioLogFilters(Migrate.Step):
+    """
+    Add LogFilters for zeneventserver-stdio logs issue addressed by ZEN-28081
+    """
+    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
+
+    def cutover(self, dmd):
+        try:
+            ctx = sm.ServiceContext()
+        except sm.ServiceMigrationError:
+            log.info("Couldn't generate service context, skipping.")
+            return
+
+        service = ctx.getTopService()
+        log.info("Found top level service: '{0}'".format(service.name))
+        if service.name.find("Zenoss") != 0 and service.name.find("UCS-PM") != 0:
+            log.info("Top level service name isn't Zenoss or UCS-PM; skipping.")
+            return
+
+        filterName = "zeneventserver-stdio"
+        filename = 'Products/ZenModel/migrate/data/%s-6.0.0.conf' % filterName
+        with open(zenPath(filename)) as filterFile:
+            try:
+                filterDef = filterFile.read()
+            except Exception, e:
+                log.error("Error reading {0} logfilter file: {1}".format(filename, e))
+                return
+            log.info("Updating log filter named {0}".format(filterName))
+            ctx.addLogFilter(filterName, filterDef)
+
+        # Note that the logstash.conf will not be properly updated until a later
+        # step in the overall upgrade process sets the Version of the toplevel
+        # service to 6.0.0
+        ctx.commit()
+
+AddZeneventserverstdioLogFilters()
