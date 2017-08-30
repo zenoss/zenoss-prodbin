daa955b31095408a7823a0aa21835466065eb1cd
John Hamilton <johnhamilton@zenoss.com>
2017-08-30 14:52:21 -0500

    Get rid of solr endpoint added in 5.3.0 migrations

--- Products/ZenModel/migrate/tests/test_addSolrService.py
+++ Products/ZenModel/migrate/tests/test_addSolrService.py
@@ -11,7 +11,7 @@ class Test_addSolrService(unittest.TestCase, common.ServiceMigrationTestCase):
     """
     Test that missedRuns threshold is added to collector services.
     """
-    initial_servicedef = 'zenoss-resmgr-5.1.5.json'
+    initial_servicedef = 'zenoss-resmgr-5.1.5_badSolrEP.json'
     expected_servicedef = 'zenoss-resmgr-5.1.5-addSolrService.json'
     migration_module_name = 'addSolrService'
     migration_class_name = 'AddSolrService'
