6649f3843545372264c2d4e82f662c69c234026b
lundybernard <lundy.bernard@gmail.com>
2017-09-06 12:23:06 -0500

    add Zenoss.render.date to renderers

--- Products/ZenUI3/browser/resources/js/zenoss/tests/renderers_spec.js
+++ Products/ZenUI3/browser/resources/js/zenoss/tests/renderers_spec.js
@@ -1,33 +1,32 @@
-
-describe("Zenoss.date Test Suite", function(){
+describe("Zenoss.render.date Test Suite", function(){
     beforeEach(function(){
         Zenoss.USER_DATE_FORMAT = "YYYY-MM-DD";
         Zenoss.USER_TIME_FORMAT = "hh:mm:ss a";
         Zenoss.USER_TIMEZONE = "Singapore";
     });
 
-    it('formatWithTimezone applies user format and timezone', function(){
+    it('render.date applies user format and timezone', function(){
         expect(
-            Zenoss.date.renderWithTimeZone(1)
+            Zenoss.render.date(1)
         ).toBe("1970-01-01 07:30:01 am SGT");
     });
 
-    it('formatWithTimezone handles epoc 0', function(){
+    it('render.date handles epoc 0', function(){
         expect(
-            Zenoss.date.renderWithTimeZone(0)
+            Zenoss.render.date(0)
         ).toBe("1970-01-01 07:30:00 am SGT");
     });
 
-    it('formatWithTimezone applies given format', function(){
+    it('render.date applies given format', function(){
         expect(
-            Zenoss.date.renderWithTimeZone(1, 'YYYY.MM.DD.hh.mm.ss')
+            Zenoss.render.date(1, 'YYYY.MM.DD.hh.mm.ss')
         ).toBe("1970.01.01.07.30.01");
     });
 
-    it('formats Date objects properly', function(){
+    it('render.date handles Date objects properly', function(){
         var extdate = new Date('1/10/2017 03:05:07 PM GMT-0600')
         expect(
-            Zenoss.date.renderWithTimeZone(extdate)
+            Zenoss.render.date(extdate)
         ).toBe('2017-01-11 05:05:07 am SGT')
     });
 });
