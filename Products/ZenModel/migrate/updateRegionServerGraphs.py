6e499632ef981bdb2ec1a95e5e319a0c36f20fe6
Eric Cifreo <ecifreo@zenoss.com>
2017-09-12 14:26:34 -0500

    Change RegionServer Operation Count graphs to display rates.

---
+++ Products/ZenModel/migrate/updateRegionServerGraphs.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2017, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import json
+import logging
+import os
+log = logging.getLogger("zen.migrate")
+
+import Migrate
+import servicemigration as sm
+sm.require("1.1.10")
+from servicemigration import HealthCheck
+from Products.ZenUtils.Utils import zenPath
+from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION
+
+class UpdateRegionServerGraphs(Migrate.Step):
+    """
+    Add Solr service and associated healthchecks.
+    """
+    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
+
+    legends = {'totalRequestCount': 'Total',
+               'readRequestCount': 'Read',
+               'writeRequestCount': 'Write'}
+
+    def cutover(self, dmd):
+        try:
+            ctx = sm.ServiceContext()
+        except sm.ServiceMigrationError:
+            log.info("Couldn't generate service context, skipping.")
+            return
+
+        changed = False
+
+        regionServers = filter(lambda s: s.name == "RegionServer", ctx.services)
+        for regionserver in regionServers:
+            graphs = filter(lambda x: x.graphID == 'opcounts',
+                                  regionserver.monitoringProfile.graphConfigs)
+            if graphs:
+                changed = True
+                for cfg in graphs:
+                    cfg.description = "Operations"
+                    cfg.name = "Operations"
+                    for datapoint in cfg.datapoints:
+                        log.info("Updating graph: %s" % datapoint.metric)
+                        datapoint.rate = True
+                        datapoint.legend = "%s request rate" % self.legends[datapoint.metric]
+                        datapoint.name = "%s request rate" % self.legends[datapoint.metric]
+
+        if changed:
+            ctx.commit()
+
+
+UpdateRegionServerGraphs()
