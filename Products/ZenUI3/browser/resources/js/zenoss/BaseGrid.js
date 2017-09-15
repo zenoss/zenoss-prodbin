97c5a2f002de93896dd50d54ca1f0aed4b1eba89
Dmytro Budko <hoster@mail.rv.ua>
2017-09-15 13:38:32 +0000

    Changed function for convert Data/Time to Unix time.

--- Products/ZenUI3/browser/resources/js/zenoss/BaseGrid.js
+++ Products/ZenUI3/browser/resources/js/zenoss/BaseGrid.js
@@ -459,11 +459,11 @@
                     if (timeParam.indexOf(" TO ") != -1) {
                         var dateValues = timeParam.split(" TO ");
                         values[paramKey] = dateValues.map(function(rangeValue){
-                            return moment.unix(moment.tz(rangeValue, Zenoss.USER_DATE_FORMAT+' '+Zenoss.USER_TIME_FORMAT, Zenoss.USER_TIMEZONE)).format("X")
+                            return moment.tz(rangeValue, Zenoss.USER_DATE_FORMAT+' '+Zenoss.USER_TIME_FORMAT, Zenoss.USER_TIMEZONE).format("x")
                         }).join(" TO ");
                     }
                     else {
-                        values[paramKey] = moment.unix(moment.tz(timeParam, Zenoss.USER_DATE_FORMAT+' '+Zenoss.USER_TIME_FORMAT, Zenoss.USER_TIMEZONE)).format("X")
+			values[paramKey] = moment.tz(timeParam, Zenoss.USER_DATE_FORMAT+' '+Zenoss.USER_TIME_FORMAT, Zenoss.USER_TIMEZONE).format("x")
                     }
                 }
             });
