5c652610f8b90606bb35317786013120f01e8b7f
rderkach <rderk@softserveinc.com>
2017-09-21 10:02:48 +0000

    Refactor: PEP8

--- Products/ZenModel/migrate/updateOpenTSDBOriginalConfig.py
+++ Products/ZenModel/migrate/updateOpenTSDBOriginalConfig.py
@@ -31,12 +31,24 @@ class updateOpenTSDBOriginalConfig(Migrate.Step):
             log.info("Couldn't generate service context, skipping.")
             return
 
-        tsdbs = filter(lambda s: "opentsdb" in ctx.getServicePath(s), ctx.services)
-        log.info("Found {} services with 'opentsdb' in their service path.".format(len(tsdbs)))
+        tsdbs = filter(
+            lambda s: "opentsdb" in
+                      ctx.getServicePath(s), ctx.services
+        )
+        log.info(
+            "Found {} services with 'opentsdb' in their service path."
+            .format(len(tsdbs))
+        )
 
-        tsdbs = filter(lambda s: "/opt/zenoss/etc/opentsdb/opentsdb.conf" in [i.name for i in s.originalConfigs], tsdbs)
-        log.info("Of those, {} services use /opt/zenoss/etc/opentsdb/opentsdb.conf.".format(len(tsdbs)))
-        
+        tsdbs = filter(
+                       lambda s: "/opt/zenoss/etc/opentsdb/opentsdb.conf"
+                                 in [i.name for i in s.originalConfigs], tsdbs
+        )
+        log.info(
+            "Of those, {} services use /opt/zenoss/etc/opentsdb/opentsdb.conf."
+            .format(len(tsdbs))
+        )
+
         commit = False
 
         filename = 'Products/ZenModel/migrate/data/opentsdb.conf'
@@ -48,8 +60,14 @@ class updateOpenTSDBOriginalConfig(Migrate.Step):
                 return
 
         for tsdb in tsdbs:
-            original_configs = filter(lambda f: f.name == "/opt/zenoss/etc/opentsdb/opentsdb.conf", tsdb.originalConfigs)
-            log.info("Found {} original config files named '/opt/zenoss/etc/opentsdb/opentsdb.conf'.".format(len(original_configs)))
+            original_configs = filter(
+                lambda f: f.name == "/opt/zenoss/etc/opentsdb/opentsdb.conf",
+                          tsdb.originalConfigs
+            )
+            log.info(
+                "Found {} original config files named '/opt/zenoss/etc/opentsdb/opentsdb.conf'."
+                .format(len(original_configs))
+            )
             for config in original_configs:
                 if config.content != configCnt:
                     config.content = configCnt
