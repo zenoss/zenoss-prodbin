c17e25aef532e9eb3d749bd6db5a130eb22dcc79
Maya <maya@zenoss.com>
2017-09-27 10:38:19 -0500

    Disable logging of passwords

--- Products/ZenHub/zenhub.py
+++ Products/ZenHub/zenhub.py
@@ -881,7 +881,10 @@ class ZenHub(ZCmdBase):
             job.deferred.errback(result)
         else:
             try:
-                self.log.debug("worker %s result -> %s", wId, result)
+                # log messages that do not contain passwords
+                result_str = "worker %s result -> %s" % (wId, result)
+                if ' -p ' not in result_str:
+                    self.log.debug(result_str)
                 result = pickle.loads(''.join(result))
             except Exception as e:
                 error = e
