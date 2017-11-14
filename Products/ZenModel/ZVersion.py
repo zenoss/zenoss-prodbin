8ab2a7a8023670cb7d406e8cbae22b1b92f60165
Ron Phillips <rphillips@zenoss.com>
2017-11-14 17:34:24 -0600

    Dump ZODB for 6.0.1 release

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -8,5 +8,5 @@
 ##############################################################################
 
 # this is now generated during the build step; see make generate-zversion
-VERSION="6.0.0"
+VERSION="6.0.1"
 BUILD_NUMBER="DEV"
