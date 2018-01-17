ae8144fa4be26f65237fc6e4b49f443c8e2034dc
lundybernard <lundy.bernard@gmail.com>
2018-01-17 13:07:03 -0600

    Add hbase connection healthchecks to tsdb services
    Fixes ZEN-29404

---
+++ Products/ZenModel/migrate/tests/test_addOpentsdbHbaseConnectionHealthCheck.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2018, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import unittest
+import common
+
+
+class test_addOpentsdbHbaseConnectionHealthCheck(
+    unittest.TestCase, common.ServiceMigrationTestCase
+):
+    """Test Opentsdb reader and writer healthchecks for Hbase connectivity
+    were added successfully
+    """
+
+    initial_servicedef = 'zenoss-resmgr-5.3.0.json'
+    expected_servicedef = 'zenoss-resmgr-5.3.0-addOpentsdbHbaseConnectionHealthCheck.json'
+    migration_module_name = 'addOpentsdbHbaseConnectionHealthCheck'
+    migration_class_name = 'AddOpentsdbHbaseConnectionHealthCheck'
