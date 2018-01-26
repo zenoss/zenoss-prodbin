f857d65413245a0effcdcc9867634573e0e40187
rderkach <rderk@softserveinc.com>
2018-01-26 15:48:38 +0000

    ZEN-29497: add an instruction for manual removal of CatalogService

--- bin/remove_CatalogServiceZenPack.sh
+++ bin/remove_CatalogServiceZenPack.sh
@@ -12,11 +12,18 @@ echo "Found Catalog Service egg at $CATALOG_SERVICE_EGG"
 #show log only if something goes wrong
 tmp_output="/tmp/"`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
 /opt/zenoss/bin/zenpack --remove ZenPacks.zenoss.CatalogService  > $tmp_output 2>&1
-if [[ ! $? -eq 0 ]]; then
-    echo "Couldn't remove CatalogService zenpack"
+status=$?
+if [[ ! $status -eq 0 ]]; then
     cat $tmp_output
     rm $tmp_output
-    exit
+    echo "Couldn't remove CatalogService zenpack"
+    echo "You should remove CatalogService zenpack manually before starting the upgrade"
+    echo "To remove the CatalogService ZenPack:"
+    echo -e "1. Ensure the following services are running:\n\t * Zenoss.resmgr/Infrastructure/mariadb-model\n\t * Zenoss.resmgr/Infrastructure/mariadb-events\n\t * Zenoss.resmgr/Infrastructure/RabbitMQ\n\t * Zenoss.resmgr/Zenoss/Events/zeneventserver\n\t * Zenoss.resmgr/Infrastructure/redis\n\t * Zenoss.resmgr/Infrastructure/memcached"
+    echo -e "2. Ensure the following service has stopped:\n\t *Zenoss.resmgr/Infrastructure/zencatalogservice"
+    echo -e "3. Log onto the host of the Control Center master and run the following command:\n\t serviced service run zope zenpack-manager uninstall ZenPacks.zenoss.CatalogService"
+    exit $status
 fi
 rm $tmp_output
 echo "Successfully removed Catalog Service"
+
