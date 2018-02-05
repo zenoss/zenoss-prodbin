93649c47dfeb41477824c7bda84c468c0c3c6b2b
Jason Peacock <jpeacock@zenoss.com>
2018-02-05 14:37:06 -0600

    pep8 fixes

--- Products/Zuul/catalog/events.py
+++ Products/Zuul/catalog/events.py
@@ -1,33 +1,37 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2009, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
 from zope.event import notify
-from zope.interface import implements
+from zope.interface import implementer
 from zope.component import adapter, getUtility
 from zope.container.interfaces import IObjectAddedEvent, IObjectMovedEvent
 from zope.container.interfaces import IObjectRemovedEvent
 from OFS.interfaces import IObjectWillBeMovedEvent, IObjectWillBeAddedEvent
-from interfaces import IIndexingEvent, IAfterIndexingEventSubscriber, \
-                       IGloballyIndexed, ITreeSpanningComponent, IDeviceOrganizer
-from paths import devicePathsFromComponent
+from interfaces import (
+    IIndexingEvent, IAfterIndexingEventSubscriber, IGloballyIndexed,
+    ITreeSpanningComponent, IDeviceOrganizer
+)
 
 from Products.Zuul.catalog.interfaces import IModelCatalog
 from Products.Zuul.catalog.exceptions import BadIndexingEvent
 
 
+@implementer(IIndexingEvent)
 class IndexingEvent(object):
-    implements(IIndexingEvent)
-    def __init__(self, object, idxs=None, update_metadata=True, triggered_by_zope_event=False):
+    def __init__(
+            self, object,
+            idxs=None, update_metadata=True, triggered_by_zope_event=False):
         """
-        @param triggered_by_zope_event : flag to indicate whether the IndexingEvent was triggered
-        by zope events (IObjectWillBeMovedEvent, IObjectAddedEvent etc) or not.
+        @param triggered_by_zope_event : flag to indicate whether the
+            IndexingEvent was triggered by zope events
+            (IObjectWillBeMovedEvent, IObjectAddedEvent etc) or not.
         """
         self.object = object
         self.idxs = idxs
@@ -38,7 +42,7 @@ class IndexingEvent(object):
 
 def _get_object_to_index(ob):
     """
-    Returns the object to be indexed/unindexed or None 
+    Returns the object to be indexed/unindexed or None
     if the object does not have to be indexed
     """
     try:
@@ -47,38 +51,40 @@ def _get_object_to_index(ob):
         evob = ob
     path = evob.getPrimaryPath()
     # Ignore things dmd or above
-    if len(path)<=3 or path[2]!='dmd':
-        return None
-    else:
-        return evob
+    return evob if (len(path) > 3 and path[2] == "dmd") else None
 
 
 @adapter(IGloballyIndexed, IIndexingEvent)
 def onIndexingEvent(ob, event):
-    model_catalog = getUtility(IModelCatalog)
+
     object_to_index = _get_object_to_index(ob)
-        
+    if not object_to_index:
+        return
+
     idxs = event.idxs
     if isinstance(idxs, basestring):
         idxs = [idxs]
 
-    if object_to_index:
-        if idxs:
-            s_idxs = set(idxs)
-            indexed, stored, _ = model_catalog.get_indexes(object_to_index)
-            obj_indexes = indexed.union(stored)
-            # Every time we index "path" we also need to index "deviceOrganizers"
-            if "path" in s_idxs and "deviceOrganizers" in obj_indexes:
-               s_idxs.add("deviceOrganizers")
-               idxs = s_idxs
-            # check idxs are valid indexes
-            bad_idxs = s_idxs - obj_indexes
-            if bad_idxs:
-                raise BadIndexingEvent("Indexing event contains unknown indexes: {}".format(bad_idxs))
+    model_catalog = getUtility(IModelCatalog)
 
-        model_catalog.catalog_object(object_to_index, idxs)
-        if IAfterIndexingEventSubscriber.providedBy(object_to_index):
-            object_to_index.after_indexing_event(event)
+    if idxs:
+        s_idxs = set(idxs)
+        indexed, stored, _ = model_catalog.get_indexes(object_to_index)
+        obj_indexes = indexed.union(stored)
+        # Every time "path" is indexed, also index "deviceOrganizers"
+        if "path" in s_idxs and "deviceOrganizers" in obj_indexes:
+            s_idxs.add("deviceOrganizers")
+            idxs = s_idxs
+        # check idxs are valid indexes
+        bad_idxs = s_idxs - obj_indexes
+        if bad_idxs:
+            raise BadIndexingEvent(
+                "Indexing event contains unknown indexes: %s" % (bad_idxs,)
+            )
+
+    model_catalog.catalog_object(object_to_index, idxs)
+    if IAfterIndexingEventSubscriber.providedBy(object_to_index):
+        object_to_index.after_indexing_event(event)
 
 
 @adapter(IGloballyIndexed, IObjectWillBeMovedEvent)
@@ -115,19 +121,21 @@ def onObjectMoved(ob, event):
 def onOrganizerBeforeDelete(ob, event):
     """
     Before we delete the organizer we need to remove its references
-    to the devices. 
+    to the devices.
     """
     if not IObjectWillBeAddedEvent.providedBy(event):
         for device in ob.devices.objectValuesGen():
-            notify(IndexingEvent(device, idxs='path', triggered_by_zope_event=True))
+            notify(IndexingEvent(
+                device, idxs='path', triggered_by_zope_event=True
+            ))
 
 
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 #    Methods to deal with tree spanning components.
 #    When a tree spanning component is updated, we need
 #    to make sure that the object the component is linked
 #    to in the other tree is updated if needed
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 
 
 class ObjectsAffectedBySpanningComponent(object):
@@ -141,7 +149,7 @@ class ObjectsAffectedBySpanningComponent(object):
         if hasattr(component, "get_indexable_peers"):
             peers = component.get_indexable_peers()
             if not hasattr(peers, '__iter__'):
-                peers = [ peers ]
+                peers = [peers]
         return peers
 
     def index_affected_objects(self):
@@ -153,7 +161,6 @@ class ObjectsAffectedBySpanningComponent(object):
 def onTreeSpanningComponentBeforeDelete(ob, event):
     """ Before tree spanning component is deleted """
     if not IObjectWillBeAddedEvent.providedBy(event):
-        ppath = "/".join(ob.getPrimaryPath())
         affected_objects = ObjectsAffectedBySpanningComponent(ob)
         affected_objects.index_affected_objects()
 
