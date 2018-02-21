a7217f280d8d4e8913d20dd9821f815a7af9e683
Dmytro Budko <hoster@mail.rv.ua>
2018-02-21 23:15:23 +0200

    ZEN-29612 ZenPacks.zenossSE.SEDemoItems' distribution was not found during upgrade to 6.1.1 from 5.3.2

--- legacy/sitecustomize.py
+++ legacy/sitecustomize.py
@@ -1,7 +1,7 @@
 import sys, os, site
 sys.setdefaultencoding('utf-8')
 site.addsitedir(os.path.join(os.getenv('ZENHOME'), 'ZenPacks'))
-if not os.getenv("RESTORE_ZENPACKS", None):
-	site.addsitedir('/var/zenoss/ZenPacks')
+site.addsitedir('/var/zenoss/ZenPacks')
+site.addsitedir('/var/zenoss/ZenPackSource')
 import warnings
 warnings.filterwarnings('ignore', '.*', DeprecationWarning)
