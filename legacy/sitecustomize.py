acf1e4b459959a846613ac7a4411e689683b2280
Dmytro Budko <hoster@mail.rv.ua>
2018-02-21 23:19:53 +0200

    fixed sitedir path

--- legacy/sitecustomize.py
+++ legacy/sitecustomize.py
@@ -2,6 +2,5 @@ import sys, os, site
 sys.setdefaultencoding('utf-8')
 site.addsitedir(os.path.join(os.getenv('ZENHOME'), 'ZenPacks'))
 site.addsitedir('/var/zenoss/ZenPacks')
-site.addsitedir('/var/zenoss/ZenPackSource')
 import warnings
 warnings.filterwarnings('ignore', '.*', DeprecationWarning)
