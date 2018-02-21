c8632abde06906716cd73bc51ab84ec4c541cd2a
Jason Peacock <jpeacock@zenoss.com>
2018-02-21 16:35:43 -0600

    Bump support/6.1.x to 6.1.2
    Bump support/6.1.x schema to 200.1.2

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -8,5 +8,5 @@
 ##############################################################################
 
 # this is now generated during the build step; see make generate-zversion
-VERSION="6.1.1"
+VERSION="6.1.2"
 BUILD_NUMBER="DEV"
