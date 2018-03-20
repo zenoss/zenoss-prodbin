bec299a0bb71fc98d773e42a5403543ac6b453e8
Dmytro Budko <hoster@mail.rv.ua>
2018-03-20 16:44:18 +0200

    ZEN-29662 DynamicView: ERROR zen.Relations: Remote remove failed

---
+++ Products/ZenModel/migrate/removeCatalogServiceBrokenRelation.py
@@ -0,0 +1,31 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2018, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import logging
+
+import Migrate
+from Products.ZenRelations.ToOneRelationship import ToOneRelationship
+
+log = logging.getLogger('zen.migrate')
+
+
+class RemoveCatalogServiceBrokenRelation(Migrate.Step):
+    version = Migrate.Version(200, 0, 0)
+
+    def cutover(self, dmd):
+        for nodes in dmd.Processes.Zenoss.getChildNodes():
+            if isinstance(nodes, ToOneRelationship):
+                if nodes.pack.obj.id == "ZenPacks.zenoss.CatalogService":
+                    try:
+                        nodes.pack().packables.removeRelation()
+                    except Exception as ex:
+                        log.info("Cant remove relation for CatalogService, %s", ex)
+
+
+RemoveCatalogServiceBrokenRelation()
