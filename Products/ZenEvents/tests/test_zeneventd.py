7ac44f79b5965bff0df5135c08f9908cfbe7c693
lundybernard <lundy.bernard@gmail.com>
2018-04-16 15:57:33 -0500

    Add optional Timeout for messages which block zep
    fixes: ZEN-29823
    
    * Add unit tests for zeneventd.py
    * Add Timeout context manager using the signal library
    * Wrap zep pipeline with Timeout context
    * Make process_event_timeout a configurable option

---
+++ Products/ZenEvents/tests/test_zeneventd.py
@@ -0,0 +1,169 @@
+from unittest import TestCase
+from mock import patch, call, Mock, MagicMock
+
+from Products.ZenEvents.zeneventd import (
+    Timeout,
+    TimeoutError,
+    EventPipelineProcessor,
+    Event,
+    ZepRawEvent,
+    CheckInputPipe,
+    EventContext,
+    time
+)
+from Products.ZenEvents.events2.processing import EventProcessorPipe
+from zenoss.protocols.protobufs.zep_pb2 import EventActor
+
+
+PATH = {'zeneventd': 'Products.ZenEvents.zeneventd'}
+
+
+class EventPipelineProcessorTest(TestCase):
+
+    def setUp(self):
+        self.dmd = Mock()
+        self.manager_patcher = patch(
+            '{zeneventd}.Manager'.format(**PATH), autospec=True
+        )
+        # silence 'new thread' error
+        self.metric_reporter_patcher = patch(
+            '{zeneventd}.MetricReporter'.format(**PATH), autospec=True
+        )
+        self.manager_patcher.start()
+        self.metric_reporter_patcher.start()
+
+        self.epp = EventPipelineProcessor(self.dmd)
+
+        self.message = Event(
+            uuid="75e7c760-39d4-11e8-a97b-0242ac11001a",
+            created_time=1523044529575,
+            event_class="/ZenossRM",
+            actor=EventActor(
+                element_type_id=10,
+                element_uuid="575bcf2d-8ca0-47d9-8e63-b4f2c1242ef3",
+                element_identifier="device.loc",
+                element_sub_type_id=100,
+                element_sub_identifier="zeneventd",
+            ),
+            summary='Event Summary',
+            severity=1,
+            event_key="RMMonitor.collect.docker",
+            agent="zenpython",
+            monitor="localhost",
+            first_seen_time=1523044529575,
+        )
+
+    def tearDown(self):
+        self.manager_patcher.stop()
+        self.metric_reporter_patcher.stop()
+
+    def test_processMessage(self):
+        self.epp._pipes = (CheckInputPipe(self.epp._manager), )
+
+        zep_raw_event = self.epp.processMessage(self.message)
+
+        self.assertIsInstance(zep_raw_event, ZepRawEvent)
+        self.assertIsInstance(zep_raw_event.event, Event)
+        self.assertEqual(zep_raw_event.event.message, self.message.summary)
+
+    def test_exception_in_pipe(self):
+        error_pipe = self.ErrorPipe(self.epp._manager)
+        self.epp._pipes = (error_pipe, )
+        self.epp._pipe_timers[error_pipe.name] = MagicMock()
+
+        zep_raw_event = self.epp.processMessage(self.message)
+
+        self.assertIsInstance(zep_raw_event, ZepRawEvent)
+        self.assertIsInstance(zep_raw_event.event, Event)
+
+        exception_event = self.epp.create_exception_event(
+            self.message, self.ErrorPipe.ERR
+        )
+
+        self.assertEqual(
+            zep_raw_event.event.message,
+            exception_event.event.message
+        )
+
+    class ErrorPipe(EventProcessorPipe):
+        ERR = Exception('pipeline failure')
+
+        def __call__(self, eventContext):
+            raise self.ERR
+
+    def test_synchronize_with_database(self):
+        self.epp._synchronize_with_database()
+        self.dmd._p_jar.sync.assert_called_once_with()
+
+    def test_synchronize_with_database_delay(self):
+        self.epp.nextSync = time() + 0.5
+        self.epp._synchronize_with_database()
+        self.dmd._p_jar.sync.assert_not_called()
+
+    def test_synchronize_with_database_every_event(self):
+        self.epp.SYNC_EVERY_EVENT = True
+        self.epp.nextSync = time() + 0.5
+        self.epp._synchronize_with_database()
+        self.dmd._p_jar.sync.assert_called_once_with()
+
+    def test_create_exception_event(self):
+        error = Exception('test exception')
+        event_context = self.epp.create_exception_event(self.message, error)
+        self.assertIsInstance(event_context, EventContext)
+
+        exception_event = event_context.event
+
+        self.assertIsInstance(exception_event, Event)
+        self.assertEqual(
+            exception_event.summary,
+            "Internal exception processing event: Exception('test exception',)"
+        )
+        self.assertTrue(
+            str(error) in exception_event.message
+        )
+
+
+class TimeoutTest(TestCase):
+
+    def setUp(self):
+        # Patch external dependencies
+        self.signal_patcher = patch(
+            '{zeneventd}.signal'.format(**PATH), autospec=True
+        )
+        self.signal = self.signal_patcher.start()
+
+    def tearDown(self):
+        self.signal_patcher.stop()
+
+    def test_context_manager(self):
+        timeout_duration = 10
+
+        with Timeout('event', timeout_duration) as ctx:
+            self.signal.signal.assert_called_with(
+                self.signal.SIGALRM, ctx.handle_timeout
+            )
+
+        self.signal.alarm.assert_has_calls(
+            [call(timeout_duration), call(0)]
+        )
+
+    def test_handle_timeout_raises_exception(self):
+        with self.assertRaises(TimeoutError):
+            with Timeout(1) as ctx:
+                ctx.handle_timeout(1, 'frame')
+
+
+class BaseQueueConsumerTaskTest(TestCase):
+    pass
+
+
+class TwistedQueueConsumerTaskTest(TestCase):
+    pass
+
+
+class EventDTwistedWorkerTest(TestCase):
+    pass
+
+
+class ZenEventDTest(TestCase):
+    pass
--- Products/ZenEvents/zeneventd.py
+++ Products/ZenEvents/zeneventd.py
@@ -1,14 +1,15 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2018, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2010, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
 #
 ##############################################################################
+
 import logging
-import time
-from datetime import datetime, timedelta
+import signal
+from time import time
 
 from twisted.internet import defer, reactor
 from zope.component import getUtility, provideUtility
@@ -47,6 +48,7 @@ from Products.ZenUtils.MetricReporter import MetricReporter
 from Products.ZenUtils.Utils import zenPath
 from Products.ZenUtils.ZCmdBase import ZCmdBase
 
+
 def monkey_patch_rotatingfilehandler():
     try:
         from cloghandler import ConcurrentRotatingFileHandler
@@ -71,6 +73,7 @@ QUEUE_RAW_ZEN_EVENTS = '$RawZenEvents'
 class EventPipelineProcessor(object):
 
     SYNC_EVERY_EVENT = False
+    PROCESS_EVENT_TIMEOUT = 0
 
     def __init__(self, dmd):
         self.dmd = dmd
@@ -111,105 +114,109 @@ class EventPipelineProcessor(object):
         if not self.SYNC_EVERY_EVENT:
             # don't call sync() more often than 1 every 0.5 sec
             # helps throughput when receiving events in bursts
-            self.nextSync = datetime.now()
-            self.syncInterval = timedelta(0, 0, 500000)
+            self.nextSync = time()
+            self.syncInterval = 0.5
 
-    def processMessage(self, message):
+    def processMessage(self, message, retry=True):
         """
         Handles a queue message, can call "acknowledge" on the Queue Consumer
         class when it is done with the message
         """
-
-        if self.SYNC_EVERY_EVENT:
-            doSync = True
-        else:
-            # sync() db if it has been longer than
-            # self.syncInterval since the last time
-            currentTime = datetime.now()
-            doSync = currentTime > self.nextSync
-            self.nextSync = currentTime + self.syncInterval
-
-        if doSync:
-            self.dmd._p_jar.sync()
+        self._synchronize_with_database()
 
         try:
-            retry = True
-            processed = False
-            while not processed:
-                try:
-                    # extract event from message body
-                    zepevent = ZepRawEvent()
-                    zepevent.event.CopyFrom(message)
-                    log.debug("Received event: %s", to_dict(zepevent.event))
+            # extract event from message body
+            zepevent = ZepRawEvent()
+            zepevent.event.CopyFrom(message)
+            log.debug("Received event: %s", to_dict(zepevent.event))
+            eventContext = EventContext(log, zepevent)
 
-                    eventContext = EventContext(log, zepevent)
+            with Timeout(
+                zepevent, self.PROCESS_EVENT_TIMEOUT,
+                error_message='while processing event'
+            ):
+                for pipe in self._pipes:
+                    with self._pipe_timers[pipe.name]:
+                        eventContext = pipe(eventContext)
+                    log.debug(
+                        'After pipe %s, event context is %s',
+                        pipe.name, to_dict(eventContext.zepRawEvent)
+                    )
+                    if eventContext.event.status == STATUS_DROPPED:
+                        raise DropEvent(
+                            'Dropped by %s' % pipe, eventContext.event
+                        )
 
-                    for pipe in self._pipes:
-                        if log.isEnabledFor(logging.DEBUG):
-                            with self._pipe_timers[pipe.name]:
-                                eventContext = pipe(eventContext)
-                        else:
-                            eventContext = pipe(eventContext)
-
-                        log.debug('After pipe %s, event context is %s',
-                                  pipe.name, to_dict(eventContext.zepRawEvent))
-                        if eventContext.event.status == STATUS_DROPPED:
-                            raise DropEvent(
-                                'Dropped by %s' % pipe,
-                                eventContext.event
-                            )
-
-                    processed = True
-
-                except AttributeError:
-                    # _manager throws Attribute errors if connection to
-                    # zope is lost - reset and retry ONE time
-                    if retry:
-                        retry = False
-                        log.debug("Resetting connection to catalogs")
-                        self._manager.reset()
-                    else:
-                        raise
+        except AttributeError:
+            # _manager throws Attribute errors
+            # if connection to zope is lost - reset and retry ONE time
+            if retry:
+                log.debug("Resetting connection to catalogs")
+                self._manager.reset()
+                self.processMessage(message, retry=False)
+            else:
+                raise
 
         except DropEvent:
             # we want these to propagate out
             raise
-        except Exception as e:
-            log.info("Failed to process event, forward original raw event: %s",
-                     to_dict(zepevent.event))
-            # Pipes and plugins may raise ProcessingException's for their own
-            # reasons - only log unexpected
-            # exceptions of other type (will insert stack trace in log)
-            if not isinstance(e, ProcessingException):
-                log.exception(e)
 
-            # construct wrapper event to report this event processing failure
-            # (including content of the original event)
-            origzepevent = ZepRawEvent()
-            origzepevent.event.CopyFrom(message)
-            failReportEvent = {
-                'uuid': guid.generate(),
-                'created_time': int(time.time() * 1000),
-                'fingerprint': '|'.join(
-                    ['zeneventd', 'processMessage', repr(e)]
-                ),
-                # Don't send the *same* event class or we crash endlessly
-                'eventClass': '/',
-                'summary': 'Internal exception processing event: %r' % e,
-                'message': 'Internal exception processing event: %r/%s' % (
-                    e, to_dict(origzepevent.event)
-                ),
-                'severity': 4,
-            }
-            zepevent = ZepRawEvent()
-            zepevent.event.CopyFrom(from_dict(Event, failReportEvent))
-            eventContext = EventContext(log, zepevent)
-            eventContext.eventProxy.device = 'zeneventd'
-            eventContext.eventProxy.component = 'processMessage'
+        except Exception as error:
+            log.info(
+                "Failed to process event, forward original raw event: %s",
+                to_dict(zepevent.event)
+            )
+            # Pipes and plugins may raise ProcessingException's for their own
+            # reasons. only log unexpected exceptions of other type
+            # will insert stack trace in log
+            if not isinstance(error, ProcessingException):
+                log.exception(error)
+
+            eventContext = self.create_exception_event(message, error)
 
         log.debug("Publishing event: %s", to_dict(eventContext.zepRawEvent))
-
         return eventContext.zepRawEvent
+
+    def _synchronize_with_database(self):
+        '''sync() db if it has been longer than
+        self.syncInterval seconds since the last time,
+        and no _synchronize has not been called for self.syncInterval seconds
+        KNOWN ISSUE: ZEN-29884
+        '''
+        if self.SYNC_EVERY_EVENT:
+            doSync = True
+        else:
+            current_time = time()
+            doSync = current_time > self.nextSync
+            self.nextSync = current_time + self.syncInterval
+
+        if doSync:
+            self.dmd._p_jar.sync()
+
+    def create_exception_event(self, message, exception):
+        # construct wrapper event to report this event processing failure
+        # including content of the original event
+        orig_zep_event = ZepRawEvent()
+        orig_zep_event.event.CopyFrom(message)
+        failure_event = {
+            'uuid': guid.generate(),
+            'created_time': int(time() * 1000),
+            'fingerprint':
+                '|'.join(['zeneventd', 'processMessage', repr(exception)]),
+            # Don't send the *same* event class or we loop endlessly
+            'eventClass': '/',
+            'summary': 'Internal exception processing event: %r' % exception,
+            'message':
+                'Internal exception processing event: %r/%s' %
+                (exception, to_dict(orig_zep_event.event)),
+            'severity': 4,
+        }
+        zep_raw_event = ZepRawEvent()
+        zep_raw_event.event.CopyFrom(from_dict(Event, failure_event))
+        event_context = EventContext(log, zep_raw_event)
+        event_context.eventProxy.device = 'zeneventd'
+        event_context.eventProxy.component = 'processMessage'
+        return event_context
 
 
 class BaseQueueConsumerTask(object):
@@ -303,6 +310,7 @@ class ZenEventD(ZCmdBase):
     def __init__(self, *args, **kwargs):
         super(ZenEventD, self).__init__(*args, **kwargs)
         EventPipelineProcessor.SYNC_EVERY_EVENT = self.options.syncEveryEvent
+        EventPipelineProcessor.PROCESS_EVENT_TIMEOUT = self.options.process_event_timeout
         self._heartbeatSender = QueueHeartbeatSender(
             'localhost', 'zeneventd', self.options.heartbeatTimeout
         )
@@ -339,6 +347,14 @@ class ZenEventD(ZCmdBase):
                   ' to sync() no more often than once every 1/2 second.')
         )
         self.parser.add_option(
+            '--process_event_timeout', dest='process_event_timeout',
+            type='int', default='0',
+            help=('Set the Timeout(in seconds) for processing each event.'
+                  ' The timeout may be extended for a transforms using,'
+                  'signal.alarm(<timeout seconds>) in the transform'
+                  'set to 0 to disable')
+        ),
+        self.parser.add_option(
             '--messagesperworker', dest='messagesPerWorker', default=1,
             type="int",
             help=('Sets the number of messages each worker gets from the queue'
@@ -359,6 +375,32 @@ class ZenEventD(ZCmdBase):
             help='Sets the path to save pickle files.'
         )
         objectEventNotify(BuildOptionsEvent(self))
+
+
+class Timeout:
+
+    def __init__(self, event, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+        self.event = event
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message, self.event)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+
+class TimeoutError(Exception):
+
+    def __init__(self, message, event=None):
+        super(TimeoutError, self).__init__(message)
+        self.event = event
 
 
 if __name__ == '__main__':
