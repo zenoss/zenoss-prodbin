8c845b33441f870c492ecedd783fa9c45220eae3
Ron Phillips <rphillips@zenoss.com>
2018-04-26 14:49:37 -0500

    Fix Auth0 to use silent authentication, checking for callback errors.

--- Products/ZenUtils/Auth0/Auth0Callback.py
+++ Products/ZenUtils/Auth0/Auth0Callback.py
@@ -20,7 +20,6 @@ import urllib
 
 log = logging.getLogger('Auth0')
 
-
 class Auth0Callback(BrowserView):
     """
     Auth0 redirects to this callback after a login attempt.
@@ -36,40 +35,53 @@ class Auth0Callback(BrowserView):
         args = getQueryArgsFromRequest(self.request)
         state_arg = args.get('state')
         code = args.get('code')
+        error = args.get('error', None)
+
+        if error:
+            log.debug('Auth0 error response: {}'.format(error))
 
         domain = conf['tenant'].replace('https://', '').replace('/', '')
 
-        data = {
-            "grant_type": "authorization_code",
-            "client_id": conf['clientid'],
-            "client_secret": conf['client-secret'],
-            "code": code,
-            "audience": "%s/userinfo" % domain,
-            "scope": "openid profile",
-            "redirect_uri": "%s/zport/Auth0Callback" % zenoss_uri
-        }
+        if error:
+            # We need to make a new request to authorize without the
+            # prompt parameter so that auth0 presents a login screen.
+            uri = "%sauthorize?" % conf['tenant'] + \
+                  "response_type=code&" + \
+                  "client_id=%s&" % conf['clientid'] + \
+                  "state=%s&" % state_arg + \
+                  "scope=openid offline_access&" + \
+                  "redirect_uri=%s/zport/Auth0Callback" % zenoss_uri
+            return self.request.response.redirect(uri)
+        else:
+            data = {
+                "grant_type": "authorization_code",
+                "client_id": conf['clientid'],
+                "client_secret": conf['client-secret'],
+                "code": code,
+                "audience": "%s/userinfo" % domain,
+                "scope": "openid profile",
+                "redirect_uri": "%s/zport/Auth0Callback" % zenoss_uri
+            }
 
-        resp_string = ''
-        conn = httplib.HTTPSConnection(domain)
-        headers = {"content-type": "application/json"}
-        try:
-            conn.request('POST', '/oauth/token', json.dumps(data), headers)
-            resp_string = conn.getresponse().read()
-        except Exception as a:
-            log.error('Unable to obtain token from Auth0: %s', a)
-            return self.request.response.redirect(zenoss_uri + zport_dmd)
+            conn = httplib.HTTPSConnection(domain)
+            headers = {"content-type": "application/json"}
+            try:
+                conn.request('POST', '/oauth/token', json.dumps(data), headers)
+                resp_string = conn.getresponse().read()
+            except Exception as a:
+                log.error('Unable to obtain token from Auth0: %s', a)
+                return self.request.response.redirect(zenoss_uri + zport_dmd)
 
-        resp_data = json.loads(resp_string)
-        refresh_token = resp_data.get('refresh_token')
-        id_token = resp_data.get('id_token')
+            resp_data = json.loads(resp_string)
+            refresh_token = resp_data.get('refresh_token')
+            id_token = resp_data.get('id_token')
 
-        sessionInfo = Auth0.storeIdToken(id_token, self.request.SESSION, conf)
-        sessionInfo.refreshToken = refresh_token
+            Auth0.storeIdToken(id_token, self.request.SESSION, conf, refresh_token)
 
-        came_from = json.loads(base64.b64decode(urllib.unquote(state_arg)))['came_from']
-        virtual_root = getCSEConf().get('virtualroot', '')
-        if virtual_root and \
-            virtual_root not in came_from \
-            and zport_dmd in came_from:
-            came_from = came_from.replace(zport_dmd, virtual_root + zport_dmd)
-        return self.request.response.redirect(came_from)
+            came_from = json.loads(base64.b64decode(urllib.unquote(state_arg)))['came_from']
+            virtual_root = getCSEConf().get('virtualroot', '')
+            if virtual_root and \
+                virtual_root not in came_from \
+                and zport_dmd in came_from:
+                came_from = came_from.replace(zport_dmd, virtual_root + zport_dmd)
+            return self.request.response.redirect(came_from)
