63bb61878e6aa308eb23942e55f1d5b659e6b0eb
Oleksandr Styhar <ostyhar@gmail.com>
2018-04-30 11:11:11 +0300

    ZEN-29896: Solr: zeneventd eventContext solr timeout

--- Products/ZenEvents/events2/processing.py
+++ Products/ZenEvents/events2/processing.py
@@ -58,9 +58,6 @@ class EventLoggerAdapter(logging.LoggerAdapter):
                                             msg=msg)
         return msg, kwargs
 
-def quote_and_escape(value):
-    return '"{}"'.format(value.replace('"', '\\"'))
-
 class Manager(object):
     """
     Provides lookup access to processing pipes and performs caching.
@@ -130,10 +127,9 @@ class Manager(object):
         if cls:
             catalog = catalog or self._catalogs.get(element_type_id)
             if catalog:
-                quoted_id = quote_and_escape(id)
                 results = IModelCatalogTool(catalog).search(cls,
-                                                       query=Or(Eq('id', quoted_id),
-                                                                Eq('name', quoted_id)),
+                                                       query=Or(Eq('id', id),
+                                                                Eq('name', id)),
                                                        filterPermissions=False,
                                                        limit=1, fields=["uuid"])
                 if results.total:
@@ -180,8 +176,7 @@ class Manager(object):
             ip_address = None
             ip_decimal = None
 
-        quoted_id = quote_and_escape(identifier)
-        query_set = Or(Eq('id', quoted_id), Eq('name', quoted_id))
+        query_set = Or(Eq('id', identifier), Eq('name', identifier))
         if ip_decimal is not None:
             query_set.addSubquery(Eq('decimal_ipAddress', str(ip_decimal)))
         device_brains = list(dev_cat.search(types=Device,
