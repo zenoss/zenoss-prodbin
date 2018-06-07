51039e67040b90e2433f16aae7de53bc7567d756
bmoskalyk <37300470+bmoskalyk@users.noreply.github.com>
2018-06-07 09:04:54 +0300

    ZEN-30062 - add CSE_VIRTUAL_ROOT to context on show device details (#3201)

--- Products/ZenUI3/browser/resources/js/zenoss/itinfrastructure.js
+++ Products/ZenUI3/browser/resources/js/zenoss/itinfrastructure.js
@@ -1394,7 +1394,8 @@ Ext.onReady(function () {
                 'administration': true,
                 'overriddenobjects': true
             };
-            var uid = Zenoss.env.PARENT_CONTEXT;
+            // ZEN-30062 
+            var uid = Zenoss.render.link(false,Zenoss.env.PARENT_CONTEXT);
             if (config.contextRegex) {
                 var re = new RegExp(config.contextRegex);
                 return re.test(uid);
