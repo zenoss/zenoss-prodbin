5414ddaecb5c969225a38cab50d0d7801a578e00
bmoskalyk <37300470+bmoskalyk@users.noreply.github.com>
2018-07-06 13:26:29 +0300

    ZEN-30386 - rewrite UI search functionality (#3256)

--- Products/ZenUI3/browser/search/resources/zenoss-search.js
+++ Products/ZenUI3/browser/search/resources/zenoss-search.js
@@ -4,9 +4,7 @@ Ext.onReady(function () {
 // the searchbox.  No context windows such as the event detail popout have
 // no searchbox-container
 
-    if (Ext.get('searchbox-container') === null) {
-        return;
-    } else {
+    if (!Ext.get('searchbox-container')) return;
         /**
          * So we do indeed want the search.
          * But we almost want two different searches:
@@ -21,34 +19,25 @@ Ext.onReady(function () {
          */
 
         var router = Zenoss.remote.SearchRouter;
-        var ManageSavedSearchDialog;
 
-        // Create two pickers to be swapped out.  (A picker is the drop down box)
-        var savedSearchPicker;
-        var freeSearchPicker;
         // Create two variables to hold the store ids
         var storedSearchesDataStoreID = "storedSearchesDataStoreID";
-        var  freeSearchDataStoreID = "freeSearchDataStoreID";
+        var freeSearchDataStoreID = "freeSearchDataStoreID";
 
         // Custom rendering Template for the Free Search Results
         // this defines the way the picker (drop down box) will look
         var resultTpl = new Ext.XTemplate(
             '<tpl for=".">',
-            '<table class="search-result"><tr class="x-combo-list-item">',
-            '<th><tpl if="values.category && (xindex == 1 || parent[xindex - 2].category != values.category)"',
-            '>{category}</tpl></th>',
-            '<td colspan="2" class="x-boundlist-item">{content}</td>',
+            '<tpl if="isSavedResult">',
+                '<div class="x-boundlist-item">{name}</div>',
+            '<tpl else>',
+                '<table class="search-result"><tr class="x-combo-list-item">',
+                '<th><tpl if="values.category && (xindex == 1 || parent[xindex - 2].category != values.category)"',
+                '>{category}</tpl></th>',
+                '<td colspan="2" class="x-boundlist-item">{content}</td>',
+            '</tpl>',
             '</tpl>');
 
-        // create an input field that everything hangs off of.
-        var searchfield = new Zenoss.SearchField({
-            black:true,
-            id:'searchbox-query',
-            fieldClass:'searchbox-query',
-            name:'query',
-            width:150,
-            renderTo:'searchbox-container'
-        });
         /**
          * @class Zenoss.search.FreeSearchModel
          * @extends Ext.data.Model
@@ -59,18 +48,50 @@ Ext.onReady(function () {
             idProperty:'url',
             fields:[
                 {name:'url'},
+                {name:'id'},
+                {name:'name'},
                 {name:'content'},
                 {name:'popout'},
-                {name:'category'}
+                {name:'category'},
+                {name:'isSavedResult', type: 'boolean', convert: function(val, record) {
+                    return !!record.get('id');
+                }}
             ]
         });
 
         // Create the Store for the Free Search
         var freeSearchDataStore = Ext.create('Zenoss.NonPaginatedStore', {
-            storeId:freeSearchDataStoreID,
-            directFn:Zenoss.remote.SearchRouter.getLiveResults,
+            storeId: freeSearchDataStoreID,
+            directFn: router.getLiveResults,
             model: 'Zenoss.search.FreeSearchModel',
-            root:'results'
+            root: 'results',
+            listeners: {
+                beforeload: {
+                    fn: function(store, operation) {
+                        // clear store before load to not show previous search result;
+                        store.loadData([]);
+                        // clear last query in combo to trigger search query for same text;
+                        theSearchBox.lastQuery = null;
+                        if (!operation.params.query) {
+                            // if we click on trigger "query" will be empty this mean that we want to see
+                            // stored search results, simply load stored store and load it's data to combo store;
+                            theSearchBox.getPicker().setLoading(true);
+                            theSearchBox.clearValue();
+                            storedSearchesDataStore.load({
+                                params: {
+                                    'addManageSavedSearch': true
+                                },
+                                callback: function(records, operation, success) {
+                                    store.loadData(records);
+                                    theSearchBox.getPicker().setLoading(false);
+                                }
+                            });
+                            return false;
+                        }
+                        return true;
+                    }
+                }
+            }
         });
 
         /**
@@ -86,7 +107,9 @@ Ext.onReady(function () {
                 {name:'name'},
                 {name:'uid'},
                 {name:'createor'},
-                {name:'query'}
+                {name:'query'},
+                // default true for liveSearch store we compute this field on store load;
+                {name:'isSavedResult', type: 'boolean', defaultValue: true}
             ]
         });
 
@@ -100,9 +123,9 @@ Ext.onReady(function () {
             constructor:function (config) {
                 config = config || {};
                 Ext.applyIf(config, {
-                    model:'Zenoss.search.SavedSearchModel',
-                    directFn:Zenoss.remote.SearchRouter.getAllSavedSearches,
-                    root:'data'
+                    model: 'Zenoss.search.SavedSearchModel',
+                    directFn: router.getAllSavedSearches,
+                    root: 'data'
                 });
                 this.callParent(arguments);
             }
@@ -110,159 +133,78 @@ Ext.onReady(function () {
 
         // Create the Store for the saved searches
         var storedSearchesDataStore = Ext.create('Zenoss.search.SavedSearchStore', {
-            storeId:storedSearchesDataStoreID,
-            listeners:{
-                beforeload:function () {
-                    this.setBaseParam('addManageSavedSearch', true);
-                },
-                scope:this.savedSearchCombo
-            }
+            storeId:storedSearchesDataStoreID
         });
 
-        function getSearchApplyTo(targetEl) {
-            var target = Ext.get(targetEl);
-            if (target) {
-                var parent = target.parent();
-                target.remove();
-                return parent;
-            }
-            return targetEl;
-        }
-
-        // Create a searchable Combo Box and apply it to the Search Field that we defined above.
-        var theSearchBox = new Ext.form.ComboBox({
-            store:freeSearchDataStore,
-            typeAhead:false,
-            triggerAction:'all',
-            width:148,
-            maxWidth:375,
-            pageSize:0,
-            // delay all requests by one second
-            delayQuery:1000,
-            minChars:3,
-            hideTrigger:false,
-            renderTo: getSearchApplyTo(searchfield.getEl()),
-            queryMode:'remote',
-            listConfig: {
-                cls:'search-result',
-                loadingText:_t('Searching..'),
-                resizable:true
-            },
-            matchFieldWidth:false,
-            displayField:'name',
-            valueField:'url',
-            listConfig:{
-                emptyText:_t('No Results'),
-                tpl:resultTpl,
-                minWidth:375
-            },
-
-            /**
-             * Override the get picker method to switch pickers based on the store.
-             * @return {Ext.Component} The picker component
-             */
-            getPicker: function() {
-                var me = this;
-                // Which picker should we return????
-                if(this.store.storeId == storedSearchesDataStoreID ){
-                    // We want the Saved Searches.
-                    // flip the template for the drop down box
-                    this.listConfig.tpl =  null;
-                    // return the correct picker, create it first if you have to
-                    return savedSearchPicker || (savedSearchPicker = me.createPicker());
-                }
-                else{
-                    // We want the Free Text Entry Search
-                    // flip the template for the drop down box
-                    this.listConfig.tpl =  resultTpl;
-                    // return the correct picker, create it first if you have to
-                    return freeSearchPicker || (freeSearchPicker = me.createPicker());
-                }
-            },
-
-            /**
-             * Override the align picker method to make sure that
-             * the width of the picker is 375, which is different than the width of the input
-             **/
-            alignPicker:function () {
-                var me = this;
-                var picker;
-                if (me.isExpanded) {
-                    picker = me.getPicker();
-                    // Auto the height (it will be constrained by min and max width) unless there are no records to display.
-                    picker.setSize(375, picker.store && picker.store.getCount() ? null : 20);
-                    if (picker.isFloating()) {
-                        me.doAlign();
-                    }
-                }
-            },
-            // Override the on click event to switch the store over to the Saved Searches
-            onTriggerClick:function () {
-                if (theSearchBox.store.storeId != storedSearchesDataStoreID) {
-                    // load the data into it.
-                    storedSearchesDataStore.load();
-                    theSearchBox.store = storedSearchesDataStore;
-                }
-                if (!this.isExpanded) {
-                    this.expand();
-                }
-            },
-
-            listeners:{
-                // Fire this off when they just select away and the drop down box is closed
-                // we want to make sure that the store is returned to the free search
-                collapse:function(){
-                    theSearchBox.store = freeSearchDataStore;
-                    // sometimes EXT forgets to actually hide the pickers
-                    if(savedSearchPicker ){savedSearchPicker.hide();}
-                    if(freeSearchPicker){ freeSearchPicker.hide();}
-
+        // Create a container with Combo Box and render it to "searchbox-container".
+        var searchContainer = new Ext.container.Container({
+            width: 160,
+            renderTo: 'searchbox-container',
+            layout: 'hbox',
+            items: [{
+                xtype: 'combobox',
+                flex: 1,
+                store:freeSearchDataStore,
+                typeAhead:false,
+                triggerAction:'all',
+                width:148,
+                maxWidth:375,
+                fieldCls: 'x-form-field searchbox-query',
+                pageSize:0,
+                // delay all requests by one second
+                delayQuery:1000,
+                minChars:3,
+                hideTrigger:false,
+                queryMode:'remote',
+                matchFieldWidth:false,
+                displayField:'name',
+                valueField:'url',
+                listConfig:{
+                    cls:'search-result',
+                    loadingText:_t('Searching..'),
+                    resizable:true,
+                    emptyText:_t('No Results'),
+                    tpl:resultTpl,
+                    minWidth:375
                 },
-                // Fire this off when a selection is made
-                select:function (box, records) {
-                    // Which selection box was active at the time????
-                    if (theSearchBox.store.storeId != storedSearchesDataStoreID) {
-                        // We are looking at a free search.
-                        var record = records[0];
-                        if (record.get('url') !== '') {
+                listeners: {
+                    // Fire this off when a selection is made
+                    select: function (box, records) {
+                        // we are looking at a saved search
+                        var record = records[0],
+                            id = record.get("id"),
+                            url = record.get('url');
+
+                        // open saved search result manager popUp;
+                        if (id === 'manage_saved_search') {
+                            var decodedUrl = Ext.urlDecode(location.search.substring(1, location.search.length)),
+                                win = Ext.create('Zenoss.search.ManageSavedSearchDialog', {
+                                    xtype: 'managesavedsearchdialog',
+                                    id: 'manageSavedSearchesDialog',
+                                    searchId: decodedUrl.search
+                                });
+                            win.show();
+                            // and clear out the typed in search box
+                            this.clearValue();
+                        } else if (url) {
                             // IE only supports these window names: _blank _media _parent _search _self _top
-                            var windowname = Ext.isIE ? '_blank' : record.data.url;
+                            var windowname = Ext.isIE ? '_blank' : url;
                             if (record.get('popout')) {
-                                window.open(Ext.String.format('{0}', record.data.url),
+                                window.open(Ext.String.format('{0}', url),
                                     windowname, 'status=1,width=600,height=500');
                             }
                             else {
-                                window.location =
-                                    Ext.String.format('{0}', record.data.url);
+                                window.location = Ext.String.format('{0}', url);
                             }
-                        }
-                    } else {
-                        // we are looking at a saved search
-                        var record = records[0];
-                        // magic string that means the user wishes to manage
-                        // their saved searches
-                        if (record.get("id") == 'manage_saved_search') {
-                            var decodedUrl = Ext.urlDecode(location.search.substring(1, location.search.length)),
-                                searchId = decodedUrl.search,
-                                win = Ext.create('Zenoss.search.ManageSavedSearchDialog', {
-                                    xtype:'managesavedsearchdialog',
-                                    id:'manageSavedSearchesDialog',
-                                    searchId:searchId
-                                });
-                            win.show();
-                            // Make sure that we reset the store to be saved search
-                            theSearchBox.store = freeSearchDataStore;
-                            // and clear out the typed in search box
-                            theSearchBox.clearValue();
-
-                        } else {
+                        } else if (id) {
                             // otherwise go to the selected search results page
-                            window.location = Ext.String.format('/zport/dmd/search?search={0}', record.get("id"));
+                            window.location = Ext.String.format('/zport/dmd/search?search={0}', id);
                         }
                     }
                 }
-            }
-        });
+            }]
+        }),
+        theSearchBox = searchContainer.down('combobox');
 
         // Now set the combo box to the env
         Zenoss.env.search = theSearchBox;
@@ -278,7 +220,7 @@ Ext.onReady(function () {
                     me = this;
                 Ext.apply(config, {
                     title:_t('Manage Saved Searches'),
-                    layout: 'anchor',
+                    layout: 'fit',
                     stateful: false,
                     autoHeight:true,
                     width:460,
@@ -307,6 +249,7 @@ Ext.onReady(function () {
                                 items: [{
                                         xtype:'button',
                                         ref:'../deleteButton',
+                                        disabled: true,
                                         iconCls:'delete',
                                         tooltip:_t('Delete the selected saved search'),
                                         handler:function (button, e) {
@@ -324,12 +267,17 @@ Ext.onReady(function () {
                                 singleSelect:true
                             }),
                             columns:[
-                                {dataIndex:'name', header:_t('Name'), width:225},
-                                {dataIndex:'query', header:_t('Query'), width:225}
+                                {dataIndex:'name', header:_t('Name'), flex: 1},
+                                {dataIndex:'query', header:_t('Query'), flex: 1}
                             ],
                             store:Ext.create('Zenoss.search.SavedSearchStore', {
                                 autoLoad:true
-                            })
+                            }),
+                            listeners: {
+                                selectionchange: function(t, selection) {
+                                    this.deleteButton.setDisabled(!selection.length);
+                                }
+                            }
                         }
                     ],
 
@@ -350,6 +298,4 @@ Ext.onReady(function () {
                 this.savedSearchGrid.getStore().load();
             }
         });
-
-    }
 });
