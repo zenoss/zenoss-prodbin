b2bf48853413f4407249fec92771ef054e53755d
Alex Chow <achow@zenoss.com>
2018-07-12 17:15:00 -0500

    finalize

--- Products/ZenossStartup/metaglobal.py
+++ Products/ZenossStartup/metaglobal.py
@@ -17,24 +17,15 @@ _FEATURE_PREFIX = "zcml-"
 _flen = len(_FEATURE_PREFIX)
 
 class IAddGlobalFeatures(Interface):
-    """ The 'if-global-include'' directive
-    """
-    configuration = TextLine(
-        title=u"Configuration",
-        description=u"Global configuration file",
-        required=True
-    )
+    pass
 
-def addGlobalFeatures(_context, configuration='/opt/zenoss/etc/global.conf'):
-    """ The 'if-global-include'' directive
-        include the file parameter if a string 'zcml-' is defined in /opt/zenoss/etc/global.conf
-    """
+def addGlobalFeatures(_context):
     # inject into zcml context the global features specified in global.conf
     conf = globalConfToDict()
     for key, value in conf.iteritems():
         if key[:_flen] == _FEATURE_PREFIX:
             feature = key[_flen:]
-            if not feature or not isinstance(value, basestring) or value.lower() != "true":
+            if not feature or not isinstance(value, basestring) or ( value.lower() != "feature" and value.lower() != "yes" ):
                 continue
             # add the feature to the existing global zcml context
             _context.provideFeature(unicode(feature))
