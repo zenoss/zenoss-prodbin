9ae60df37f4f28aba6ac2027aaf9276d9e08278e
Roman Zino <rzino@zenoss.com>
2018-08-01 14:26:06 +0000

    Fix formatting

--- Products/ZenUI3/browser/eventconsole/columns.py
+++ Products/ZenUI3/browser/eventconsole/columns.py
@@ -239,7 +239,7 @@ COLUMN_CONFIG = {
         filter={
             'xtype':'daterange',
             'format':'Y-m-d H:i:s',
-            'invalidText' : "{0} is not a valid date - it must be in the format yyyy/mm/dd hh:mm:ss",
+            'invalidText': "{0} is not a valid date - it must be in the format yyyy/mm/dd hh:mm:ss",
             'altFormats':'m/d/Y|n/j/Y|n/j/y|m/j/y|n/d/y|m/j/Y|n/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d|Y-m-d|n-j|n/j|Y-m-d H:i:s \\T\\O Y-m-d H:i:s'
         },
         width=120,
