b0bd91e05d4f3349a5013341a74a517fa1b37e4a
Glenn Jones <gjones@zenoss.com>
2018-08-10 11:41:06 -0500

    Bump version to 7.0.3

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -8,5 +8,5 @@
 ##############################################################################
 
 # this is now generated during the build step; see make generate-zversion
-VERSION="7.0.2"
+VERSION="7.0.3"
 BUILD_NUMBER="DEV"
