ec102e5076e41c7502b70de284a9774bdfa8c9a4
Dmitri Budko <ISPHOST@users.noreply.github.com>
2018-08-29 10:40:19 +0300

    ZEN-30540 Reports should not automatically run when selected (#3352)

---
+++ Products/ZenReports/plugins/modelcollreport.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2018. All rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import logging
+log = logging.getLogger('zen.Reports')
+
+import Globals
+import transaction
+from Products.ZenReports.Utils import Record
+
+
+class modelcollreport(object):
+
+    def run(self, dmd, args):
+        # ZEN-30539
+        if args.get('adapt', ''):
+            return []
+        return dmd.getDmdRoot('Devices').getSubDevices(lambda x: x.snmpAgeCheck(48) and \
+                x.getProductionState() > x.getZ('zProdStateThreshold') \
+                and not x.getZ('zSnmpMonitorIgnore'))
+
