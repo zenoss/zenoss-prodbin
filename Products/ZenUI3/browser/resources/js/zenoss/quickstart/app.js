3e21402df0e69f6ab44ef362166ffc4239edb4e7
Roman Zino <rzino@zenoss.com>
2018-08-31 10:12:57 +0000

    Remove old code from Ext application

--- Products/ZenUI3/browser/resources/js/zenoss/quickstart/app.js
+++ Products/ZenUI3/browser/resources/js/zenoss/quickstart/app.js
@@ -9,41 +9,13 @@
 (function(){
     Ext.ns('Zenoss.quickstart.Wizard');
 
-    Zenoss.quickstart.Wizard.wizardSteps = [];
-    Zenoss.quickstart.Wizard.controllers = ["AutoDiscoveryController", "AddDeviceController"];
     Zenoss.quickstart.Wizard.events = Ext.create('Ext.util.Observable', {});
     Zenoss.quickstart.Wizard.events.addEvents('beforeapplaunch');
-
-    /**
-     * Allow zenpacks to add/remove steps
-     **/
-    Zenoss.quickstart.Wizard.addStep = function(config, position) {
-        var steps = Zenoss.quickstart.Wizard.wizardSteps;
-        steps = steps.splice(position, 0, config);
-    };
-
-    Zenoss.quickstart.Wizard.overrideStep = function(config, position) {
-        Zenoss.quickstart.Wizard.wizardSteps[position] = config;
-    };
-
-    Zenoss.quickstart.Wizard.removeStep = function(position) {
-        delete Zenoss.quickstart.Wizard.wizardSteps[position];
-    };
-
-
-    Zenoss.quickstart.Wizard.addController = function(name, id,  app) {
-        var controller = Ext.create(name, {
-            application: app,
-            id: id
-        });
-        controller.doInit(app);
-        app.controllers.add(controller);
-    };
 
     Ext.application({
         name: 'Zenoss.quickstart.Wizard',
         appFolder: "/++resource++zenui/js/zenoss/quickstart",
-        controllers: Zenoss.quickstart.Wizard.controllers,
+        controllers: ["AutoDiscoveryController", "AddDeviceController"],
         launch: function() {
             Zenoss.quickstart.Wizard.events.fireEvent('beforeapplaunch', this);
 
