f472d90bee93df78405a7a673abf65573cde2ae5
lundybernard <lundy.bernard@gmail.com>
2018-09-05 15:32:49 -0500

    Begin adding tests for zenhub

---
+++ Products/ZenHub/tests/test_zenhub.py
@@ -0,0 +1,144 @@
+from unittest import TestCase
+from mock import Mock, patch, create_autospec
+
+# Breaks test isolation ImportError: No module named Globals
+from Products.ZenHub.zenhub import (
+    AuthXmlRpcService,
+    XmlRpcService,
+    HubAvitar,
+    RemoteBadMonitor,
+    pb
+)
+
+
+PATH = {'src': 'Products.ZenHub.zenhub'}
+
+
+class AuthXmlRpcServiceTest(TestCase):
+
+    def setUp(t):
+        t.dmd = Mock(name='dmd', spec_set=['ZenEventManager'])
+        t.checker = Mock(name='checker', spec_set=['requestAvatarId'])
+
+        t.axrs = AuthXmlRpcService(t.dmd, t.checker)
+
+    @patch('{src}.XmlRpcService.__init__'.format(**PATH), autospec=True)
+    def test___init__(t, XmlRpcService__init__):
+        dmd = Mock(name='dmd', spec_set=[])
+        checker = Mock(name='checker', spec_set=[])
+
+        axrs = AuthXmlRpcService(dmd, checker)
+
+        XmlRpcService__init__.assert_called_with(axrs, dmd)
+        t.assertEqual(axrs.checker, checker)
+
+    def test_doRender(t):
+        '''should be refactored to call self.render,
+        instead of the parrent class directly
+        '''
+        render = create_autospec(XmlRpcService.render, name='render')
+        XmlRpcService.render = render
+        request = Mock(name='request', spec_set=[])
+
+        ret = t.axrs.doRender('unused arg', request)
+
+        XmlRpcService.render.assert_called_with(t.axrs, request)
+        t.assertEqual(ret, render.return_value)
+
+    @patch('{src}.xmlrpc'.format(**PATH), name='xmlrpc', autospec=True)
+    def test_unauthorized(t, xmlrpc):
+        request = Mock(name='request', spec_set=[])
+        t.axrs._cbRender = create_autospec(t.axrs._cbRender)
+
+        t.axrs.unauthorized(request)
+
+        xmlrpc.Fault.assert_called_with(t.axrs.FAILURE, 'Unauthorized')
+        t.axrs._cbRender.assert_called_with(xmlrpc.Fault.return_value, request)
+
+    @patch('{src}.server'.format(**PATH), name='server', autospec=True)
+    @patch(
+        '{src}.credentials'.format(**PATH), name='credentials', autospec=True
+    )
+    def test_render(t, credentials, server):
+        request = Mock(name='request', spec_set=['getHeader'])
+        auth = Mock(name='auth', spec_set=['split'])
+        encoded = Mock(name='encoded', spec_set=['decode'])
+        encoded.decode.return_value.split.return_value = ('user', 'password')
+        auth.split.return_value = ('Basic', encoded)
+
+        request.getHeader.return_value = auth
+
+        ret = t.axrs.render(request)
+
+        request.getHeader.assert_called_with('authorization')
+        encoded.decode.assert_called_with('base64')
+        encoded.decode.return_value.split.assert_called_with(':')
+        credentials.UsernamePassword.assert_called_with('user', 'password')
+        t.axrs.checker.requestAvatarId.assert_called_with(
+            credentials.UsernamePassword.return_value
+        )
+        deferred = t.axrs.checker.requestAvatarId.return_value
+        deferred.addCallback.assert_called_with(t.axrs.doRender, request)
+
+        t.assertEqual(ret, server.NOT_DONE_YET)
+
+
+class HubAvitarTest(TestCase):
+
+    def setUp(t):
+        t.hub = Mock(name='hub', spec_set=['getService', 'log', 'workers'])
+        t.avitar = HubAvitar(t.hub)
+
+    def test___init__(t):
+        t.assertEqual(t.avitar.hub, t.hub)
+
+    def test_perspective_ping(t):
+        ret = t.avitar.perspective_ping()
+        t.assertEqual(ret, 'pong')
+
+    @patch('{src}.os.environ'.format(**PATH), name='os.environ', autospec=True)
+    def test_perspective_getHubInstanceId(t, os_environ):
+        ret = t.avitar.perspective_getHubInstanceId()
+        os_environ.get.assert_called_with(
+            'CONTROLPLANE_INSTANCE_ID', 'Unknown'
+        )
+        t.assertEqual(ret, os_environ.get.return_value)
+
+    def test_perspective_getService(t):
+        service_name = 'serviceName'
+        instance = 'collector_instance_name'
+        listener = Mock(name='listener', spec_set=[])
+        options = Mock(name='options', spec_set=[])
+        service = t.hub.getService.return_value
+
+        ret = t.avitar.perspective_getService(
+            service_name, instance=instance,
+            listener=listener, options=options
+        )
+
+        t.hub.getService.assert_called_with(service_name, instance)
+        service.addListener.assert_called_with(listener, options)
+        t.assertEqual(ret, service)
+
+    def test_perspective_getService_raises_RemoteBadMonitor(t):
+        t.hub.getService.side_effect = RemoteBadMonitor('tb', 'msg')
+        with t.assertRaises(RemoteBadMonitor):
+            t.avitar.perspective_getService('service_name')
+
+    def test_perspective_reportingForWork(t):
+        worker = Mock(pb.RemoteReference, autospec=True)
+        pid = 9999
+        t.hub.workers = []
+
+        t.avitar.perspective_reportingForWork(worker, pid=pid)
+
+        t.assertFalse(worker.busy)
+        t.assertEqual(worker.pid, pid)
+        t.assertIn(worker, t.hub.workers)
+
+        # Ugly test for the notifyOnDisconnect method, please refactor
+        args, kwargs = worker.notifyOnDisconnect.call_args
+        removeWorker = args[0]
+
+        removeWorker(worker)
+        t.assertNotIn(worker, t.hub.workers)
