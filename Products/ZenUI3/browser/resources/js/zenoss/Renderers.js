4478107a87b8c0f01b2258c698c23ca9ec377401
Roman Zino <rzino@zenoss.com>
2018-09-06 15:02:23 +0000

    ZEN-30685: Move escaping to front-end side

--- Products/ZenUI3/browser/resources/js/zenoss/Renderers.js
+++ Products/ZenUI3/browser/resources/js/zenoss/Renderers.js
@@ -539,10 +539,9 @@ Ext.apply(Zenoss.render, {
         if (!msg || msg === "None" ) {
             msg = record.data.summary;
         }
-        msg = Zenoss.render.conditionalEscaping(msg);
-        msg = "<pre style='white-space:normal;'>" + msg + "</pre>";
-        msg = msg.replace(/\"/g, '&quot;');
-        metadata.tdAttr = 'data-qtip="' + Zenoss.render.conditionalEscaping(msg) + '" data-qwidth="500"';
+
+        msg = "<pre style='white-space:normal;'>" + Ext.htmlEncode(msg) + "</pre>";
+        metadata.tdAttr = 'data-qtip="' + Ext.htmlEncode(msg) + '" data-qwidth="500"';
 
         data = Zenoss.render.conditionalEscaping(data);
         return data;
