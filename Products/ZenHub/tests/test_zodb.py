bebec2b77fdcf26c831023992fa47f68f2458834
Lundy Bernard <lundybernard@users.noreply.github.com>
2018-09-25 09:35:25 -0500

    ZEN-30620: Add unit tests to ZenHub (#3338)
    * Add tests for invalidationfilter
    
    Fixes ZEN-30642
    
    * REFACTOR: cleanup
    
    * Add tests for HubService
    
    Fixes ZEN-30620
    
    * Add tests for invalidationoid
    
    Fixes feature/ZEN-30620
    
    * Fix typo in interface definition
    
    * Add interface checking to invalidationfilters
    
    * Add tests for invalidations
    
    * REFACTOR: cleanup minor issues with mocks
    
    * Add tests for notify
    
    * Add tests for notify
    
    * Add tests for PBDaemon
    
    RemoteExceptionsTest
    DefaultFingerprintGeneratorTest
    load_utilities_Test
    BaseEventQueueTest
    DequeEventQueueTest
    DeDupingEventQueueTest
    
    * REFACTOR: source path for test_notify
    
    remove failing test
    
    * Add tests for DeDupingEventQueue
    
    * add tests for EventQueueManager
    
    * finish EventQueueManagerTest
    
    * Add tests for PBDaemon
    
    * Add more tests for PBDaemon
    
    * add more tests for PBDaemon
    
    * Disable failing test
    
    class attribute PBDaemon.heartbeatEvent has different values if the tests are run for all of ZenHub, or just PBDaemon.py
    
    * finish tests for PBDaemon
    
    * REFACTOR: replace self with t for unittests
    
    * Add tests for WorkerSelection module
    
    * REFACTOR
    
    * Add tests for XmlRpcService
    
    * Begin adding tests for zenhub
    
    * add more tests for zenhub
    
    * add more tests for zenhub
    
    * add more tests for zenhub
    
    * add more tests for zenhub
    
    * finish adding tests for zenhub
    
    * start adding tests for zenhubworker
    
    * fix failing tests
    
    * finish adding tests for zenhubworker
    
    * add tests for ZenHub.zodb

---
+++ Products/ZenHub/tests/test_zodb.py
@@ -0,0 +1,108 @@
+from unittest import TestCase
+from mock import sentinel, patch
+
+from zope.interface.verify import verifyObject
+
+from Products.ZenHub.zodb import (
+    InvalidationEvent, ObjectEvent,
+    UpdateEvent, IUpdateEvent,
+    DeletionEvent, IDeletionEvent,
+    onUpdate,
+    onDelete,
+)
+
+PATH = {'src': 'Products.ZenHub.zodb'}
+
+
+class InvalidationEventTest(TestCase):
+
+    def test___init__(t):
+        obj = sentinel.object
+        oid = sentinel.oid
+
+        iet = InvalidationEvent(obj, oid)
+
+        t.assertIsInstance(iet, ObjectEvent)
+        t.assertEqual(iet.oid, oid)
+
+
+class UpdateEventTest(TestCase):
+
+    def test_implements_IUpdateEvent(t):
+        # the class Implements the Interface
+        t.assertTrue(IUpdateEvent.implementedBy(UpdateEvent))
+
+    def test___init__(t):
+        obj = sentinel.object
+        oid = sentinel.oid
+
+        update_event = UpdateEvent(obj, oid)
+
+        # the object provides the interface
+        t.assertTrue(IUpdateEvent.providedBy(update_event))
+        # Verify the object implments the interface properly
+        verifyObject(IUpdateEvent, update_event)
+
+
+class DeletionEventTest(TestCase):
+
+    def test_implements_IDeletionEvent(t):
+        # the class Implements the Interface
+        t.assertTrue(IDeletionEvent.implementedBy(DeletionEvent))
+
+    def test___init__(t):
+        obj = sentinel.object
+        oid = sentinel.oid
+
+        deletion_event = DeletionEvent(obj, oid)
+
+        # the object provides the interface
+        t.assertTrue(IDeletionEvent.providedBy(deletion_event))
+        # Verify the object implments the interface properly
+        verifyObject(IDeletionEvent, deletion_event)
+
+
+class _listener_decorator_factoryTest(TestCase):
+
+    '''Used to create decorators
+    that register class methods as event handlers
+    '''
+
+    @patch('{src}.provideHandler'.format(**PATH), autospec=True)
+    def test_onUpdate_decorator(t, provideHandler):
+        '''Decorator for a class method
+        causes the decorated method to run when an update event
+        of the specified type (onUpdate arg) fires
+        event type examples: PerformanceConf, ZenPack, Device
+        '''
+        eventtype = sentinel.eventtype  # EX: PerformanceConf
+
+        class MyClass(object):
+            @onUpdate(eventtype)
+            def eventtype_update_handler(self, obj, event):
+                pass
+
+        mc = MyClass()
+
+        provideHandler.assert_called_with(
+            mc.eventtype_update_handler, (eventtype, IUpdateEvent)
+        )
+
+    @patch('{src}.provideHandler'.format(**PATH), autospec=True)
+    def test_onDeleted_decorator(t, provideHandler):
+        '''Decorator for a class method
+        causes the decorated method to run when a deleted event
+        of the specified type (onUpdate arg) fires
+        '''
+        eventtype = sentinel.eventtype
+
+        class MyClass(object):
+            @onDelete(eventtype)
+            def eventtype_deleted_handler(self, obj, event):
+                pass
+
+        mc = MyClass()
+
+        provideHandler.assert_called_with(
+            mc.eventtype_deleted_handler, (eventtype, IDeletionEvent)
+        )
