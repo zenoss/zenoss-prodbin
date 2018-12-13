eaffac49cf76d081caddf0665ac2080e0d595895
Jason Peacock <jpeacock@zenoss.com>
2018-12-13 10:13:12 -0600

    Bump versions for 7.0.8

--- Products/ZenModel/ZVersion.py
+++ Products/ZenModel/ZVersion.py
@@ -8,5 +8,5 @@
 ##############################################################################
 
 # this is now generated during the build step; see make generate-zversion
-VERSION="7.0.7"
+VERSION="7.0.8"
 BUILD_NUMBER="DEV"
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-VERSION  ?= 7.0.7
+VERSION  ?= 7.0.8
 BUILD_NUMBER ?= DEV
 BRANCH   ?= develop
 ARTIFACT_TAG ?= $(shell echo $(BRANCH) | sed 's/\//-/g')
