bf34b5bfabdf07e71a77712dc3bf32421174520c
Roman Zino <rzino@zenoss.com>
2019-01-28 15:41:12 +0000

    Make Add Data Point window more user-friendly

--- Products/ZenUI3/browser/resources/js/zenoss/monitoringTemplates/graph.js
+++ Products/ZenUI3/browser/resources/js/zenoss/monitoringTemplates/graph.js
@@ -131,12 +131,25 @@ Ext.define("Zenoss.GraphPointGrid", {
                                     //id: 'addDataPointToGraphDialog',
                                     title: _t('Add Data Point'),
                                     closeAction: 'hide',
+                                    width: 600,
                                     items:[{
                                         xtype: 'combo',
                                         ref: 'comboBox',
+                                        anchor: '100%',
+                                        listConfig: {
+                                            cls: 'x-boundlist-scrollable',
+                                        },
                                         getInnerTpl: function() {
                                             return '<tpl for="."><div ext:qtip="{name}" class="x-combo-list-item">{name}</div></tpl>';
                                         },
+                                        tpl: Ext.create(
+                                            'Ext.XTemplate',
+                                            '<tpl for=".">',
+                                                '<div class="x-boundlist-item">',
+                                                    '<div class="x-boundlist-item-inner">{name}</div>',
+                                                '</div>',
+                                            '</tpl>'
+                                        ),
                                         fieldLabel: _t('Data Point'),
                                         valueField: 'uid',
                                         displayField: 'name',
@@ -163,7 +176,8 @@ Ext.define("Zenoss.GraphPointGrid", {
                                         name: 'include_related_thresholds',
                                         ref: 'includeRelatedThresholds',
                                         fieldLabel: _t('Include Related Thresholds'),
-                                        checked: true
+                                        checked: true,
+                                        anchor: '100%',
                                     }],
                                     listeners: {
                                         show: function() {
