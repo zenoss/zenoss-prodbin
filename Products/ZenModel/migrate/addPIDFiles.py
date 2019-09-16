6208ccbdcb86b52c2a2795f9b9d2489e7a8afbf0
Oleksandr Styhar <ostyhar@zenoss.com>
2019-09-16 10:52:59 +0000

    CC-4259: stopping services causes nearly all services to be terminate

--- Products/ZenModel/migrate/addPIDFiles.py
+++ Products/ZenModel/migrate/addPIDFiles.py
@@ -10,15 +10,16 @@
 import logging
 import Migrate
 import servicemigration as sm
+from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION
 
 log = logging.getLogger("zen.migrate")
-sm.require("1.1.13")
+sm.require("1.1.14")
 
 
 class AddPIDFiles(Migrate.Step):
     "Add PIDFiles to services"
 
-    version = Migrate.Version(300, 0, 11)
+    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
 
     def cutover(self, dmd):
         try:
@@ -27,20 +28,8 @@ class AddPIDFiles(Migrate.Step):
             log.info("Couldn't generate service context, skipping.")
             return
 
-        service_names = ['zencommand', 'zenjmx', 'zenmodeler', 'zenperfsnmp',
-                         'zenping', 'zenprocess', 'zenpropertymonitor',
-                         'zenpython', 'zenstatus', 'zensyslog', 'zentrap',
-                         'zenwebtx']
-        services = filter(lambda s: s.name in service_names, ctx.services)
-        for service in services:
-            service.pidFile = "exec echo $ZENHOME/var/$SERVICE-$MONITOR.pid"
-
         pidFiles = {
-        "zenhub": "exec echo $ZENHOME/var/zenhub-{{(parent .).Name}}.pid",
-        "zenhubiworker": "exec echo $ZENHOME/var/worker-{{(parent .).Name}}.pid",
-        "zenhubworker (adm)": "exec echo $ZENHOME/var/zenhubworker-{{(parent .).Name}}.pid",
-        "zenhubworker (default)": "exec echo $ZENHOME/var/zenhubworker-{{(parent .).Name}}.pid",
-        "Impact": "exec echo /opt/zenoss_impact/var/zenoss_impact.pid",
+            "Impact": "exec echo /opt/zenoss_impact/var/zenoss_impact.pid",
         }
 
         services = filter(lambda s: s.name in pidFiles.keys(), ctx.services)
--- Products/ZenModel/migrate/editServiceCommand.py
+++ Products/ZenModel/migrate/editServiceCommand.py
@@ -8,17 +8,19 @@
 ##############################################################################
 
 import logging
+import string
 import Migrate
 import servicemigration as sm
+from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION
 
 log = logging.getLogger("zen.migrate")
-sm.require("1.1.13")
+sm.require("1.1.14")
 
 
 class EditServiceCommand(Migrate.Step):
     "Edit service command"
 
-    version = Migrate.Version(300, 0, 11)
+    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
 
     def cutover(self, dmd):
         try:
@@ -28,27 +30,40 @@ class EditServiceCommand(Migrate.Step):
             return
 
         commands = {
-        "Capacity": "/bin/bash -c \"cp /opt/zenoss/etc/capacity/capacity_supervisor.conf.in /opt/zenoss/etc/capacity/capacity_supervisor.conf; sed -i 's@%INSTANCES%@{{.Instances}}@g;' /opt/zenoss/etc/capacity/capacity_supervisor.conf; /opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/capacity/supervisord.conf\"",
-        "MetricShipper": "/bin/supervisord -n -c /opt/zenoss/etc/metricshipper/supervisord.conf",
-        "zminion": "/bin/supervisord -n -c /opt/zenoss/etc/zminion/supervisord.conf",
-        "zenactiond": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenactiond/supervisord.conf",
-        "zeneventd": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zeneventd/supervisord.conf",
-        "zeneventserver": "/bin/bash -c \"cp /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf.in /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf; sed -i 's@%RAM_COMMITMENT%@{{.RAMCommitment}}@g;' /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf; /opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zeneventserver/supervisord.conf\"",
-        "zenimpactstate": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenimpactstate/supervisord.conf",
-        "CentralQuery": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/central-query/supervisord.conf",
-        "MetricConsumer": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/metric-consumer-app/supervisord.conf",
-        "MetricShipper": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/metricshipper/supervisord.conf",
-        "Zauth": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zauth/supervisord.conf",
-        "Zope": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zope/supervisord.conf",
-        "zenapi": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenapi/supervisord.conf",
-        "zenjobs": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenjobs/supervisord.conf",
-        "zenjserver": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenjserver/supervisord.conf",
-        "zenreports": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zenreports/supervisord.conf",
+            "Capacity": "/bin/bash -c \"cp /opt/zenoss/etc/capacity/capacity_supervisor.conf.in /opt/zenoss/etc/capacity/capacity_supervisor.conf; sed -i 's@%INSTANCES%@{{.Instances}}@g;' /opt/zenoss/etc/capacity/capacity_supervisor.conf; /opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/capacity/supervisord.conf\"",
+            "zeneventserver": "/bin/bash -c \"cp /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf.in /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf; sed -i 's@%RAM_COMMITMENT%@{{.RAMCommitment}}@g;' /opt/zenoss/etc/zeneventserver/zeneventserver_supervisor.conf; /opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/zeneventserver/supervisord.conf\"",
+            "CentralQuery": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/central-query/supervisord.conf",
+            "MetricConsumer": "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/metric-consumer-app/supervisord.conf",
+            "zenhubworker (default)": "/opt/zenoss/bin/zenhubworker run -c --logfileonly --monitor {{(parent .).Name}} --workerid $CONTROLPLANE_INSTANCE_ID default",
+            "zenhubworker (adm)": "/opt/zenoss/bin/zenhubworker run -c --logfileonly --monitor {{(parent .).Name}} --workerid $CONTROLPLANE_INSTANCE_ID adm",
+            "zenhubiworker": "/opt/zenoss/bin/zenhubiworker run -c --duallog --hub {{(parent .).Name}} --workerid $CONTROLPLANE_INSTANCE_ID",
         }
 
         services = filter(lambda s: s.name in commands.keys(), ctx.services)
         for service in services:
             service.startup = commands[service.name]
+
+        service_names = ["MetricShipper", "zminion", "zenactiond", "zeneventd", "zenimpactstate",
+                    "Zauth", "Zope", "zenapi", "zenjobs", "zenjserver", "zenreports"]
+        command = "/opt/zenoss/bin/supervisord -n -c /opt/zenoss/etc/{}/supervisord.conf",
+        services = filter(lambda s: s.name in service_names, ctx.services)
+        for service in services:
+            service.startup = command.format(string.lower(service.name))
+
+        service_names = ["zencommand", "zenjmx", "zenmodeler", "zenperfsnmp", "zenping", "zenprocess",
+                    "zenpropertymonitor", "zenpython", "zenstatus", "zenwebtx"]
+        command ="/opt/zenoss/bin/{} run -c --logfileonly --workers {{.Instances}} --workerid $CONTROLPLANE_INSTANCE_ID --monitor {{(parent .).Name}} "
+        services = filter(lambda s: s.name in service_names, ctx.services)
+        for service in services:
+            service.startup = command.format(service.name)
+            service.runAs = "zenoss"
+
+        service_names = ["zentrap", "zensyslog", "zenhub"]
+        command = "/opt/zenoss/bin/{} run -c --logfileonly --monitor {{(parent .).Name}} "
+        for service in services:
+            service.startup = command.format(service.name)
+            service.runAs = "zenoss"
+
         ctx.commit()
 
 EditServiceCommand()
