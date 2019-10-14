8eb2f3c6654fa0a984e940411a28167161f7173e
LundyBernard <lundy.bernard@gmail.com>
2019-10-14 11:59:08 -0500

    allow tracer test to pass

--- Products/Zing/tests/test_lb.py
+++ Products/Zing/tests/test_lb.py
@@ -4,5 +4,5 @@ from unittest import TestCase
 class TestBanchCheckLB(TestCase):
 
     def test_branch_is_used_by_build(t):
-        t.assertTrue(False)
-
+        print('LBTEST: confirm bugfix branch used')
+        #t.assertTrue(False)
