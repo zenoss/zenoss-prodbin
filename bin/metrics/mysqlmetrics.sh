806d101bbe21249e85e5b482e962a01c559ab85a
Eric Thirolle <ethirolle@users.noreply.github.com>
2019-12-10 19:36:32 -0500

    Update mysqlmetrics.sh
    for ZEN-31238.  Must account for RM 6.4+, where mariadb services are no longer running from the "resmgr" image, but from a separate "mariadb" image (per ZEN-31701). 
    * Use "/usr/bin/python" rather than "/opt/zenoss/bin/python" because the latter is not available in mariadb image.
    * The file "/opt/zenoss/Products/ZenUtils/ZenDB.py" will have to be copied into the mariadb image via product-assembly repo's "mariadb/Dockerfile.in"

--- bin/metrics/mysqlmetrics.sh
+++ bin/metrics/mysqlmetrics.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-mysql -s --skip-column-names -B <<SQL
+if [ $1 = "mariadb-model" ]
+  then
+     DB="zodb"
+  else
+    DB="zep"
+fi
+
+# Use "/usr/bin/python" rather than "/opt/zenoss/bin/python" because the latter is
+# not available in mariadb image.
+# The file "/opt/zenoss/Products/ZenUtils/ZenDB.py" will have to be copied into the
+# mariadb image via product-assembly repo's "mariadb/Dockerfile.in"
+/usr/bin/python /opt/zenoss/Products/ZenUtils/ZenDB.py --usedb $DB --execsql=<<SQL
 USE information_schema;
 
 SELECT
