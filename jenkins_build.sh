5e62ed07799fb9f83ef5e6e9b256679f06cb7ca7
Josh Wilmes <jwilmes@zenoss.com>
2020-06-03 13:32:14 -0500

    Temporarily disable devimg so that it can build the first time

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -51,7 +51,7 @@ cleanup() {
     RC="$?"
     if [[ $RC == 0 ]]; then
         zendev drop ${ZENDEV_ENV}
-        docker rmi zendev/devimg:${ZENDEV_ENV} zendev/devimg-base:${ZENDEV_ENV}
+#        docker rmi zendev/devimg:${ZENDEV_ENV} zendev/devimg-base:${ZENDEV_ENV}
     fi
 }
 
