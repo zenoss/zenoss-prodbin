0e21abaa003526b42b2b167bd512c7e478fc3926
zsalinas <zsalinas@zenoss.com>
2020-11-11 02:09:23 -0600

    Fixes ZEN-3672 (#3744)
    * Fixes ZEN-3672
    
    Replaces functionality that got lost when we moved away from the old 'viewIpInterface.pt' page template that provided the SNMP Index information.

--- Products/Zuul/interfaces/component.py
+++ Products/Zuul/interfaces/component.py
@@ -103,6 +103,9 @@ class IIpInterfaceInfo(IComponentInfo):
         title=_t(u"MTU"), group="Details",
         order=12)
 
+    ifindex = schema.TextLine(
+        title=_t(u"SNMP Index"), group="Details",
+        order=13)
 
 class IFileSystemInfo(IComponentInfo):
     """
