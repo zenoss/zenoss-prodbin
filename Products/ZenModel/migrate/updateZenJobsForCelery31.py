202bc0fbdab9ef58c3ede77afc6e24e5578db540
Roman Bakaleyko <roman.bakaleyko@gmail.com>
2020-11-16 13:20:18 +0000

    ZEN-33170 upgrade from 7.0.15 to 7.0.16 is not working

--- Products/ZenModel/migrate/updateZenJobsForCelery31.py
+++ Products/ZenModel/migrate/updateZenJobsForCelery31.py
@@ -25,33 +25,15 @@ class UpdateZenJobsForCelery31(Migrate.Step):
     def cutover(self, dmd):
         prior_version = getattr(dmd.JobManager, "_jobmanager_version", 1)
         if prior_version < JOBMANAGER_VERSION:
-            count = self._log_unfinished_jobs(dmd)
-            log.info(
-                "Removing old JobManager%s.",
-                "" if count == 0 else " and %s unfinished jobs" % count,
-            )
+            log.info("Removing old JobManager.")
             try:
                 del dmd.JobManager
             except Exception:
                 dmd._delOb("JobManager")
-            log.info(
-                "Old JobManager%s removed.",
-                "" if count == 0 else " and %s unfinished jobs" % count,
-            )
+            log.info("Old JobManager%s removed.")
             manage_addJobManager(dmd)
             log.info("New JobManager added.")
         else:
             log.info("New JobManager already added.")
-
-    def _log_unfinished_jobs(self, dmd):
-        jobs = list(dmd.JobManager.getUnfinishedJobs())
-        if not jobs:
-            log.info("No unfinished jobs found.")
-            return 0
-        log.info("Found %s unfinished jobs", len(jobs))
-        for job in jobs:
-            log.info(".. %s %s %s", job.type, job.job_name, job.description)
-        return len(jobs)
-
 
 UpdateZenJobsForCelery31()
