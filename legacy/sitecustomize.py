4e9acc3e8d9dd318de82226295d8e5af2aadb222
Jason Peacock <jpeacock@zenoss.com>
2021-01-28 10:19:49 -0600

    Remove sitecustomize.py from prodbin.
    product-assembly will install sitecustomize.py instead.
    
    Fixes ZEN-33400.

--- legacy/sitecustomize.py
+++
@@ -1,6 +0,0 @@
-import sys, os, site
-sys.setdefaultencoding('utf-8')
-site.addsitedir(os.path.join(os.getenv('ZENHOME'), 'ZenPacks'))
-site.addsitedir('/var/zenoss/ZenPacks')
-import warnings
-warnings.filterwarnings('ignore', '.*', DeprecationWarning)
