e7dd4192086b94b74077df29bd6a981b12d97b06
d-terletskyi <dterletskyi@zenoss.com>
2022-04-13 14:29:42 +0000

    Remove Report "All Monitored Components"
    Fixes ZEN-33821.
    
    To resolve the issue, the device report All Monitored Components was
    removed since it no longer works for a large number of devices.

---
+++ Products/ZenModel/migrate/removeAllMonitoredComponentsReport.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2022, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+__doc__ = """
+This removes the All Monitored Components report since it no longer works for
+large amount of devices.
+"""
+
+import logging
+import Migrate
+from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, \
+    SCHEMA_REVISION
+
+log = logging.getLogger("zen.migrate")
+
+
+class RemoveAllMonitoredComponentsReport(Migrate.Step):
+    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
+
+    def cutover(self, dmd):
+        deviceReports = dmd.Reports._getOb('Device Reports', None)
+        if deviceReports and deviceReports._getOb(
+                'All Monitored Components', None):
+            deviceReports._delObject('All Monitored Components')
+
+
+RemoveAllMonitoredComponentsReport()
