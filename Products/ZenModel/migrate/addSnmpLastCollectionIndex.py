594a25c9a95613443b0f601706fa665b324d8665
Jason Peacock <jpeacock@zenoss.com>
2022-09-23 10:22:41 -0500

    Pin new ZODB migrations to explicit version.

--- Products/ZenModel/migrate/addSnmpLastCollectionIndex.py
+++ Products/ZenModel/migrate/addSnmpLastCollectionIndex.py
@@ -17,14 +17,13 @@ import logging
 from Products.Zuul.catalog.model_catalog_init import reindex_model_catalog
 from Products.Zuul.catalog.indexable import DeviceIndexable
 from Products.Zuul.catalog.interfaces import IModelCatalogTool
-from Products.ZenModel.ZMigrateVersion import SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION
 
 log = logging.getLogger("zen.migrate")
 
 
 class AddSnmpLastCollectionIndex(Migrate.Step):
 
-    version = Migrate.Version(SCHEMA_MAJOR, SCHEMA_MINOR, SCHEMA_REVISION)
+    version = Migrate.Version(200, 6, 0)
 
     def cutover(self, dmd):
 
