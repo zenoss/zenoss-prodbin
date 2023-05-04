d28b0a2f90fbf17e916f3896d73f9afa581c4fa0
Jason Peacock <jpeacock@zenoss.com>
2023-05-04 16:54:02 -0500

    Formatted ZenossStartup code using the black formatting tool

--- Products/ZenossStartup/metaglobal.py
+++ Products/ZenossStartup/metaglobal.py
@@ -1,23 +1,25 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2018, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+import six
 
 from zope.interface import Interface
-from zope.configuration.fields import GlobalObject
-from zope.schema import TextLine
 
 from Products.ZenUtils.GlobalConfig import globalConfToDict
+
 _FEATURE_PREFIX = "zcml-"
 _flen = len(_FEATURE_PREFIX)
 
+
 class IAddGlobalFeatures(Interface):
     pass
+
 
 def addGlobalFeatures(_context):
     # inject into zcml context the global features specified in global.conf
@@ -26,8 +28,12 @@ def addGlobalFeatures(_context):
         if key[:_flen] == _FEATURE_PREFIX:
             feature = key[_flen:]
 
-            if not feature or not isinstance(value, basestring) or ( value.lower() != "feature" and value.lower() != "yes" ):
+            if (
+                not feature
+                or not isinstance(value, six.string_types)
+                or (value.lower() != "feature" and value.lower() != "yes")
+            ):
 
                 continue
             # add the feature to the existing global zcml context
-            _context.provideFeature(unicode(feature))
+            _context.provideFeature(six.text_type(feature))
