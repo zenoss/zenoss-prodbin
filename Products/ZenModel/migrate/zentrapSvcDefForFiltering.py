55eb1055a7009b191184a3505faef34d39f0ea72
zensaiello <saiello@zenoss.com>
2023-06-01 08:12:25 -0500

    ZEN-34387 Migration script enhancement

--- Products/ZenModel/migrate/zentrapSvcDefForFiltering.py
+++ Products/ZenModel/migrate/zentrapSvcDefForFiltering.py
@@ -14,23 +14,20 @@ Update zentrap graphs config to add eventFilterDroppedCount number.
 """
 
 import logging
+import re
 
 import Migrate
-try:
-    import servicemigration as sm
-    sm.require("1.0.0")
-    VERSION5 = True
-except ImportError:
-    sm = None
-    VERSION5 = False
+import servicemigration as sm
 
 from servicemigration.metrics import Metric
+from servicemigration.graphrange import GraphRange
+from servicemigration.graphconfig import GraphConfig
 from servicemigration.graphdatapoint import GraphDatapoint
 
 
 log = logging.getLogger("zen.migrate")
 svcNamesToUpdate = ['zentrap']
-version = "6.7.0"
+sm.require("1.0.0")
 
 
 class ZentrapSvcDevForMsgParsing(Migrate.Step):
@@ -52,22 +49,137 @@ class ZentrapSvcDevForMsgParsing(Migrate.Step):
         svcs = filter(lambda s: s.name in svcNamesToUpdate, ctx.services)
         log.info("Found %i %r services to update.", len(svcs), svcNamesToUpdate)
         for svc in svcs:
-            # Update the existing 'Events' graph with new graphpoint
+            # Update new global config with any previous, file defined filters
+            fc = next(
+                (x for x in svc.configFiles if x.name == '/opt/zenoss/etc/zentrap.filter.conf'),
+                None
+            )
+            collectorName = ctx.getServiceParent(svc).name
+            if fc is None:
+                log.error(
+                    "%s %s service: No 'zentrap.filter.conf' File configuration found; "
+                    "broken service def; skipping.", collectorName, svc.name)
+                continue
+
+            ofc = next(
+                (x for x in svc.originalConfigs if x.name == '/opt/zenoss/etc/zentrap.filter.conf'),
+                None
+            )
+            if fc.content == ofc.content:
+                log.info(
+                    "%s %s service: 'zentrap.filter.conf' contents are the default"
+                    "; skipping.", collectorName, svc.name)
+            else:
+                log.info(
+                    "%s %s service: found collector specific trap filter "
+                    "configurations in 'zentrap.filter.conf'",
+                    collectorName, svc.name)
+                collectorCfg = []
+                for lineNumber, line in enumerate(fc.content.split('\n')):
+                    if line.startswith('#'):
+                        continue
+                    # skip blank lines
+                    if not line.strip():
+                        continue
+                    if not re.search(
+                            '^({} )*{}$'.format(
+                                collectorName,
+                                line.replace('*', '\*')),
+                            dmd.ZenEventManager.trapFilters,
+                            re.MULTILINE):
+                        log.info(
+                            '%s %s service: migrating to global config. "%s %s"',
+                            collectorName, svc.name,
+                            collectorName, line)
+                        collectorCfg.append("{} {}".format(
+                            collectorName,
+                            line))
+                if collectorCfg:
+                    collectorCfg.insert(
+                        0,
+                        '# Migrated from {} "zentrap.filter.conf" file.'.format(
+                            collectorName
+                        )
+                    )
+                    collectorCfg.insert(0, '')
+                    collectorCfg.append('')
+                    dmd.ZenEventManager.trapFilters += '\n'.join(collectorCfg)
+                else:
+                    log.info(
+                        '%s %s service: No configs migrated, must be already defined/migrated.',
+                        collectorName, svc.name)
+
+            # 'Events' graph ....
             gc = next(
                 (x for x in svc.monitoringProfile.graphConfigs if x.name == 'Events'),
                 None
             )
             if gc is None:
-                log.error(
-                    "%s service: No 'Events' graph configuration found; "
-                    "broken service def; skipping.", svc.name)
-                continue
-            if not filter(lambda x: x.pointID == "eventFilterDroppedCount", gc.datapoints):
+                # 'Events' graph is missing ?!?
                 log.info(
-                    "%s service: Adding 'Filter Dropped Events' graphpoint to the"
-                    " 'Events' graph.", svc.name)
-                gc.datapoints.append(
-                    GraphDatapoint(
+                    "%s %s service: No 'Events' graph configuration found; "
+                    "creating it.", collectorName, svc.name)
+                gcs = svc.monitoringProfile.graphConfigs
+                gcs.insert(
+                    2,
+                    GraphConfig(
+                        graphID="events",
+                        name="Events",
+                        footer=False,
+                        returnset='EXACT',
+                        graphType='line',
+                        yAxisLabel='Events',
+                        description='Events',
+                        graphRange=GraphRange(
+                            start='1h-ago',
+                            end='0s-ago'
+                        ),
+                        units="Events",
+                        datapoints=[
+                            GraphDatapoint(
+                                aggregator="avg",
+                                fill=False,
+                                pointID="events",
+                                legend="Queued",
+                                metric="events",
+                                metricSource="zentrap",
+                                name="Events",
+                                rate=True,
+                                pointType="line"
+                            ),
+                            GraphDatapoint(
+                                aggregator="avg",
+                                fill=False,
+                                pointID="events",
+                                legend="Events",
+                                metric="eventCount",
+                                metricSource="zentrap",
+                                name="Event Count",
+                                rate=False,
+                                pointType="line"
+                            ),
+                            GraphDatapoint(
+                                aggregator='avg',
+                                fill=False,
+                                pointID='eventFilterDroppedCount',
+                                legend='eventFilterDroppedCount',
+                                metric='eventFilterDroppedCount',
+                                metricSource='zentrap',
+                                name='Filter Dropped Events',
+                                rate=False,
+                                pointType='line'
+                            )
+                        ]
+                    )
+                )
+            else:
+                # Update the existing 'Events' graph with new graphpoint
+                if not filter(lambda x: x.pointID == "eventFilterDroppedCount", gc.datapoints):
+                    log.info(
+                        "%s %s service: Adding 'Filter Dropped Events' graphpoint to the"
+                        " 'Events' graph.", collectorName, svc.name)
+                    gc.datapoints.append(
+                        GraphDatapoint(
                             aggregator='avg',
                             fill=False,
                             pointID='eventFilterDroppedCount',
@@ -77,13 +189,13 @@ class ZentrapSvcDevForMsgParsing(Migrate.Step):
                             name='Filter Dropped Events',
                             rate=False,
                             pointType='line'
+                        )
                     )
-                )
-                commit = True
-            else:
-                log.info(
-                    "%s service: 'Filter Dropped Events' graphpoint exists"
-                    " on the 'Events' graph; skipping.", svc.name)
+                    commit = True
+                else:
+                    log.info(
+                        "%s %s service: 'Filter Dropped Events' graphpoint exists"
+                        " on the 'Events' graph; skipping.", collectorName, svc.name)
             # Add new Service metric
             mc = next(
                 (x for x in svc.monitoringProfile.metricConfigs
@@ -92,14 +204,14 @@ class ZentrapSvcDevForMsgParsing(Migrate.Step):
             )
             if mc is None:
                 log.error(
-                    "%s service: No 'zentrap internal metrics' metric "
-                    "config found; broken service def; skipping.", svc.name)
+                    "%s %s service: No 'zentrap internal metrics' metric "
+                    "config found; broken service def; skipping.", collectorName, svc.name)
                 continue
             else:
                 if not filter(lambda x: x.ID == "eventFilterDroppedCount", mc.metrics):
                     log.info(
-                        "%s service: Adding 'Filter Dropped Events' "
-                        "metric", svc.name)
+                        "%s %s service: Adding 'Filter Dropped Events' "
+                        "metric", collectorName, svc.name)
                     mc.metrics.append(
                         Metric(
                             ID='eventFilterDroppedCount',
@@ -112,8 +224,8 @@ class ZentrapSvcDevForMsgParsing(Migrate.Step):
                     commit = True
                 else:
                     log.info(
-                        "%s service: 'Filter Dropped Events' metric "
-                        "exists; skipping.", svc.name)
+                        "%s %s service: 'Filter Dropped Events' metric "
+                        "exists; skipping.", collectorName, svc.name)
 
         if commit:
             ctx.commit()
