6534cd12274b2fe4d4febb512319876225779bfc
Jason Peacock <jpeacock@zenoss.com>
2023-07-10 13:23:48 -0500

    Add/improve docstrings and comments on some ZenHub services. (#4246)

--- Products/ZenHub/HubService.py
+++ Products/ZenHub/HubService.py
@@ -17,20 +17,54 @@ from Products.ZenUtils.deprecated import deprecated
 
 
 class HubService(pb.Referenceable):
+    """
+    The base class for a ZenHub service class.
+
+    :attr log: The logger object for this service.
+    :type log: logging.Logger
+    :attr fqdn: This attribute is deprecated.
+    :type fqdn: str
+    :attr dmd: Root ZODB object
+    :type dmd: Products.ZenModel.DataRoot.DataRoot
+    :attr instance: The name of the Collection Hub.
+    :type instance: str
+    :attr callTime: The total time, in seconds, this service has spent processing remote requests.
+    :type callTime: float
+
+    :attr listeners: ZenHub clients for this service
+    :type listeners: List[twisted.spread.pb.RemoteReference]
+    :attr listenerOptions: Options associated with the client for this service.
+    :type listenerOptions: Mapping[twisted.spread.pb.RemoteReference, Mapping[Any, Any]]
+    """  # noqa E501
+
     def __init__(self, dmd, instance):
+        """
+        Initialize a HubService instance.
+
+        :param dmd: The root Zenoss object in ZODB.
+        :type dmd: Products.ZenModel.DataRoot.DataRoot
+        :param instance: The name of the collection monitor.
+        :type instance: str
+        """
         self.log = logging.getLogger("zen.hub")
         self.fqdn = socket.getfqdn()
         self.dmd = dmd
         self.zem = dmd.ZenEventManager
         self.instance = instance
-        self.listeners = []
+        self.callTime = 0.0
+        self.listeners = []  # Clients of this service
         self.listenerOptions = {}
-        self.callTime = 0
 
     def getPerformanceMonitor(self):
+        """
+        Return the performance monitor (collection hub) instance.
+
+        :return type: Products.ZenModel.interfaces.IMonitor
+        """
         return self.dmd.Monitors.getPerformanceMonitor(self.instance)
 
     def remoteMessageReceived(self, broker, message, args, kw):
+        """Overrides pb.Referenceable method."""
         self.log.debug("Servicing %s in %s", message, self.name())
         now = time.time()
         try:
@@ -53,6 +87,11 @@ class HubService(pb.Referenceable):
         pass
 
     def name(self):
+        """
+        Return the name of this ZenHub service class.
+
+        :return type: str
+        """
         return self.__class__.__name__
 
     def addListener(self, remote, options=None):
@@ -63,15 +102,18 @@ class HubService(pb.Referenceable):
             self.listenerOptions[remote] = options
 
     def removeListener(self, listener):
-        self.log.debug(
-            "removing listener for %s:%s", self.instance, self.name()
-        )
-        try:
+        if listener in self.listeners:
             self.listeners.remove(listener)
-        except ValueError:
-            self.warning("Unable to remove listener... ignoring")
-
-        self.listenerOptions.pop(listener, None)
+            self.listenerOptions.pop(listener, None)
+            self.log.debug(
+                "removed listener for %s:%s", self.instance, self.name()
+            )
+        else:
+            self.log.debug(
+                "listener is not registered for %s:%s",
+                self.instance,
+                self.name(),
+            )
 
     def sendEvents(self, events):
         map(self.sendEvent, events)
