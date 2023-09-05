cd9b72487138825e654c3daf1aea93502607fa96
Jason Peacock <jpeacock@zenoss.com>
2023-09-05 11:42:25 -0500

    Add device class to invalidation.config.build metric.  Ensure the name of the service class is used.  Ensure the name of the device is used rather than its ID.

--- Products/ZenCollector/services/metrics.py
+++ Products/ZenCollector/services/metrics.py
@@ -5,13 +5,14 @@ from contextlib import contextmanager
 from zope.component import queryUtility
 
 from Products.ZenHub.metricmanager import IMetricManager
+from Products.ZenHub.server.service import ServiceReference
 
 METRIC_MANAGER = "invalidation_worker_metricmanager"
 BUILD = "invalidation.config.build"
 
 
 @contextmanager
-def measureConfigBuild(service, deviceId):
+def measureConfigBuild(service, device):
     metrics = queryUtility(IMetricManager, METRIC_MANAGER)
     try:
         begin = time.time()
@@ -20,16 +21,24 @@ def measureConfigBuild(service, deviceId):
         if not metrics:
             return
         end = time.time()
+        path = device.deviceClass().getPrimaryPath()
         metrics.metric_writer.write_metric(
             BUILD,
             toMillis(end - begin),  # duration
             toMillis(time.time()),  # timestamp
             {
                 "monitor": service.instance,
-                "service": service.name(),
-                "device": deviceId,
+                "service": getServiceName(service),
+                "device": device.name(),
+                "deviceclass": "/".join(path[0:1] + path[4:]),
             },
         )
+
+
+def getServiceName(service):
+    if isinstance(service, ServiceReference):
+        service = service.service
+    return service.__class__.__name__
 
 
 def toMillis(seconds):
