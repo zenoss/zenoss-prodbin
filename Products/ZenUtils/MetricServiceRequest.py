7326888021482b3799004c1ddfb6e904ae3ef364
azam <deerwarlord@gmail.com>
2024-03-01 16:02:04 +0200

    ZEN-34707: [Java 21] An error 'Response from the server (return code 400): Unrecognized field "format" generated in the event log for GCP device

--- Products/ZenUtils/MetricServiceRequest.py
+++ Products/ZenUtils/MetricServiceRequest.py
@@ -100,7 +100,6 @@ class MetricServiceRequest(object):
         for metric in metrics:
             log.debug("fetchMetrics metrics %s", metric)
             cf = metric.get('cf', 'average')
-            rpn = metric.get('rpn', '')
             rate = metric.get('rate', False)
             tags = metric['tags']
             downsample = metric.get('downsample', '5m-avg')
@@ -109,7 +108,6 @@ class MetricServiceRequest(object):
                 metric=metricName,
                 downsample=downsample,
                 aggregator=self._aggMapping.get(cf.lower(), cf.lower()),
-                rpn=rpn,
                 rate=rate,
                 format='%.2lf',
                 tags=tags,
