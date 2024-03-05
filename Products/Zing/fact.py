871eda1cfdb2d0a9a2fbfb876d7d2f36ee0895b3
Andrew Chorny <kha9326@zenoss.com>
2024-03-05 13:14:23 +0200

    Propagate CZ prod state monitoring threshold to cloud (#4353)

--- Products/Zing/fact.py
+++ Products/Zing/fact.py
@@ -56,6 +56,7 @@ class MetadataKeys(object):
     PROD_STATE_VALUE_KEY = "prod_state_value"
     PRIORITY_MAP_KEY = "priority_conversion_map"
     PROD_STATE_MAP_KEY = "prod_state_map"
+    PROD_STATE_THRESHOLD = "zen_czProdStateThreshold"
     DELETED_KEY = "_zen_deleted_entity"
     COMPONENT_GROUPS_KEY = "component_groups"
     IMPACT_DS_ORG_KEY = "impact_ds_organizer"
@@ -190,6 +191,8 @@ def device_info_fact(device):
     f.data[MetadataKeys.PRIORITY_MAP_KEY] = str(device.getPriorityConversions())
     f.data[MetadataKeys.PROD_STATE_MAP_KEY] = str(device.getProdStateConversions())
     f.data[MetadataKeys.PROD_STATE_KEY] = device.convertProdState(f.data[MetadataKeys.PROD_STATE_VALUE_KEY])
+    if device.device(): # zProdStateThreshold is for devices, component shouldn't have it
+        f.data[MetadataKeys.PROD_STATE_THRESHOLD] = str(device.device().getProdStateThreshold())
     valid_types = (str, int, long, float, bool, list, tuple, set,)
     for propdict in device._propertyMap():
         propId = propdict.get("id")
