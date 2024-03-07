eb491085043113af28430908cb888ae930f4b6a5
Jason Peacock <jpeacock@zenoss.com>
2024-03-07 13:43:24 -0600

    fix: add absolute import support to CheckRelations.py
    Also added argument checking to checkrel.py
    
    ZEN-34580

--- Products/ZenUtils/CheckRelations.py
+++ Products/ZenUtils/CheckRelations.py
@@ -1,28 +1,19 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
-
-__doc__="""CmdBase
-
-Add data base access functions for command line programs
-
-$Id: CheckRelations.py,v 1.2 2004/10/19 22:28:59 edahl Exp $"""
-
-__version__ = "$Revision: 1.2 $"[11:-2]
-
+from __future__ import absolute_import
 
 import transaction
 
-
 from Products.ZenUtils.Utils import getAllConfmonObjects
+from Products.ZenUtils.ZenScriptBase import ZenScriptBase
 
-from ZenScriptBase import ZenScriptBase
 
 class CheckRelations(ZenScriptBase):
 
@@ -32,10 +23,13 @@ class CheckRelations(ZenScriptBase):
         self.log.info("Checking relations...")
         for object in getAllConfmonObjects(self.dmd):
             ccount += 1
-            self.log.debug("checking relations on object %s", object.getPrimaryDmdId())
+            self.log.debug(
+                "checking relations on object %s", object.getPrimaryDmdId()
+            )
             object.checkRelations(repair=repair)
             ch = object._p_changed
-            if not ch: object._p_deactivate()
+            if not ch:
+                object._p_deactivate()
             if ccount >= self.options.savepoint:
                 transaction.savepoint()
                 ccount = 0
@@ -43,24 +37,34 @@ class CheckRelations(ZenScriptBase):
             self.log.info("not commiting any changes")
         else:
             trans = transaction.get()
-            trans.note('CheckRelations cleaned relations' )
+            trans.note("CheckRelations cleaned relations")
             trans.commit()
 
-
     def buildOptions(self):
-        ZenScriptBase.buildOptions(self)
+        super(CheckRelations, self).buildOptions()
 
-        self.parser.add_option('-r', '--repair',
-                    dest='repair', action="store_true",
-                    help='repair all inconsistant relations')
-
-        self.parser.add_option('-x', '--savepoint',
-                    dest='savepoint', default=500, type="int",
-                    help='how many lines should be loaded before savepoint')
-
-        self.parser.add_option('-n', '--nocommit',
-                    dest='nocommit', action="store_true",
-                    help='Do not store changes to the Dmd (for debugging)')
+        self.parser.add_option(
+            "-r",
+            "--repair",
+            dest="repair",
+            action="store_true",
+            help="repair all inconsistant relations",
+        )
+        self.parser.add_option(
+            "-x",
+            "--savepoint",
+            dest="savepoint",
+            default=500,
+            type="int",
+            help="how many lines should be loaded before savepoint",
+        )
+        self.parser.add_option(
+            "-n",
+            "--nocommit",
+            dest="nocommit",
+            action="store_true",
+            help="Do not store changes to the Dmd (for debugging)",
+        )
 
 
 if __name__ == "__main__":
