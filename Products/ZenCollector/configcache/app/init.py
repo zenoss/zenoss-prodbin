5a63d8a7770700d098f5dd992d5cd8f6e1947288
Jason Peacock <jpeacock@zenoss.com>
2024-03-08 09:50:55 -0600

    Add a 'remove' command to delete configs from the cache.
    The CLI code was also refactored into a sub-package.
    
    ZEN-34657

--- Products/ZenCollector/configcache/app/init.py
+++ Products/ZenCollector/configcache/app/init.py
@@ -1,11 +1,15 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2023, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2024, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
 #
 ##############################################################################
+
+import sys
+
+import six
 
 from zope.configuration import xmlconfig
 
@@ -41,6 +45,8 @@ def _no_zope(configs, overrides):
 
 def _load_configs(ctx, configs):
     for filename, module in configs:
+        if isinstance(module, six.string_types):
+            module = sys.modules[module]
         xmlconfig.file(filename, package=module, context=ctx)
 
 
