54d942347684ee49b980d1aef58571784ca8256f
Jason Peacock <jpeacock@zenoss.com>
2024-04-24 11:09:02 -0500

    Refactored invalidator to reduce redundant code.
    ZEN-34712

--- Products/ZenCollector/configcache/task.py
+++ Products/ZenCollector/configcache/task.py
@@ -186,65 +186,6 @@ def _job_is_old(status, submitted, ctx, log):
     return False
 
 
-class BuildConfigTaskDispatcher(object):
-    """Encapsulates the act of dispatching the build_device_config task."""
-
-    def __init__(self, configClasses):
-        """
-        Initialize a BuildConfigTaskDispatcher instance.
-
-        The `configClasses` parameter should be the classes used to create
-        the device configurations.
-
-        @type configClasses: Sequence[Class]
-        """
-        self._classnames = {
-            cls.__module__: ".".join((cls.__module__, cls.__name__))
-            for cls in configClasses
-        }
-
-    @property
-    def service_names(self):
-        return self._classnames.keys()
-
-    def dispatch_all(self, monitorid, deviceid, timeout, submitted):
-        """
-        Submit a task to build a device configuration from each
-        configuration service.
-        """
-        soft_limit, hard_limit = _get_limits(timeout)
-        for name in self._classnames.values():
-            build_device_config.apply_async(
-                args=(monitorid, deviceid, name),
-                kwargs={"submitted": submitted},
-                soft_time_limit=soft_limit,
-                time_limit=hard_limit,
-            )
-
-    def dispatch(self, servicename, monitorid, deviceid, timeout, submitted):
-        """
-        Submit a task to build device configurations for the specified device.
-
-        @type servicename: str
-        @type monitorid: str
-        @type deviceId: str
-        """
-        name = self._classnames.get(servicename)
-        if name is None:
-            raise ValueError("service name '%s' not found" % servicename)
-        soft_limit, hard_limit = _get_limits(timeout)
-        build_device_config.apply_async(
-            args=(monitorid, deviceid, name),
-            kwargs={"submitted": submitted},
-            soft_time_limit=soft_limit,
-            time_limit=hard_limit,
-        )
-
-
-def _get_limits(timeout):
-    return timeout, (timeout + (timeout * 0.1))
-
-
 def _getStore():
     client = getRedisClient(url=getRedisUrl())
     return createObject("configcache-store", client)
