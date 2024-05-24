2f55a26923014aec9e5903eef8be94c45570dbe1
vsaliieva <vsaliieva@zenoss.com>
2024-05-24 12:25:44 +0000

    Fixed Zenhubworker deletion during the upgrade
    Fixes ZEN-34876.

--- bin/service-migrate.py
+++ bin/service-migrate.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 
+import json
 import sys
 import servicemigration as sm
 sm.require("1.0.0")
 
 TMP_FILENAME = "/tmp/zenoss-service-migration.json"
+SERVICE_NAME_TO_REMOVE = "zenhubiworker"
+
+
+def remove_service_from_file(file_path, service_name):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    if data.get("Deploy"):
+        data['Deploy'] = [service for service in data['Deploy'] if service['Service']['Name'] != service_name]
+
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
+
 
 if sys.argv[-1] == "begin":
     ctx = sm.ServiceContext()
     ctx.commit(TMP_FILENAME)
-    print TMP_FILENAME,
+    remove_service_from_file(TMP_FILENAME, SERVICE_NAME_TO_REMOVE)
+    print TMP_FILENAME
 elif sys.argv[-1] == "end":
     ctx = sm.ServiceContext(TMP_FILENAME)
     ctx.commit()
