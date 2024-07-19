c59c0bb0fdcfd0dd4b3277f2bdd9bf0586032c39
Jason Peacock <jpeacock@zenoss.com>
2024-07-19 11:08:47 -0500

    Save the zenjobs metrics to the current service.
    Refactored thread locking for metric updates to ensure all metrics are
    updated together for each Celery event.
    
    Added more logging to identify missing data or issues related to
    retrieving metric data.
    
    ZEN-34974

--- Products/Jobber/monitor/broker.py
+++ Products/Jobber/monitor/broker.py
@@ -13,9 +13,10 @@ import requests
 
 from six.moves.urllib.parse import urlparse, urljoin, quote_plus, unquote
 
+from .logger import getLogger
+
 
 class Broker(object):
-
     def __new__(cls, broker_url):
         scheme = urlparse(broker_url).scheme
         if scheme == "amqp":
@@ -42,13 +43,34 @@ class RabbitMQ(object):
             host=self._host,
             port=self._port,
         )
+        self._log = getLogger(self)
 
     def queues(self, names):
         if not names:
             return ()
+        attempts = 1
+        timeout = 1.0
         url = urljoin(self._http_api, "queues/" + self._vhost)
         params = {"columns": ",".join(["name", "messages"])}
-        r = requests.get(url, params=params, timeout=1.0)
+        while True:
+            try:
+                r = requests.get(url, params=params, timeout=timeout)
+            except requests.Timeout:
+                if attempts < 3:
+                    attempts += 1
+                    timeout *= 2
+                else:
+                    self._log.warning(
+                        "timed out requesting data from RabbitMQ"
+                    )
+                    return ()
+            except Exception:
+                self._log.exception(
+                    "unexpected error while requesting data from RabbitMQ"
+                )
+            else:
+                break
+
         if r.status_code != 200:
             r.raise_for_status()
         return tuple(q for q in r.json() if q["name"] in names)
