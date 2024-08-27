61dda984d731e5e8d2ccd56b4725a11f7a2cb675
Jason Peacock <jpeacock@zenoss.com>
2024-08-27 09:55:30 -0500

    Run the ConfigurationLoaderTask outside the scheduler.
    ZEN-34238

--- Products/ZenCollector/config/task.py
+++ Products/ZenCollector/config/task.py
@@ -7,124 +7,51 @@
 #
 ##############################################################################
 
-"""
-The config module provides the implementation of the IConfigurationProxy
-interface used within Zenoss Core. This implementation provides basic
-configuration retrieval services directly from a remote ZenHub service.
-"""
-
 import itertools
 import logging
 import time
 
-from metrology import Metrology
 from twisted.internet import defer
-from zope.component import getUtility, queryUtility
-from zope.interface import implementer
-
-from Products.ZenHub.PBDaemon import HubDown
-from Products.ZenUtils.observable import ObservableMixin
-
-from ..tasks import TaskStates
-from ..interfaces import (
-    ICollector,
-    IDataService,
-    IEventService,
-    IScheduledTask,
-    IFrameworkFactory,
-)
 
 log = logging.getLogger("zen.collector.config")
 
 
-@implementer(IScheduledTask)
-class ConfigurationLoaderTask(ObservableMixin):
+class ConfigurationLoaderTask(object):
     """
     Periodically retrieves collector configuration via the
     IConfigurationProxy service.
     """
 
-    STATE_CONNECTING = "CONNECTING"
-    STATE_FETCH_MISC_CONFIG = "FETCHING_MISC_CONFIG"
-    STATE_FETCH_DEVICE_CONFIG = "FETCHING_DEVICE_CONFIG"
-    STATE_PROCESS_DEVICE_CONFIG = "PROCESSING_DEVICE_CONFIG"
+    # Deprecated attribute kept because zenvsphere uses it for reasons
+    # that are no longer relevant.
+    STATE_FETCH_DEVICE_CONFIG = "n/a"
 
-    def __init__(
-        self,
-        name,
-        configId=None,
-        scheduleIntervalSeconds=None,
-        taskConfig=None,
-    ):
-        if taskConfig is None:
-            raise TypeError("taskConfig cannot be None")
-
-        super(ConfigurationLoaderTask, self).__init__()
-
-        self._fetchConfigTimer = Metrology.timer("collectordaemon.configs")
-
-        # Needed for interface
-        self.name = name
-        self.configId = configId if configId else name
-        self.state = TaskStates.STATE_IDLE
-
-        self._dataService = queryUtility(IDataService)
-        self._eventService = queryUtility(IEventService)
-
-        self._prefs = taskConfig
-        self.interval = self._prefs.configCycleInterval * 60
-        self.options = self._prefs.options
-
-        self._collector = getUtility(ICollector)
-        self._collector.heartbeatTimeout = self.options.heartbeatTimeout
-        log.debug(
-            "heartbeat timeout set to %ds", self._collector.heartbeatTimeout
-        )
-
-        frameworkFactory = queryUtility(
-            IFrameworkFactory, self._collector.frameworkFactoryName
-        )
-        self._configProxy = frameworkFactory.getConfigurationProxy()
-
-        self.startDelay = 0
+    def __init__(self, collector, proxy):
+        self._collector = collector
+        self._proxy = proxy
 
     @defer.inlineCallbacks
-    def doTask(self):
-        """
-        Contact zenhub and gather configuration data.
-
-        @return: A task to gather configs
-        @rtype: Twisted deferred object
-        """
-        log.debug("%s gathering configuration", self.name)
-        self.startTime = time.time()
-
-        proxy = self._configProxy
+    def __call__(self):
         try:
-            propertyItems = yield proxy.getPropertyItems()
-            self._processPropertyItems(propertyItems)
+            properties = yield self._proxy.getPropertyItems()
+            self._processPropertyItems(properties)
 
-            thresholdClasses = yield proxy.getThresholdClasses()
+            thresholdClasses = yield self._proxy.getThresholdClasses()
             self._processThresholdClasses(thresholdClasses)
 
-            thresholds = yield proxy.getThresholds()
+            thresholds = yield self._proxy.getThresholds()
             self._processThresholds(thresholds)
 
             yield self._collector.runPostConfigTasks()
-        except Exception as ex:
-            log.exception("task '%s' failed", self.name)
-
-            # stop if a single device was requested and nothing found
-            if self.options.device or not self.options.cycle:
-                self._collector.stop()
-
-            if isinstance(ex, HubDown):
-                # Allow the loader to be reaped and re-added
-                self.state = TaskStates.STATE_COMPLETED
+        except Exception:
+            log.exception(
+                "failed to retrieve collector configuration  "
+                "collection-daemon=%s",
+                self._collector.name,
+            )
 
     def _processPropertyItems(self, propertyItems):
         log.debug("processing received property items")
-        self.state = self.STATE_FETCH_MISC_CONFIG
         if propertyItems:
             self._collector._setCollectorPreferences(propertyItems)
 
@@ -137,9 +64,6 @@ class ConfigurationLoaderTask(ObservableMixin):
         log.debug("processing received thresholds")
         if thresholds:
             self._collector._configureThresholds(thresholds)
-
-    def cleanup(self):
-        pass  # Required by interface
 
 
 class DeviceConfigLoader(object):
@@ -187,7 +111,7 @@ class DeviceConfigLoader(object):
                 for cfg in itertools.chain(new, updated)
                 if self._options.device == cfg.configId
             ),
-            None
+            None,
         )
 
     def _update_local_cache(self, new, updated, removed):
