20d773d4717c1762ba4152405576f3dc2a70fb87
Jason Peacock <jpeacock@zenoss.com>
2024-08-30 15:48:43 -0500

    Refactored zentrap off the collection framework.
    Removing the collection framework simplifies how zentrap starts up so
    that traps are not processed until the OID map and trap filters are
    loaded.
    
    ZEN-34792

--- Products/ZenEvents/zentrap/__init__.py
+++ Products/ZenEvents/zentrap/__init__.py
@@ -1,17 +1,8 @@
-#! /usr/bin/env bash
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2024, all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
-
-. $ZENHOME/bin/zenfunctions
-
-PRGHOME=$ZENHOME/Products/ZenEvents
-PRGNAME=zentrap
-CFGFILE=$CFGDIR/zentrap.conf
-
-generic "$@"
