4f6ea0ba364812a2731db692c787409506bdb2d9
Jason Peacock <jpeacock@zenoss.com>
2024-10-11 08:57:44 -0500

    Refactored zensyslog off the collection framework.
    Removing the collection framework simplifies zensyslog's start-up so that
    syslog messages are not processed until its configuration has loaded.
    
    ZEN-34584

--- Products/ZenEvents/zensyslog/__init__.py
+++ Products/ZenEvents/zensyslog/__init__.py
@@ -1,18 +1,15 @@
-#! /usr/bin/env bash
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2024, all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+from __future__ import absolute_import, print_function
 
-. $ZENHOME/bin/zenfunctions
 
-PRGHOME=$ZENHOME/Products/ZenEvents
-PRGNAME=zensyslog
-CFGFILE=$CFGDIR/zensyslog.conf
-
-generic "$@"
+def main():
+    from .daemon import SyslogDaemon
+    SyslogDaemon().run()
