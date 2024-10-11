5f748077a49d47d50a574536c3a4b814d21a76bc
Jason Peacock <jpeacock@zenoss.com>
2024-10-11 08:57:44 -0500

    Refactored zensyslog off the collection framework.
    Removing the collection framework simplifies zensyslog's start-up so that
    syslog messages are not processed until its configuration has loaded.
    
    ZEN-34584

--- Products/ZenEvents/zensyslog/rfc3164/__init__.py
+++ Products/ZenEvents/zensyslog/rfc3164/__init__.py
@@ -1,18 +1,15 @@
-#! /usr/bin/env bash
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2024 all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+from __future__ import absolute_import
 
-. $ZENHOME/bin/zenfunctions
+from .parser import parse, SyslogMessageError
+from .severity import Severity
 
-PRGHOME=$ZENHOME/Products/ZenEvents
-PRGNAME=zensyslog
-CFGFILE=$CFGDIR/zensyslog.conf
-
-generic "$@"
+__all__ = ("parse", "Severity", "SyslogMessageError")
