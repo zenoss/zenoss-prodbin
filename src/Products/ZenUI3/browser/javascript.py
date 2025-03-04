f9eb20057ac85d3e20e76c7ec4bd498266d60366
Jason Peacock <jpeacock@zenoss.com>
2025-03-04 14:12:58 -0600

    Fix bad zcml config for ext javascript reference.
    Also modernized some code (fixed lints)

--- src/Products/ZenUI3/browser/javascript.py
+++ src/Products/ZenUI3/browser/javascript.py
@@ -7,25 +7,39 @@
 #
 ##############################################################################
 
+from __future__ import absolute_import
 
-import os
-import Globals
-import zope.interface
 import md5
+import os
+
 from urlparse import urljoin
-from interfaces import IMainSnippetManager
-from Products.ZenUI3.utils.javascript import JavaScriptSnippetManager,\
-    JavaScriptSnippet, SCRIPT_TAG_TEMPLATE
-from Products.ZenUI3.browser.interfaces import IJavaScriptSrcViewlet,\
-    IJavaScriptBundleViewlet, IJavaScriptSrcManager, IXTraceSrcManager, ICSSBundleViewlet, ICSSSrcManager
+
+import Globals
+
 from Products.Five.viewlet.viewlet import ViewletBase
-from Products.ZenUI3.navigation.manager import WeightOrderedViewletManager
-from Products.ZenUtils.extdirect.zope.metaconfigure import allDirectRouters
 from zope.publisher.browser import TestRequest
 from zope.component import getAdapter
+from zope.interface import implementer
+
+from Products.ZenUI3.browser.interfaces import (
+    ICSSBundleViewlet,
+    ICSSSrcManager,
+    IJavaScriptBundleViewlet,
+    IJavaScriptSrcManager,
+    IJavaScriptSrcViewlet,
+    IXTraceSrcManager,
+)
+from Products.ZenUI3.navigation.manager import WeightOrderedViewletManager
+from Products.ZenUI3.utils.javascript import (
+    JavaScriptSnippet,
+    JavaScriptSnippetManager,
+    SCRIPT_TAG_TEMPLATE,
+)
+from Products.ZenUtils.extdirect.zope.metaconfigure import allDirectRouters
 from Products.ZenUtils.Utils import monkeypatch
 from Products.Zuul.decorators import memoize
 
+from .interfaces import IMainSnippetManager
 from .resources import hasCompiledJavascript
 
 
@@ -35,7 +49,7 @@ dummyRequest = TestRequest()
 _registered_resources = []
 
 
-@monkeypatch('Products.Five.browser.metaconfigure')
+@monkeypatch("Products.Five.browser.metaconfigure")
 def resourceDirectory(*args, **kwargs):
     """
     There isn't a way to ask the site manager for a list of registered
@@ -47,12 +61,15 @@ def resourceDirectory(*args, **kwargs):
     global _registered_resources
     # will be name and directory
     _registered_resources.append(kwargs)
-    return original(*args, **kwargs)
+    return original(*args, **kwargs)  # noqa S821
 
 
 def getAllZenPackResources():
     # make a copy so the original isn't mutated
-    return [x for x in _registered_resources if "zenpack" in x['directory'].lower()]
+    return [
+        x for x in _registered_resources if "zenpack" in x["directory"].lower()
+    ]
+
 
 @memoize
 def getPathModifiedTime(path):
@@ -62,57 +79,68 @@ def getPathModifiedTime(path):
     returns the last time the file was modified.
     """
     if "++resource++" in path:
-        identifier = path.split('/')[1].replace("++resource++", "")
-        filePath = path.replace("/++resource++" + identifier , "")
+        identifier = path.split("/")[1].replace("++resource++", "")
+        filePath = path.replace("/++resource++" + identifier, "")
         resource = getAdapter(dummyRequest, name=identifier)
         fullPath = resource.context.path + filePath
         if os.path.exists(fullPath):
             return os.path.getmtime(fullPath)
 
+
 SCRIPT_TAG_SRC_TEMPLATE = '<script type="text/javascript" src="%s"></script>\n'
-LINK_TAG_SRC_TEMPLATE = '<link rel="stylesheet" type="text/css" href="%s"></link>\n'
+LINK_TAG_SRC_TEMPLATE = (
+    '<link rel="stylesheet" type="text/css" href="%s"></link>\n'
+)
 
 
 def absolutifyPath(path):
-    return urljoin('/zport/dmd', path)
+    return urljoin("/zport/dmd", path)
+
+
 getVersionedPath = absolutifyPath
 
 
+@implementer(IMainSnippetManager)
 class MainSnippetManager(JavaScriptSnippetManager):
     """
     A viewlet manager to handle Ext.Direct API definitions.
     """
-    zope.interface.implements(IMainSnippetManager)
 
+
+@implementer(ICSSSrcManager)
 class CSSSrcManager(WeightOrderedViewletManager):
-    zope.interface.implements(ICSSSrcManager)
+    pass
 
+
+@implementer(IJavaScriptSrcManager)
 class JavaScriptSrcManager(WeightOrderedViewletManager):
-    zope.interface.implements(IJavaScriptSrcManager)
+    pass
 
+
+@implementer(IXTraceSrcManager)
 class XTraceSrcManager(WeightOrderedViewletManager):
-    zope.interface.implements(IXTraceSrcManager)
+    pass
 
 
+@implementer(ICSSBundleViewlet)
 class CSSSrcBundleViewlet(ViewletBase):
-    zope.interface.implements(ICSSBundleViewlet)
-    #space delimited string of src paths
-    paths = ''
+    # space delimited string of src paths
+    paths = ""
 
     def render(self):
         vals = []
         if self.paths:
             for path in self.paths.split():
                 vals.append(LINK_TAG_SRC_TEMPLATE % absolutifyPath(path))
-        js = ''
+        js = ""
         if vals:
             js = "".join(vals)
         return js
 
 
+@implementer(IJavaScriptSrcViewlet)
 class JavaScriptSrcViewlet(ViewletBase):
-    zope.interface.implements(IJavaScriptSrcViewlet)
-    path = None
+    path = ""
 
     def render(self):
         if not self.path:
@@ -120,46 +148,50 @@ class JavaScriptSrcViewlet(ViewletBase):
         return SCRIPT_TAG_SRC_TEMPLATE % absolutifyPath(self.path)
 
 
+@implementer(IJavaScriptBundleViewlet)
 class JavaScriptSrcBundleViewlet(ViewletBase):
-    zope.interface.implements(IJavaScriptBundleViewlet)
-    #space delimited string of src paths
-    paths = ''
+    # space delimited string of src paths
+    paths = ""
 
     def render(self):
         vals = []
         if self.paths:
             for path in self.paths.split():
                 vals.append(SCRIPT_TAG_SRC_TEMPLATE % absolutifyPath(path))
-        js = ''
+        js = ""
         if vals:
             js = "".join(vals)
         return js
+
 
 class ExtDirectViewlet(JavaScriptSrcViewlet):
     """
     A specialized renderer for ExtDirect. We can not cache-bust this
     file by the modified time so we use a hash of the defined routers
     """
+
     directHash = None
 
     def render(self):
         if self.directHash is None:
             # append the extdirect request with a hash or all routers
             # so that it is updated when a new zenpack is installed
-            routernames = sorted([r['name'] for r in allDirectRouters.values()])
+            routernames = sorted(
+                [r["name"] for r in allDirectRouters.values()]
+            )
             self.directHash = md5.new(" ".join(routernames)).hexdigest()
-        path = self.path  + "?v=" + self.directHash
+        path = self.path + "?v=" + self.directHash
         return SCRIPT_TAG_SRC_TEMPLATE % path
 
 
+@implementer(IJavaScriptSrcViewlet)
 class ZenossAllJs(JavaScriptSrcViewlet):
     """
-    When Zope is in debug mode, we want to use the development JavaScript source
-    files, so we don't have to make changes to a single huge file. If Zope is in
-    production mode and the compressed file is not available, we will use the
-    source files instead of just giving up.
+    When Zope is in debug mode, we want to use the development JavaScript
+    source files, so we don't have to make changes to a single huge file.
+    If Zope is in production mode and the compressed file is not available,
+    we will use the source files instead of just giving up.
     """
-    zope.interface.implements(IJavaScriptSrcViewlet)
 
     def update(self):
         if Globals.DevelopmentMode or not hasCompiledJavascript():
@@ -170,10 +202,8 @@ class ZenossAllJs(JavaScriptSrcViewlet):
             self.path = "/++resource++zenui/deploy/zenoss-compiled.js"
 
 
+@implementer(IJavaScriptSrcViewlet)
 class ExtAllJs(JavaScriptSrcViewlet):
-    zope.interface.implements(IJavaScriptSrcViewlet)
-    path = None
-
     def update(self):
         if Globals.DevelopmentMode:
             self.path = "/++resource++extjs/ext-all-dev.js"
@@ -182,10 +212,9 @@ class ExtAllJs(JavaScriptSrcViewlet):
 
 
 class FireFoxExtCompat(JavaScriptSnippet):
-
     def snippet(self):
         js = """
-         (function() {
+        (function() {
             var ua = navigator.userAgent.toLowerCase();
             if (ua.indexOf("firefox/3.6") > -1) {
                 Ext.toArray = function(a, i, j, res) {
@@ -196,28 +225,29 @@ class FireFoxExtCompat(JavaScriptSnippet):
             }
         })();
         """
-        return  SCRIPT_TAG_TEMPLATE % js
-
+        return SCRIPT_TAG_TEMPLATE % js
 
 
 class VisualizationInit(JavaScriptSnippet):
     """
     Performs necessary initialization for the visualization library
     """
+
     def snippet(self):
         js = """
             if (window.zenoss !== undefined) {
                 zenoss.visualization.url = window.location.protocol + "//" + window.location.host;
                 zenoss.visualization.debug = false;
             }
-        """
-        return  SCRIPT_TAG_TEMPLATE % js
+        """  # noqa E501
+        return SCRIPT_TAG_TEMPLATE % js
 
 
 class ZenossSettings(JavaScriptSnippet):
     """
     Renders client side settings.
     """
+
     def snippet(self):
         settings = self.context.dmd.UserInterfaceSettings
         js = ["Ext.namespace('Zenoss.settings');"]
@@ -225,14 +255,18 @@ class ZenossSettings(JavaScriptSnippet):
             js.append("Zenoss.settings.%s = %s;" % (name, str(value).lower()))
         return "\n".join(js)
 
+
 class ZenossData(JavaScriptSnippet):
     """
     This preloads some data for the UI so that every page doesn't have to send
     a separate router request to fetch it.
     """
+
     def snippet(self):
         # collectors
-        collectors = [[s] for s in self.context.dmd.Monitors.getPerformanceMonitorNames()]
+        collectors = [
+            [s] for s in self.context.dmd.Monitors.getPerformanceMonitorNames()
+        ]
 
         # priorities
         priorities = [
@@ -256,23 +290,32 @@ class ZenossData(JavaScriptSnippet):
         date_fmt = user.dateFormat
         time_fmt = user.timeFormat
         snippet = """
-          (function(){
-            Ext.namespace('Zenoss.env');
+        (function(){
+          Ext.namespace('Zenoss.env');
 
-            Zenoss.env.COLLECTORS = %r;
-            Zenoss.env.priorities = %r;
-            Zenoss.env.productionStates = %r;
-            Zenoss.USER_TIMEZONE = "%s" || jstz.determine().name();
-            Zenoss.USER_DATE_FORMAT = "%s" || "YYYY/MM/DD";
-            Zenoss.USER_TIME_FORMAT = "%s" || "HH:mm:ss";
-          })();
-        """ % ( collectors, priorities, productionStates, timezone, date_fmt, time_fmt )
+          Zenoss.env.COLLECTORS = %r;
+          Zenoss.env.priorities = %r;
+          Zenoss.env.productionStates = %r;
+          Zenoss.USER_TIMEZONE = "%s" || jstz.determine().name();
+          Zenoss.USER_DATE_FORMAT = "%s" || "YYYY/MM/DD";
+          Zenoss.USER_TIME_FORMAT = "%s" || "HH:mm:ss";
+        })();
+        """ % (
+            collectors,
+            priorities,
+            productionStates,
+            timezone,
+            date_fmt,
+            time_fmt,
+        )
         return SCRIPT_TAG_TEMPLATE % snippet
+
 
 class BrowserState(JavaScriptSnippet):
     """
     Restores the browser state.
     """
+
     # return the js snippet that will make a request for browser state
     # and than set it
     def snippet(self):
--- src/Products/ZenUI3/navigation/manager.py
+++ src/Products/ZenUI3/navigation/manager.py
@@ -1,28 +1,28 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2009, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+from __future__ import absolute_import
 
-import zope.interface
 import zope.component
-from zope.viewlet.interfaces import IViewletManager, IViewlet
-from zope.contentprovider.interfaces import BeforeUpdateEvent
+import zope.interface
 
 from Products.Five.viewlet.manager import ViewletManagerBase
 
-from interfaces import IPrimaryNavigationMenu, ISecondaryNavigationMenu
+from .interfaces import IPrimaryNavigationMenu, ISecondaryNavigationMenu
 
 
-def viewletSortKey((name, viewlet)):
+def viewletSortKey(args):
     """
     Creates a sort key for this viewlet. Primary sort is viewlet weight, and
     secondary is viewlet name, guaranteeing the same order on each call.
     """
+    name, viewlet = args
     try:
         return (float(viewlet.weight), name)
     except (AttributeError, ValueError):
@@ -36,29 +36,32 @@ class WeightOrderedViewletManager(ViewletManagerBase):
         return sorted(viewlets, key=viewletSortKey)
 
 
-
+@zope.interface.implementer(IPrimaryNavigationMenu)
 class PrimaryNavigationManager(WeightOrderedViewletManager):
-    zope.interface.implements(IPrimaryNavigationMenu)
+    pass
 
 
+@zope.interface.implementer(ISecondaryNavigationMenu)
 class SecondaryNavigationManager(WeightOrderedViewletManager):
     """
     A secondary level of navigation.
 
     Knows how to look up the parent item to see if it is selected.
     """
-    zope.interface.implements(ISecondaryNavigationMenu)
 
     def getViewletsByParentName(self, name):
-        if not hasattr(self, 'viewlets'): self.update()
-        return [v for v in self.viewlets if v.parentItem==name]
+        if not hasattr(self, "viewlets"):
+            self.update()
+        return [v for v in self.viewlets if v.parentItem == name]
 
     def getActivePrimaryName(self):
-        primary = PrimaryNavigationManager(self.context, self.request,
-                                           self.__parent__)
+        primary = PrimaryNavigationManager(
+            self.context, self.request, self.__parent__
+        )
         primary.update()
         for v in primary.viewlets:
-            if v.selected: return v.__name__
+            if v.selected:
+                return v.__name__
         return None
 
     def getActiveViewlets(self):
@@ -69,4 +72,4 @@ class SecondaryNavigationManager(WeightOrderedViewletManager):
         return viewlets
 
     def render(self):
-        return '\n'.join(v.render() for v in self.getActiveViewlets())
+        return "\n".join(v.render() for v in self.getActiveViewlets())
