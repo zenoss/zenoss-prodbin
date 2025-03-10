0f2389bd5257bf500b08b9853cf09f07341a9695
Jason Peacock <jpeacock@zenoss.com>
2025-03-10 09:59:39 -0500

    Fix 'comment' formatting in Version class.
    ZEN-35183

--- src/Products/ZenUtils/tests/testVersion.py
+++ src/Products/ZenUtils/tests/testVersion.py
@@ -1,25 +1,17 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
-
-'''
-Note that this is meant to be run from zopecctl using the "test" option. If you
-would like to run these tests from python, simply to the following:
-
-    python ZenUtils/Version.py
-'''
 import unittest
+
 from zope.testing.doctestunit import DocTestSuite
 
-def test_suite():
-    suite = DocTestSuite('Products.ZenUtils.Version')
-    return unittest.TestSuite([suite])
 
-if __name__ == '__main__':
-    print __doc__
+def test_suite():
+    suite = DocTestSuite("Products.ZenUtils.Version")
+    return unittest.TestSuite([suite])
