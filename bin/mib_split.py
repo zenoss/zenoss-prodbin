ae9e934b7c7b509e32b76628d70d0a7de131e68e
Jason Peacock <jpeacock@zenoss.com>
2025-06-03 08:40:56 -0500

    wip

--- bin/mib_split.py
+++ bin/mib_split.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2008, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+from __future__ import print_function
 
-import sys, re
+import re
+import sys
 
-if (len(sys.argv) < 2):
-    print "Usage: mib_split <MIB filaname>"
+if len(sys.argv) < 2:
+    print("Usage: mib_split <MIB filaname>")
     sys.exit(1)
 
-begun = 0
-begin_re = re.compile(r'^(\S+)\s+DEFINITIONS\s+::=\s+BEGIN')
+begin_re = re.compile(r"^(\S+)\s+DEFINITIONS\s+::=\s+BEGIN")
 
-source_mib = open(sys.argv[1], 'r')
-dest_mib = None
+with open(sys.argv[1], "r") as source_mib:
+    dest_mib = None
 
-for line in source_mib:
-    matches = begin_re.search(line)
-    if matches:
-        begun += 1
+    for line in source_mib:
+        matches = begin_re.search(line)
+        if matches:
+            filename = matches.groups()[0] + ".txt"
+            print("Writing " + filename)
 
-        filename = matches.groups()[0] + '.txt'
-        print "Writing " + filename
+            if dest_mib:
+                dest_mib.close()
+                dest_mib = None
+            dest_mib = open(filename, "w")
 
-        if dest_mib: dest_mib.close()
-        dest_mib = open(filename, 'w')
+        if dest_mib:
+            dest_mib.write(line)
 
-    if begun:
-        dest_mib.write(line)
-
-dest_mib.close()
-source_mib.close()
+    if dest_mib:
+        dest_mib.close()
